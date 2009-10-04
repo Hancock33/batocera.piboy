Hancock33/batocera.piboy

commit 5ad0246552e45857b253a9d4a7aca32e5b6b53bf

zlib: remove dedicated target-headers option
Instead, simply only copy headers/static lib if BR2_HAVE_DEVFILES
is enabled.

Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

package/zlib/zlib.mk
@@ -68,9 +68,7 @@ $(TARGET_DIR)/usr/lib/libz.a: $(STAGING_DIR)/usr/lib/libz.a
 	$(INSTALL) -D $(STAGING_DIR)/usr/lib/libz.a $(TARGET_DIR)/usr/lib/libz.a
 	touch -c $@
 
-zlib-headers: $(TARGET_DIR)/usr/lib/libz.a
-
-zlib: $(ZLIB_TARGET)
+zlib: $(ZLIB_TARGET) $(if $(BR2_HAVE_DEVFILES,$(TARGET_DIR)/usr/lib/libz.a)
 
 zlib-source: $(DL_DIR)/$(ZLIB_SOURCE)
 
@@ -93,7 +91,4 @@ zlib-dirclean:
 #############################################################
 ifeq ($(BR2_PACKAGE_ZLIB),y)
 TARGETS+=zlib
-endif
-ifeq ($(BR2_PACKAGE_ZLIB_TARGET_HEADERS),y)
-TARGETS+=zlib-headers
 endif

package/zlib/Config.in
@@ -5,9 +5,3 @@ config BR2_PACKAGE_ZLIB
 	  gzip and libpng.
 
 	  http://www.gzip.org/zlib/
-
-config BR2_PACKAGE_ZLIB_TARGET_HEADERS
-	bool "zlib headers in target"
-	depends on BR2_PACKAGE_ZLIB
-	help
-	  Put headers files in the target.

commit a11d7351a3ebaeb4e7682496d08378f84deeaa21

zlib: fix typo
Fix typo introduced by 5ad0246552 (remove dedicated target-headers option).
Noticed by Alexander Clouter <alex@digriz.org.uk>.

Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

package/zlib/zlib.mk
@@ -68,7 +68,7 @@ $(TARGET_DIR)/usr/lib/libz.a: $(STAGING_DIR)/usr/lib/libz.a
 	$(INSTALL) -D $(STAGING_DIR)/usr/lib/libz.a $(TARGET_DIR)/usr/lib/libz.a
 	touch -c $@
 
-zlib: $(ZLIB_TARGET) $(if $(BR2_HAVE_DEVFILES,$(TARGET_DIR)/usr/lib/libz.a)
+zlib: $(ZLIB_TARGET) $(if $(BR2_HAVE_DEVFILES),$(TARGET_DIR)/usr/lib/libz.a)
 
 zlib-source: $(DL_DIR)/$(ZLIB_SOURCE)
 