Hancock33/batocera.piboy

commit 0c08a97adefe64b42b1eae3cf2e6e876d03c67fb

ifplugd: only install default action if not provided in skeleton
Signed-off-by: Danomi Manchego <danomimanchego123@gmail.com>
Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

package/ifplugd/ifplugd.mk
@@ -21,12 +21,14 @@ ifeq ($(BR2_PACKAGE_BUSYBOX),y)
 endif
 
 define IFPLUGD_INSTALL_FIXUP
-	@if [ ! -f $(TARGET_DIR)/etc/ifplugd/ifplugd.conf ]; then \
+	if [ ! -f $(TARGET_DIR)/etc/ifplugd/ifplugd.conf ]; then \
 		$(INSTALL) -D $(@D)/conf/ifplugd.conf $(TARGET_DIR)/etc/ifplugd/ifplugd.conf; \
 		$(SED) 's^\(ARGS=.*\)w^\1^' $(TARGET_DIR)/etc/ifplugd/ifplugd.conf; \
 	fi
-	$(INSTALL) -D -m 0755 $(@D)/conf/ifplugd.action \
-		$(TARGET_DIR)/etc/ifplugd/ifplugd.action
+	if [ ! -f $(TARGET_DIR)/etc/ifplugd/ifplugd.action ]; then \
+		$(INSTALL) -D -m 0755 $(@D)/conf/ifplugd.action \
+			$(TARGET_DIR)/etc/ifplugd/ifplugd.action
+	fi
 	$(INSTALL) -D -m 0755 $(@D)/conf/ifplugd.init \
 		$(TARGET_DIR)/etc/init.d/S45ifplugd
 	# don't use bash for init script

commit 22efbb2dddcf2ae812a91de98cf35bffea4521b5

ifplugd: fix typo introduced in previous commit
Commit 0c08a97adefe64b42b1eae3cf2e6e876d03c67fb ('ifplugd: only
install default action if not provided in skeleton') introduced a
small typo, which we fix in this commit.

Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

package/ifplugd/ifplugd.mk
@@ -27,7 +27,7 @@ define IFPLUGD_INSTALL_FIXUP
 	fi
 	if [ ! -f $(TARGET_DIR)/etc/ifplugd/ifplugd.action ]; then \
 		$(INSTALL) -D -m 0755 $(@D)/conf/ifplugd.action \
-			$(TARGET_DIR)/etc/ifplugd/ifplugd.action
+			$(TARGET_DIR)/etc/ifplugd/ifplugd.action ; \
 	fi
 	$(INSTALL) -D -m 0755 $(@D)/conf/ifplugd.init \
 		$(TARGET_DIR)/etc/init.d/S45ifplugd