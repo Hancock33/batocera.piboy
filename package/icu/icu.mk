Hancock33/batocera.piboy

commit 0328abbbb24f774e17542dc759dfb87b36e4b806

icu: add an option to add a custom data library file
[Thomas:
 - use one single BR2_PACKAGE_ICU_CUSTOM_DATA_PATH string option
   instead of a boolean option + a string option, which always causes
   a lot of issues with random configurations that enable the boolean
   option, but do not provide a valid value for the string option.
 - enclose the definition of ICU_COPY_CUSTOM_DATA into the condition.]

Signed-off-by: Johan Derycke <johan.derycke@barco.com>
Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

package/icu/icu.mk
@@ -26,5 +26,12 @@ ICU_MAKE = $(MAKE1)
 ICU_SUBDIR = source
 HOST_ICU_SUBDIR = source
 
+ifneq ($(BR2_PACKAGE_ICU_CUSTOM_DATA_PATH),)
+define ICU_COPY_CUSTOM_DATA
+	cp $(BR2_PACKAGE_ICU_CUSTOM_DATA_PATH) $(@D)/source/data/in/
+endef
+ICU_POST_PATCH_HOOKS += ICU_COPY_CUSTOM_DATA
+endif
+
 $(eval $(autotools-package))
 $(eval $(host-autotools-package))

package/icu/Config.in
@@ -13,6 +13,18 @@ config BR2_PACKAGE_ICU
 
 	  http://site.icu-project.org/
 
+if BR2_PACKAGE_ICU
+
+config BR2_PACKAGE_ICU_CUSTOM_DATA_PATH
+	string "Path to custom data library file"
+	help
+	  This option allows to define the path to a custom data
+	  library generated with
+	  http://apps.icu-project.org/datacustom/ICUData51.html. Leave
+	  empty to not use this functionality.
+
+endif
+
 comment "icu needs a toolchain w/ C++, wchar, threads"
 	depends on !BR2_arc
 	depends on !BR2_BINFMT_FLAT

commit 725791d4aadacbcf43cf8087365e4169f1bdbb3e

icu: fix build after custom data library patch
Commit 0328abbbb24f774e17542dc759dfb87b36e4b806 ("icu: add an option
to add a custom data library file") broke the build because of an
improper test on a non-qstripped variable. This commit fixes that.

Fixes:

  http://autobuild.buildroot.org/results/02a/02a0800b01a4bf8734d601f79dc12663fe8f7542/
  and many other similar occurences of the same issue.

Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

package/icu/icu.mk
@@ -26,9 +26,11 @@ ICU_MAKE = $(MAKE1)
 ICU_SUBDIR = source
 HOST_ICU_SUBDIR = source
 
-ifneq ($(BR2_PACKAGE_ICU_CUSTOM_DATA_PATH),)
+ICU_CUSTOM_DATA_PATH = $(call qstrip,$(BR2_PACKAGE_ICU_CUSTOM_DATA_PATH))
+
+ifneq ($(ICU_CUSTOM_DATA_PATH),)
 define ICU_COPY_CUSTOM_DATA
-	cp $(BR2_PACKAGE_ICU_CUSTOM_DATA_PATH) $(@D)/source/data/in/
+	cp $(ICU_CUSTOM_DATA_PATH) $(@D)/source/data/in/
 endef
 ICU_POST_PATCH_HOOKS += ICU_COPY_CUSTOM_DATA
 endif