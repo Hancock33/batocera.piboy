Hancock33/batocera.piboy

commit 9be2d590cee2271b724ba742e27d1315a1da2a4e

sysklogd: install configuration file unconditionally
As discussed, users should use a rootfs overlay or a post-build script
instead of a custom skeleton to override files installed by Buildroot,
so there is no point in having conditions when installing init scripts
or configuration files.

Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
Acked-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

package/sysklogd/sysklogd.mk
@@ -21,10 +21,8 @@ endef
 define SYSKLOGD_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0500 $(@D)/syslogd $(TARGET_DIR)/sbin/syslogd
 	$(INSTALL) -D -m 0500 $(@D)/klogd $(TARGET_DIR)/sbin/klogd
-	if [ ! -f $(TARGET_DIR)/etc/syslog.conf ]; then \
-		$(INSTALL) -D -m 0644 package/sysklogd/syslog.conf \
-			$(TARGET_DIR)/etc/syslog.conf; \
-	fi
+	$(INSTALL) -D -m 0644 package/sysklogd/syslog.conf \
+		$(TARGET_DIR)/etc/syslog.conf; \
 endef
 
 define SYSKLOGD_INSTALL_INIT_SYSV

commit 07f9f752ce07d2feb258d1454ecd8d438e7ca551

sysklogd: fix redundant newline escape
Broken since 9be2d590cee22 (sysklogd: install configuration file
unconditionally). Fixes:

package/sysklogd/sysklogd.mk:21: *** missing 'endef', unterminated 'define'.  Stop.

Signed-off-by: Baruch Siach <baruch@tkos.co.il>
Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

package/sysklogd/sysklogd.mk
@@ -22,7 +22,7 @@ define SYSKLOGD_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0500 $(@D)/syslogd $(TARGET_DIR)/sbin/syslogd
 	$(INSTALL) -D -m 0500 $(@D)/klogd $(TARGET_DIR)/sbin/klogd
 	$(INSTALL) -D -m 0644 package/sysklogd/syslog.conf \
-		$(TARGET_DIR)/etc/syslog.conf; \
+		$(TARGET_DIR)/etc/syslog.conf
 endef
 
 define SYSKLOGD_INSTALL_INIT_SYSV