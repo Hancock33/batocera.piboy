Hancock33/batocera.piboy

commit 88f810a8cd16e0acebb8a3da4fcb539abbb433dc

binutils: switch to version 2.25 by default
binutils 2.26 has been released, it is time to switch to 2.25 as the
default version.

Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
Acked-by: Romain Naour <romain.naour@gmail.com>
Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

package/binutils/binutils.mk
@@ -11,7 +11,7 @@ ifeq ($(BINUTILS_VERSION),)
 ifeq ($(BR2_arc),y)
 BINUTILS_VERSION = arc-2015.12
 else
-BINUTILS_VERSION = 2.24
+BINUTILS_VERSION = 2.25
 endif
 endif # BINUTILS_VERSION
 

package/binutils/Config.in.host
@@ -3,7 +3,7 @@ comment "Binutils Options"
 choice
 	prompt "Binutils Version"
 	depends on !BR2_arc
-	default BR2_BINUTILS_VERSION_2_24_X
+	default BR2_BINUTILS_VERSION_2_25_X
 	help
 	  Select the version of binutils you wish to use.
 

commit 26332f818d5dc966175981838e064b6bf28dec4e

binutils: use binutils 2.25.1 by default
Recent commit 88f810a8cd16e0acebb8a3da4fcb539abbb433dc broke the build
by default to binutils 2.25 for the target, while we only have hashes
for 2.25.1. Use 2.25.1 instead.

Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

package/binutils/binutils.mk
@@ -11,7 +11,7 @@ ifeq ($(BINUTILS_VERSION),)
 ifeq ($(BR2_arc),y)
 BINUTILS_VERSION = arc-2015.12
 else
-BINUTILS_VERSION = 2.25
+BINUTILS_VERSION = 2.25.1
 endif
 endif # BINUTILS_VERSION
 