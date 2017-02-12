Hancock33/batocera.piboy

commit 9d34e07c165be1d61c0597afd56af14234348927

classpath: bump to version 0.99
Drop both patches since they are included upstream.
We don't need to autoreconf now.

Note: classpath 0.99 has a bug in it's autoconf scripts, such that
configure always fails when specifying --disable-tools[1].
We enable these for now.

Also explicitly disable GJDoc generation, as this defaults to on now.
--disable-plugin is not needed any more, since this is now the default.

[1] https://gcc.gnu.org/bugzilla/show_bug.cgi?id=70660

Signed-off-by: Marcus Hoffmann <m.hoffmann@cartelsol.com>
Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

package/classpath/classpath.mk
@@ -4,21 +4,20 @@
 #
 ################################################################################
 
-CLASSPATH_VERSION = 0.98
+CLASSPATH_VERSION = 0.99
 CLASSPATH_SITE = $(BR2_GNU_MIRROR)/classpath
 CLASSPATH_CONF_OPTS = \
 	--disable-examples \
-	--disable-plugin \
 	--disable-rpath \
 	--disable-Werror \
 	--disable-gconf-peer \
-	--disable-tools
+	--disable-gjdoc
+	--enable-tools
 
 # classpath assumes qt runs on top of X11, but we
 # don't support qt4 on X11
 CLASSPATH_CONF_OPTS += --disable-qt-peer
 CLASSPATH_DEPENDENCIES = host-pkgconf
-CLASSPATH_AUTORECONF = YES
 CLASSPATH_LICENSE = GPLv2+ with exception
 CLASSPATH_LICENSE_FILES = COPYING
 

package/classpath/0001-fix-gmp-include.patch
@@ -1,46 +1,0 @@
-The GMP include is hardcoded to /usr/include causing obvious problems.
-
-From classpath CVS:
-http://cvs.savannah.gnu.org/viewvc/classpath/configure.ac?root=classpath&r1=1.245&r2=1.246
-
-[Gustavo: ditch -L or libtool complains loudly]
-
-Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
-
---- a/configure.ac	2010/01/30 01:33:50	1.245
-+++ b/configure.ac	2010/05/04 12:29:23	1.246
-@@ -752,10 +752,32 @@
-   dnl __gmpz_mul_si for earlier versions (>= 3.1).
-   dnl IMPORTANT: if you decide to look for __gmpz_combit, don't forget to
-   dnl change the name of the corresponding ac_ variable on lines 860...
-+  AC_ARG_WITH(gmp, [  --with-gmp=PATH         specify prefix directory for the installed GMP package.
-+                          Equivalent to --with-gmp-include=PATH/include
-+                          plus --with-gmp-lib=PATH/lib])
-+  AC_ARG_WITH(gmp_include, [  --with-gmp-include=PATH specify directory for installed GMP include files])
-+  AC_ARG_WITH(gmp_lib, [  --with-gmp-lib=PATH     specify directory for the installed GMP library])
-+
-   if test "x${COMPILE_GMP}" = xyes; then
-+    AC_MSG_CHECKING([for GMP directories])
-+    if test "x$with_gmp" != x; then
-+       gmplibs="-lgmp"
-+       gmpinc="-I$with_gmp/include"
-+       if test "x$with_gmp_include" != x; then
-+       	  gmpinc="-I$with_gmp_include $gmpinc"
-+       fi
-+       if test "x$with_gmp_lib" != x; then
-+       	  gmplibs="$gmplibs"
-+       fi
-+    else
-+        with_gmp="/usr"
-+	gmplibs="-lgmp"
-+    	gmpinc="-I/usr/include"
-+    fi
-+    AC_MSG_RESULT([prefix=$with_gmp, libs=$gmplibs, inc=$gmpinc])
-     AC_CHECK_LIB(gmp, __gmpz_mul_si,
--      [GMP_CFLAGS=-I/usr/include
--       GMP_LIBS=-lgmp ],
-+      [GMP_CFLAGS="$gmpinc"
-+       GMP_LIBS="$gmplibs" ],
-       [GMP_CFLAGS=
-        GMP_LIBS= ])
-     AC_SUBST(GMP_CFLAGS)

package/classpath/classpath.hash
@@ -1,2 +1,2 @@
-# From http://www.gnu.org/software/classpath/announce/20090205.html
-sha1	1d6e8d1b3542a35bbd1013c61fab3cfae083decb	classpath-0.98.tar.gz
+# From https://www.gnu.org/software/classpath/announce/20120307.html
+sha256	f929297f8ae9b613a1a167e231566861893260651d913ad9b6c11933895fecc8	classpath-0.99.tar.gz

commit 3ad5b6e86883895b2957713373133f998b6a815e

classpath: fix typo in CLASSPATH_CONF_OPTS definition
This typo was introduced by commit
9d34e07c165be1d61c0597afd56af14234348927 ("classpath: bump to version
0.99").

Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

package/classpath/classpath.mk
@@ -11,7 +11,7 @@ CLASSPATH_CONF_OPTS = \
 	--disable-rpath \
 	--disable-Werror \
 	--disable-gconf-peer \
-	--disable-gjdoc
+	--disable-gjdoc \
 	--enable-tools
 
 # classpath assumes qt runs on top of X11, but we