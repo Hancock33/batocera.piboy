Hancock33/batocera.piboy

commit c099fcdb2ce7a6952eff79e0e79874cd1add7dc1

package/firejail: mark as broken
The firejail package does not cross-compile at all, because it is not
using automake, but a hand-written Makefile.in. So it does not use the
cross-compiler found by ./configure.

But when we fix it by passing:
    FIREJAIL_MAKE_ENV = $(TARGET_CONFIGURE_OPTS)

it then fails to build with symbols redefinitions:

    /home/ymorin/dev/buildroot/O/host/usr/bin/arm-linux-gnueabihf-gcc -D_LARGEFILE_SOURCE -D_LARGEFILE64_SOURCE -D_FILE_OFFSET_BITS=64  -Os  -ggdb  -O2 -DVERSION='"0.9.44.8"' -fstack-protector-all -D_FORTIFY_SOURCE=2 -fPIC -Wformat -Wformat-security  -c libtrace.c -o libtrace.o
    /tmpfs/cckgOLVz.s: Assembler messages:
    /tmpfs/cckgOLVz.s:1115: Error: symbol `stat64' is already defined
    /tmpfs/cckgOLVz.s:1282: Error: symbol `lstat64' is already defined
    /tmpfs/cckgOLVz.s:2296: Error: symbol `fopen64' is already defined
    /tmpfs/cckgOLVz.s:2631: Error: symbol `freopen64' is already defined
    Makefile:16: recipe for target 'libtrace.o' failed

(ditto for libtracelog)

Fixing this is not trivial, so we just mark the package as broken.

Signed-off-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
Cc: Chris Frederick <chrisf@cdf123.net>
[Thomas: also mark the Config.in comment as BROKEN.]
Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

package/firejail/Config.in
@@ -1,5 +1,6 @@
 config BR2_PACKAGE_FIREJAIL
 	bool "firejail"
+	depends on BROKEN # does not cross-compile at all
 	depends on BR2_USE_MMU # fork()
 	depends on BR2_TOOLCHAIN_HAS_THREADS
 	# uClibc: error: ‘EM_ARM’ undeclared
@@ -16,4 +17,5 @@ config BR2_PACKAGE_FIREJAIL
 
 comment "firejail needs a glibc or musl toolchain w/ threads"
 	depends on BR2_USE_MMU
+	depends on BROKEN
 	depends on BR2_TOOLCHAIN_USES_UCLIBC || !BR2_TOOLCHAIN_HAS_THREADS

commit e51923460221e8958bfd78ad5c4b49c27fad9b8d

package/firejail: remove broken package
firejail has been marked as broken since c099fcdb2ce7a6952eff79e0e79874cd1add7dc1

Signed-off-by: Romain Naour <romain.naour@gmail.com>
Cc: Chris Frederick <chrisf@cdf123.net>
Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

DEVELOPERS
@@ -322,9 +322,6 @@ F:	package/libdvbsi/
 F:	package/libsvg/
 F:	package/libsvg-cairo/
 
-N:	Chris Frederick <chrisf@cdf123.net>
-F:	package/firejail/
-
 N:	Chris Packham <judge.packham@gmail.com>
 F:	package/eventlog/
 F:	package/micropython/

package/Config.in
@@ -1810,7 +1810,6 @@ menu "System tools"
 	source "package/efibootmgr/Config.in"
 	source "package/efivar/Config.in"
 	source "package/emlog/Config.in"
-	source "package/firejail/Config.in"
 	source "package/ftop/Config.in"
 	source "package/getent/Config.in"
 	source "package/htop/Config.in"

package/firejail/Config.in
@@ -1,21 +1,0 @@
-config BR2_PACKAGE_FIREJAIL
-	bool "firejail"
-	depends on BROKEN # does not cross-compile at all
-	depends on BR2_USE_MMU # fork()
-	depends on BR2_TOOLCHAIN_HAS_THREADS
-	# uClibc: error: ‘EM_ARM’ undeclared
-	depends on !BR2_TOOLCHAIN_USES_UCLIBC
-	help
-	  Firejail is a SUID program that reduces the risk of security
-	  breaches by restricting the running environment of untrusted
-	  applications using Linux namespaces and seccomp-bpf. It
-	  allows a process and all its descendants to have their own
-	  private view of the globally shared kernel resources, such
-	  as the network stack, process table, mount table.
-
-	  https://firejail.wordpress.com/
-
-comment "firejail needs a glibc or musl toolchain w/ threads"
-	depends on BR2_USE_MMU
-	depends on BROKEN
-	depends on BR2_TOOLCHAIN_USES_UCLIBC || !BR2_TOOLCHAIN_HAS_THREADS

package/firejail/firejail.hash
@@ -1,3 +1,0 @@
-# From https://sourceforge.net/projects/firejail/files/firejail/
-md5 7e6dca7202b1d70105b39646755cc620 firejail-0.9.44.8.tar.xz
-sha1 019423df0aee84d474f9fcd1f6a871a2fe8aa9a5 firejail-0.9.44.8.tar.xz

package/firejail/firejail.mk
@@ -1,28 +1,0 @@
-################################################################################
-#
-# firejail
-#
-################################################################################
-
-FIREJAIL_VERSION = 0.9.44.8
-FIREJAIL_SITE = http://download.sourceforge.net/firejail
-FIREJAIL_SOURCE = firejail-$(FIREJAIL_VERSION).tar.xz
-FIREJAIL_LICENSE = GPL-2.0+
-FIREJAIL_LICENSE_FILES = COPYING
-
-FIREJAIL_CONF_OPTS = \
-	--enable-bind \
-	--enable-file-transfer \
-	--enable-network \
-	--enable-seccomp \
-	--enable-userns
-
-ifeq ($(BR2_PACKAGE_BUSYBOX),y)
-FIREJAIL_CONF_OPTS += --enable-busybox-workaround
-endif
-
-define FIREJAIL_PERMISSIONS
-	/usr/bin/firejail f 4755 0 0 - - - - -
-endef
-
-$(eval $(autotools-package))