Hancock33/batocera.piboy

commit b23b28f58e4920efe5053ddd1c78cdff33fadb0d

package/glibc: install locale utility when NLS is enabled
Signed-off-by: Pascal de Bruijn <p.debruijn@unilogic.nl>
Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

package/glibc/glibc.mk
@@ -146,7 +146,10 @@ endif
 ifeq ($(BR2_PACKAGE_GLIBC_UTILS),y)
 GLIBC_TARGET_UTILS_USR_BIN = posix/getconf elf/ldd
 GLIBC_TARGET_UTILS_SBIN = elf/ldconfig
+endif
 
+ifeq ($(BR2_SYSTEM_ENABLE_NLS),y)
+GLIBC_TARGET_UTILS_USR_BIN += locale/locale
 endif
 
 define GLIBC_INSTALL_TARGET_CMDS

package/glibc/Config.in
@@ -14,7 +14,7 @@ config BR2_PACKAGE_GLIBC_UTILS
 	depends on BR2_PACKAGE_BASH
 	help
 	  Enabling this option will compile and install the getconf,
-	  ldconfig and ldd glibc utilities for the target.
+	  ldconfig, ldd and locale glibc utilities for the target.
 
 comment "glibc utilities need bash"
 	depends on !BR2_PACKAGE_BASH

commit 9c6adafcc7e87211113eb586049450e2fc58bdac

package/glibc: install locale utility only when BR2_PACKAGE_GLIBC_UTILS=y
Fixes: b23b28f58e4920efe5053ddd1c78cdff33fadb0d

Signed-off-by: Pascal de Bruijn <p.debruijn@unilogic.nl>
Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

package/glibc/glibc.mk
@@ -146,10 +146,9 @@ endif
 ifeq ($(BR2_PACKAGE_GLIBC_UTILS),y)
 GLIBC_TARGET_UTILS_USR_BIN = posix/getconf elf/ldd
 GLIBC_TARGET_UTILS_SBIN = elf/ldconfig
-endif
-
 ifeq ($(BR2_SYSTEM_ENABLE_NLS),y)
 GLIBC_TARGET_UTILS_USR_BIN += locale/locale
+endif
 endif
 
 define GLIBC_INSTALL_TARGET_CMDS