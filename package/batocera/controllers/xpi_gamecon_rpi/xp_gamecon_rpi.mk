9cdfd9402d00caf5220c2cf812a09521d27ea325
lee <lee.hancock4@ntlworld.com>
2021-05-15 14:20:25 +0100

    make XPI_GAMECON compile

--- package/batocera/controllers/xpi_gamecon_rpi/xp_gamecon_rpi.mk
+++ package/batocera/controllers/xpi_gamecon_rpi/xp_gamecon_rpi.mk
@@ -7,14 +7,15 @@
 XPI_GAMECON_RPI_VERSION = cc96ca5777c2a17b9b44c1bbc15f37cfbcdd465f
 XPI_GAMECON_RPI_SITE = $(call github,losernator,PiBoyControls,$(XPI_GAMECON_RPI_VERSION))
 XPI_GAMECON_RPI_DEPENDENCIES = linux
+XPI_GAMECON_LINUX_VER = $(shell dirname $(TARGET_DIR)/lib/modules/*/build)
 
 define XPI_GAMECON_RPI_BUILD_CMDS
-	$(MAKE) -C $(LINUX_DIR) M=$(@D) modules
+        $(MAKE) -C $(@D) $(LINUX_MAKE_FLAGS) KERNELDIR=$(LINUX_DIR) KVERSION=$(XPI_GAMECON_LINUX_VER)
 endef
 
-define XPI_GAMECON_RPI_INSTALL_TARGET_CMDS
-	$(MAKE) -C $(LINUX_DIR) M=$(@D) modules_install
-endef
+#define XPI_GAMECON_RPI_INSTALL_TARGET_CMDS
+#	$(MAKE) -C $(LINUX_DIR)/build M=$(@D) modules_install
+#endef
 
 $(eval $(kernel-module))
 $(eval $(generic-package))
