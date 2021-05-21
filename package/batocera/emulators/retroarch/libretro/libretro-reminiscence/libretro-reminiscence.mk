dee0ec7e3b4edb83dbb2cfc1764169e4e29d136f
Lee <lee.hancock4@ntlworld.com>
2021-05-21 23:49:15 +0100

    Add libretro-reminiscence (flashback)

---
+++ package/batocera/emulators/retroarch/libretro/libretro-reminiscence/libretro-reminiscence.mk
@@ -0,0 +1,20 @@
+################################################################################
+#
+# REMINISCENCE
+#
+################################################################################
+#Commits on May 12, 2021
+LIBRETRO_REMINISCENCE_VERSION = b065c21af69e432f578524839c192a4061773d6e
+LIBRETRO_REMINISCENCE_SITE = $(call github,libretro,REminiscence,$(LIBRETRO_REMINISCENCE_VERSION))
+LIBRETRO_REMINISCENCE_LICENSE = GPL-3.0
+
+define LIBRETRO_REMINISCENCE_BUILD_CMDS
+    $(TARGET_CONFIGURE_OPTS) $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D)/ -f Makefile platform="$(LIBRETRO_PLATFORM)"
+endef
+
+define LIBRETRO_REMINISCENCE_INSTALL_TARGET_CMDS
+	$(INSTALL) -D $(@D)/reminiscence_libretro.so \
+	$(TARGET_DIR)/usr/lib/libretro/reminiscence_libretro.so
+endef
+
+$(eval $(generic-package))
