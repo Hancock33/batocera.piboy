ebc0b77e1f636b4b9b26b965a35d1a94be764f32
Lee <lee.hancock4@ntlworld.com>
2021-05-21 21:05:51 +0100

    Add libretro-ecwolf

---
+++ package/batocera/emulators/retroarch/libretro/libretro-ecwolf/libretro-ecwolf.mk
@@ -0,0 +1,32 @@
+################################################################################
+#
+# ECWOLF
+#
+################################################################################
+#Commits on Apr 30, 2021
+LIBRETRO_ECWOLF_VERSION = fe041979e5e51b2ca0c90bb40ea6acf79714c3c7
+LIBRETRO_ECWOLF_SITE = $(call github,libretro,ecwolf,$(LIBRETRO_ECWOLF_VERSION))
+LIBRETRO_ECWOLF_LICENSE = GPL-2.0, ID-Software, MAME
+LIBRETRO_ECWOLF_LICENSE_FILES = docs/license-gpl.txt docs/license-id.txt docs/license-mame.txt
+LIBRETRO_ECWOLF_NON_COMMERCIAL = y
+
+LIBRETRO_ECWOLF_PLATFORM = $(LIBRETRO_PLATFORM)
+
+ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_RPI3),y)
+	LIBRETRO_ECWOLF_PLATFORM = rpi3
+endif
+
+ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_ODROIDGOA),y)
+	LIBRETRO_ECWOLF_PLATFORM = classic_armv8_a35
+endif
+
+define LIBRETRO_ECWOLF_BUILD_CMDS
+	$(TARGET_CONFIGURE_OPTS) $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D)/src/libretro -f Makefile platform="$(LIBRETRO_ECWOLF_PLATFORM)"
+endef
+
+define LIBRETRO_ECWOLF_INSTALL_TARGET_CMDS
+	$(INSTALL) -D $(@D)/src/libretro/ecwolf_libretro.so \
+	$(TARGET_DIR)/usr/lib/libretro/ecwolf_libretro.so
+endef
+
+$(eval $(generic-package))
--- package/batocera/emulators/retroarch/libretro/libretro-xrick/libretro-xrick.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-xrick/libretro-xrick.mk
@@ -9,16 +9,12 @@ LIBRETRO_XRICK_SITE = $(call github,libretro,xrick-libretro,$(LIBRETRO_XRICK_VER
 LIBRETRO_XRICK_LICENSE = GPL-3.0
 
 define LIBRETRO_XRICK_BUILD_CMDS
-	$(SED) "s|-O2|-O3|g" $(@D)/Makefile.libretro
-	CFLAGS="$(TARGET_CFLAGS) $(COMPILER_COMMONS_CFLAGS_SO)" \
-		CXXFLAGS="$(TARGET_CXXFLAGS) $(COMPILER_COMMONS_CXXFLAGS_SO)" \
-		LDFLAGS="$(TARGET_LDFLAGS) $(COMPILER_COMMONS_LDFLAGS_SO)" \
-		$(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D)/ -f Makefile.libretro platform="unix"
+	$(TARGET_CONFIGURE_OPTS) $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D)/ -f Makefile.libretro platform="unix"
 endef
 
 define LIBRETRO_XRICK_INSTALL_TARGET_CMDS
 	$(INSTALL) -D $(@D)/xrick_libretro.so \
-		$(TARGET_DIR)/usr/lib/libretro/xrick_libretro.so
+    $(TARGET_DIR)/usr/lib/libretro/xrick_libretro.so
 	mkdir -p "$(TARGET_DIR)/usr/share/batocera/datainit/roms/xrick"
 	$(INSTALL) -D $(@D)/data.zip "$(TARGET_DIR)/usr/share/batocera/datainit/roms/xrick/data.zip"
 endef
