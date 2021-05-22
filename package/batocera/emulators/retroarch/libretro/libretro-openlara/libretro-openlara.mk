132d4393df0b37b9ce8353c264bf7859d5d0a04a
Lee <lee.hancock4@ntlworld.com>
2021-05-22 15:28:21 +0100

    Change xpi_gamecon repo

---
+++ package/batocera/emulators/retroarch/libretro/libretro-openlara/libretro-openlara.mk
@@ -0,0 +1,21 @@
+################################################################################
+#
+# OPENLARA
+#
+################################################################################
+#Commits on Jan 21, 2021
+LIBRETRO_OPENLARA_VERSION = fe88d74673344afc71891717b2474d33f90c165f
+LIBRETRO_OPENLARA_SITE = $(call github,libretro,openlara,$(LIBRETRO_OPENLARA_VERSION))
+LIBRETRO_OPENLARA_LICENSE = BSD 2
+LIBRETRO_OPENLARA_LICENSE_FILES = LICENSE
+
+define LIBRETRO_OPENLARA_BUILD_CMDS
+	$(TARGET_CONFIGURE_OPTS) $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D)/src/platform/libretro/ -f Makefile platform="rpi4"
+endef
+
+define LIBRETRO_OPENLARA_INSTALL_TARGET_CMDS
+	$(INSTALL) -D $(@D)/openlara_libretro.so \
+		$(TARGET_DIR)/usr/lib/libretro/openlara_libretro.so
+endef
+
+$(eval $(generic-package))
