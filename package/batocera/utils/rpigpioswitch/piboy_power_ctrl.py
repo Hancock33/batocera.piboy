db43d7d73802249bd3a66213bb0cbaee199e0437
Lee <lee.hancock4@ntlworld.com>
2021-05-30 11:16:08 +0100

    Add power control script

---
+++ package/batocera/utils/rpigpioswitch/piboy_power_ctrl.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import time
+import sys
+import os
+# Configuration
+WAIT_TIME = 1.0  # [s] Time to wait between each refresh
+pwrctrl = 0
+pwrctrlOld = 0
+hyst = 1
+# Volume Controller
+try:
+    while 1:
+        # Read Volume 
+        pwrctrlFile = open("/sys/kernel/xpi_gamecon/status", "r")
+        pwrctrl = int(pwrctrlFile.read())
+        pwrctrlFile.close()
+        if abs(pwrctrl - pwrctrlOld) > hyst:
+            if pwrctrl == 6:
+                os.system("shutdown -h now")
+
+        pwrctrlOld = pwrctrl
+        # Wait until next refresh
+        time.sleep(WAIT_TIME)
+        
+# If a keyboard interrupt occurs (ctrl   c)
+except KeyboardInterrupt:
+    sys.exit()
