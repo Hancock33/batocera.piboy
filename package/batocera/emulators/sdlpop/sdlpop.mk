e157ed9a983327f14aca855ba36a8fb7087ea843
Lee <lee.hancock4@ntlworld.com>
2021-06-05 14:23:22 +0100

    bump sdlpop and daphne versions

--- package/batocera/emulators/sdlpop/sdlpop.mk
+++ package/batocera/emulators/sdlpop/sdlpop.mk
@@ -3,8 +3,8 @@
 # SDLPOP
 #
 ################################################################################
-# Version.: Commits on Mar 6, 2021
-SDLPOP_VERSION = c244c739e99cc79b1e357dd4a8e5808350f1fcd7
+# Version.: Commits on May 30, 2021
+SDLPOP_VERSION = 819058624bcf0cada46e6ea3757e7b051d49565e
 SDLPOP_SITE = $(call github,NagyD,SDLPoP,$(SDLPOP_VERSION))
 SDLPOP_SUBDIR = src
 SDLPOP_LICENSE = GPLv3
