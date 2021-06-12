197341fbbb77ab2a09e204758754ce3ce7dc05ff
Lee <lee.hancock4@ntlworld.com>
2021-06-12 13:20:59 +0100

    oops spelling

--- package/batocera/controllers/xpi_gamecon_rpi/xp_gamecon_rpi.mk
+++ package/batocera/controllers/xpi_gamecon_rpi/xp_gamecon_rpi.mk
@@ -1,19 +1,19 @@
 ################################################################################
 #
-# XPI_GAMECON_RPI
+# XP_GAMECON_RPI
 #
 ################################################################################
 # Version.: Commits on May 30, 2021
 XP_GAMECON_RPI_VERSION = 2bea492383af51cc945d2391ca3a41c7b05964c0
-XPI_GAMECON_RPI_SITE = $(call github,hancock33,piboycontrols,$(XPI_GAMECON_RPI_VERSION))
-XPI_GAMECON_RPI_DEPENDENCIES = linux
-XPI_GAMECON_LINUX_VER = $(shell dirname $(TARGET_DIR)/lib/modules/*/build)
+XP_GAMECON_RPI_SITE = $(call github,hancock33,piboycontrols,$(XP_GAMECON_RPI_VERSION))
+XP_GAMECON_RPI_DEPENDENCIES = linux
+XP_GAMECON_LINUX_VER = $(shell dirname $(TARGET_DIR)/lib/modules/*/build)
 
-define XPI_GAMECON_RPI_BUILD_CMDS
-        $(MAKE) -C $(@D) $(LINUX_MAKE_FLAGS) KERNELDIR=$(LINUX_DIR) KVERSION=$(XPI_GAMECON_LINUX_VER)
+define XP_GAMECON_RPI_BUILD_CMDS
+        $(MAKE) -C $(@D) $(LINUX_MAKE_FLAGS) KERNELDIR=$(LINUX_DIR) KVERSION=$(XP_GAMECON_LINUX_VER)
 endef
 
-#define XPI_GAMECON_RPI_INSTALL_TARGET_CMDS
+#define XP_GAMECON_RPI_INSTALL_TARGET_CMDS
 #	$(MAKE) -C $(LINUX_DIR)/build M=$(@D) modules_install
 #endef
 
