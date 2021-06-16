0fe54536fc99a4c136706607dbb42a98cd6f7ce8
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2021-06-16 21:39:25 +0100

    Update mupen64plus-core.mk

--- package/batocera/emulators/mupen64plus/mupen64plus-core/mupen64plus-core.mk
+++ package/batocera/emulators/mupen64plus/mupen64plus-core/mupen64plus-core.mk
@@ -3,8 +3,8 @@
 # mupen64plus core
 #
 ################################################################################
-# Version.: Commits on Jun 11, 2021
-MUPEN64PLUS_CORE_VERSION = dfeda1f289a3ad2d9265d64e32cf6577e8bcd28f
+# Version.: Commits on Jun 16, 2021
+MUPEN64PLUS_CORE_VERSION = 1e150c5b0a9989e543a50da0dc329f67e7747e86
 MUPEN64PLUS_CORE_SITE = $(call github,mupen64plus,mupen64plus-core,$(MUPEN64PLUS_CORE_VERSION))
 MUPEN64PLUS_CORE_LICENSE = GPLv2
 MUPEN64PLUS_CORE_DEPENDENCIES = sdl2 alsa-lib freetype dejavu
