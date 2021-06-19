46463710cca16f88da10c89ffe21d730958ca680
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2021-06-19 23:41:55 +0100

    Update es-theme-carbon.mk

--- package/batocera/emulationstation/es-theme-carbon/es-theme-carbon.mk
+++ package/batocera/emulationstation/es-theme-carbon/es-theme-carbon.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 # Version.: Commits on Jun 19, 2021
-ES_THEME_CARBON_VERSION = a24ae4d571228b429f6e737e1a8cfb3e00f38dbe
+ES_THEME_CARBON_VERSION = 4d117981d8c7a56e9ff99549698890f40b632908
 ES_THEME_CARBON_SITE = $(call github,hancock33,es-theme-carbon,$(ES_THEME_CARBON_VERSION))
 
 define ES_THEME_CARBON_INSTALL_TARGET_CMDS
