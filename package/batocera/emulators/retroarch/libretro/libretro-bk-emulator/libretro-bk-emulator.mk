89881ce884e3461cf44d830c193c6d098b1a2b4e
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2021-06-19 23:13:50 +0100

    add libretro-bk

---
+++ package/batocera/emulators/retroarch/libretro/libretro-bk-emulator/libretro-bk-emulator.mk
@@ -0,0 +1,22 @@
+################################################################################
+#
+# BK-0010 / BK-0010.01 / BK-0011 emulator
+#
+################################################################################
+# Version.: Commits on Mar 15, 2021
+LIBRETRO_BK_EMULATOR_VERSION = 70f4a2d812379b8e3e3c7ed3025d08583dee98b1
+LIBRETRO_BK_EMULATOR_SITE = $(call github,libretro,bk-emulator,$(LIBRETRO_BK_EMULATOR_VERSION))
+LIBRETRO_BK_EMULATOR_LICENSE = Custom
+
+LIBRETRO_BK_EMULATOR_PLATFORM = $(LIBRETRO_PLATFORM)
+
+define LIBRETRO_BK_EMULATOR_BUILD_CMDS
+	$(TARGET_CONFIGURE_OPTS) $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D) -f Makefile platform="unix"
+endef
+
+define LIBRETRO_BK_EMULATOR_INSTALL_TARGET_CMDS
+	$(INSTALL) -D $(@D)/bk_libretro.so \
+		$(TARGET_DIR)/usr/lib/libretro/bk_libretro.so
+endef
+
+$(eval $(generic-package))
