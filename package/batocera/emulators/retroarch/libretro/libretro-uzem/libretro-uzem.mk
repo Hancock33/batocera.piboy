1b7a4b2369d2875b29a1cc8cc2e41226eba2e606
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2021-06-20 10:17:37 +0100

    add uzebox

---
+++ package/batocera/emulators/retroarch/libretro/libretro-uzem/libretro-uzem.mk
@@ -0,0 +1,20 @@
+################################################################################
+#
+# UzeBox
+#
+################################################################################
+# Version.: Commits on Mar 14, 2021
+LIBRETRO_UZEM_VERSION = 675b4485b776fe5166612192466c95a25a927a63
+LIBRETRO_UZEM_SITE = $(call github,libretro,libretro-uzem,$(LIBRETRO_UZEM_VERSION))
+LIBRETRO_UZEM_LICENSE = GPL-3.0
+
+define LIBRETRO_UZEM_BUILD_CMDS
+	$(TARGET_CONFIGURE_OPTS) $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D) -f Makefile
+endef
+
+define LIBRETRO_UZEM_INSTALL_TARGET_CMDS
+	$(INSTALL) -D $(@D)/uzem_libretro.so \
+		$(TARGET_DIR)/usr/lib/libretro/uzem_libretro.so
+endef
+
+$(eval $(generic-package))
