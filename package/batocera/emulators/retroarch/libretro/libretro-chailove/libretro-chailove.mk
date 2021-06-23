2cb91809499caa696fb9adb7a0d6a82adf8631df
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2021-06-23 06:30:25 +0100

    add libretro-chailove game engine

---
+++ package/batocera/emulators/retroarch/libretro/libretro-chailove/libretro-chailove.mk
@@ -0,0 +1,20 @@
+################################################################################
+#
+# CHAILOVE
+#
+################################################################################
+# Version.: Commits on Mar 08, 2021
+LIBRETRO_CHAILOVE_VERSION = 51caa9a8be34bfe9c68e9a3dd7fbbe81d832bc2a
+LIBRETRO_CHAILOVE_SITE = $(call github,libretro,libretro-chailove,$(LIBRETRO_CHAILOVE_VERSION))
+LIBRETRO_CHAILOVE_LICENSE = MIT
+
+define LIBRETRO_CHAILOVE_BUILD_CMDS
+	$(TARGET_CONFIGURE_OPTS) $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D)/ -f Makefile.libretro platform="unix"
+endef
+
+define LIBRETRO_CHAILOVE_INSTALL_TARGET_CMDS
+	$(INSTALL) -D $(@D)/chailove_libretro.so \
+    $(TARGET_DIR)/usr/lib/libretro/chailove_libretro.so
+endef
+
+$(eval $(generic-package))
