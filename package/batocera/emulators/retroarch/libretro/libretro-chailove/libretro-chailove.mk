55b4f57f069d7337da439e5fb149597494c0316f
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2021-06-23 06:41:40 +0100

    Update libretro-chailove.mk

--- package/batocera/emulators/retroarch/libretro/libretro-chailove/libretro-chailove.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-chailove/libretro-chailove.mk
@@ -5,7 +5,10 @@
 ################################################################################
 # Version.: Commits on Mar 08, 2021
 LIBRETRO_CHAILOVE_VERSION = 51caa9a8be34bfe9c68e9a3dd7fbbe81d832bc2a
-LIBRETRO_CHAILOVE_SITE = $(call github,libretro,libretro-chailove,$(LIBRETRO_CHAILOVE_VERSION))
+LIBRETRO_CHAILOVE_SITE = https://github.com/libretro/libretro-chailove.git
+
+LIBRETRO_CHAILOVE_SITE_METHOD=git
+LIBRETRO_CHAILOVE_GIT_SUBMODULES=YES
 LIBRETRO_CHAILOVE_LICENSE = MIT
 
 define LIBRETRO_CHAILOVE_BUILD_CMDS
