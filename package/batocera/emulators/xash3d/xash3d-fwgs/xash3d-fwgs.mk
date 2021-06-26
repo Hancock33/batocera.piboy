7970bed002d0e26f2fd3826e3e745fd893af866f
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2021-06-26 14:31:23 +0100

    daily update

--- package/batocera/emulators/xash3d/xash3d-fwgs/xash3d-fwgs.mk
+++ package/batocera/emulators/xash3d/xash3d-fwgs/xash3d-fwgs.mk
@@ -3,8 +3,8 @@
 # xash3d-fwgs
 #
 ################################################################################
-# Version.: Commits on Jun 22, 2021
-XASH3D_FWGS_VERSION = 4abe1a77f4053eaa73d963bad287c50ae998a49c
+# Version.: Commits on Jun 25, 2021
+XASH3D_FWGS_VERSION = f8e6033ae6d769b48a13aab9e50c2690d8eb8517
 XASH3D_FWGS_SITE = https://github.com/FWGS/xash3d-fwgs.git
 XASH3D_FWGS_SITE_METHOD = git
 XASH3D_FWGS_GIT_SUBMODULES = yes
