60d759dd6ff8ada57407440637adcbee30a2c8e7
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2021-06-27 01:05:37 +0100

    update utils

--- package/batocera/utils/flatpak/flatpak.mk
+++ package/batocera/utils/flatpak/flatpak.mk
@@ -5,8 +5,7 @@
 ################################################################################
 # Version.: Commits on Jun 17, 2021
 FLATPAK_VERSION = 6ebe33117c90194d6ed776cf33d8e8399a50fbf5
-FLATPAK_SOURCE = flatpak-$(FLATPAK_VERSION).tar.xz
-FLATPAK_SITE = https://github.com/flatpak/flatpak/releases/download/$(FLATPAK_VERSION)
+FLATPAK_SITE = $(call github,flatpak,flatpak,$(FLATPAK_VERSION))
 
 FLATPAK_DEPENDENCIES += pkgconf host-pkgconf libcap libarchive libglib2 libsoup libgpgme polkit
 FLATPAK_DEPENDENCIES += libostree json-glib appstream-glib yaml-cpp
