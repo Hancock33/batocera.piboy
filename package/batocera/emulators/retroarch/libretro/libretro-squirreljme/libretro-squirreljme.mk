51f98926e30d154eea92d2238af940df5e6c9555
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2021-07-06 22:43:06 +0100

    add java cores

---
+++ package/batocera/emulators/retroarch/libretro/libretro-squirreljme/libretro-squirreljme.mk
@@ -0,0 +1,28 @@
+################################################################################
+#
+# SQUIRRELJME
+#
+################################################################################
+# Version.: Commits on Jul 05, 2021
+LIBRETRO_SQUIRRELJME_VERSION = 4138ef9e33c2d8971503e7f4e57aacf90287be6b
+LIBRETRO_SQUIRRELJME_SITE = $(call github,SquirrelJME,SquirrelJME,$(LIBRETRO_SQUIRRELJME_VERSION))
+LIBRETRO_SQUIRRELJME_LICENSE = GPL-3.0
+
+ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_RPI4),y)
+LIBRETRO_SQUIRRELJME_MAKE_FILE=Makefile.linux_rpi4
+else ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_RPI3),y)
+LIBRETRO_SQUIRRELJME_MAKE_FILE=Makefile.linux_rpi3
+else
+LIBRETRO_SQUIRRELJME_MAKE_FILE=Makefile.linux_x86_64
+endif
+
+define LIBRETRO_SQUIRRELJME_BUILD_CMDS
+	$(TARGET_CONFIGURE_OPTS) $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D)/ratufacoat/libretro -f "$(LIBRETRO_SQUIRRELJME_MAKE_FILE)"
+endef
+
+define LIBRETRO_SQUIRRELJME_INSTALL_TARGET_CMDS
+	$(INSTALL) -D $(@D)/ratufacoat/libretro/squirreljme_libretro.so \
+		$(TARGET_DIR)/usr/lib/libretro/squirreljme_libretro.so
+endef
+
+$(eval $(generic-package))
