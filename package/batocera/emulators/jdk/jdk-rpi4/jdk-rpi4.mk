31001fb57098322bf7aef2c180416a4138e8b588
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2021-07-08 19:47:39 +0100

    add jdk

---
+++ package/batocera/emulators/jdk/jdk-rpi4/jdk-rpi4.mk
@@ -0,0 +1,19 @@
+################################################################################
+#
+# jdk-rpi4
+#
+################################################################################
+JDK_RPI4_VERSION = 16.0.1+9
+JDK_RPI4_SOURCE = openjdk-$(JDK_RPI4_VERSION)_aarch64.tar.xz
+
+JDK_RPI4_SITE = https://github.com/Hancock33/openjdk-batocera/releases/download/32-dev/$(JDK_RPI4_VERSION)
+
+define JDK_RPI4_EXTRACT_CMDS
+	mkdir -p $(@D)/target && cd $(@D)/target && tar xf $(DL_DIR)/$(JDK_RPI4_DL_SUBDIR)/$(JDK_RPI4_SOURCE)
+endef
+
+define JDK_RPI4_INSTALL_TARGET_CMDS
+	cp -prn $(@D)/target/* $(TARGET_DIR)
+endef
+
+$(eval $(generic-package))
