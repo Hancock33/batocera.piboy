5c1420b01a438703fccdb776e38164fe519b314b
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2021-07-08 20:39:57 +0100

    update download locations

--- package/batocera/emulators/jdk/jdk-x86_64/jdk-x86_64.mk
+++ package/batocera/emulators/jdk/jdk-x86_64/jdk-x86_64.mk
@@ -6,7 +6,7 @@
 JDK_X86_64_VERSION = 16.0.1+9
 JDK_X86_64_SOURCE = openjdk-$(JDK_X86_64_VERSION)_x86_64.tar.xz
 
-JDK_X86_64_SITE = https://github.com/Hancock33/openjdk-batocera/releases/download/32-dev/$(JDK_X86_64_SOURCE)
+JDK_X86_64_SITE = https://github.com/Hancock33/openjdk-batocera/releases/download/32-dev
 
 define JDK_X86_64_EXTRACT_CMDS
 	mkdir -p $(@D)/target && cd $(@D)/target && tar xf $(DL_DIR)/$(JDK_X86_64_DL_SUBDIR)/$(JDK_X86_64_SOURCE)
