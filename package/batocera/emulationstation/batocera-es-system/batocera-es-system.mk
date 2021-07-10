4a5694e4406e0c17f601259d308ed338fc0287df
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2021-07-10 07:32:44 +0100

    Update batocera-es-system.mk

--- package/batocera/emulationstation/batocera-es-system/batocera-es-system.mk
+++ package/batocera/emulationstation/batocera-es-system/batocera-es-system.mk
@@ -28,7 +28,8 @@ define BATOCERA_ES_SYSTEM_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0644 -D $(@D)/es_features.cfg $(TARGET_DIR)/usr/share/emulationstation/es_features.cfg
         mkdir -p $(@D)/roms # in case there is no rom
 	cp -pr $(@D)/roms $(TARGET_DIR)/usr/share/batocera/datainit/
-	cp -pr $(@D)/bios $(TARGET_DIR)/usr/share/batocera/datainit/
+	cp -pr $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/emulationstation/batocera-es-system/roms/piboy $(TARGET_DIR)/usr/share/batocera/datainit/roms
+	cp -pr $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/emulationstation/batocera-es-system/bios $(TARGET_DIR)/usr/share/batocera/datainit/
 endef
 
 $(eval $(generic-package))
