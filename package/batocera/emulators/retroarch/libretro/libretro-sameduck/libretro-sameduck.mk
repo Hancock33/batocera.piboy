ed2138ae53deea5a9cb99f7e94caabb1527ff97b
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2021-07-20 22:24:23 +0100

    sameduck update

--- package/batocera/emulators/retroarch/libretro/libretro-sameduck/libretro-sameduck.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-sameduck/libretro-sameduck.mk
@@ -4,15 +4,15 @@
 #
 ################################################################################
 # Version.: Commits on Nov 16, 2020
-LIBRETRO_SAMEBOY_VERSION = a5e5462f79164d21053f81fc680bfa9f59a2bf59
-LIBRETRO_SAMEBOY_SITE = $(call github,LIJI32,SameBoy,$(LIBRETRO_SAMEBOY_VERSION))
-LIBRETRO_SAMEBOY_LICENSE = GPL-3.0
+LIBRETRO_SAMEDUCK_VERSION = a5e5462f79164d21053f81fc680bfa9f59a2bf59
+LIBRETRO_SAMEDUCK_SITE = $(call github,LIJI32,SameBoy,$(LIBRETRO_SAMEDUCK_VERSION))
+LIBRETRO_SAMEDUCK_LICENSE = GPL-3.0
 
-define LIBRETRO_SAMEBOY_BUILD_CMDS
+define LIBRETRO_SAMEDUCK_BUILD_CMDS
 	$(TARGET_CONFIGURE_OPTS) $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D)/libretro -f Makefile platform="unix"
 endef
 
-define LIBRETRO_SAMEBOY_INSTALL_TARGET_CMDS
+define LIBRETRO_SAMEDUCK_INSTALL_TARGET_CMDS
 	$(INSTALL) -D $(@D)/sameduck_libretro.so \
     $(TARGET_DIR)/usr/lib/libretro/sameduck_libretro.so
 endef
