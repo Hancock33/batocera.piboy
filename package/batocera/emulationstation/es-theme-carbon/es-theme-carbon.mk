09319f578efdb6c2251a9c930e1dd5a30e81b350
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2021-07-20 21:26:51 +0100

    add sega dreamcast vmu

--- package/batocera/emulationstation/es-theme-carbon/es-theme-carbon.mk
+++ package/batocera/emulationstation/es-theme-carbon/es-theme-carbon.mk
@@ -3,8 +3,8 @@
 # EmulationStation theme "Carbon"
 #
 ################################################################################
-# Version.: Commits on Jul 19, 2021
-ES_THEME_CARBON_VERSION = 2a39170c5928141481bda6619212372f3f36d86d
+# Version.: Commits on Jul 20, 2021
+ES_THEME_CARBON_VERSION = 716e44374d66941a02f88a56dc5d43403c966df0
 ES_THEME_CARBON_SITE = $(call github,hancock33,es-theme-carbon,$(ES_THEME_CARBON_VERSION))
 
 define ES_THEME_CARBON_INSTALL_TARGET_CMDS
