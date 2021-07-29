a912f7560e0ca21570157d509236bb99a8ee4b58
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2021-07-29 12:16:44 +0100

    Update glslang.mk

--- package/batocera/gpu/vulkan/glslang/glslang.mk
+++ package/batocera/gpu/vulkan/glslang/glslang.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 # Version.: Commits on Jul 28, 2021
-GLSLANG_VERSION = d433cccb8acb76514cf5b70a3e1d9247a21288aab5c046e58b216ee27fbdc1e3826a6d26868bfd86refs/remotes/origin/
+GLSLANG_VERSION = d433cccb8acb76514cf5b70a3e1d9247a21288aa
 
 GLSLANG_SITE =  $(call github,KhronosGroup,glslang,$(GLSLANG_VERSION))
 GLSLANG_DEPENDENCIES = vulkan-headers vulkan-loader
