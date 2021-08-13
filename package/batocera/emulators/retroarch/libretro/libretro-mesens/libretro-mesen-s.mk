c485c367c284566123bbdaacbe4716ac097e6aa3
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2021-08-13 07:10:01 +0100

    Update libretro-mesen-s.mk

--- package/batocera/emulators/retroarch/libretro/libretro-mesens/libretro-mesen-s.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-mesens/libretro-mesen-s.mk
@@ -5,7 +5,7 @@
 ################################################################################
 # Version.: Commits on May 11, 2021
 LIBRETRO_MESEN_S_VERSION = 42eb0e8ad346608dae86feb8a04833d16ad21541
-LIBRETRO_MESENS_SITE = $(call github,libretro,Mesen-S,$(LIBRETRO_MESENS_VERSION))
+LIBRETRO_MESENS_SITE = $(call github,libretro,Mesen-S,$(LIBRETRO_MESEN_S_VERSION))
 LIBRETRO_MESENS_LICENSE = GPL
 
 define LIBRETRO_MESENS_BUILD_CMDS
