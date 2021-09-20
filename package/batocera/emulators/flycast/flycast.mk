61fff87b3cca49760d6ef9a154a3e0eec129cf10
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2021-09-20 20:11:31 +0100

    Update flycast.mk

--- package/batocera/emulators/flycast/flycast.mk
+++ package/batocera/emulators/flycast/flycast.mk
@@ -3,8 +3,8 @@
 # FLYCAST
 #
 ################################################################################
-# Version.: Commits on Sep 18, 2021
-FLYCAST_VERSION = d75072362a77952d391e72f24c75247f2736518f
+# Version.: Commits on Sep 05, 2021
+FLYCAST_VERSION = baa0ac44cad4cbd452f7183c864d4d7cf1f2a7a6
 FLYCAST_SITE = https://github.com/flyinghead/flycast.git
 
 FLYCAST_DEPENDENCIES = sdl2 libpng libzip
