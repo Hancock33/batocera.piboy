b24b1f4109a86893e413a8357e9400f9480edadc
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2021-09-23 18:44:06 +0100

    update gl4es

--- package/batocera/gpu/gl4es/gl4es/gl4es.mk
+++ package/batocera/gpu/gl4es/gl4es/gl4es.mk
@@ -3,22 +3,23 @@
 # gl4es
 #
 ################################################################################
-# Version.: Commits on Aug 04, 2021
-GL4ES_VERSION = 2d3dc681ed3f54c76f626d9b674f836eef90f982
+# Version.: Commits on Sep 11, 2021
+GL4ES_VERSION = dab338dc5d16807a071454e6116faf09c37f8a31
 GL4ES_SITE = $(call github,ptitSeb,gl4es,$(GL4ES_VERSION))
 GL4ES_LICENSE = MIT License
 
 GL4ES_CONF_OPTS += -DODROID=1
 GL4ES_CONF_OPTS += -DNOX11=ON
-GL4ES_CONF_OPTS += -DDEFAULT_ES=2
+GL4ES_CONF_OPTS += -DGBM=ON
 GL4ES_CONF_OPTS += -DCMAKE_BUILD_TYPE=RELEASE
 
 define GL4ES_INSTALL_TARGET_CMDS
     $(INSTALL) -D $(@D)/lib/libGL.so.1 $(TARGET_DIR)/usr/lib/libGL.so
     $(INSTALL) -D $(@D)/lib/libGL.so.1 $(TARGET_DIR)/usr/lib/libGL.so.1
     $(INSTALL) -D $(@D)/lib/libGL.so.1 $(HOST_DIR)/aarch64-buildroot-linux-gnu/sysroot/usr/lib/libGL.so
-    cp -pvr $(@D)/include/gl4es*.h $(HOST_DIR)/aarch64-buildroot-linux-gnu/sysroot/usr/include
-    cp -pva $(@D)/include/GL $(HOST_DIR)/aarch64-buildroot-linux-gnu/sysroot/usr/include
+    mkdir -p $(HOST_DIR)/aarch64-buildroot-linux-gnu/sysroot/usr/include/GL
+    cp -pva $(@D)/include/GL/gl.h $(HOST_DIR)/aarch64-buildroot-linux-gnu/sysroot/usr/include/GL/gl.h
+    cp -pvr $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/gpu/gl4es/gl4es/gl.pc $(HOST_DIR)/aarch64-buildroot-linux-gnu/sysroot/usr/lib/pkgconfig
 endef
 
 $(eval $(cmake-package))
