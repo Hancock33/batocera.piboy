b921b321d9fff2174486d83d0a18547652bb0962
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2021-10-11 21:23:16 +0100

    Add Shadow Warrior addons

--- package/batocera/core/batocera-configgen/configgen/configgen/generators/voidsw/voidswGenerator.py
+++ package/batocera/core/batocera-configgen/configgen/configgen/generators/voidsw/voidswGenerator.py
@@ -8,7 +8,14 @@ import controllersConfig
 class VoidswGenerator(Generator):
 
     def generate(self, system, rom, playersControllers, gameResolution):
-        commandArray = ["voidsw", "-j/userdata/roms/voidsw", "-g", rom]
+        addon = "-addon0"
+        if (rom.__contains__("WT")):
+            addon = "-addon1"
+        
+        if (rom.__contains__("TD")):
+            addon = "-addon2"
+
+        commandArray = ["voidsw", addon, "-j/userdata/roms/voidsw"]
 
         return Command.Command(
             array=commandArray,
