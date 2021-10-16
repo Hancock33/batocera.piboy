12afe4a1abecb999510f00e037f56b4a03d23843
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2021-10-16 18:26:57 +0100

    add cdogs artwork

--- package/batocera/emulationstation/es-theme-carbon/es-theme-carbon.mk
+++ package/batocera/emulationstation/es-theme-carbon/es-theme-carbon.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 # Version.: Commits on Oct 16, 2021
-ES_THEME_CARBON_VERSION = 2641c81d55cce5b748583a49993d37ea375023ab
+ES_THEME_CARBON_VERSION = 3413650cdfc540eb3a662fa95ed3cd24197e6941
 ES_THEME_CARBON_SITE = $(call github,hancock33,es-theme-carbon,$(ES_THEME_CARBON_VERSION))
 
 define ES_THEME_CARBON_INSTALL_TARGET_CMDS
