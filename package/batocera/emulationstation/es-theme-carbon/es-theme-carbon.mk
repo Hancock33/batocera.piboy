e6166fabed161e3c6ffa9a9fa53340c3404a3d27
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2021-10-16 14:11:59 +0100

    add abuse artwork

--- package/batocera/emulationstation/es-theme-carbon/es-theme-carbon.mk
+++ package/batocera/emulationstation/es-theme-carbon/es-theme-carbon.mk
@@ -3,8 +3,8 @@
 # EmulationStation theme "Carbon"
 #
 ################################################################################
-# Version.: Commits on Oct 09, 2021
-ES_THEME_CARBON_VERSION = 9633dc60997df36482b9b3c34556dfc0f7712f23
+# Version.: Commits on Oct 16, 2021
+ES_THEME_CARBON_VERSION = 2641c81d55cce5b748583a49993d37ea375023ab
 ES_THEME_CARBON_SITE = $(call github,hancock33,es-theme-carbon,$(ES_THEME_CARBON_VERSION))
 
 define ES_THEME_CARBON_INSTALL_TARGET_CMDS
