38ced24748f67f6aea6c40ba69a990f8d2032517
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2021-10-30 11:44:39 +0100

    Update Blake stone artwork

--- package/batocera/emulationstation/es-theme-carbon/es-theme-carbon.mk
+++ package/batocera/emulationstation/es-theme-carbon/es-theme-carbon.mk
@@ -3,8 +3,8 @@
 # EmulationStation theme "Carbon"
 #
 ################################################################################
-# Version.: Commits on Oct 29, 2021
-ES_THEME_CARBON_VERSION = 2386782f511ec33274802c4aedfca37b5561aa05
+# Version.: Commits on Oct 30, 2021
+ES_THEME_CARBON_VERSION = 805f32aa7259be96edb763e219b13c2d356d2e2e
 ES_THEME_CARBON_SITE = $(call github,hancock33,es-theme-carbon,$(ES_THEME_CARBON_VERSION))
 
 define ES_THEME_CARBON_INSTALL_TARGET_CMDS
