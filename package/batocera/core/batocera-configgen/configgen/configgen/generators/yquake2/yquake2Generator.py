a150e5b22e8cb4c556dcc06e4ebfe229b5546552
Lee <lee.hancock4@ntlworld.com>
2021-11-06 17:09:37 +0000

    inital import of yquake2 (WIP)

---
+++ package/batocera/core/batocera-configgen/configgen/configgen/generators/yquake2/yquake2Generator.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import Command
+from generators.Generator import Generator
+import controllersConfig
+
+
+class Yquake2Generator(Generator):
+
+    def generate(self, system, rom, playersControllers, gameResolution):
+        gamemod = "baseq2"
+        if (rom.__contains__("rogue")):
+            gamemod = "rogue" 
+            
+        if (rom.__contains__("xatrix")):
+            gamemod = "xatrix"             
+            
+        commandArray = ["quake2", "-datadir", "/userdata/roms/yquake2", "+set", "vid_renderer", "gl3", "+set", "r_vsync 1", "+set", "game" , gamemod]
+
+        return Command.Command(
+            array=commandArray,
+            env={
+                'LIBGL_FB': '4',
+                'LIBGL_NOTEST': '1', 
+                'SDL_GAMECONTROLLERCONFIG': controllersConfig.generateSdlGameControllerConfig(playersControllers)
+            })
---
+++ package/batocera/ports/quake2/yquake2/yquake2.mk
@@ -0,0 +1,25 @@
+################################################################################
+#
+# Yamagi Quake II
+#
+################################################################################
+# Version.: Commits on Nov 06, 2021
+YQUAKE2_VERSION = 42d61449b11bd0993d0f9d6636854276ef04b43e
+YQUAKE2_SITE = $(call github,yquake2,yquake2,$(YQUAKE2_VERSION))
+
+YQUAKE2_DEPENDENCIES = sdl2 sdl2_mixer
+YQUAKE2_LICENSE = GPL-2.0
+
+YQUAKE2_SUPPORTS_IN_SOURCE_BUILD = NO
+YQUAKE2_CONF_OPTS += -DCMAKE_BUILD_TYPE=Release
+
+define YQUAKE2_INSTALL_TARGET_CMDS
+	cp -pvr $(@D)/buildroot-build/release/* $(TARGET_DIR)/usr/bin
+	mkdir -p $(TARGET_DIR)/usr/share/batocera/datainit/roms/yquake2
+	mv $(TARGET_DIR)/usr/bin/baseq2 $(TARGET_DIR)/usr/share/batocera/datainit/roms/yquake2
+	# evmap config
+	mkdir -p $(TARGET_DIR)/usr/share/evmapy
+	cp $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/ports/quake2/yquake2/yquake2.keys $(TARGET_DIR)/usr/share/evmapy
+endef
+
+$(eval $(cmake-package))
