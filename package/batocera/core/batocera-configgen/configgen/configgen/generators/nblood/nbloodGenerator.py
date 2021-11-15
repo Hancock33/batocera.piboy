00c4a32d7b9201437e080e16c2930a995a540f51
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2021-11-15 22:58:30 +0000

    speedup mouse movement is eduke clones

--- package/batocera/core/batocera-configgen/configgen/configgen/generators/nblood/nbloodGenerator.py
+++ package/batocera/core/batocera-configgen/configgen/configgen/generators/nblood/nbloodGenerator.py
@@ -14,5 +14,6 @@ class NbloodGenerator(Generator):
             array=commandArray,
             env={
                 'SDL_AUTO_UPDATE_JOYSTICKS': '0',
+                'SDL_MOUSE_RELATIVE_SPEED_SCALE': '7.0',
                 'PIPEWIRE_LATENCY': '1024/48000'
             })
--- package/batocera/core/batocera-configgen/configgen/configgen/generators/voidsw/voidswGenerator.py
+++ package/batocera/core/batocera-configgen/configgen/configgen/generators/voidsw/voidswGenerator.py
@@ -21,5 +21,6 @@ class VoidswGenerator(Generator):
             array=commandArray,
             env={
                 'SDL_AUTO_UPDATE_JOYSTICKS': '0',
+                'SDL_MOUSE_RELATIVE_SPEED_SCALE': '7.0',
                 'PIPEWIRE_LATENCY': '1024/48000'
             })
