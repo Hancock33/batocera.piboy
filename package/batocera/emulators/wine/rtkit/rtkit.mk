7122a9aa9ea51416a9b1576bf5554eeb709743d4
Lee <lee.hancock4@ntlworld.com>
2021-11-18 21:49:05 +0000

    x86 build fixes

--- package/batocera/emulators/wine/rtkit/rtkit.mk
+++ package/batocera/emulators/wine/rtkit/rtkit.mk
@@ -3,10 +3,10 @@
 # rtkit
 #
 ################################################################################
-# Version.: Commits on Apr 05, 2020
-RTKIT_VERSION = c295fa849f52b487be6433e69e08b46251950399
+
+RTKIT_VERSION = v0.13
 RTKIT_SITE = $(call github,heftig,RTKIT,$(RTKIT_VERSION))
-# host-vim needed for xxd
+# host-vim needed for xxd 
 RTKIT_DEPENDENCIES = dbus host-vim libcap polkit
 
 define RTKIT_USERS
