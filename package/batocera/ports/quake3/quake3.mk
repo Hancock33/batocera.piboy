ceb127c8fe5dd33ef8eceedb11e051f7292bb1c2
Lee <lee.hancock4@ntlworld.com>
2021-11-19 22:51:20 +0000

    x86 build fixes

--- package/batocera/ports/quake3/quake3.mk
+++ package/batocera/ports/quake3/quake3.mk
@@ -10,17 +10,23 @@ QUAKE3_SITE = $(call github,suijingfeng,vkQuake3,$(QUAKE3_VERSION))
 QUAKE3_DEPENDENCIES = sdl2 sdl2_mixer
 QUAKE3_LICENSE = GPL-2.0
 
+ifeq ($(BR2_PACKAGE_XPI_GAMECON_RPI3)$(BR2_PACKAGE_XPI_GAMECON_RPI4),y)   
+    COMPILE_ARCH=arm64
+else
+    COMPILE_ARCH=x86_64
+endif
+
 define QUAKE3_BUILD_CMDS
 	$(TARGET_CONFIGURE_OPTS) $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D)/ -f Makefile \
 	DEFAULT_BASEDIR=/userdata/roms/quake3 \
-	COMPILE_ARCH=arm64
+	COMPILE_ARCH="$(COMPILE_ARCH)"
 endef
 
 define QUAKE3_INSTALL_TARGET_CMDS
     mkdir -p $(TARGET_DIR)/usr/share/batocera/datainit/roms/quake3/baseq3
-    cp -pvr $(@D)/build/release-linux-arm64/ioquake3.arm64 $(TARGET_DIR)/usr/bin/ioquake3
-    cp -pvr $(@D)/build/release-linux-arm64/renderer_*.so $(TARGET_DIR)/usr/share/batocera/datainit/roms/quake3
-	cp -pvr $(@D)/build/release-linux-arm64/baseq3/*.so $(TARGET_DIR)/usr/share/batocera/datainit/roms/quake3/baseq3
+    cp -pvr $(@D)/build/release-linux-*/ioquake3.* $(TARGET_DIR)/usr/bin/ioquake3
+    cp -pvr $(@D)/build/release-linux-*/renderer_*.so $(TARGET_DIR)/usr/share/batocera/datainit/roms/quake3
+	cp -pvr $(@D)/build/release-linux-*/baseq3/*.so $(TARGET_DIR)/usr/share/batocera/datainit/roms/quake3/baseq3
     # evmap config
 	mkdir -p $(TARGET_DIR)/usr/share/evmapy
 	cp $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/ports/quake3/quake3.keys $(TARGET_DIR)/usr/share/evmapy
