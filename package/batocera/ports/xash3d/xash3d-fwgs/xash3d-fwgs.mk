bfbecaacdcb48b9ea45d59d603d90482d40ed46c
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2021-12-05 09:16:25 +0000

    fix install, waf has changed prefix to destdir.

--- package/batocera/ports/xash3d/xash3d-fwgs/xash3d-fwgs.mk
+++ package/batocera/ports/xash3d/xash3d-fwgs/xash3d-fwgs.mk
@@ -32,4 +32,12 @@ XASH3D_FWGS_CONF_OPTS += --disable-gl
 endif
 endif
 
+define XASH3D_FWGS_INSTALL_TARGET_CMDS
+	mkdir -p $(TARGET_DIR)/usr/lib/xash3d/
+	cp -pvr $(@D)/build/mainui/libmenu.so $(TARGET_DIR)/usr/lib/xash3d/
+	cp -pvr $(@D)/build/ref_soft/libref_soft.so $(TARGET_DIR)/usr/lib/xash3d/
+	cp -pvr $(@D)/build/engine/libxash.so $(TARGET_DIR)/usr/lib/xash3d/
+	cp -pvr $(@D)/build/game_launch/xash3d $(TARGET_DIR)/usr/lib/xash3d/
+endef
+
 $(eval $(waf-package))
