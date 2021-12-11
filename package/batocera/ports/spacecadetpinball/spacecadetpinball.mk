eb99b9024d2fdc026df40c95ca7ce92ed53e6111
Lee <lee.hancock4@ntlworld.com>
2021-12-11 08:58:07 +0000

    cleanup spacecadetpinball

--- package/batocera/ports/spacecadetpinball/spacecadetpinball.mk
+++ package/batocera/ports/spacecadetpinball/spacecadetpinball.mk
@@ -14,31 +14,12 @@ SPACECADETPINBALL_SUPPORTS_IN_SOURCE_BUILD = NO
 
 SPACECADETPINBALL_CONF_OPTS += -DCMAKE_BUILD_TYPE=Release
 
-ifeq ($(BR2_PACKAGE_XPI_GAMECON_RPI3)$(BR2_PACKAGE_XPI_GAMECON_RPI4),y)
-   SPACECADETPINBALL_POST_INSTALL_TARGET_HOOKS +=SPACECADETPINBALL_INSTALL_BOOT_PIBOY
-else
-   SPACECADETPINBALL_POST_INSTALL_TARGET_HOOKS +=SPACECADETPINBALL_INSTALL_BOOT_X86
-endif
-
-
 define SPACECADETPINBALL_INSTALL_TARGET_CMDS
     mkdir -p $(TARGET_DIR)/usr/share/batocera/datainit/roms/spacecadetpinball
-	$(INSTALL) -D $(@D)/bin/SpaceCadetPinball $(TARGET_DIR)/usr/share/batocera/datainit/roms/spacecadetpinball
-	echo "cd /userdata/roms/spacecadetpinball && /userdata/roms/spacecadetpinball/SpaceCadetPinball" > $(TARGET_DIR)/usr/bin/SpaceCadetPinball.sh
-	chmod 0754 $(TARGET_DIR)/usr/bin/SpaceCadetPinball.sh
+	$(INSTALL) -D $(@D)/bin/SpaceCadetPinball $(TARGET_DIR)/usr/bin
 	# evmap config
 	mkdir -p $(TARGET_DIR)/usr/share/evmapy
 	cp $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/ports/spacecadetpinball/spacecadetpinball.keys $(TARGET_DIR)/usr/share/evmapy
-endef
-
-define SPACECADETPINBALL_INSTALL_BOOT_PIBOY
-    cp $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/ports/spacecadetpinball/SpaceCadetPinball.rpi4 $(TARGET_DIR)/usr/bin/SpaceCadetPinball
-	chmod 0754 $(TARGET_DIR)/usr/bin/SpaceCadetPinball
-endef
-
-define SPACECADETPINBALL_INSTALL_BOOT_X86
-    cp $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/ports/spacecadetpinball/SpaceCadetPinball.x86 $(TARGET_DIR)/usr/bin/SpaceCadetPinball
-	chmod 0754 $(TARGET_DIR)/usr/bin/SpaceCadetPinball
 endef
 
 $(eval $(cmake-package))
