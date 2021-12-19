a8143984a132919e44b6b6b5d4efa2537629ccc4
Lee <lee.hancock4@ntlworld.com>
2021-12-19 10:01:35 +0000

    fix sdlpop

--- package/batocera/ports/droidports/droidports.mk
+++ package/batocera/ports/droidports/droidports.mk
@@ -9,7 +9,7 @@ DROIDPORTS_SITE = https://github.com/JohnnyonFlame/droidports.git
 DROIDPORTS_SITE_METHOD=git
 DROIDPORTS_GIT_SUBMODULES=YES
 
-DROIDPORTS_DEPENDENCIES = sdl2 zlib libzip freetype bzip2 libpng
+DROIDPORTS_DEPENDENCIES = sdl2 sdl2_image zlib libzip freetype bzip2 libpng openal
 DROIDPORTS_LICENSE = GPL-2.0
 
 DROIDPORTS_SUPPORTS_IN_SOURCE_BUILD = NO
@@ -17,7 +17,8 @@ DROIDPORTS_SUPPORTS_IN_SOURCE_BUILD = NO
 DROIDPORTS_CONF_OPTS += -DCMAKE_BUILD_TYPE=Release 
 DROIDPORTS_CONF_OPTS += -DPLATFORM=linux
 DROIDPORTS_CONF_OPTS += -DPORT=gmloader
-DROIDPORTS_CONF_OPTS += -DCMAKE_TOOLCHAIN_FILE=$(HOST_DIR)/share/buildroot/toolchainfile.cmake
+DROIDPORTS_CONF_OPTS += -DCMAKE_CXX_FLAGS="-w -mlittle-endian -fsingle-precision-constant -Wdouble-promotion -Wfloat-conversion -lm"
+DROIDPORTS_CONF_OPTS += -DCMAKE_C_FLAGS="-w -mlittle-endian -fsingle-precision-constant -Wdouble-promotion -Wfloat-conversion -lm"
 
 define DROIDPORTS_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/usr/share/DROIDPORTS
