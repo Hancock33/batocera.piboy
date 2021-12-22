ef67ac349cc5349938f8be43e11e4628a053b70a
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2021-12-22 23:12:31 +0000

    Update libretro-scummvm.mk

--- package/batocera/emulators/retroarch/libretro/libretro-scummvm/libretro-scummvm.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-scummvm/libretro-scummvm.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 # Version.: Commits on Dec 22, 2021
-LIBRETRO_SCUMMVM_VERSION = 8b1829ab5adddd7120ac62ee1914d3002696bd00
+LIBRETRO_SCUMMVM_VERSION = 80cb7269a33b233dcea27d8d01df084b0d35c80a
 LIBRETRO_SCUMMVM_SITE = $(call github,libretro,scummvm,$(LIBRETRO_SCUMMVM_VERSION))
 LIBRETRO_SCUMMVM_LICENSE = GPLv2
 
