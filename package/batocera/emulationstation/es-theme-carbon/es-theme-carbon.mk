de6db303cc095e8ae0a29bfa5e80d99393ea8b8f
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2021-12-23 16:48:48 +0000

    add hode artwork

--- package/batocera/emulationstation/es-theme-carbon/es-theme-carbon.mk
+++ package/batocera/emulationstation/es-theme-carbon/es-theme-carbon.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 # Version.: Commits on Dec 23, 2021
-ES_THEME_CARBON_VERSION = a563fcdef5955b893ba9353d855fe9e2b99c26a0
+ES_THEME_CARBON_VERSION = 786f613e025f6259a946cbc5c1692464c1c897c4
 ES_THEME_CARBON_SITE = $(call github,hancock33,es-theme-carbon,$(ES_THEME_CARBON_VERSION))
 
 define ES_THEME_CARBON_INSTALL_TARGET_CMDS
