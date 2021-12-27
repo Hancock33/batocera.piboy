ba5ad4faa599ff2e14ec843428e5efb3d13104b0
Lee <lee.hancock4@ntlworld.com>
2021-12-27 17:13:22 +0000

    clean up 32bit libs, fix sound in droid ports

--- package/batocera/ports/32bit/lib32bit/lib32bit.mk
+++ package/batocera/ports/32bit/lib32bit/lib32bit.mk
@@ -10,14 +10,16 @@ LIB32BIT_SITE = https://github.com/Hancock33/batocera-rp4-32bit-libs/releases/do
 define LIB32BIT_EXTRACT_CMDS
 	mkdir -p $(@D)/target && cd $(@D)/target && tar xf $(DL_DIR)/$(LIB32BIT_DL_SUBDIR)/$(LIB32BIT_SOURCE)
 	cp -a $(@D)/target/lib32 $(TARGET_DIR)/usr
+	cp -a $(@D)/target/bin32 $(TARGET_DIR)/usr
+	cp -a $(@D)/target/share $(TARGET_DIR)/usr
 	ln -sf /usr/lib32/ld-linux-armhf.so.3 $(TARGET_DIR)/lib
 endef
 
 define LIB32BIT_INSTALL_TARGET_CMDS
 	# evmap config
 	mkdir -p $(TARGET_DIR)/usr/share/evmapy
-	cp $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/libraries/lib32bit/sorr.keys $(TARGET_DIR)/usr/share/evmapy/sorr.keys
-	cp $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/libraries/lib32bit/droidports.keys $(TARGET_DIR)/usr/share/evmapy/droidports.keys	
+	cp $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/ports/32bit/lib32bit/sorr.keys $(TARGET_DIR)/usr/share/evmapy/sorr.keys
+	cp $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/ports/32bit/lib32bit/droidports.keys $(TARGET_DIR)/usr/share/evmapy/droidports.keys	
 endef
 
 $(eval $(generic-package))
