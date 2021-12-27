ea6a2fdf71262384a36c6e2baa8b48fde175f221
Lee <lee.hancock4@ntlworld.com>
2021-12-27 00:34:19 +0000

    implemet SorR with 32bit libs

--- package/batocera/core/batocera-configgen/configgen/configgen/generators/sorr/sorrGenerator.py
+++ package/batocera/core/batocera-configgen/configgen/configgen/generators/sorr/sorrGenerator.py
@@ -3,16 +3,22 @@
 import Command
 from generators.Generator import Generator
 import controllersConfig
-
+import os
 
 class SorrGenerator(Generator):
 
     def generate(self, system, rom, playersControllers, gameResolution):
+        os.chdir("/userdata/roms/sorr")
         commandArray = ["bgdi", "-i", "/userdata/roms/sorr", rom]
 
         return Command.Command(
             array=commandArray,
             env={
-                'SDL_GAMECONTROLLERCONFIG': controllersConfig.generateSdlGameControllerConfig(playersControllers),
+                'LD_LIBRARY_PATH': '/usr/lib32:/usr/lib32/alsa-lib:/usr/lib/spa-0.2/support',
+                'LIBGL_DRIVERS_PATH': '/usr/lib32/dri',
+                'ALSA_PLUGIN_DIR': '/usr/lib32/alsa-lib',
+                'SPA_PLUGIN_DIR': '/usr/lib32/spa-0.2',
+                'PIPEWIRE_MODULE_DIR': '/usr/lib32/pipewire-0.3',
+                'SDL_AUTO_UPDATE_JOYSTICKS': '0',
                 'PIPEWIRE_LATENCY': '1024/48000'
             })
