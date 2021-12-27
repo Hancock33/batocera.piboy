ceee3368e1200c9554e830163d58dbeb70d5cd98
Lee <lee.hancock4@ntlworld.com>
2021-12-27 15:45:43 +0000

    investigate build and usage of box86

---
+++ package/batocera/ports/32bit/box86/box86.mk
@@ -0,0 +1,23 @@
+################################################################################
+#
+# Box86
+#
+################################################################################
+# Version.: Commits on Dec 27, 2021
+BOX86_VERSION = cc32d6cb9820af2ac4df016e7197904b570aeed2
+BOX86_SITE = https://github.com/ptitseb/box86.git
+BOX86_SITE_METHOD=git
+BOX86_GIT_SUBMODULES=YES
+BOX86_SUPPORTS_IN_SOURCE_BUILD = NO
+
+BOX86_DEPENDENCIES = sdl2 sdl2_image zlib libzip freetype bzip2 libpng openal
+BOX86_LICENSE = GPL-3.0
+
+BOX86_CONF_OPTS += -DCMAKE_BUILD_TYPE=Release
+BOX86_CONF_OPTS += -DRPI4=1
+
+define BOX86_INSTALL_TARGET_CMDS
+	cp -pvr $(@D)/buildroot-build/box86 $(TARGET_DIR)/usr/bin
+endef
+
+$(eval $(cmake-package))
