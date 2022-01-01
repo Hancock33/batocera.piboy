976df229a33ed86f52658657f71b08e3ed3fb1a6
Lee <lee.hancock4@ntlworld.com>
2022-01-01 17:32:24 +0000

    add OpenJedi Academy / Outcast

---
+++ package/batocera/core/batocera-configgen/configgen/configgen/generators/openjkjo/openjkjoGenerator.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import Command
+from generators.Generator import Generator
+import controllersConfig
+
+
+class OpenjkjoGenerator(Generator):
+
+    def generate(self, system, rom, playersControllers, gameResolution):
+        commandArray = ["openjo_sp", "+set", "fs_basepath", "/userdata/roms/openjkjo", "+set", "r_mode", "-1", "+set", "r_customwidth", "640", "+set", "r_customheight", "480", "+set", "r_swapInterval", "1", "+set", "com_jk2", "1"]
+        return Command.Command(
+            array=commandArray,
+            env={
+                'LIBGL_FB': '4',
+                'LIBGL_NOTEST': '1',
+                'SDL_AUTO_UPDATE_JOYSTICKS': '0',
+                'PIPEWIRE_LATENCY': '1024/48000'
+            })
---
+++ package/batocera/ports/openjk/openjk.mk
@@ -0,0 +1,44 @@
+################################################################################
+#
+# OpenJK: JediAcademy / Jedi Outcast
+#
+################################################################################
+# Version.: Commits on Jan 01, 2022
+OPENJK_VERSION = 43e9a3da555a487e23081bbd03101e7c926a1e83
+OPENJK_SITE = $(call github,JACoders,OpenJK,$(OPENJK_VERSION))
+
+OPENJK_DEPENDENCIES = sdl2 openal
+OPENJK_LICENSE = GPL-2.0
+OPENJK_SUPPORTS_IN_SOURCE_BUILD = NO
+
+#Single player
+OPENJK_CONF_OPTS += -DBuildSPEngine=ON
+OPENJK_CONF_OPTS += -DBuildSPGame=ON
+OPENJK_CONF_OPTS += -DBuildSPRdVanilla=ON
+OPENJK_CONF_OPTS += -DBuildJK2SPEngine=ON
+OPENJK_CONF_OPTS += -DBuildJK2SPGame=ON
+OPENJK_CONF_OPTS += -DBuildJK2SPRdVanilla=ON
+#Multi player
+OPENJK_CONF_OPTS += -DBuildMPEngine=OFF
+OPENJK_CONF_OPTS += -DBuildMPRdVanilla=OFF
+OPENJK_CONF_OPTS += -DBuildMPDed=OFF
+OPENJK_CONF_OPTS += -DBuildMPGame=OFF
+OPENJK_CONF_OPTS += -DBuildMPCGame=OFF
+OPENJK_CONF_OPTS += -DBuildMPUI=OFF
+
+define OPENJK_INSTALL_TARGET_CMDS
+    mkdir -p $(TARGET_DIR)/usr/share/batocera/datainit/roms/openjkja 
+    $(INSTALL) -D $(@D)/buildroot-build/openjk_sp.* $(TARGET_DIR)/usr/bin/openjk_sp
+    $(INSTALL) -D $(@D)/buildroot-build/code/rd-vanilla/rdsp-vanilla_*.so $(TARGET_DIR)/usr/share/batocera/datainit/roms/openjkja
+    $(INSTALL) -D $(@D)/buildroot-build/code/game/jagame*.so $(TARGET_DIR)/usr/share/batocera/datainit/roms/openjkja
+    mkdir -p $(TARGET_DIR)/usr/share/batocera/datainit/roms/openjkjo
+    $(INSTALL) -D $(@D)/buildroot-build/openjo_sp.* $(TARGET_DIR)/usr/bin/openjo_sp     
+    $(INSTALL) -D $(@D)/buildroot-build/code/rd-vanilla/rdjosp-vanilla_*.so $(TARGET_DIR)/usr/share/batocera/datainit/roms/openjkjo
+    $(INSTALL) -D $(@D)/buildroot-build/codeJK2/game/jospgame*.so $(TARGET_DIR)/usr/share/batocera/datainit/roms/openjkjo
+	# evmap config
+	 mkdir -p $(TARGET_DIR)/usr/share/evmapy
+	cp $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/ports/openjk/openjkja.keys $(TARGET_DIR)/usr/share/evmapy
+	cp $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/ports/openjk/openjkjo.keys $(TARGET_DIR)/usr/share/evmapy
+endef
+
+$(eval $(cmake-package))
