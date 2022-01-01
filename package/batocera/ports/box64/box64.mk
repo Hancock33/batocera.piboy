fea5ee3b9a737b1e0c17684c5f82c9efb339eb04
Lee <lee.hancock4@ntlworld.com>
2022-01-01 18:57:28 +0000

    box64 experimentation

---
+++ package/batocera/ports/box64/box64.mk
@@ -0,0 +1,25 @@
+################################################################################
+#
+# BOX64
+#
+################################################################################
+# Version.: Commits on Jan 01, 2022
+BOX64_VERSION = 0f3fe62327cb477d2e2d45a1bbff69b4fd857d50
+BOX64_SITE = https://github.com/ptitseb/box64.git
+BOX64_SITE_METHOD=git
+BOX64_GIT_SUBMODULES=YES
+BOX64_SUPPORTS_IN_SOURCE_BUILD = NO
+
+BOX64_DEPENDENCIES = sdl2 sdl2_image zlib libzip freetype bzip2 libpng openal
+BOX64_LICENSE = GPL-3.0
+
+BOX64_CONF_OPTS += -DCMAKE_BUILD_TYPE=Release
+BOX64_CONF_OPTS += -DRPI4=1
+
+define BOX64_INSTALL_TARGET_CMDS
+	cp -pvr $(@D)/buildroot-build/box64 $(TARGET_DIR)/usr/bin
+	mkdir -p $(TARGET_DIR)/usr/share/box64/lib
+	cp -pvr $(@D)/x64lib/* $(TARGET_DIR)/usr/share/box64/lib
+endef
+
+$(eval $(cmake-package))
