b335c446ad0a32d8e30be671bca0fc3a141fa9c7
Lee <lee.hancock4@ntlworld.com>
2022-01-02 12:53:39 +0000

    Add Vanilla Conquer, WIP random crashes

---
+++ package/batocera/core/batocera-configgen/configgen/configgen/generators/vanillatd/vanillatdGenerator.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import Command
+from generators.Generator import Generator
+import controllersConfig
+
+
+class VanillatdGenerator(Generator):
+
+    def generate(self, system, rom, playersControllers, gameResolution):
+        commandArray = ["vanillatd"]
+        return Command.Command(
+            array=commandArray,
+            env={
+                'LIBGL_FB': '4',
+                'LIBGL_NOTEST': '1',
+                'SDL_AUTO_UPDATE_JOYSTICKS': '0',
+                'PIPEWIRE_LATENCY': '1024/48000'
+            })
---
+++ package/batocera/ports/candc/candc.mk
@@ -0,0 +1,23 @@
+################################################################################
+#
+# Vanilla Conquer
+#
+################################################################################
+# Version.: Commits on Dec 18, 2021
+CANDC_VERSION = edb8391ffa225fcea73d135bbbb3f80540541137
+CANDC_SITE = $(call github,TheAssemblyArmada,Vanilla-Conquer,$(CANDC_VERSION))
+
+CANDC_DEPENDENCIES = sdl2 openal
+CANDC_LICENSE = GPL-2.0
+CANDC_SUPPORTS_IN_SOURCE_BUILD = NO
+
+define CANDC_INSTALL_TARGET_CMDS
+    $(INSTALL) -D $(@D)/buildroot-build/vanillara $(TARGET_DIR)/usr/bin/vanillara
+    $(INSTALL) -D $(@D)/buildroot-build/vanillatd $(TARGET_DIR)/usr/bin/vanillatd     
+	# evmap config
+	mkdir -p $(TARGET_DIR)/usr/share/evmapy
+	cp $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/ports/candc/vanillara.keys $(TARGET_DIR)/usr/share/evmapy
+	cp $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/ports/candc/vanillatd.keys $(TARGET_DIR)/usr/share/evmapy
+endef
+
+$(eval $(cmake-package))
