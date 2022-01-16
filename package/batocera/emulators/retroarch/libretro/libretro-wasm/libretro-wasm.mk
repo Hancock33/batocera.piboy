99c1c6abe7ecd6fe23c7a70af7f5951d88106e3d
Lee <lee.hancock4@ntlworld.com>
2022-01-16 09:33:46 +0000

    add libretro wasm4

---
+++ package/batocera/emulators/retroarch/libretro/libretro-wasm/libretro-wasm.mk
@@ -0,0 +1,26 @@
+################################################################################
+#
+# LIBRETRO_WASM
+#
+################################################################################
+
+LIBRETRO_WASM_VERSION = 002508bfcc27251b53bdc308576daf4819e49254
+LIBRETRO_WASM_SITE = https://github.com/aduros/wasm4.git
+LIBRETRO_WASM_SITE_METHOD=git
+LIBRETRO_WASM_GIT_SUBMODULES=YES
+
+LIBRETRO_WASM_LICENSE = ISC
+LIBRETRO_WASM_DEPENDENCIES = retroarch
+
+LIBRETRO_WASM_SUPPORTS_IN_SOURCE_BUILD = NO
+LIBRETRO_WASM_SUBDIR = runtimes/native
+
+LIBRETRO_WASM_CONF_OPTS  = -DCMAKE_BUILD_TYPE=Release
+LIBRETRO_WASM_CONF_OPTS += -DLIBRETRO=ON
+
+define LIBRETRO_WASM_INSTALL_TARGET_CMDS
+    $(INSTALL) -D $(@D)/runtimes/native/buildroot-build/wasm4_libretro.so \
+	    $(TARGET_DIR)/usr/lib/libretro/wasm4_libretro.so
+endef
+
+$(eval $(cmake-package))
