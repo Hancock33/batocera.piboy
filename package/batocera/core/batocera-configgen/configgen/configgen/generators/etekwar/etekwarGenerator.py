a49c90901f449a66d7cf93877cc1df40857148e7
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2022-01-20 20:33:04 +0000

    update tekwar keys

--- package/batocera/core/batocera-configgen/configgen/configgen/generators/etekwar/etekwarGenerator.py
+++ package/batocera/core/batocera-configgen/configgen/configgen/generators/etekwar/etekwarGenerator.py
@@ -8,10 +8,11 @@ import controllersConfig
 class EtekwarGenerator(Generator):
 
     def generate(self, system, rom, playersControllers, gameResolution):
-        commandArray = ["etekwar", rom]
+        commandArray = ["etekwar"]
 
         return Command.Command(
             array=commandArray,
             env={
-                'SDL_GAMECONTROLLERCONFIG': controllersConfig.generateSdlGameControllerConfig(playersControllers)
+                'SDL_AUTO_UPDATE_JOYSTICKS': '0',
+                'SDL_MOUSE_RELATIVE_SPEED_SCALE': '3.0'
             })
