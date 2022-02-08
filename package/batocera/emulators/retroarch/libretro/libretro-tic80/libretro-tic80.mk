a7f912dac9169d2b32df4aeea03fb0f8e5edfccc
Lee <lee.hancock4@ntlworld.com>
2022-02-08 20:52:07 +0000

    Update libretro-tic80

--- package/batocera/emulators/retroarch/libretro/libretro-tic80/libretro-tic80.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-tic80/libretro-tic80.mk
@@ -3,8 +3,8 @@
 # tic80 - TIC-80 emulator
 #
 ################################################################################
-# Version.: Commits on Dec 08, 2021
-LIBRETRO_TIC80_VERSION = 600341db23fa1b6ce3deaccd54ab180d6caee6e2
+# Version.: Commits on Feb 04, 2022
+LIBRETRO_TIC80_VERSION = 7c303670044a5cef1a16d7e60df875c6f77928ec
 LIBRETRO_TIC80_SITE = https://github.com/nesbox/TIC-80.git
 LIBRETRO_TIC80_SITE_METHOD=git
 LIBRETRO_TIC80_GIT_SUBMODULES=YES
@@ -12,15 +12,18 @@ LIBRETRO_TIC80_LICENSE = MIT
 
 LIBRETRO_TIC80_PLATFORM = $(LIBRETRO_PLATFORM)
 
+LIBRETRO_TIC80_CONF_OPTS += \
+						-DBUILD_PLAYER=OFF \
+                        -DBUILD_SDL=OFF \
+                        -DBUILD_SOKOL=OFF \
+                        -DBUILD_DEMO_CARTS=OFF \
+                        -DBUILD_LIBRETRO=ON \
+                        -DBUILD_WITH_MRUBY=OFF \
+                        -DCMAKE_BUILD_TYPE=Release
+                        
 ifeq ($(BR2_PACKAGE_BATOCERA_RPI_ANY),y)
-LIBRETRO_TIC80_CONF_OPTS = -DRPI=ON
+LIBRETRO_TIC80_CONF_OPTS += -DRPI=ON
 endif
-
-LIBRETRO_TIC80_CONF_OPTS += -DBUILD_PLAYER=OFF -DBUILD_SOKOL=OFF -DBUILD_SDL=OFF -DBUILD_DEMO_CARTS=OFF -DBUILD_LIBRETRO=ON
-
-define LIBRETRO_TIC80_BUILD_CMDS
-	$(TARGET_CONFIGURE_OPTS) $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D)/ -f Makefile platform="$(LIBRETRO_TIC80_PLATFORM)"
-endef
 
 define LIBRETRO_TIC80_INSTALL_TARGET_CMDS
 	$(INSTALL) -D $(@D)/lib/tic80_libretro.so \
@@ -28,3 +31,4 @@ define LIBRETRO_TIC80_INSTALL_TARGET_CMDS
 endef
 
 $(eval $(cmake-package))
+
