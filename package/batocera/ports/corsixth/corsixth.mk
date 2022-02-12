9ce07995c7bd2078b2be58f42a2a19dfddaf3224
Lee <lee.hancock4@ntlworld.com>
2022-02-12 17:32:45 +0000

    add theme hospital

---
+++ package/batocera/ports/corsixth/corsixth.mk
@@ -0,0 +1,24 @@
+################################################################################
+#
+# Theme Hospital
+#
+################################################################################
+# Version.: Commits on Feb 11, 2022
+CORSIXTH_VERSION = fb32a555fdc1cfb5a874cf8a98f84989caf6597a
+CORSIXTH_SITE = $(call github,CorsixTH,CorsixTH,$(CORSIXTH_VERSION))
+
+CORSIXTH_DEPENDENCIES = sdl2 sdl2_mixer freepats luafilesystem lua-lpeg-patterns
+CORSIXTH_LICENSE = GPL-2.0
+CORSIXTH_SUPPORTS_IN_SOURCE_BUILD = NO
+
+CORSIXTH_CONF_OPTS += -DCMAKE_BUILD_TYPE=Release -DWITH_LIBAV=ON
+
+define CORSIXTH_INSTALL_EXTRAS
+	# evmap config
+	mkdir -p $(TARGET_DIR)/usr/share/evmapy
+	cp $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/ports/corsixth/corsixth.keys $(TARGET_DIR)/usr/share/evmapy/themehospital.keys
+endef
+
+CORSIXTH_PRE_INSTALL_TARGET_HOOKS += CORSIXTH_INSTALL_EXTRAS
+
+$(eval $(cmake-package))
