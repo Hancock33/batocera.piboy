d19e7d22dbae20cb32b50a89f7e83804932a8ff4
Lee <lee.hancock4@ntlworld.com>
2022-02-13 14:21:46 +0000

    add libretro-quicknes

---
+++ package/batocera/emulators/retroarch/libretro/libretro-quicknes/libretro-quicknes.mk
@@ -0,0 +1,21 @@
+################################################################################
+#
+# QUICKNES
+#
+################################################################################
+# Version.: Commits on Feb 11, 2022
+LIBRETRO_QUICKNES_VERSION = 9f16c3fc13908eac423e39a1ea967afa0b7479ae
+LIBRETRO_QUICKNES_SITE = $(call github,libretro,QuickNES_Core,$(LIBRETRO_QUICKNES_VERSION))
+LIBRETRO_QUICKNES_LICENSE = GPL
+
+define LIBRETRO_QUICKNES_BUILD_CMDS
+	$(TARGET_CONFIGURE_OPTS) $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D)/ -f Makefile platform=unix \
+		GIT_VERSION="-$(shell echo $(LIBRETRO_QUICKNES_VERSION) | cut -c 1-7)"
+endef
+
+define LIBRETRO_QUICKNES_INSTALL_TARGET_CMDS
+	$(INSTALL) -D $(@D)/quicknes_libretro.so \
+		$(TARGET_DIR)/usr/lib/libretro/quicknes_libretro.so
+endef
+
+$(eval $(generic-package))
