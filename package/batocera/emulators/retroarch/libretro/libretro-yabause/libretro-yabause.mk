0828cb12aaf425754883c6b723ddf0987fb6c319
Lee <lee.hancock4@ntlworld.com>
2022-02-13 15:32:09 +0000

    add libretro-yabause (not working but being actively developed)

---
+++ package/batocera/emulators/retroarch/libretro/libretro-yabause/libretro-yabause.mk
@@ -0,0 +1,32 @@
+################################################################################
+#
+# YABAUSE
+#
+################################################################################
+# Version.: Commits on Feb 04, 2022
+LIBRETRO_YABAUSE_VERSION = 3e71480b189ff644a94f7b194a7eaae71594ce0d
+LIBRETRO_YABAUSE_SITE = $(call github,libretro,yabause,$(LIBRETRO_YABAUSE_VERSION))
+LIBRETRO_YABAUSE_LICENSE = GPL
+
+LIBRETRO_YABAUSE_EXTRA_ARGS=
+LIBRETRO_YABAUSE_BOARD=
+
+ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_RPI4),y)
+LIBRETRO_YABAUSE_EXTRA_ARGS=ARCH=aarch64
+LIBRETRO_YABAUSE_PLATFORM=rpi4_64
+else
+LIBRETRO_YABAUSE_PLATFORM=$(LIBRETRO_PLATFORM)
+endif
+
+define LIBRETRO_YABAUSE_BUILD_CMDS
+	$(TARGET_CONFIGURE_OPTS) LDFLAGS="$(LIBRETRO_YABAUSE_TARGET_LDFLAGS)" $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" \
+		-C $(@D)/yabause/src/libretro -f Makefile \
+		platform="$(LIBRETRO_YABAUSE_PLATFORM)" $(LIBRETRO_YABAUSE_EXTRA_ARGS)
+endef
+
+define LIBRETRO_YABAUSE_INSTALL_TARGET_CMDS
+	$(INSTALL) -D $(@D)/yabause/src/libretro/yabause_libretro.so \
+		$(TARGET_DIR)/usr/lib/libretro/yabause_libretro.so
+endef
+
+$(eval $(generic-package))
