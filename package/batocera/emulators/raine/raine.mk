caf0b2e3e9c8f6d7d8f92b85e952cc43aff8463d
Lee <lee.hancock4@ntlworld.com>
2022-02-13 21:50:42 +0000

    raine (wip)

---
+++ package/batocera/emulators/raine/raine.mk
@@ -0,0 +1,40 @@
+################################################################################
+#
+# raine
+#
+################################################################################
+# Version.: Commits on Feb 12, 2022
+RAINE_VERSION = 82d5958574365c1bdee16db58c77ce21b4a58346
+RAINE_SITE = $(call github,zelurker,raine,$(RAINE_VERSION))
+RAINE_LICENSE = BSD
+RAINE_DEPENDENCIES = sdl2 sdl2_net sdl2_image sdl2_ttf muparser
+
+define RAINE_BUILD_CMDS
+	cd $(@D); \
+	PATH="$(STAGING_DIR)/usr/bin:$(HOST_DIR)/bin:$$PATH" \
+	LDFLAGS="--sysroot=$(STAGING_DIR)" \
+	CFLAGS="--sysroot=$(STAGING_DIR)" \
+	CXXFLAGS="--sysroot=$(STAGING_DIR)" \
+	$(MAKE) \
+	CC="$(TARGET_CC)" \
+	CXX="$(TARGET_CXX)" \
+	LD="$(TARGET_LD)" \
+	AR="$(TARGET_AR)" \
+	STRIP="$(TARGET_STRIP)" \
+	NATIVE=$(@D)/linux-gnu-sdl2 \
+	CPU=cortex-a72
+endef
+
+define RAINE_INSTALL_TARGET_CMDS
+	mkdir -p $(TARGET_DIR)/usr/bin
+	mkdir -p $(TARGET_DIR)/usr/lib
+
+	$(INSTALL) -D $(@D)/raine \
+		$(TARGET_DIR)/usr/bin/raine
+
+	# evmap config
+	mkdir -p $(TARGET_DIR)/usr/share/evmapy
+	cp $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/emulators/raine/raine.keys $(TARGET_DIR)/usr/share/evmapy
+endef
+
+$(eval $(generic-package))
