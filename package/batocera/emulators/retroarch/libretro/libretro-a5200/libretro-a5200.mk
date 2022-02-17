763aa482991fe030694747f733d2a2fbf1da72b6
Lee <lee.hancock4@ntlworld.com>
2022-02-17 19:54:07 +0000

    add libretro-a5200

---
+++ package/batocera/emulators/retroarch/libretro/libretro-a5200/libretro-a5200.mk
@@ -0,0 +1,40 @@
+################################################################################
+#
+# A5200
+#
+################################################################################
+# Version.: Commits on Jan 13, 2022
+LIBRETRO_A5200_VERSION = 20c92c9bb159ddb9cc80afb66ad67148c995f7a1
+LIBRETRO_A5200_SITE = $(call github,libretro,a5200,$(LIBRETRO_A5200_VERSION))
+LIBRETRO_A5200_LICENSE = GPL
+
+LIBRETRO_A5200_PLATFORM = $(LIBRETRO_PLATFORM)
+
+ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_RPI1),y)
+LIBRETRO_A5200_PLATFORM = rpi1
+
+else ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_RPI2),y)
+LIBRETRO_A5200_PLATFORM = rpi2
+
+else ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_RPI3)$(BR2_PACKAGE_BATOCERA_TARGET_RPIZERO2),y)
+    ifeq ($(BR2_arm),y)
+        LIBRETRO_A5200_PLATFORM = rpi3
+    else
+        LIBRETRO_A5200_PLATFORM = rpi3_64
+    endif
+
+else ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_RPI4),y)
+LIBRETRO_A5200_PLATFORM = rpi4
+endif
+
+define LIBRETRO_A5200_BUILD_CMDS
+	$(TARGET_CONFIGURE_OPTS) $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D)/ -f Makefile platform="$(LIBRETRO_A5200_PLATFORM)" \
+        GIT_VERSION="-$(shell echo $(LIBRETRO_A5200_VERSION) | cut -c 1-7)"
+endef
+
+define LIBRETRO_A5200_INSTALL_TARGET_CMDS
+	$(INSTALL) -D $(@D)/a5200_libretro.so \
+		$(TARGET_DIR)/usr/lib/libretro/a5200_libretro.so
+endef
+
+$(eval $(generic-package))
