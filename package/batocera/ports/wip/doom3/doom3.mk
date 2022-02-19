02cc6b0beef2c5e2ccd2e6228a3d95e5a7b0624e
Lee <lee.hancock4@ntlworld.com>
2022-02-19 12:53:17 +0000

    Add Wayland check to Vulkan-tools

--- package/batocera/ports/wip/doom3/doom3.mk
+++ package/batocera/ports/wip/doom3/doom3.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 # Version.: Commits on Jan 22, 2022
-DOOM3_VERSION = 699779e9cae047d8b93287fd98dfb4a568bab798
-DOOM3_SITE = $(call github,dhewm,dhewm3,$(DOOM3_VERSION))
+DOOM3_VERSION = 089db4cbb2a39bee98195120c30bfc9a307df6ca
+DOOM3_SITE = $(call github,MadDeCoDeR/Classic-RBDOOM-3-BFG,$(DOOM3_VERSION))
 
 DOOM3_DEPENDENCIES = sdl2 sdl2_mixer
 DOOM3_LICENSE = GPL-2.0
@@ -17,7 +17,6 @@ DOOM3_CONF_OPTS += -DCMAKE_BUILD_TYPE=Release
 DOOM3_CONF_OPTS += -DSDL2=ON
 
 define DOOM3_INSTALL_TARGET_CMDS
-	cp -pvr $(@D)/neo/buildroot-build/idlib/libidlib.so $(TARGET_DIR)/usr/lib
 	cp -pvr $(@D)/neo/buildroot-build/RBDoom3BFG $(TARGET_DIR)/usr/bin
 
 	# evmap config
