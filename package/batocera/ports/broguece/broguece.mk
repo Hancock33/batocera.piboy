4686aac35310ea8256311ce18c471e7f32b164fe
Lee <lee.hancock4@ntlworld.com>
2022-02-19 14:44:48 +0000

    Brogue: Community Edition (Too small for piboy screen)

---
+++ package/batocera/ports/broguece/broguece.mk
@@ -0,0 +1,27 @@
+################################################################################
+#
+#  Brogue Community Edition
+#
+################################################################################
+# Version.: Commits on Feb 16, 2022
+BROGUECE_VERSION = 80cd7cf44a7409b34eec3affc74e3a987609e44b
+BROGUECE_SITE = $(call github,tmewett,BrogueCE,$(BROGUECE_VERSION))
+
+BROGUECE_DEPENDENCIES = sdl2 sdl2_image
+BROGUECE_LICENSE = AGPL-3.0
+
+define BROGUECE_BUILD_CMDS
+		$(TARGET_CONFIGURE_OPTS) $(MAKE) \
+		CPP="$(TARGET_CPP)" CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" \
+		AS="$(TARGET_CC)" LD="$(TARGET_LD)" STRIP="$(TARGET_STRIP)" \
+		-C $(@D) -f Makefile GRAPHICS=YES TERMINAL=NO WEBROGUE=NO
+endef
+
+define BROGUECE_INSTALL_TARGET_CMDS
+	mkdir -p $(TARGET_DIR)/usr/share/brogue
+	$(INSTALL) -D $(@D)/brogue $(TARGET_DIR)/usr/bin/brogue
+	chmod 0754 $(TARGET_DIR)/usr/share/stuntcar/stuntcar
+    cp -av $(@D)/bin/assests $(TARGET_DIR)/usr/share/brogue
+endef
+
+$(eval $(generic-package))
