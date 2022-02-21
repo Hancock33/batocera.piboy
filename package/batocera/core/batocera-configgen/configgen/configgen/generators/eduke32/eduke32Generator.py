823b7208f354663585d6542a2c95fac9e876de28
Al Robs <75033413+iconoclusterdotexe@users.noreply.github.com>
2022-02-21 11:19:03 +0100

    whoops

--- package/batocera/core/batocera-configgen/configgen/configgen/generators/eduke32/eduke32Generator.py
+++ package/batocera/core/batocera-configgen/configgen/configgen/generators/eduke32/eduke32Generator.py
@@ -13,7 +13,6 @@ class Eduke32Generator(Generator):
 
         return Command.Command(
             array=commandArray,
-            env={
             env={"XDG_DATA_HOME":batoceraFiles.CONF, "QT_QPA_PLATFORM":"xcb",
                 "SDL_GAMECONTROLLERCONFIG": controllersConfig.generateSdlGameControllerConfig(playersControllers)
             })
