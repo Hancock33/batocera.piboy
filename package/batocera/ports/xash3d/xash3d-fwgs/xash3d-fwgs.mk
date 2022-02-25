89d0f9e295fa401e9fe5fa2fc1d4b3207ba4d09c
Lee <lee.hancock4@ntlworld.com>
2022-02-25 20:33:48 +0000

    fix xash3d-fwgs

--- package/batocera/ports/xash3d/xash3d-fwgs/xash3d-fwgs.mk
+++ package/batocera/ports/xash3d/xash3d-fwgs/xash3d-fwgs.mk
@@ -26,7 +26,7 @@ XASH3D_FWGS_DEPENDENCIES += libgl
 else
 ifeq ($(BR2_PACKAGE_HAS_LIBGLES),y)
 XASH3D_FWGS_DEPENDENCIES += libgles
-XASH3D_FWGS_CONF_OPTS += --disable-gl
+XASH3D_FWGS_CONF_OPTS += --disable-gl --enable-gl4es
 else
 XASH3D_FWGS_CONF_OPTS += --disable-gl
 endif
@@ -36,7 +36,7 @@ define XASH3D_FWGS_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/usr/lib/xash3d/
 	cp -pvr $(@D)/build/mainui/libmenu.so $(TARGET_DIR)/usr/lib/xash3d/
 	cp -pvr $(@D)/build/ref_soft/libref_soft.so $(TARGET_DIR)/usr/lib/xash3d/
-	cp -pvr $(@D)/build/ref_gl/libref_gl.so $(TARGET_DIR)/usr/lib/xash3d/
+	cp -pvr $(@D)/build/ref_gl/libref_gl*.so $(TARGET_DIR)/usr/lib/xash3d/
 	cp -pvr $(@D)/build/engine/libxash.so $(TARGET_DIR)/usr/lib/xash3d/
 	cp -pvr $(@D)/build/game_launch/xash3d $(TARGET_DIR)/usr/lib/xash3d/
 	# evmap config
