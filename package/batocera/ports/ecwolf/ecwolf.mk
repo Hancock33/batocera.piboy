dd9c57bdb5bda8b8202945d8607401bd4dbc8e51
Lee <lee.hancock4@ntlworld.com>
2022-03-11 23:01:41 +0000

    enable ecwolf for rpi4

--- package/batocera/ports/ecwolf/ecwolf.mk
+++ package/batocera/ports/ecwolf/ecwolf.mk
@@ -9,11 +9,23 @@ ECWOLF_SITE = https://bitbucket.org/ecwolf/ecwolf.git
 ECWOLF_SITE_METHOD=git
 ECWOLF_GIT_SUBMODULES=YES
 ECWOLF_LICENSE = Non-commercial
-ECWOLF_DEPENDENCIES = sdl2
+ECWOLF_DEPENDENCIES = sdl2 host-ecwolf
 
-ECWOLF_CONF_OPTS += -DCMAKE_BUILD_TYPE=Release -DGPL=ON
+HOST_ECWOLF_CONF_OPTS += -DCMAKE_BUILD_TYPE=Release -DNO_GTK=ON
+ECWOLF_CONF_OPTS += -DCMAKE_BUILD_TYPE=Release -DNO_GTK=ON -DFORCE_CROSSCOMPILE=ON \
+                    -DIMPORT_EXECUTABLES=$(BUILD_DIR)/host-ecwolf-$(ECWOLF_VERSION)/ImportExecutables.cmake
 
 ECWOLF_CONF_ENV += LDFLAGS="-lpthread -lvorbisfile -lopusfile -lFLAC -lmodplug -lfluidsynth"
+
+define ECWOLF_CROSS
+ gcc -o $(@D)/deps/gdtoa/arithchk $(@D)/deps/gdtoa/arithchk.c -Wall -Wextra
+             $(@D)/deps/gdtoa/arithchk > $(@D)/deps/gdtoa/arith.h
+
+ gcc -o $(@D)/deps/gdtoa/qnan $(@D)/deps/gdtoa/qnan.c -Wall -Wextra
+             $(@D)/deps/gdtoa/qnan > $(@D)/deps/gdtoa/gd_qnan.h
+endef
+
+ECWOLF_POST_EXTRACT_HOOKS += ECWOLF_CROSS
 
 define ECWOLF_INSTALL_TARGET_CMDS
 		mkdir -p $(TARGET_DIR)/usr/bin
@@ -28,3 +40,5 @@ define ECWOLF_INSTALL_TARGET_CMDS
 endef
 
 $(eval $(cmake-package))
+$(eval $(host-cmake-package))
+
