183f51d96039fdca479963d95a266dcdc26505f2
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2022-04-02 22:35:15 +0100

    Update mame.mk

--- package/batocera/emulators/mame/mame.mk
+++ package/batocera/emulators/mame/mame.mk
@@ -3,8 +3,8 @@
 # MAME
 #
 ################################################################################
-# Version: Release 0.241
-MAME_VERSION = mame0241
+# Version: Release 0.242
+MAME_VERSION = mame0242
 MAME_SITE = $(call github,mamedev,mame,$(MAME_VERSION))
 MAME_DEPENDENCIES = sdl2 sdl2_ttf zlib libpng fontconfig sqlite jpeg flac rapidjson expat glm
 MAME_LICENSE = MAME
