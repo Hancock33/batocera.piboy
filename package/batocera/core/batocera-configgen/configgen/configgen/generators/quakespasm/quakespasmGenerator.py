592fbcd9a65539371c47b6611098627ba54e8f91
Lee <lee.hancock4@ntlworld.com>
2022-04-03 15:58:56 +0100

    tweak quakespasm generator

--- package/batocera/core/batocera-configgen/configgen/configgen/generators/quakespasm/quakespasmGenerator.py
+++ package/batocera/core/batocera-configgen/configgen/configgen/generators/quakespasm/quakespasmGenerator.py
@@ -8,17 +8,16 @@ import controllersConfig
 class QuakespasmGenerator(Generator):
 
     def generate(self, system, rom, playersControllers, gameResolution):
-        commandArray = ["quakespasm", "-basedir", "/userdata/roms/tyrquake"]
+        commandArray = ["quakespasm", "-basedir", "/userdata/roms/tyrquake", "-sndspeed", "44100"]
 
-        if (rom.__contains__("hipnotic")):
-            commandArray = ["quakespasm", "-basedir", "/userdata/roms/tyrquake", "-hipnotic"]
+        if (rom.lower().__contains__("hipnotic")):
+            commandArray = ["quakespasm", "-basedir", "/userdata/roms/tyrquake", "-sndspeed", "44100", "-hipnotic"]
 
-        if (rom.__contains__("rogue")):
-            commandArray = ["quakespasm", "-basedir", "/userdata/roms/tyrquake", "-rogue"]
+        if (rom.lower().__contains__("rogue")):
+            commandArray = ["quakespasm", "-basedir", "/userdata/roms/tyrquake", "-sndspeed", "44100", "-rogue"]
 
-        if (rom.__contains__("dopa")):
-            commandArray = ["quakespasm", "-basedir", "/userdata/roms/tyrquake", "-game", "dopa"]
-
+        if (rom.lower().__contains__("dopa")):
+            commandArray = ["quakespasm", "-basedir", "/userdata/roms/tyrquake", "-sndspeed", "44100", "-game", "dopa"]
 
         return Command.Command(
             array=commandArray,
