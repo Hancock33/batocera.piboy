1a47731bcac0b55ef9878489bcf7deb0f3f83e4d
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2022-04-10 22:25:27 +0100

    Update amiberry.mk

--- package/batocera/emulators/amiberry/amiberry.mk
+++ package/batocera/emulators/amiberry/amiberry.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-# Version: Commits on Apr 09, 2022
-AMIBERRY_VERSION = e017a97b9623e3f6cc38e2dcd44a7023f6acab4a
+# Version: Commits on Apr 10, 2022
+AMIBERRY_VERSION = 26253a6b0b09aeb12e72842876035eaad3e88100
 AMIBERRY_SITE = $(call github,midwan,amiberry,$(AMIBERRY_VERSION))
 AMIBERRY_LICENSE = GPLv3
 AMIBERRY_DEPENDENCIES = sdl2 sdl2_image sdl2_ttf mpg123 libxml2 libmpeg2 flac
