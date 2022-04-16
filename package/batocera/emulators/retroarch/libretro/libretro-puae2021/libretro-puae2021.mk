7b8e7a25ff97f5929aa4b55a170b8a214c11e359
Lee <lee.hancock4@ntlworld.com>
2022-04-16 16:03:35 +0100

    add libretro-puae2021

---
+++ package/batocera/emulators/retroarch/libretro/libretro-puae2021/libretro-puae2021.mk
@@ -0,0 +1,39 @@
+################################################################################
+#
+# libretro-puae
+#
+################################################################################
+# Version: Commits on Apr 15, 2022 (from 2.6.1 branch)
+LIBRETRO_PUAE2021_VERSION = 462cbcaf6f636078af788653f89199cf7a21ee4a
+LIBRETRO_PUAE2021_SITE = $(call github,libretro,libretro-uae,$(LIBRETRO_PUAE2021_VERSION))
+LIBRETRO_PUAE2021__LICENSE = GPLv2
+
+LIBRETRO_PUAE2021_PLATFORM=$(LIBRETRO_PLATFORM)
+
+ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_RPI1),y)
+LIBRETRO_PUAE2021_PLATFORM = rpi1
+
+else ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_RPI2),y)
+LIBRETRO_PUAE2021_PLATFORM = rpi2
+
+else ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_RPI3)$(BR2_PACKAGE_BATOCERA_TARGET_RPIZERO2),y)
+    ifeq ($(BR2_arm),y)
+        LIBRETRO_PUAE2021_PLATFORM = rpi3
+    else
+        LIBRETRO_PUAE2021_PLATFORM = rpi3_64
+    endif
+
+else ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_RPI4),y)
+LIBRETRO_PUAE2021_PLATFORM = rpi4
+endif
+
+define LIBRETRO_PUAE2021_BUILD_CMDS
+	$(TARGET_CONFIGURE_OPTS) $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D)/ -f Makefile platform="$(LIBRETRO_PUAE2021_PLATFORM)"
+endef
+
+define LIBRETRO_PUAE2021_INSTALL_TARGET_CMDS
+	$(INSTALL) -D $(@D)/puae2021_libretro.so \
+		$(TARGET_DIR)/usr/lib/libretro/puae2021_libretro.so
+endef
+
+$(eval $(generic-package))
