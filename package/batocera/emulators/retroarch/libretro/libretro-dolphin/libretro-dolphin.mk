6c7abcf4394f9c634372a29e72bc618f691cd556
Lee <lee.hancock4@ntlworld.com>
2022-04-22 21:58:24 +0100

    modified:   package/batocera/emulators/retroarch/libretro/libretro-dolphin/libretro-dolphin.mk

--- package/batocera/emulators/retroarch/libretro/libretro-dolphin/libretro-dolphin.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-dolphin/libretro-dolphin.mk
@@ -10,14 +10,14 @@ LIBRETRO_DOLPHIN_LICENSE = GPLv2
 
 LIBRETRO_DOLPHIN_SUPPORTS_IN_SOURCE_BUILD = NO
 
-LIBRETRO_DOLPHIN_CONF_OPTS  = -DLIBRETRO=ON
-LIBRETRO_DOLPHIN_CONF_OPTS += -DCMAKE_BUILD_TYPE=Release
+LIBRETRO_DOLPHIN_CONF_OPTS += -DENABLE_X11=OFF
+LIBRETRO_DOLPHIN_CONF_OPTS += -DLIBRETRO=ON
+LIBRETRO_DOLPHIN_CONF_OPTS += -DENABLE_NOGUI=OFF
 LIBRETRO_DOLPHIN_CONF_OPTS += -DENABLE_QT=OFF
 LIBRETRO_DOLPHIN_CONF_OPTS += -DENABLE_TESTS=OFF
 LIBRETRO_DOLPHIN_CONF_OPTS += -DUSE_DISCORD_PRESENCE=OFF
 LIBRETRO_DOLPHIN_CONF_OPTS += -DBUILD_SHARED_LIBS=OFF
-LIBRETRO_DOLPHIN_CONF_OPTS += -DDISTRIBUTOR='batocera.linux'
-LIBRETRO_DOLPHIN_CONF_OPTS += -DENABLE_X11=ON
+LIBRETRO_DOLPHIN_CONF_OPTS += -DCMAKE_BUILD_TYPE=Release
 
 # enable vulkan if we are building with it
 ifeq ($(BR2_PACKAGE_BATOCERA_VULKAN),y)
@@ -27,8 +27,10 @@ else
 endif
 
 define LIBRETRO_DOLPHIN_INSTALL_TARGET_CMDS
-        $(INSTALL) -D $(@D)/buildroot-build/dolphin_libretro.so \
-                $(TARGET_DIR)/usr/lib/libretro/dolphin_libretro.so
+    $(INSTALL) -D $(@D)/buildroot-build/dolphin_libretro.so \
+    $(TARGET_DIR)/usr/lib/libretro/dolphin_libretro.so
+    mkdir -p $(TARGET_DIR)/usr/share/batocera/datainit/bios/dolphin-emu
+    cp -vr $(@D)/Data/Sys $(TARGET_DIR)/usr/share/batocera/datainit/bios/dolphin-emu
 endef
 
 $(eval $(cmake-package))
