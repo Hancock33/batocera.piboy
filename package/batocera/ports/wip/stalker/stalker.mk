cb0eec48c4852743b7f2207e9981042e9ed3153e
Lee <lee.hancock4@ntlworld.com>
2022-05-18 19:47:44 +0100

    update locales

---
+++ package/batocera/ports/wip/stalker/stalker.mk
@@ -0,0 +1,21 @@
+################################################################################
+#
+# S.T.A.L.K.E.R.
+#
+################################################################################
+# Version: Commits on May 16, 2022
+STALKER_VERSION = 32ebf81a252c7179e2824b2874f911a91e822ad1
+STALKER_SITE = https://github.com/OpenXRay/xray-16.git
+STALKER_SITE_METHOD=git
+STALKER_GIT_SUBMODULES=YES
+
+STALKER_DEPENDENCIES = sdl2 sdl2_mixer
+STALKER_LICENSE = GPL-2.0
+
+STALKER_SUPPORTS_IN_SOURCE_BUILD = NO
+STALKER_CONF_OPTS += -DCMAKE_BUILD_TYPE=Release
+STALKER_CONF_OPTS += -DSDL2=ON
+STALKER_CONF_OPTS += -DCMAKE_C_FLAGS="-O3 -pipe"
+STALKER_CONF_OPTS += -DCMAKE_CXX_FLAGS="-O3 -pipe"
+
+$(eval $(cmake-package))
