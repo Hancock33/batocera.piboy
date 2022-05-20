58f6b54287b9cf45068840838d9b97241f8d8a04
Lee <lee.hancock4@ntlworld.com>
2022-05-21 00:08:51 +0100

    add bermuda widescreen option

--- package/batocera/core/batocera-configgen/configgen/configgen/generators/bermuda/bermudaGenerator.py
+++ package/batocera/core/batocera-configgen/configgen/configgen/generators/bermuda/bermudaGenerator.py
@@ -11,6 +11,10 @@ class BermudaGenerator(Generator):
         os.chdir("/userdata/roms/bermuda")
         commandArray = ["bermuda", "--datapath=/userdata/roms/bermuda", "--savepath=/userdata/saves/bermuda"]
 
+        if system.isOptSet("bermuda_aspect"):
+            if system.config['bermuda_aspect'] == '1':
+                commandArray.append("--widescreen=16:9")
+
         return Command.Command(
             array=commandArray,
             env={
--- package/batocera/core/batocera-configgen/configgen/configgen/generators/hurrican/hurricanGenerator.py
+++ package/batocera/core/batocera-configgen/configgen/configgen/generators/hurrican/hurricanGenerator.py
@@ -10,34 +10,34 @@ class HurricanGenerator(Generator):
     def generate(self, system, rom, playersControllers, gameResolution):
         commandArray = ["hurrican"]
         os.chdir(os.path.dirname(os.path.abspath(rom)))
-        
-        if system.isOptSet("crt"):
-            if system.config['crt'] == '1':
+
+        if system.isOptSet("hurrican_crt"):
+            if system.config['hurrican_crt'] == '1':
                 commandArray.append("--crt")
 
-        if system.isOptSet("custlevel"):
-            if system.config['crt'] == 'ChJees':
+        if system.isOptSet("hurrican_custlevel"):
+            if system.config['hurrican_custlevel'] == 'ChJees':
                 commandArray.append("--custom")
                 commandArray.append("ChJees")
-            elif system.config['crt'] == 'Genocide':
+            elif system.config['hurrican_custlevel'] == 'Genocide':
                 commandArray.append("--custom")
                 commandArray.append("Genocide")
-            elif system.config['crt'] == 'Second_Empire':
+            elif system.config['hurrican_custlevel'] == 'Second_Empire':
                 commandArray.append("--custom")
                 commandArray.append("--Second_Empire")
-            elif system.config['crt'] == 'SecretPlanet':
+            elif system.config['hurrican_custlevel'] == 'SecretPlanet':
                 commandArray.append("--custom")
                 commandArray.append("SecretPlanet")
-            elif system.config['crt'] == 'valentine':
+            elif system.config['hurrican_custlevel'] == 'valentine':
                 commandArray.append("--custom")
                 commandArray.append("valentine")
-            elif system.config['crt'] == 'Volcano_Temple':
+            elif system.config['hurrican_custlevel'] == 'Volcano_Temple':
                 commandArray.append("--custom")
                 commandArray.append("Volcano_Temple")
-            elif system.config['crt'] == 'Walkers_World':
+            elif system.config['hurrican_custlevel'] == 'Walkers_World':
                 commandArray.append("--custom")
                 commandArray.append("Walkers_World")
-                
+
         return Command.Command(
             array=commandArray,
             env={
