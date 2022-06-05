13817f80f5a484598c76977e4c7d3ef906634152
Lee <lee.hancock4@ntlworld.com>
2022-06-05 12:12:02 +0100

    modified:   package/batocera/utils/evmapy-system-config/evmapy-system-config.mk

--- package/batocera/utils/evmapy-system-config/evmapy-system-config.mk
+++ package/batocera/utils/evmapy-system-config/evmapy-system-config.mk
@@ -9,12 +9,11 @@ EVMAPY_SYSTEM_CONFIG_SOURCE =
 
 define EVMAPY_SYSTEM_CONFIG_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/usr/share/evmapy
-	cp $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/utils/evmapy-system-config/hotkey.keys $(TARGET_DIR)/usr/share/evmapy/windows.keys
-	cp $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/utils/evmapy-system-config/mouse.keys $(TARGET_DIR)/usr/share/evmapy/windows_installers.keys
-	
+	cp -avr $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/utils/evmapy-system-config/keys/* $(TARGET_DIR)/usr/share/evmapy
+
 	@if [ "$(BR2_PACKAGE_XPI_GAMECON_RPI4)" = "y" ]; then \
-		mkdir $(TARGET_DIR)/usr/share/evmapy/piboy ;
-	    cp -avr $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/utils/evmapy/keys.piboy/* $(TARGET_DIR)/usr/share/evmapy/piboy ; fi
+		mkdir $(TARGET_DIR)/usr/share/evmapy/piboy ; \
+	    cp -avr $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/utils/evmapy-system-config/keys.piboy/* $(TARGET_DIR)/usr/share/evmapy/piboy ; fi
 endef
 
 $(eval $(generic-package))
