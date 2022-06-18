d12e420b629c544740bf403c82783a5d53a712e8
Lee <lee.hancock4@ntlworld.com>
2022-06-18 09:32:37 +0100

    move cdogs to internal space

--- package/batocera/ports/cdogs/cdogs.mk
+++ package/batocera/ports/cdogs/cdogs.mk
@@ -13,20 +13,21 @@ CDOGS_LICENSE = GPL-2.0
 CDOGS_SUPPORTS_IN_SOURCE_BUILD = NO
 
 CDOGS_CONF_OPTS += -DCMAKE_BUILD_TYPE=Release
-CDOGS_CONF_OPTS += -DCDOGS_DATA_DIR=/userdata/roms/cdogs/
+CDOGS_CONF_OPTS += -DCDOGS_DATA_DIR=/usr/share/cdogs/
 CDOGS_CONF_OPTS += -DBUILD_EDITOR=OFF
 
 define CDOGS_INSTALL_TARGET_CMDS
     $(INSTALL) -D $(@D)/buildroot-build/src/cdogs-sdl $(TARGET_DIR)/usr/bin/cdogs
-    mkdir -p $(TARGET_DIR)/usr/share/batocera/datainit/roms/cdogs
-    cp -pav $(@D)/data $(TARGET_DIR)/usr/share/batocera/datainit/roms/cdogs
-    cp -pav $(@D)/doc $(TARGET_DIR)/usr/share/batocera/datainit/roms/cdogs
-    cp -pav $(@D)/dogfights $(TARGET_DIR)/usr/share/batocera/datainit/roms/cdogs
-    cp -pav $(@D)/graphics $(TARGET_DIR)/usr/share/batocera/datainit/roms/cdogs
-    cp -pav $(@D)/missions $(TARGET_DIR)/usr/share/batocera/datainit/roms/cdogs
-    cp -pav $(@D)/music $(TARGET_DIR)/usr/share/batocera/datainit/roms/cdogs
-    cp -pav $(@D)/sounds $(TARGET_DIR)/usr/share/batocera/datainit/roms/cdogs
-    cp -f $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/ports/cdogs/gamecontrollerdb.txt $(TARGET_DIR)/usr/share/batocera/datainit/roms/cdogs/data
+    mkdir -p $(TARGET_DIR)/usr/share/emulationstation/ports/cdogs
+	mkdir -p $(TARGET_DIR)/usr/share/cdogs
+    cp -pa $(@D)/data      $(TARGET_DIR)/usr/share/cdogs
+    cp -pa $(@D)/dogfights $(TARGET_DIR)/usr/share/cdogs
+    cp -pa $(@D)/graphics  $(TARGET_DIR)/usr/share/cdogs
+    cp -pa $(@D)/missions  $(TARGET_DIR)/usr/share/cdogs
+    cp -pa $(@D)/music     $(TARGET_DIR)/usr/share/cdogs
+    cp -pa $(@D)/sounds    $(TARGET_DIR)/usr/share/cdogs
+    cp -f  $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/ports/cdogs/gamecontrollerdb.txt $(TARGET_DIR)/usr/share/cdogs/data
+	cp -a  $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/ports/cdogs/media/*              $(TARGET_DIR)/usr/share/emulationstation/ports/cdogs/
 
     # evmap config
 	mkdir -p $(TARGET_DIR)/usr/share/evmapy
