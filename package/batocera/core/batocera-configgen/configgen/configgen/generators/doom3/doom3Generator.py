0781c70070cc585dacdb49a9db5a1d2d3da39730
Lee <lee.hancock4@ntlworld.com>
2022-06-18 14:14:07 +0100

    clean up ports part 4 - move doom ports to ports folder

--- package/batocera/core/batocera-configgen/configgen/configgen/generators/doom3/doom3Generator.py
+++ package/batocera/core/batocera-configgen/configgen/configgen/generators/doom3/doom3Generator.py
@@ -9,7 +9,7 @@ class Doom3Generator(Generator):
 
     def generate(self, system, rom, playersControllers, guns, gameResolution):
 
-        commandArray = ["dhewm3", "+set", "fs_basepath", "/userdata/roms/doom3", "+set", "r_mode", "3"]
+        commandArray = ["dhewm3", "+set", "fs_basepath", "/userdata/roms/ports/doom3", "+set", "r_mode", "3"]
 
         return Command.Command(
             array=commandArray,
