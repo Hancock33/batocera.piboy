138d2fd030519806cecb9b1230c195cdebc1392e
Lee <lee.hancock4@ntlworld.com>
2022-06-25 10:23:17 +0100

    rename BR2_PACKAGE_BATOCERA_RPI4_WITH_XORG > BR2_PACKAGE_BATOCERA_TARGET_RPI4

--- package/batocera/ports/quake/vkquake/vkquake.mk
+++ package/batocera/ports/quake/vkquake/vkquake.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 # Version: Commits on Jun 24, 2022
-VKQUAKE_VERSION = 2938e73c96645f3f382daa4505f3e1e9fe14aa71
+VKQUAKE_VERSION = 1.13.1
 VKQUAKE_SITE = $(call github,Novum,vkQuake,$(VKQUAKE_VERSION))
 
 VKQUAKE_DEPENDENCIES = sdl2 sdl2_image
