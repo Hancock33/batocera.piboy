c0fe45088e6acb3ec2d9e55bc9caf6b4a79a2880
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2022-07-03 19:27:30 +0100

    tidyups

--- package/batocera/ports/doom/zmusic/zmusic.mk
+++ package/batocera/ports/doom/zmusic/zmusic.mk
@@ -7,7 +7,7 @@
 ZMUSIC_VERSION = 9fbc2ec095ec14267d00caba75cc3e7f59976697
 ZMUSIC_SITE = $(call github,coelckers,ZMusic,$(ZMUSIC_VERSION))
 
-ZMUSIC_DEPENDENCIES = sdl2 sdl2_mixer host-gzdoom host-lzdoom
+ZMUSIC_DEPENDENCIES = zlib mpg123 libsndfile alsa-lib host-gzdoom host-lzdoom
 ZMUSIC_LICENSE = GPL-2.0
 ZMUSIC_SUPPORTS_IN_SOURCE_BUILD = NO
 ZMUSIC_INSTALL_STAGING = YES
