991d9c11db8756b83c74c3316b1e5fb2b6288a2a
Lee <lee.hancock4@ntlworld.com>
2022-07-17 08:11:53 +0100

    add missing files

---
+++ package/batocera/core/batocera-configgen/configgen/configgen/generators/ionfury/ionfuryGenerator.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+import os
+from os import path
+import Command
+from generators.Generator import Generator
+import controllersConfig
+
+class IonfuryGenerator(Generator):
+    if os.path.isfile('/tmp/piboy'):
+        os.system('piboy_keys ionfury.keys')
+
+    def generate(self, system, rom, playersControllers, guns, gameResolution):
+        commandArray = ["ionfury", "-game_dir", os.path.dirname(os.path.abspath(rom)), "-g", rom, "-nologo" if system.getOptBoolean("nologo") else ""]
+        os.chdir(os.path.dirname(os.path.abspath(rom)))
+
+        if os.path.isfile('/tmp/piboy'):
+            return Command.Command(
+                array=commandArray,
+                env={
+                'SDL_AUTO_UPDATE_JOYSTICKS': '0',
+                'SDL_MOUSE_RELATIVE_SPEED_SCALE': '2.0'
+            })
+        else:
+            return Command.Command(
+                array=commandArray,
+                env={
+                "SDL_GAMECONTROLLERCONFIG": controllersConfig.generateSdlGameControllerConfig(playersControllers)
+            })
+
