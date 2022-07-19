96d19a15501e14b498dad38397073ed55fb668ed
Lee <lee.hancock4@ntlworld.com>
2022-07-19 20:38:15 +0100

    update cgenius

--- package/batocera/ports/cgenius/cgenius.mk
+++ package/batocera/ports/cgenius/cgenius.mk
@@ -3,13 +3,14 @@
 # Commander Genius
 #
 ################################################################################
-# Version: Commits on Dec 30, 2021
-CGENIUS_VERSION = 8c69bd96297776b70b2ea5a35471431de095fee5
+# Version: Commits on Jul 01, 2022
+CGENIUS_VERSION = 67e5e036dadddc9b72b05a90a7c704698fc8b206
 CGENIUS_SITE = https://github.com/gerstrong/Commander-Genius
 
 CGENIUS_DEPENDENCIES = sdl2 sdl2_mixer sdl2_image sdl2_ttf boost libcurl
 CGENIUS_SITE_METHOD=git
 CGENIUS_GIT_SUBMODULES=YES
+CGENIUS_SUPPORTS_IN_SOURCE_BUILD = NO
 
 # No OpenGL ES support
 ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_X86_ANY),y)
@@ -17,24 +18,15 @@ CGENIUS_CONF_OPTS += -DUSE_OPENGL=ON
 else ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_RPI4),y)
 CGENIUS_CONF_OPTS += -DUSE_OPENGL=ON
 else
-CGENIUS_CONF_OPTS += -DUSE_OPENGL=OFF
+CGENIUS_CONF_OPTS += -DUSE_OPENGL=OFF -DEMBEDDED=ON
 endif
 
-CGENIUS_CONF_OPTS += -DCMAKE_BUILD_TYPE=Release
+CGENIUS_CONF_OPTS += -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF
 CGENIUS_CONF_OPTS += -DBUILD_COSMOS=1
 CGENIUS_CONF_OPTS += -DGAMES_SHAREDIR=/userdata/roms/ports/cgenius
 
-define CGENIUS_INSTALL_TARGET_SHARED_LIBRARIES
-	mkdir -p $(TARGET_DIR)/usr/lib
-	cp $(@D)/GsKit/base/lua/libGsKit_base_lua.so $(TARGET_DIR)/usr/lib
-	cp $(@D)/GsKit/base/utils/property_tree/libGsKit_base_utils_ptree.so $(TARGET_DIR)/usr/lib
-	cp $(@D)/GsKit/base/utils/libGsKit_base_utils.so $(TARGET_DIR)/usr/lib
-	cp $(@D)/GsKit/base/video/libGsKit_base_video.so $(TARGET_DIR)/usr/lib
-	cp $(@D)/GsKit/base/libGsKit_base.so $(TARGET_DIR)/usr/lib
-	cp $(@D)/GsKit/fileio/libGsKit_fileio.so $(TARGET_DIR)/usr/lib
-	cp $(@D)/GsKit/graphics/libGsKit_graphics.so $(TARGET_DIR)/usr/lib
-	cp $(@D)/GsKit/widgets/libGsKit_widgets.so $(TARGET_DIR)/usr/lib
-	cp $(@D)/GsKit/libGsKit.so $(TARGET_DIR)/usr/lib
+define CGENIUS_INSTALL_TARGET_EVMAP
+	rm -rf $(TARGET_DIR)/usr/share/batocera/datainit/roms/ports/cgenius
 	mv $(TARGET_DIR)/userdata/roms/ports/cgenius/commandergenius $(TARGET_DIR)/usr/share/batocera/datainit/roms/ports/cgenius
 	rm -r $(TARGET_DIR)/userdata/roms
 
@@ -43,6 +35,6 @@ define CGENIUS_INSTALL_TARGET_SHARED_LIBRARIES
 	cp $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/ports/cgenius/cgenius.keys $(TARGET_DIR)/usr/share/evmapy
 endef
 
-CGENIUS_POST_INSTALL_TARGET_HOOKS += CGENIUS_INSTALL_TARGET_SHARED_LIBRARIES
+CGENIUS_POST_INSTALL_TARGET_HOOKS += CGENIUS_INSTALL_TARGET_EVMAP
 
 $(eval $(cmake-package))
