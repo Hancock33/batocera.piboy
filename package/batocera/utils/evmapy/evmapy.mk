1553e450a756ec4562dee763cd08f38db972f44d
Lee <lee.hancock4@ntlworld.com>
2022-07-23 22:49:36 +0100

    update evmapy

--- package/batocera/utils/evmapy/evmapy.mk
+++ package/batocera/utils/evmapy/evmapy.mk
@@ -7,11 +7,6 @@ EVMAPY_VERSION = bd65338c236cd30b4f2d7835733ea5d6b108b75d
 EVMAPY_SITE =  $(call github,kempniu,evmapy,$(EVMAPY_VERSION))
 EVMAPY_SETUP_TYPE = setuptools
 
-define EVMAPY_FIXCHARS
-        sed -i -e s+"MichaÅ, KÄTpieÅ""+"Michal Kepien"+ $(@D)/*.py $(@D)/evmapy/*.py
-endef
-EVMAPY_PRE_CONFIGURE_HOOKS += EVMAPY_FIXCHARS
-
 define EVMAPY_INSTALL_SCRIPTS
 	mkdir -p $(TARGET_DIR)/usr/bin
 	mkdir -p $(TARGET_DIR)/usr/share/evmapy
