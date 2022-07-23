94206ff455b64a37ba440550ab9f58ed1155b7df
Lee <lee.hancock4@ntlworld.com>
2022-07-23 13:02:52 +0100

    add optimisation setting to supermodel

--- package/batocera/emulators/supermodel/supermodel.mk
+++ package/batocera/emulators/supermodel/supermodel.mk
@@ -15,7 +15,7 @@ define SUPERMODEL_BUILD_CMDS
 	$(SED) "s|CXX = g++|CXX = $(TARGET_CXX)|g" $(@D)/Makefile
 	$(SED) "s|LD = gcc|LD = $(TARGET_CC)|g" $(@D)/Makefile
 	$(SED) "s|sdl2-config|$(STAGING_DIR)/usr/bin/sdl2-config|g" $(@D)/Makefile
-	$(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D) -f Makefile NET_BOARD=1 VERBOSE=1
+	$(TARGET_CONFIGURE_OPTS) $(MAKE) -j1 -C $(@D) -f Makefile NET_BOARD=1 VERBOSE=1 ARCH=$(BR2_TARGET_OPTIMIZATION)
 endef
 
 define SUPERMODEL_INSTALL_TARGET_CMDS
