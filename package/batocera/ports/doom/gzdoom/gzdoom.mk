646afa51a88da3e661ae3527debd1609f8de2f7f
Lee <lee.hancock4@ntlworld.com>
2022-07-23 10:37:45 +0100

    update doom ports

--- package/batocera/ports/doom/gzdoom/gzdoom.mk
+++ package/batocera/ports/doom/gzdoom/gzdoom.mk
@@ -1,25 +1,35 @@
 ################################################################################
 #
-# GZDOOM
+# gzdoom
 #
 ################################################################################
 # Version: Commits on Jul 09, 2022
 GZDOOM_VERSION = 9ab65578226b69583ca257edbe96f260ac365e57
 GZDOOM_SITE = $(call github,coelckers,gzdoom,$(GZDOOM_VERSION))
-
-GZDOOM_DEPENDENCIES = sdl2 sdl2_mixer host-zmusic zmusic
-GZDOOM_LICENSE = GPL-2.0
+GZDOOM_LICENSE = GPL-3.0
+GZDOOM_DEPENDENCIES = sdl2 bzip2 fluidsynth openal mesa3d libglu libglew host-zmusic zmusic
 
 GZDOOM_SUPPORTS_IN_SOURCE_BUILD = NO
-
 GZDOOM_CONF_OPTS += -DCMAKE_BUILD_TYPE=Release
 GZDOOM_CONF_OPTS += -DNO_GTK=ON
 GZDOOM_CONF_OPTS += -DFORCE_CROSSCOMPILE=ON
-GZDOOM_CONF_OPTS += -DHAVE_VULKAN=ON
-GZDOOM_CONF_OPTS += -DHAVE_GLES2=ON
 GZDOOM_CONF_OPTS += -DIMPORT_EXECUTABLES=$(HOST_GZDOOM_BUILDDIR)/ImportExecutables.cmake
 GZDOOM_CONF_OPTS += -DINSTALL_SOUNDFONT_PATH="/usr/share/gzdoom"
 GZDOOM_CONF_OPTS += -DINSTALL_PK3_PATH="/usr/share/gzdoom"
+
+ifeq ($(BR2_PACKAGE_VULKAN_HEADERS)$(BR2_PACKAGE_VULKAN_LOADER),yy)
+    GZDOOM_CONF_OPTS += -DHAVE_VULKAN=ON
+else
+    GZDOOM_CONF_OPTS += -DHAVE_VULKAN=OFF
+endif
+
+ifeq ($(BR2_PACKAGE_BATOCERA_GLES2),y)
+    GZDOOM_CONF_OPTS += -DHAVE_GLES2=ON
+else ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_RPI4),y)
+    GZDOOM_CONF_OPTS += -DHAVE_GLES2=ON
+else
+    GZDOOM_CONF_OPTS += -DHAVE_GLES2=OFF
+endif
 
 define GZDOOM_INSTALL
 	cp $(@D)/buildroot-build/libraries/discordrpc/src/libdiscord-rpc.so $(TARGET_DIR)/usr/lib
--- package/batocera/ports/doom/lzdoom/lzdoom.mk
+++ package/batocera/ports/doom/lzdoom/lzdoom.mk
@@ -1,31 +1,40 @@
 ################################################################################
 #
-# LZDOOM
+# lzdoom
 #
 ################################################################################
 # Version: Commits on Apr 10, 2022
 LZDOOM_VERSION = 7f3de9e225deace07ac86dadbe41ccf45ceaae3e
 LZDOOM_SITE = $(call github,drfrag666,gzdoom,$(LZDOOM_VERSION))
-
-LZDOOM_DEPENDENCIES = sdl2 sdl2_mixer host-zmusic zmusic
-LZDOOM_LICENSE = GPL-2.0
+LZDOOM_LICENSE = GPL-3.0
+LZDOOM_DEPENDENCIES = sdl2 bzip2 fluidsynth openal mesa3d libglu libglew host-zmusic zmusic
 
 LZDOOM_SUPPORTS_IN_SOURCE_BUILD = NO
-
 LZDOOM_CONF_OPTS += -DCMAKE_BUILD_TYPE=Release
 LZDOOM_CONF_OPTS += -DNO_GTK=ON
 LZDOOM_CONF_OPTS += -DFORCE_CROSSCOMPILE=ON
-LZDOOM_CONF_OPTS += -DHAVE_VULKAN=ON
-GZDOOM_CONF_OPTS += -DHAVE_GLES2=ON
-LZDOOM_CONF_OPTS += -DIMPORT_EXECUTABLES=$(HOST_LZDOOM_BUILDDIR)/ImportExecutables.cmake
+LZDOOM_CONF_OPTS += -DIMPORT_EXECUTABLES=$(HOST_GZDOOM_BUILDDIR)/ImportExecutables.cmake
 LZDOOM_CONF_OPTS += -DINSTALL_SOUNDFONT_PATH="/usr/share/lzdoom"
 LZDOOM_CONF_OPTS += -DINSTALL_PK3_PATH="/usr/share/lzdoom"
 
-define LZDOOM_KEYS
+ifeq ($(BR2_PACKAGE_VULKAN_HEADERS)$(BR2_PACKAGE_VULKAN_LOADER),yy)
+    LZDOOM_CONF_OPTS += -DHAVE_VULKAN=ON
+else
+    LZDOOM_CONF_OPTS += -DHAVE_VULKAN=OFF
+endif
+
+ifeq ($(BR2_PACKAGE_BATOCERA_GLES2),y)
+    LZDOOM_CONF_OPTS += -DHAVE_GLES2=ON
+else ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_RPI4),y)
+    LZDOOM_CONF_OPTS += -DHAVE_GLES2=ON
+else
+    LZDOOM_CONF_OPTS += -DHAVE_GLES2=OFF
+endif
+
+define LZDOOM_INSTALL
 	mv $(TARGET_DIR)/usr/bin/lzdoom $(TARGET_DIR)/usr/share/lzdoom/lzdoom
 endef
 
-LZDOOM_POST_INSTALL_TARGET_HOOKS += LZDOOM_KEYS
+LZDOOM_POST_INSTALL_TARGET_HOOKS += LZDOOM_INSTALL
 
 $(eval $(cmake-package))
-$(eval $(host-cmake-package))
