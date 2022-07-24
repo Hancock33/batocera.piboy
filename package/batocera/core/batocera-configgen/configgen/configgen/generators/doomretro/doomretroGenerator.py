105ecbfbd559be6682809a7e3a7ee9ab753a4f61
Lee <lee.hancock4@ntlworld.com>
2022-07-24 14:25:22 +0100

    update doomretro mapping

--- package/batocera/core/batocera-configgen/configgen/configgen/generators/doomretro/doomretroGenerator.py
+++ package/batocera/core/batocera-configgen/configgen/configgen/generators/doomretro/doomretroGenerator.py
@@ -6,36 +6,24 @@ from generators.Generator import Generator
 import controllersConfig
 
 class DoomretroGenerator(Generator):
-    if os.path.isfile('/tmp/piboy'):
-        os.system('piboy_keys prboom.doomretro.keys')
-
     def generate(self, system, rom, playersControllers, guns, gameResolution):
-        commandArray = ["doomretro"]
+        commandArray = ['doomretro']
 
-        if (rom.__contains__(".uwad")):
+        if (rom.__contains__('.uwad')):
             f=open(rom)
             content=f.readlines()
             for line in content:
                 if 'IWAD=/' in line:
-                    commandArray.append("-iwad")
-                    commandArray.append(line.replace("IWAD=", "").replace("\n", ""))
+                    commandArray.extend(['-iwad', line.replace('IWAD=', '').replace('\n', '')])
                 elif 'PWAD=/' in line:
-                    commandArray.append("-file")
-                    commandArray.append(line.replace("PWAD=", "").replace("\n", ""))
-
+                    commandArray.extend(['-file', line.replace('PWAD=', '').replace('\n', '')])
         else:
-            commandArray.append("-iwad")
-            commandArray.append(rom)
+                    commandArray.extend(['-iwad', rom])
 
-        if os.path.isfile('/tmp/piboy'):
-            return Command.Command(
-                array=commandArray,
-                env={
-                'SDL_AUTO_UPDATE_JOYSTICKS': '0',
-            })
-        else:
-            return Command.Command(
-                array=commandArray,
-                env={
-                "SDL_GAMECONTROLLERCONFIG": controllersConfig.generateSdlGameControllerConfig(playersControllers)
-            })
+
+        return Command.Command(
+            array=commandArray,
+            env={
+            'DOOMWADDIR': '/userdata/roms/ports/doom',
+            'SDL_GAMECONTROLLERCONFIG': controllersConfig.generateSdlGameControllerConfig(playersControllers)
+        })
