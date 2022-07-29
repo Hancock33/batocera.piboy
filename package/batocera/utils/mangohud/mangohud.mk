591451527bcd1875ad2380cd029c4c73062cb65f
Lee <lee.hancock4@ntlworld.com>
2022-07-29 18:08:49 +0100

    user system curl

--- package/batocera/utils/mangohud/mangohud.mk
+++ package/batocera/utils/mangohud/mangohud.mk
@@ -7,7 +7,7 @@
 MANGOHUD_VERSION = v0.6.6-1
 MANGOHUD_SITE =  $(call github,flightlessmango,MangoHud,$(MANGOHUD_VERSION))
 
-MANGOHUD_DEPENDENCIES = host-libcurl host-python-mako host-glslang
+MANGOHUD_DEPENDENCIES = host-python-mako host-glslang
 
 ifeq ($(BR2_PACKAGE_LIBDRM),y)
 	MANGOHUD_DEPENDENCIES += libdrm
@@ -50,10 +50,10 @@ endif
 # i don't know why meson uses bad ssl certificates and doesn't manage to download them
 define MANGOHUD_DWD_DEPENDENCIES
 	mkdir -p $(@D)/subprojects/packagecache
-	$(HOST_DIR)/bin/curl -L https://github.com/ocornut/imgui/archive/v1.81.tar.gz     -o $(@D)/subprojects/packagecache/imgui-1.81.tar.gz
-	$(HOST_DIR)/bin/curl -L https://wrapdb.mesonbuild.com/v2/imgui_1.81-1/get_patch   -o $(@D)/subprojects/packagecache/imgui-1.81-1-wrap.zip
-	$(HOST_DIR)/bin/curl -L https://github.com/gabime/spdlog/archive/v1.8.5.tar.gz    -o $(@D)/subprojects/packagecache/v1.8.5.tar.gz
-	$(HOST_DIR)/bin/curl -L https://wrapdb.mesonbuild.com/v2/spdlog_1.8.5-1/get_patch -o $(@D)/subprojects/packagecache/spdlog-1.8.5-1-wrap.zip
+	curl -L https://github.com/ocornut/imgui/archive/v1.81.tar.gz     -o $(@D)/subprojects/packagecache/imgui-1.81.tar.gz
+	curl -L https://wrapdb.mesonbuild.com/v2/imgui_1.81-1/get_patch   -o $(@D)/subprojects/packagecache/imgui-1.81-1-wrap.zip
+	curl -L https://github.com/gabime/spdlog/archive/v1.8.5.tar.gz    -o $(@D)/subprojects/packagecache/v1.8.5.tar.gz
+	curl -L https://wrapdb.mesonbuild.com/v2/spdlog_1.8.5-1/get_patch -o $(@D)/subprojects/packagecache/spdlog-1.8.5-1-wrap.zip
 endef
 MANGOHUD_PRE_CONFIGURE_HOOKS += MANGOHUD_DWD_DEPENDENCIES
 
