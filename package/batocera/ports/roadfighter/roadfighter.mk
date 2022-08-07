47b05900a80669bdfb64f51415d4f24db8905501
Lee <lee.hancock4@ntlworld.com>
2022-08-07 10:39:39 +0100

    wip stuff

---
+++ package/batocera/ports/roadfighter/roadfighter.mk
@@ -0,0 +1,24 @@
+################################################################################
+#
+# roadfighter
+#
+################################################################################
+# Version: Commits on Aug 01, 2022
+ROADFIGHTER_VERSION = fd3e1ec14d6b84c408dca50081da0a61f8a4f73d
+ROADFIGHTER_SITE = $(call github,ptitSeb,roadfighter,$(ROADFIGHTER_VERSION))
+ROADFIGHTER_DEPENDENCIES = sdl sdl_image sdl_mixer sdl_sound sdl_ttf
+
+define ROADFIGHTER_BUILD_CMDS
+	$(TARGET_CONFIGURE_OPTS) $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D)/ -f Makefile
+endef
+
+define ROADFIGHTER_INSTALL_TARGET_CMDS
+	mkdir -p $(TARGET_DIR)/usr/share/roadfighter
+	$(INSTALL) -m 0755 $(@D)/roadfighter -D $(TARGET_DIR)/usr/share/roadfighter
+	cp -av $(@D)/{fonts,graphics,maps,sound} $(TARGET_DIR)/usr/share/roadfighter
+	# evmap config
+	mkdir -p $(TARGET_DIR)/usr/share/evmapy
+	cp $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/ports/roadfighter/roadfighter.keys $(TARGET_DIR)/usr/share/evmapy/roadfighter.keys
+endef
+
+$(eval $(generic-package))
