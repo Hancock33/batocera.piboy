49200b8db14c7664c826d9b2dc1674a05c6c8564
Lee <lee.hancock4@ntlworld.com>
2022-08-07 10:59:35 +0100

    box64 revert commit 374

--- package/batocera/ports/box64/box64.mk
+++ package/batocera/ports/box64/box64.mk
@@ -3,7 +3,7 @@
 # box64
 #
 ################################################################################
-# Version: Commits on Jul 31, 2022
+# Version: Commits on Aug 06, 2022
 BOX64_VERSION = 642260bdb63eb48667f35d21b376b04f0d5e1976
 BOX64_SITE = https://github.com/ptitseb/box64.git
 BOX64_SITE_METHOD=git
