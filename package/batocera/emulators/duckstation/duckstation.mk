610ed874a9b4558cc16e91a693cba4aa5ea33acf
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2022-08-14 10:54:58 +0100

    couple of reverts
    must temporary add these to the updater script blacklist

--- package/batocera/emulators/duckstation/duckstation.mk
+++ package/batocera/emulators/duckstation/duckstation.mk
@@ -3,8 +3,8 @@
 # duckstation
 #
 ################################################################################
-# Version: Commits on Aug 10, 2022
-DUCKSTATION_VERSION = 4652c5faf1e8fa8275d765cc3a380783d67b2d5a
+# Version: Commits on Jul 21, 2022
+DUCKSTATION_VERSION = fd3507c16d098fb32806c281caaefb205946da8a
 DUCKSTATION_SITE = https://github.com/stenzek/duckstation.git
 DUCKSTATION_SITE_METHOD=git
 DUCKSTATION_GIT_SUBMODULES=YES
