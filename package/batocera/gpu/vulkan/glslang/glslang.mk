75e092110f7613584c7a06cbf8688737635707fc
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2022-08-26 17:01:34 +0100

    vulkan updates

--- package/batocera/gpu/vulkan/glslang/glslang.mk
+++ package/batocera/gpu/vulkan/glslang/glslang.mk
@@ -3,8 +3,8 @@
 # glslang
 #
 ################################################################################
-# Version: Commits on Aug 12, 2022
-GLSLANG_VERSION = 10423ec659d301a0ff2daac8bbf38980abf27590
+# Version: Commits on Aug 26, 2022
+GLSLANG_VERSION = 9e78bc8108a13d4d4ed860b2c5547092059ed83e
 
 GLSLANG_SITE =  $(call github,KhronosGroup,glslang,$(GLSLANG_VERSION))
 GLSLANG_DEPENDENCIES = vulkan-headers vulkan-loader
