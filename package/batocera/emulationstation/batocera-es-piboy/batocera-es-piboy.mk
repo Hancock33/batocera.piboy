bb45606eb5e40162df5fb145ad4ac89a52626d0e
Lee <lee.hancock4@ntlworld.com>
2022-08-26 23:03:37 +0100

    update locales

--- package/batocera/emulationstation/batocera-es-piboy/batocera-es-piboy.mk
+++ package/batocera/emulationstation/batocera-es-piboy/batocera-es-piboy.mk
@@ -3,8 +3,8 @@
 # batocera-es-piboy
 #
 ################################################################################
-# Version: Commits on Aug 25, 2022
-BATOCERA_ES_PIBOY_VERSION = 87de3ee8f059796e54f9a0c9d236c94be8cb9af4
+# Version: Commits on Aug 26, 2022
+BATOCERA_ES_PIBOY_VERSION = b3f7e61f2159911a5c8770187e33eaf733166ff9
 BATOCERA_ES_PIBOY_SITE = https://github.com/batocera-linux/batocera-emulationstation
 BATOCERA_ES_PIBOY_SITE_METHOD = git
 BATOCERA_ES_PIBOY_LICENSE = MIT
