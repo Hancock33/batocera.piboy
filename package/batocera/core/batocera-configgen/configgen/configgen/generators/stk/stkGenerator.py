7178715b61cb6bfd2e234b3f0a1ec222ba0e26b9
Lee <lee.hancock4@ntlworld.com>
2022-08-27 14:11:34 +0100

    build fixes

--- package/batocera/core/batocera-configgen/configgen/configgen/generators/stk/stkGenerator.py
+++ package/batocera/core/batocera-configgen/configgen/configgen/generators/stk/stkGenerator.py
@@ -8,7 +8,7 @@ import controllersConfig
 class StkGenerator(Generator):
 
     def generate(self, system, rom, playersControllers, guns, gameResolution):
-        commandArray = ["supertuxkart" ]
+        commandArray = ["supertuxkart", "--render-driver=vulkan"]
 
         return Command.Command(
             array=commandArray)
