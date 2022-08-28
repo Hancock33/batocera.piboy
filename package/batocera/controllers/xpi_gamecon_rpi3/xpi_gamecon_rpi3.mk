07ff2d203cad7dd212c4ce38083e3fda4d4b6a3c
Lee <lee.hancock4@ntlworld.com>
2022-08-28 18:25:20 +0100

    dont build the rpi kernel twice

--- package/batocera/controllers/xpi_gamecon_rpi3/xpi_gamecon_rpi3.mk
+++ package/batocera/controllers/xpi_gamecon_rpi3/xpi_gamecon_rpi3.mk
@@ -8,9 +8,9 @@ XPI_GAMECON_RPI3_VERSION = f30cea1a30e4e9ad8fcf3b7da48f2770b4ea3368
 XPI_GAMECON_RPI3_SITE = $(call github,hancock33,piboycontrols,$(XPI_GAMECON_RPI3_VERSION))
 XPI_GAMECON_RPI3_DEPENDENCIES = linux
 
-define XPI_GAMECON_RPI3_BUILD_CMDS
-        $(MAKE) -C $(@D) $(LINUX_MAKE_FLAGS) KERNELDIR=$(LINUX_DIR) KVERSION=$(LINUX_DIR)
-endef
+#define XPI_GAMECON_RPI3_BUILD_CMDS
+#        $(MAKE) -C $(@D) $(LINUX_MAKE_FLAGS) KERNELDIR=$(LINUX_DIR) KVERSION=$(LINUX_DIR)
+#endef
 
 define XPI_GAMECON_RPI3_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0755 $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/controllers/xpi_gamecon_rpi4/S19piboy             	$(TARGET_DIR)/etc/init.d/S19piboy
@@ -21,5 +21,5 @@ define XPI_GAMECON_RPI3_INSTALL_TARGET_CMDS
 	cp -pvr $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/controllers/xpi_gamecon_rpi4/roms/piboy $(TARGET_DIR)/usr/share/batocera/datainit/roms
 endef
 
-$(eval $(kernel-module))
+#$(eval $(kernel-module))
 $(eval $(generic-package))
