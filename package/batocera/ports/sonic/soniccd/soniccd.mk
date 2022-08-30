eb5e34587e0a7e63309caecdf8964ae8ce1bad5e
Lee <lee.hancock4@ntlworld.com>
2022-08-30 23:23:19 +0100

    git add sonicmania

--- package/batocera/ports/sonic/soniccd/soniccd.mk
+++ package/batocera/ports/sonic/soniccd/soniccd.mk
@@ -29,7 +29,7 @@ endef
 
 define SONICCD_POST_PROCESS
 	mkdir -p $(TARGET_DIR)/usr/share/evmapy $(TARGET_DIR)/usr/share/soniccd
-	cp -f $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/ports/sonic/soniccd/sonicretro.keys $(TARGET_DIR)/usr/share/evmapy
+	cp -f $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/ports/sonic/soniccd/soniccd.sonicretro.keys $(TARGET_DIR)/usr/share/evmapy
 endef
 
 SONICCD_POST_INSTALL_TARGET_HOOKS += SONICCD_POST_PROCESS
