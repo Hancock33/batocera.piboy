6e974195211b8172b13289f817d4c1f559b3a690
Lee <lee.hancock4@ntlworld.com>
2022-08-30 12:57:46 +0100

    udpate quake ports

--- package/batocera/core/batocera-configgen/configgen/configgen/generators/quakespasm/quakespasmGenerator.py
+++ package/batocera/core/batocera-configgen/configgen/configgen/generators/quakespasm/quakespasmGenerator.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python
-
+import os
+import shutil
+from os import path
 import Command
 from generators.Generator import Generator
 import controllersConfig
 
+q1_src = "/usr/share/game_assets/quake1/id1/quakespasm.pak"
+q1_dst = "/userdata/roms/ports/quake1/id1/quakespasm.pak"
 
 class QuakespasmGenerator(Generator):
 
     def generate(self, system, rom, playersControllers, guns, gameResolution):
         commandArray = ["quakespasm", "-basedir", "/userdata/roms/ports/quake1", "-sndspeed", "44100"]
+        
+        shutil.copy(q1_src, q1_dst)
 
         if (rom.lower().__contains__("hipnotic")):
             commandArray = ["quakespasm", "-basedir", "/userdata/roms/ports/quake1", "-sndspeed", "44100", "-hipnotic"]
