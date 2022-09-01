e142f723403536872d00c8e8789a00137af3d15b
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2022-09-01 19:56:30 +0100

    Update fallout2.mk

--- package/batocera/ports/fallout2/fallout2.mk
+++ package/batocera/ports/fallout2/fallout2.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 # Version: Commits on Sept 01, 2022
-FALLOUT2_VERSION = 039ad6555730e590cc5b4e00a5303b14e1431e6e
+FALLOUT2_VERSION = 629978d7a6acc865ef2cebf0628882abd21bdd4e
 FALLOUT2_SITE = $(call github,alexbatalov,fallout2-ce,$(FALLOUT2_VERSION))
 
 FALLOUT2_DEPENDENCIES = sdl2 sdl2_image sdl2_mixer sdl2_net sdl2_ttf
