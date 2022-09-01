b0410c2d5376425e120da1850f4081151ba8adce
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2022-09-01 19:50:30 +0100

    update vulkan

--- package/batocera/gpu/vulkan/spirv-headers/spirv-headers.mk
+++ package/batocera/gpu/vulkan/spirv-headers/spirv-headers.mk
@@ -3,8 +3,8 @@
 # spirv-headers
 #
 ################################################################################
-# Version: Commits on Aug 24, 2022
-SPIRV_HEADERS_VERSION = 93754d52d6cbbfd61f4e87571079e8a28e65f8ca
+# Version: Commits on Sept 01, 2022
+SPIRV_HEADERS_VERSION = 87d5b782bec60822aa878941e6b13c0a9a954c9b
 SPIRV_HEADERS_SITE = $(call github,KhronosGroup,SPIRV-Headers,$(SPIRV_HEADERS_VERSION))
 
 # Only installs header files
--- package/batocera/gpu/vulkan/spirv-tools/spirv-tools.mk
+++ package/batocera/gpu/vulkan/spirv-tools/spirv-tools.mk
@@ -3,8 +3,8 @@
 # spirv-tools
 #
 ################################################################################
-# Version: Commits on Aug 31, 2022
-SPIRV_TOOLS_VERSION = b5d1040b94f390a2981468bbf2752e79c98a4376
+# Version: Commits on Sept 01, 2022
+SPIRV_TOOLS_VERSION = 33113abf453572d3cf61b58d4378bbacb4206b1a
 SPIRV_TOOLS_SITE = $(call github,KhronosGroup,SPIRV-Tools,$(SPIRV_TOOLS_VERSION))
 SPIRV_TOOLS_DEPENDENCIES = spirv-headers
 SPIRV_TOOLS_INSTALL_STAGING = YES
