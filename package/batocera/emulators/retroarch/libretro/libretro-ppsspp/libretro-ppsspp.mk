dcf131c602c058b26e0e27a56a6ad65ec5b178e4
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2022-09-01 19:53:07 +0100

    update retroarch + cores

--- package/batocera/emulators/retroarch/libretro/libretro-ppsspp/libretro-ppsspp.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-ppsspp/libretro-ppsspp.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 # Version: Commits on Sept 01, 2022
-LIBRETRO_PPSSPP_VERSION = b5e5f780f24bb434a6739850a8a5fc867ba6a0db
+LIBRETRO_PPSSPP_VERSION = 5200208a846e1f8360ebb7fc7f3ec34ea3846b38
 LIBRETRO_PPSSPP_SITE = https://github.com/hrydgard/ppsspp.git
 LIBRETRO_PPSSPP_SITE_METHOD=git
 LIBRETRO_PPSSPP_GIT_SUBMODULES=YES
