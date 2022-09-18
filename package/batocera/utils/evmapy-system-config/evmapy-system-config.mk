1427c12631f2e61e8e209cd3aeee5813ca3b7469
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2022-09-18 11:12:34 +0100

    git piboy3 some love + simply xpi_gamecon

--- package/batocera/utils/evmapy-system-config/evmapy-system-config.mk
+++ package/batocera/utils/evmapy-system-config/evmapy-system-config.mk
@@ -11,7 +11,7 @@ define EVMAPY_SYSTEM_CONFIG_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/usr/share/evmapy
 	cp -avr $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/utils/evmapy-system-config/keys/* $(TARGET_DIR)/usr/share/evmapy
 
-	@if [ "$(BR2_PACKAGE_XPI_GAMECON_RPI4)" = "y" ]; then \
+	@if [ "$(BR2_PACKAGE_XPI_GAMECON_RPI)" = "y" ]; then \
 		mkdir $(TARGET_DIR)/usr/share/evmapy/piboy ; \
 	    cp -avr $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/utils/evmapy-system-config/keys.piboy/* $(TARGET_DIR)/usr/share/evmapy/piboy ; fi
 endef
