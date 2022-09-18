6af12d1ad6e92bc6c7e05e938f22e24d8e3ab2c1
Lee <lee.hancock4@ntlworld.com>
2022-09-18 22:43:55 +0100

    Allow SorR to render in fullscreen mode in non piboy environment

--- package/batocera/ports/32bit/lib32bit/lib32bit.mk
+++ package/batocera/ports/32bit/lib32bit/lib32bit.mk
@@ -5,7 +5,7 @@
 ################################################################################
 LIB32BIT_SOURCE = batocera-rpi4-32bit-libs-$(LIB32BIT_VERSION).tar.xz
 LIB32BIT_SITE = https://github.com/Hancock33/batocera-rp4-32bit-libs/releases/download/$(LIB32BIT_VERSION)
-LIB32BIT_VERSION = 20220917
+LIB32BIT_VERSION = 20220918
 
 define LIB32BIT_EXTRACT_CMDS
 	mkdir -p $(@D)/target && cd $(@D)/target && tar xf $(DL_DIR)/$(LIB32BIT_DL_SUBDIR)/$(LIB32BIT_SOURCE)
@@ -19,6 +19,7 @@ define LIB32BIT_INSTALL_TARGET_CMDS
 	# evmap config
 	mkdir -p $(TARGET_DIR)/usr/share/evmapy
 	cp $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/ports/32bit/lib32bit/sorr.keys $(TARGET_DIR)/usr/share/evmapy/sorr.keys
+	cp $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/ports/32bit/lib32bit/sorr.piboy.keys $(TARGET_DIR)/usr/share/evmapy/piboy/sorr.keys	
 	cp $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/ports/32bit/lib32bit/droidports.keys $(TARGET_DIR)/usr/share/evmapy/droidports.keys
 endef
 
