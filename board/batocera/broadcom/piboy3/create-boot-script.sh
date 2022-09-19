243e5481bb269cc32dce2c7d4a710bd720e3d4cd
Lee <lee.hancock4@ntlworld.com>
2022-09-19 14:53:13 +0100

    Update piboy kernel to 5.19.9
    1. Use the generated dtb & dtbo files from the kernel build instead of the prebuilt files from the rpi-firmware package. This allows future versions of the kernel to be used.
    *The rpi-firmware package is still needed for the fixup and start files.
    
    2. Suppress “[drm:vc4_fkms_page_flip [vc4]] ERROR Async flips aren't allowed” message, this appears to be a xorg-server bug?

--- board/batocera/broadcom/piboy3/create-boot-script.sh
+++ board/batocera/broadcom/piboy3/create-boot-script.sh
@@ -17,10 +17,9 @@ BATOCERA_BINARIES_DIR=$6
 mkdir -p "${BATOCERA_BINARIES_DIR}/boot/boot" || exit 1
 mkdir -p "${BATOCERA_BINARIES_DIR}/boot/overlays" || exit 1
 
-cp -pr "${BINARIES_DIR}/rpi-firmware/"* "${BATOCERA_BINARIES_DIR}/boot/" || exit 1
+cp -pr "${BINARIES_DIR}/rpi-firmware/"*    "${BATOCERA_BINARIES_DIR}/boot/" || exit 1
 cp -f  "${BUILD_DIR}/linux-custom/arch/arm/boot/dts/overlays/"*.dtbo "${BATOCERA_BINARIES_DIR}/boot/overlays" || exit 1
-cp -f  "${BUILD_DIR}"/rpi-firmware-*/boot/*rpi-3-*.dtb           "${BATOCERA_BINARIES_DIR}/boot/" || exit 1
-cp -f  "${BUILD_DIR}"/rpi-firmware-*/boot/bcm2710-rpi-zero-2.dtb "${BATOCERA_BINARIES_DIR}/boot/" || exit 1
+cp     "${BINARIES_DIR}/"*.dtb             "${BATOCERA_BINARIES_DIR}/boot/" || exit 1
 cp     "${BOARD_DIR}/boot/config.txt"      "${BATOCERA_BINARIES_DIR}/boot/" || exit 1
 cp     "${BOARD_DIR}/boot/config.txt"      "${BATOCERA_BINARIES_DIR}/boot/config_piboy.txt" || exit 1
 cp     "${BOARD_DIR}/boot/config_rpi3.txt" "${BATOCERA_BINARIES_DIR}/boot/" || exit 1
@@ -28,8 +27,8 @@ cp     "${BOARD_DIR}/boot/fan.ini"         "${BATOCERA_BINARIES_DIR}/boot/" || e
 cp     "${BOARD_DIR}/boot/cmdline.txt"     "${BATOCERA_BINARIES_DIR}/boot/" || exit 1
 cp     "${BOARD_DIR}/boot/README!.txt"     "${BATOCERA_BINARIES_DIR}/boot/" || exit 1
 
-cp     "${BINARIES_DIR}/Image"             "${BATOCERA_BINARIES_DIR}/boot/boot/linux"           || exit 1
-cp     "${BINARIES_DIR}/initrd.gz"         "${BATOCERA_BINARIES_DIR}/boot/boot/"                || exit 1
-cp     "${BINARIES_DIR}/rootfs.squashfs"   "${BATOCERA_BINARIES_DIR}/boot/boot/batocera.update" || exit 1
+cp "${BINARIES_DIR}/Image"           "${BATOCERA_BINARIES_DIR}/boot/boot/linux"           || exit 1
+cp "${BINARIES_DIR}/initrd.gz"       "${BATOCERA_BINARIES_DIR}/boot/boot/"                || exit 1
+cp "${BINARIES_DIR}/rootfs.squashfs" "${BATOCERA_BINARIES_DIR}/boot/boot/batocera.update" || exit 1
 
 exit 0
