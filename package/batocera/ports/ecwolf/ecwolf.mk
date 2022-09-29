5f1823b0b81d111b297a7be9722cf5772bda25aa
Lee <lee.hancock4@ntlworld.com>
2022-09-29 07:32:13 +0100

    update locales + fix ecwolf

--- package/batocera/ports/ecwolf/ecwolf.mk
+++ package/batocera/ports/ecwolf/ecwolf.mk
@@ -3,20 +3,16 @@
 # ecwolf
 #
 ################################################################################
-# Version.: Commits on Sep 24, 2022
-ECWOLF_VERSION = db154c482943e89c16b8d4de23120e66f5312042
+# Version: Commits on Aug 21, 2022
+ECWOLF_VERSION = bf02d1e5210fcfda66431c1f28091d8503c55f5f
 ECWOLF_SITE = https://bitbucket.org/ecwolf/ecwolf.git
 ECWOLF_LICENSE = Non-commercial
 ECWOLF_DEPENDENCIES = sdl2 host-ecwolf
 ECWOLF_SITE_METHOD=git
-ECWOLF_GIT_SUBMODULES=YES
-ECWOLF_SUPPORTS_IN_SOURCE_BUILD = NO
 
-HOST_ECWOLF_CONF_OPTS += -DCMAKE_BUILD_TYPE=Release -DNO_GTK=ON -DINTERNAL_SDL=ON \
-                    -DINTERNAL_SDL_MIXER=ON -DINTERNAL_SDL_NET=ON
-					
-ECWOLF_CONF_OPTS += -DCMAKE_BUILD_TYPE=Release -DNO_GTK=ON -DFORCE_CROSSCOMPILE=ON -DINTERNAL_SDL=ON \
-                    -DINTERNAL_SDL_MIXER=ON -DINTERNAL_SDL_NET=ON \
+HOST_ECWOLF_CONF_OPTS += -DCMAKE_BUILD_TYPE=Release -DNO_GTK=ON
+
+ECWOLF_CONF_OPTS += -DCMAKE_BUILD_TYPE=Release -DNO_GTK=ON -DFORCE_CROSSCOMPILE=ON -DINTERNAL_SDL_MIXER=ON \
                     -DIMPORT_EXECUTABLES=$(BUILD_DIR)/host-ecwolf-$(ECWOLF_VERSION)/ImportExecutables.cmake
 
 ECWOLF_CONF_ENV += LDFLAGS="-lpthread -lvorbisfile -lopusfile -lFLAC -lmodplug -lfluidsynth"
@@ -34,8 +30,8 @@ ECWOLF_POST_EXTRACT_HOOKS += ECWOLF_CROSS
 define ECWOLF_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/usr/bin
 	mkdir -p $(TARGET_DIR)/usr/share/ecwolf
-	$(INSTALL) -D -m 0755 $(@D)/buildroot-build/ecwolf $(TARGET_DIR)/usr/share/ecwolf/ecwolf
-	cp -a $(@D)/buildroot-build/ecwolf.pk3 $(TARGET_DIR)/usr/share/ecwolf/
+	$(INSTALL) -D -m 0755 $(@D)/ecwolf $(TARGET_DIR)/usr/share/ecwolf/ecwolf
+	cp -a $(@D)/ecwolf.pk3 $(TARGET_DIR)/usr/share/ecwolf/
 	ln -sf /usr/share/ecwolf/ecwolf $(TARGET_DIR)/usr/bin/ecwolf
 
 	# evmap config
