26331e9f991c6caa198091f9e4f02ab01c05b616
Lee <lee.hancock4@ntlworld.com>
2022-10-28 23:15:46 +0100

    update locales + build fixes

--- package/batocera/gpu/vulkan/vulkan-tools/vulkan-tools.mk
+++ package/batocera/gpu/vulkan/vulkan-tools/vulkan-tools.mk
@@ -3,19 +3,20 @@
 # vulkan-tools
 #
 ################################################################################
-# Version: Commits on Oct 27, 2022
-VULKAN_TOOLS_VERSION = 1ac532e3f48eec4dfa9a0235edc05559b0857ad4
+# Version: Commits on Oct 26, 2022
+VULKAN_TOOLS_VERSION = 28e73b113da0c8e2d287e7c8a275611ea9c7dbc1
 VULKAN_TOOLS_SITE = $(call github,KhronosGroup,Vulkan-Tools,$(VULKAN_TOOLS_VERSION))
 VULKAN_TOOLS_DEPENDENCIES = vulkan-headers vulkan-loader host-python3 host-glslang
 VULKAN_TOOLS_INSTALL_STAGING = YES
 VULKAN_TOOLS_SUPPORTS_IN_SOURCE_BUILD = NO
 
-VULKAN_TOOLS_CONF_OPTS += -DCMAKE_BUILD_TYPE=Release -DBUILD_WSI_WAYLAND_SUPPORT=OFF \
+VULKAN_TOOLS_CONF_OPTS += -DCMAKE_BUILD_TYPE=Release -DBUILD_CUBE=OFF \
     -DVULKAN_HEADERS_INSTALL_DIR=$(STAGING_DIR)/usr/include/ -DBUILD_SHARED_LIBS=OFF
 VULKAN_TOOLS_CONF_ENV += LDFLAGS="-lpthread -ldl"
 
 ifeq ($(BR2_PACKAGE_BATOCERA_PANFROST_MESA3D),y)
-VULKAN_TOOLS_CONF_OPTS += -DBUILD_WSI_XCB_SUPPORT=OFF -DBUILD_WSI_XLIB_SUPPORT=OFF -DCUBE_WSI_SELECTION=DISPLAY -DGLSLANG_INSTALL_DIR=$(HOST_DIR)
+VULKAN_TOOLS_CONF_OPTS += -DBUILD_WSI_XCB_SUPPORT=OFF -DBUILD_WSI_XLIB_SUPPORT=OFF \
+    -DCUBE_WSI_SELECTION=DISPLAY -DGLSLANG_INSTALL_DIR=$(HOST_DIR)
 endif
 
 ifeq ($(BR2_PACKAGE_MESA3D),y)
@@ -23,7 +24,8 @@ VULKAN_TOOLS_DEPENDENCIES += mesa3d
 endif
 
 define VULKAN_TOOLS_SERVICE
-	$(INSTALL) -D -m 0755 $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/gpu/vulkan/vulkan-tools/S55vulkaninfo $(TARGET_DIR)/etc/init.d/S55vulkaninfo
+	$(INSTALL) -D -m 0755 $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/gpu/vulkan/vulkan-tools/S55vulkaninfo \
+	    $(TARGET_DIR)/etc/init.d/S55vulkaninfo
 endef
 
 # remove (at least temporary) while it prevents es to open windows with some nvidia cards
