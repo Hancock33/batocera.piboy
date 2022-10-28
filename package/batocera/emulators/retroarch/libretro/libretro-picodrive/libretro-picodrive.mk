ade948658211c6bbb641768fe8337de75915b2ec
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2022-10-28 23:33:09 +0100

    update retroarch + cores

--- package/batocera/emulators/retroarch/libretro/libretro-picodrive/libretro-picodrive.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-picodrive/libretro-picodrive.mk
@@ -3,8 +3,8 @@
 # libretro-picodrive
 #
 ################################################################################
-# Version: Commits on Oct 16, 2022
-LIBRETRO_PICODRIVE_VERSION = 7e0c38c9c7a5d1fc17e0e6a61e9d030d397327f7
+# Version: Commits on Oct 28, 2022
+LIBRETRO_PICODRIVE_VERSION = eb990fd62a7a657c21679afd9918e2d615c3e90e
 LIBRETRO_PICODRIVE_SITE = https://github.com/irixxxx/picodrive.git
 LIBRETRO_PICODRIVE_SITE_METHOD=git
 LIBRETRO_PICODRIVE_GIT_SUBMODULES=YES
