b34d465e9ae0ad1d963049636afbea2e4be13891
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2022-11-04 18:22:30 +0000

    build fixes

--- package/batocera/firmwares/rpi-eeprom/rpi-eeprom.mk
+++ package/batocera/firmwares/rpi-eeprom/rpi-eeprom.mk
@@ -12,7 +12,7 @@ RPI_EEPROM_LICENSE = BSD-3-Clause
 
 define RPI_EEPROM_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/lib/firmware/raspberrypi/bootloader/default
-	cp -pvr $(@D)/firmware/stable/{pieeprom-2022-10-18.bin,recovery.bin,vl805-000137ad.bin,vl805-000138a1.bin} \
+	cp -pvr $(@D)/firmware/stable/{pieeprom-2022-11-04.bin,recovery.bin,vl805-000137ad.bin,vl805-000138a1.bin} \
 			$(TARGET_DIR)/lib/firmware/raspberrypi/bootloader/default
 		cp -pvr $(@D)/rpi-eeprom-{config,digest,update} $(TARGET_DIR)/usr/bin
 		cp -av $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/firmwares/rpi-eeprom/batocera-update-rpi4-fw $(TARGET_DIR)/usr/bin
--- package/batocera/ports/quake/vkquake/vkquake.mk
+++ package/batocera/ports/quake/vkquake/vkquake.mk
@@ -10,21 +10,8 @@ VKQUAKE_SITE = $(call github,Novum,vkQuake,$(VKQUAKE_VERSION))
 VKQUAKE_DEPENDENCIES = sdl2 sdl2_image
 VKQUAKE_LICENSE = GPLv2
 
-define VKQUAKE_REMOVE_WERROR
-	$(SED) 's% -Werror%%' $(@D)/Quake/Makefile
-endef
-
-VKQUAKE_POST_PATCH_HOOKS += VKQUAKE_REMOVE_WERROR
-
-define VKQUAKE_BUILD_CMDS
-		$(TARGET_CONFIGURE_OPTS) $(MAKE) \
-		CPP="$(TARGET_CPP)" CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" \
-		AS="$(TARGET_CC)" LD="$(TARGET_LD)" STRIP="$(TARGET_STRIP)" \
-		-C $(@D)/Quake -f Makefile USE_SDL2=1 DO_USERDIRS=1
-endef
-
 define VKQUAKE_INSTALL_TARGET_CMDS
-	cp -pvr $(@D)/Quake/vkquake $(TARGET_DIR)/usr/bin
+	cp -pvr $(@D)/build/vkquake $(TARGET_DIR)/usr/bin
 	mkdir -p $(TARGET_DIR)/usr/share/game_assets/quake1/id1
 	cp -pvr $(@D)/Quake/vkquake.pak $(TARGET_DIR)/usr/share/game_assets/quake1/id1
 	mkdir -p $(TARGET_DIR)/usr/share/batocera/datainit/system/configs/vkquake/{id1,dopa,hipnotic,rogue}
@@ -37,4 +24,4 @@ define VKQUAKE_INSTALL_TARGET_CMDS
 	cp $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/ports/quake/vkquake/vkquake.keys $(TARGET_DIR)/usr/share/evmapy/tyrquake.vkquake.keys
 endef
 
-$(eval $(generic-package))
+$(eval $(meson-package))
