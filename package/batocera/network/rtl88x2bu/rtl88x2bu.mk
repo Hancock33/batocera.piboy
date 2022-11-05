e529b4eaf3c7d5f54ff8fc1c58ed278c77cba164
Lee <lee.hancock4@ntlworld.com>
2022-11-05 07:52:00 +0000

    wireless update network drivers

--- package/batocera/network/rtl88x2bu/rtl88x2bu.mk
+++ package/batocera/network/rtl88x2bu/rtl88x2bu.mk
@@ -3,8 +3,8 @@
 # rtl88x2bu
 #
 ################################################################################
-# Version: Commits on Oct 19, 2022
-RTL88X2BU_VERSION = 73a681a0db4a9ad132f85d567e225374c1e6a12b
+# Version: Commits on Oct 31, 2022
+RTL88X2BU_VERSION = ecf5014fe0f9f0ee87f84ea2b7472da788fd8627
 RTL88X2BU_SITE = $(call github,morrownr,88x2bu-20210702,$(RTL88X2BU_VERSION))
 RTL88X2BU_LICENSE = GPL-2.0
 RTL88X2BU_LICENSE_FILES = LICENSE
