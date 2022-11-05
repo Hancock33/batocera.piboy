e64ff07238ced6a64a17e573487cacf654120cf7
Lee <lee.hancock4@ntlworld.com>
2022-11-05 17:52:08 +0000

    update half-life

--- package/batocera/ports/xash3d/hlsdk-xash3d/hlsdk-xash3d.mk
+++ package/batocera/ports/xash3d/hlsdk-xash3d/hlsdk-xash3d.mk
@@ -12,14 +12,15 @@
 # List of games that require custom libraries (a few are covered by the `mobile_hacks` branch):
 #
 #   https://github.com/FWGS/xash3d-fwgs/blob/master/Documentation/supported-mod-list.md#list-of-games-and-mods-with-custom-gamedll
-# Version: Commits on Sept 25, 2022
-HLSDK_XASH3D_VERSION = 445f202214bbc62f4162ad6e31294003bd01fa81
-HLSDK_XASH3D_SITE = $(call github,FWGS,hlsdk-portable,$(HLSDK_XASH3D_VERSION))
+#
+# "mobile_hacks" branch on 03 Nov 2022
+HLSDK_XASH3D_VERSION = 08cb9e6b6725223e28d652a367f579e5906be212
+HLSDK_XASH3D_SITE = $(call github,FWGS,hlsdk-xash3d,$(HLSDK_XASH3D_VERSION))
 HLSDK_XASH3D_DEPENDENCIES = sdl2 sdl2_mixer sdl2_image sdl2_ttf libsodium
 HLSDK_XASH3D_LICENSE = Half Life 1 SDK LICENSE
 HLSDK_XASH3D_LICENSE_FILES = LICENSE
 
-HLSDK_XASH3D_CONF_OPTS = --build-type=release
+HLSDK_XASH3D_CONF_OPTS = --build-type=release --enable-simple-mod-hacks
 
 ifeq ($(BR2_ARCH_IS_64),y)
 HLSDK_XASH3D_CONF_OPTS += --64bits
@@ -28,9 +29,6 @@ endif
 define HLSDK_XASH3D_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 -t $(TARGET_DIR)/usr/lib/xash3d/hlsdk/hl/cl_dlls/ -D $(@D)/build/cl_dll/*.so
 	$(INSTALL) -m 0755 -t $(TARGET_DIR)/usr/lib/xash3d/hlsdk/hl/dlls/ -D $(@D)/build/dlls/*.so
-	# evmap config
-	mkdir -p $(TARGET_DIR)/usr/share/evmapy
-	cp $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/ports/xash3d/hlsdk-xash3d/hlsdk-xash3d.keys $(TARGET_DIR)/usr/share/evmapy
 endef
 
 $(eval $(waf-package))
