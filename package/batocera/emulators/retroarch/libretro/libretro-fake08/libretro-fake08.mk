2d9f2d62782733f99fe8b1727480a96f6e05f5d2
Lee <lee.hancock4@ntlworld.com>
2022-11-12 15:21:55 +0000

    add libretro-fake08 a Pico-8 player

---
+++ package/batocera/emulators/retroarch/libretro/libretro-fake08/libretro-fake08.mk
@@ -0,0 +1,23 @@
+################################################################################
+#
+# libretro-fake08
+#
+################################################################################
+# Version: Commits on Oct 14, 2022
+LIBRETRO_FAKE08_VERSION = 74fc93696a439eb7d697af44216784b0f5995039
+LIBRETRO_FAKE08_SITE = https://github.com/jtothebell/fake-08.git
+LIBRETRO_FAKE08_SITE_METHOD = git
+LIBRETRO_FAKE08_GIT_SUBMODULES = YES
+LIBRETRO_FAKE08_LICENSE = MIT
+
+define LIBRETRO_FAKE08_BUILD_CMDS
+	$(TARGET_CONFIGURE_OPTS) $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C \
+	$(@D)/platform/libretro platform="unix"
+endef
+
+define LIBRETRO_FAKE08_INSTALL_TARGET_CMDS
+	$(INSTALL) -D $(@D)/platform/libretro/fake08_libretro.so \
+		$(TARGET_DIR)/usr/lib/libretro/fake08_libretro.so
+endef
+
+$(eval $(generic-package))
