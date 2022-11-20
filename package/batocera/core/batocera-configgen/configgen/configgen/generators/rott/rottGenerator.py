35c25157dfdb376da5997907c699f6d868da8a9c
Lee <lee.hancock4@ntlworld.com>
2022-11-20 13:30:07 +0000

    switch rott to sdl2 port

--- package/batocera/core/batocera-configgen/configgen/configgen/generators/rott/rottGenerator.py
+++ package/batocera/core/batocera-configgen/configgen/configgen/generators/rott/rottGenerator.py
@@ -10,9 +10,4 @@ class RottGenerator(Generator):
     def generate(self, system, rom, playersControllers, guns, gameResolution):
         commandArray = ["rott-darkwar"]
 
-        return Command.Command(
-            array=commandArray,
-            env={
-                'SDL_AUTO_UPDATE_JOYSTICKS': '0',
-                'SDL_MOUSE_RELATIVE_SPEED_SCALE': '2.0'
-            })
+        return Command.Command(array=commandArray)
--- package/batocera/ports/rott/rott.mk
+++ package/batocera/ports/rott/rott.mk
@@ -3,25 +3,20 @@
 # rott
 #
 ################################################################################
-# Version: Commits on Sept 21, 2020
-ROTT_VERSION = 72ce197667478745d9a0ed843aa77af45503149a
-ROTT_SITE = $(call github,zerojay,RoTT,$(ROTT_VERSION))
+# Version: Commits on Nov 23, 2021
+ROTT_VERSION = 6989f9535256539dcddeac75fd4e20a16a13e8a4
+ROTT_SITE = $(call github,fabiangreffrath,rott,$(ROTT_VERSION))
 
-ROTT_DEPENDENCIES = sdl sdl_mixer
+ROTT_DEPENDENCIES = sdl2 sdl2_mixer
 ROTT_LICENSE = GPLv2
-
-define ROTT_BUILD_CMDS
-		$(TARGET_CONFIGURE_OPTS) $(MAKE) \
-		CPP="$(TARGET_CPP)" CXX="$(TARGET_CXX)" CC="$(TARGET_CC) -fPIC" \
-		AS="$(TARGET_CC)" LD="$(TARGET_LD)" STRIP="$(TARGET_STRIP)" \
-		-C $(@D) -f Makefile rott-darkwar
-endef
+ROTT_AUTORECONF = YES
+ROTT_CONF_OPTS += --enable-datadir=/userdata/roms/ports/rott
 
 define ROTT_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 0755 $(@D)/rott-darkwar -D $(TARGET_DIR)/usr/bin/rott-darkwar
+	$(INSTALL) -m 0755 $(@D)/rott/rott -D $(TARGET_DIR)/usr/bin/rott-darkwar
 	# evmap config
 	mkdir -p $(TARGET_DIR)/usr/share/evmapy
 	cp $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/ports/rott/rott.keys $(TARGET_DIR)/usr/share/evmapy/rott.keys
 endef
 
-$(eval $(generic-package))
+$(eval $(autotools-package))
