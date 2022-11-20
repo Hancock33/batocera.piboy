17a68c22c52a587cb7354a76f6e6e6d06c408daf
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2022-11-20 11:50:12 +0000

    start open jedi fullscreen

--- package/batocera/core/batocera-configgen/configgen/configgen/generators/openjkja/openjkjaGenerator.py
+++ package/batocera/core/batocera-configgen/configgen/configgen/generators/openjkja/openjkjaGenerator.py
@@ -5,6 +5,7 @@ from os import path
 import Command
 from generators.Generator import Generator
 import controllersConfig
+import utils.videoMode as videoMode
 
 jkja_src = "/usr/share/game_assets/openjkja"
 jkja_dst = "/userdata/roms/ports/openjkja"
@@ -12,7 +13,8 @@ jkja_dst = "/userdata/roms/ports/openjkja"
 class OpenjkjaGenerator(Generator):
 
     def generate(self, system, rom, playersControllers, guns, gameResolution):
-        commandArray = ["openjk_sp", "+set", "fs_basepath", "/userdata/roms/ports/openjkja", "+set", "r_mode", "-1", "+set", "r_customwidth", "640", "+set", "r_customheight", "480", "+set", "r_swapInterval", "1"]
+        gameResolution = videoMode.getCurrentResolution()
+        commandArray = ["openjk_sp", "+set", "fs_basepath", "/userdata/roms/ports/openjkja", "+set", "r_mode", "-1", "+set" ,"r_fullscreen", "1", "+set", "r_customwidth", str(gameResolution["width"]), "+set", "r_customheight",str(gameResolution["height"]), "+set", "r_swapInterval", "1"]
 
         shutil.copytree(jkja_src, jkja_dst, dirs_exist_ok=True)
 
