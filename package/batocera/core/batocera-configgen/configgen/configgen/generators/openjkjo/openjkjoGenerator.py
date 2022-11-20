0b319f923525b9a991b789ca348d7bdec6674251
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2022-11-20 12:11:01 +0000

    start rtcw in full screen

--- package/batocera/core/batocera-configgen/configgen/configgen/generators/openjkjo/openjkjoGenerator.py
+++ package/batocera/core/batocera-configgen/configgen/configgen/generators/openjkjo/openjkjoGenerator.py
@@ -13,6 +13,7 @@ jkjo_dst = "/userdata/roms/ports/openjkjo"
 class OpenjkjoGenerator(Generator):
 
     def generate(self, system, rom, playersControllers, guns, gameResolution):
+        gameResolution = videoMode.getCurrentResolution()
         commandArray = ["openjo_sp", "+set", "fs_basepath", "/userdata/roms/ports/openjkjo", "+set", "r_mode", "-1", "+set" , "r_fullscreen", "1", "+set", "r_customwidth", str(gameResolution["width"]), "+set", "r_customheight",str(gameResolution["height"]), "+set", "r_swapInterval", "1", "+set", "com_jk2", "1"]
 
         shutil.copytree(jkjo_src, jkjo_dst, dirs_exist_ok=True)
