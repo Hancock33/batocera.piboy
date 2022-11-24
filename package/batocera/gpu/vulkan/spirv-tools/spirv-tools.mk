233c6721527ada81294b8744da0cb0965afb031a
Lee <lee.hancock4@ntlworld.com>
2022-11-24 22:00:43 +0000

    build fixes

--- package/batocera/gpu/vulkan/spirv-tools/spirv-tools.mk
+++ package/batocera/gpu/vulkan/spirv-tools/spirv-tools.mk
@@ -3,8 +3,8 @@
 # spirv-tools
 #
 ################################################################################
-# Version: Commits on Nov 24, 2022
-SPIRV_TOOLS_VERSION = f33d152400c5a8acd9b8b00efd91a59328388d9a
+# Version: Commits on Nov 18, 2022
+SPIRV_TOOLS_VERSION = 46ca66e6991f16c89e17ebc9b86995143be2c706
 SPIRV_TOOLS_SITE = $(call github,KhronosGroup,SPIRV-Tools,$(SPIRV_TOOLS_VERSION))
 SPIRV_TOOLS_DEPENDENCIES = spirv-headers
 SPIRV_TOOLS_INSTALL_STAGING = YES
