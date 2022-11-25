a106b22ff171501368539b490bf8eb4be07bb5c8
Lee <lee.hancock4@ntlworld.com>
2022-11-25 18:24:49 +0000

    libretro-citra build fix

--- package/batocera/emulators/retroarch/libretro/libretro-citra/libretro-citra.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-citra/libretro-citra.mk
@@ -11,16 +11,13 @@ LIBRETRO_CITRA_GIT_SUBMODULES=YES
 LIBRETRO_CITRA_DEPENDENCIES = boost
 LIBRETRO_CITRA_LICENSE = GPLv2+
 
-# Should be set when the package cannot be built inside the source tree but needs a separate build directory.
-LIBRETRO_CITRA_SUPPORTS_IN_SOURCE_BUILD = NO
-
-LIBRETRO_CITRA_CONF_OPTS  = -DCMAKE_BUILD_TYPE=Release -DENABLE_LIBRETRO=ON \
-    -DENABLE_QT=OFF -DENABLE_SDL2=OFF -DENABLE_WEB_SERVICE=OFF \
-	-DBUILD_SHARED_LIBS=FALSE
+define LIBRETRO_CITRA_BUILD_CMDS
+	$(TARGET_CONFIGURE_OPTS) $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D)/ -f Makefile platform="unix" HAVE_FFMPEG_STATIC=0
+endef
 
 define LIBRETRO_CITRA_INSTALL_TARGET_CMDS
-	$(INSTALL) -D $(@D)/buildroot-build/src/citra_libretro/citra_libretro.so \
+	$(INSTALL) -D $(@D)/citra_libretro.so \
 		$(TARGET_DIR)/usr/lib/libretro/citra_libretro.so
 endef
 
-$(eval $(cmake-package))
+$(eval $(generic-package))
