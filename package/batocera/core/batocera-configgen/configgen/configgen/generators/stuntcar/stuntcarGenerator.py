aa3b8dd2de27a188c7f091404abf5814dd75418d
Lee <lee.hancock4@ntlworld.com>
2022-11-29 16:54:07 +0000

    fix stuntcar

--- package/batocera/core/batocera-configgen/configgen/configgen/generators/stuntcar/stuntcarGenerator.py
+++ package/batocera/core/batocera-configgen/configgen/configgen/generators/stuntcar/stuntcarGenerator.py
@@ -8,8 +8,8 @@ import os
 class StuntcarGenerator(Generator):
 
     def generate(self, system, rom, playersControllers, guns, gameResolution):
-        os.chdir("/usr/share/stuntcar", "-f")
-        commandArray = ["/usr/share/stuntcar/stuntcar"]
+        os.chdir("/usr/share/stuntcar")
+        commandArray = ["/usr/share/stuntcar/stuntcar", "-f"]
 
         return Command.Command(
             array=commandArray,
