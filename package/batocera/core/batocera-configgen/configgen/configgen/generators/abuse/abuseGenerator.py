ede2cc47e128522a950da763d793b081b2945d2e
Lee <lee.hancock4@ntlworld.com>
2022-11-29 17:35:28 +0000

    update abuse input

--- package/batocera/core/batocera-configgen/configgen/configgen/generators/abuse/abuseGenerator.py
+++ package/batocera/core/batocera-configgen/configgen/configgen/generators/abuse/abuseGenerator.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
-
+import os
+from os import path
 import Command
 from generators.Generator import Generator
 import controllersConfig
 
-
 class AbuseGenerator(Generator):
+    if os.path.isfile('/tmp/piboy'):
+        os.system('piboy_keys abuse.keys')
 
     def generate(self, system, rom, playersControllers, guns, gameResolution):
         commandArray = ["abuse", "-datadir", "/usr/share/abuse"]
