5ef1f8a81b45bc80fb59e45dbecb5c5553466325
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2022-12-06 18:32:37 +0000

    add doom3 games

--- package/batocera/core/batocera-configgen/configgen/configgen/generators/doom3/doom3Generator.py
+++ package/batocera/core/batocera-configgen/configgen/configgen/generators/doom3/doom3Generator.py
@@ -13,6 +13,11 @@ class Doom3Generator(Generator):
         gameResolution = videoMode.getCurrentResolution()
         commandArray = ["dhewm3", "+set", "fs_basepath", "/userdata/roms/ports/doom3", "+set", "r_mode", "-1", "+set" , "r_fullscreen", "1", "+set", "r_customWidth", str(gameResolution["width"]), "+set", "r_customHeight",str(gameResolution["height"])]
 
+        if (rom.lower()).endswith('demo'):
+            commandArray.extend(['+set', 'fs_game_base', 'demo'])
+        elif (rom.lower()).endswith('d3xp'):
+            commandArray.extend(['+set', 'fs_game_base', 'd3xp'])
+
         return Command.Command(
             array=commandArray,
             env={
