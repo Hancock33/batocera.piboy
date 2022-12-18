6285dc0c798d98e038893053a2e3545831f43db7
Lee <lee.hancock4@ntlworld.com>
2022-12-18 15:03:18 +0000

    fix build (patch updates)

--- package/batocera/emulators/pcsx2-avx2/pcsx2-avx2.mk
+++ package/batocera/emulators/pcsx2-avx2/pcsx2-avx2.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 # Version: Commits on Dec 18, 2022
-PCSX2_AVX2_VERSION = $(PCSX2_VERSION)
+PCSX2_AVX2_VERSION = v1.7.3732
 PCSX2_AVX2_SITE = https://github.com/pcsx2/pcsx2.git
 PCSX2_AVX2_SITE_METHOD = git
 PCSX2_AVX2_GIT_SUBMODULES = YES
