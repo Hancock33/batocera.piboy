d8a1edc8db6730e9e7a6faf70781d8d253b18488
Lee <lee.hancock4@ntlworld.com>
2022-12-25 19:05:06 +0000

    add f2b options

--- package/batocera/core/batocera-configgen/configgen/configgen/generators/f2bgl/f2bglGenerator.py
+++ package/batocera/core/batocera-configgen/configgen/configgen/generators/f2bgl/f2bglGenerator.py
@@ -9,7 +9,35 @@ class F2bglGenerator(Generator):
 
     def generate(self, system, rom, playersControllers, guns, gameResolution):
         os.chdir("/userdata/roms/ports/f2bgl")
-        commandArray = ["f2bgl", "--datapath=/userdata/roms/ports/f2bgl", "--savepath=/userdata/saves/f2bgl"]
+        commandArray = ["f2bgl", "--fullscreen", "--datapath=/userdata/roms/ports/f2bgl", "--savepath=/userdata/saves/f2bgl"]
+
+        if system.isOptSet('f2b_fog'):
+            if system.config['f2b_fog'] == '0':
+                commandArray.extend(['--no-fog'])
+
+        if system.isOptSet('f2b_gouraud'):
+            if system.config['f2b_gouraud'] == '0':
+                commandArray.extend(['--no-gouraud'])
+
+        if system.isOptSet('f2b_filter'):
+            strf2b_filter = '--texturefilter=' + system.config['f2b_filter']
+            commandArray.extend([strf2b_filter])
+
+        if system.isOptSet('f2b_scaler'):
+            strf2b_scaler = '--texturescaler=' + system.config['f2b_scaler']
+            commandArray.extend([strf2b_scaler])
+
+        if system.isOptSet('f2b_sub'):
+            if system.config['f2b_sub'] == '1':
+                commandArray.extend(['--subtitles'])
+
+        if system.isOptSet('f2b_lang'):
+            strf2b_lang = '--language=' + system.config['f2b_lang'] + ' --voice=' + system.config['f2b_lang']
+            commandArray.extend([strf2b_lang])
+
+        if system.isOptSet('f2b_level'):
+            strf2b_level = '--level=' + system.config['f2b_level']
+            commandArray.extend([strf2b_level])
 
         return Command.Command(
             array=commandArray,
