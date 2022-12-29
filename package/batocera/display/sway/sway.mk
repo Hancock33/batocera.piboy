b503b8c1c1d71722df52436b76e08d18fffdfa80
Lee <lee.hancock4@ntlworld.com>
2022-12-29 00:09:11 +0000

    Revert "update sway"
    This reverts commit 90122f648894e89344fc8dfee26091a59804f48b.

--- package/batocera/display/sway/sway.mk
+++ package/batocera/display/sway/sway.mk
@@ -4,11 +4,11 @@
 #
 ################################################################################
 
-SWAY_VERSION = 1.8
+SWAY_VERSION = 1.7
 SWAY_SITE = $(call github,swaywm,sway,$(SWAY_VERSION))
 SWAY_LICENSE = MIT
 SWAY_LICENSE_FILES = LICENSE
-SWAY_DEPENDENCIES = wlroots cairo pango libglib2 json-c grim pcre2
+SWAY_DEPENDENCIES = wlroots cairo pango libglib2 json-c grim
 
 SWAY_CONF_OPTS = -Ddefault-wallpaper=false \
                 -Dzsh-completions=false \
