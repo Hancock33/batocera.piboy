44c6cfce2b3aab194dac4c045956bc3b582eba85
Lee <lee.hancock4@ntlworld.com>
2022-12-31 16:36:50 +0000

    update doom clones

--- package/batocera/ports/doom/gzdoom/gzdoom.mk
+++ package/batocera/ports/doom/gzdoom/gzdoom.mk
@@ -3,15 +3,11 @@
 # gzdoom
 #
 ################################################################################
-# Version: Commits on Dec 21, 2022
-GZDOOM_VERSION = adfef56fd8c56c490488cb9909e2574e0b327f60
+# Version: Commits on Dec 31, 2022
+GZDOOM_VERSION = g4.10.0
 GZDOOM_SITE = $(call github,ZDoom,gzdoom,$(GZDOOM_VERSION))
 GZDOOM_LICENSE = GPL-3.0
 GZDOOM_DEPENDENCIES = sdl2 bzip2 fluidsynth openal mesa3d libglu libglew host-zmusic zmusic
-
-ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_BCM2711),y)
-    GZDOOM_VERSION = 4.9
-endif
 
 GZDOOM_SUPPORTS_IN_SOURCE_BUILD = NO
 GZDOOM_CONF_OPTS += -DCMAKE_BUILD_TYPE=Release
@@ -32,9 +28,15 @@ endif
 
 ifeq ($(BR2_PACKAGE_BATOCERA_GLES2)$(BR2_PACKAGE_BATOCERA_GLES3),y)
     GZDOOM_CONF_OPTS += -DHAVE_GLES2=ON
+	GZDOOM_POST_PATCH_HOOKS += GZDOOM_PATCH_USE_GLES2
 else
     GZDOOM_CONF_OPTS += -DHAVE_GLES2=OFF
 endif
+
+define GZDOOM_PATCH_USE_GLES2
+	$(SED) 's%#define USE_GLES2 0%#define USE_GLES2 1%' $(@D)/src/common/rendering/gles/gles_system.h
+	$(SED) '1i #define __ANDROID__' $(@D)/src/common/rendering/gles/gles_system.cpp
+endef
 
 define GZDOOM_INSTALL
 	cp $(@D)/buildroot-build/libraries/discordrpc/src/libdiscord-rpc.so $(TARGET_DIR)/usr/lib
