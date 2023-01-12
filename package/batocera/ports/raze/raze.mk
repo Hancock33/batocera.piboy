43da9106c33961663f13ecff7a39e4c070ec61b9
Lee <lee.hancock4@ntlworld.com>
2023-01-12 19:40:43 +0000

    update br + raze

--- package/batocera/ports/raze/raze.mk
+++ package/batocera/ports/raze/raze.mk
@@ -3,8 +3,8 @@
 # raze
 #
 ################################################################################
-# Version: Commits on Dec 17, 2022
-RAZE_VERSION = 1.6.1
+# Version: Commits on Jan 07, 2022
+RAZE_VERSION = 1.6.2
 RAZE_SITE = $(call github,ZDoom,Raze,$(RAZE_VERSION))
 RAZE_LICENSE = GPLv2
 RAZE_DEPENDENCIES = sdl2 bzip2 fluidsynth openal mesa3d libglu libglew zmusic gzdoom
