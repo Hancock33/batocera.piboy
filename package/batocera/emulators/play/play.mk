51c4628e308086c848f1ef20175f67db69f4da50
Lee <lee.hancock4@ntlworld.com>
2023-01-20 06:17:49 +0000

    update locales

--- package/batocera/emulators/play/play.mk
+++ package/batocera/emulators/play/play.mk
@@ -3,8 +3,8 @@
 # play
 #
 ################################################################################
-# Version: Commits on Jan 13, 2023
-PLAY_VERSION = 94f5271c180ed5c600af9a615e0a83202e5fe02a
+# Version: Commits on Jan 10, 2023
+PLAY_VERSION = 70a126e3ed6119792a5a9bea110a84eeddff53aa
 PLAY_SITE = https://github.com/jpd002/Play-.git
 PLAY_LICENSE = BSD
 PLAY_DEPENDENCIES = qt5base qt5x11extras xserver_xorg-server libglew vulkan-headers vulkan-loader
