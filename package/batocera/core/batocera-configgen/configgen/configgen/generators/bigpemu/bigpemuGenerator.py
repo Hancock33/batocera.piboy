ee5dd137c17a8d51e65305bfa7da6ba5bd424c96
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2023-01-21 11:56:33 +0000

    bigpemu is now 64bit

--- package/batocera/core/batocera-configgen/configgen/configgen/generators/bigpemu/bigpemuGenerator.py
+++ package/batocera/core/batocera-configgen/configgen/configgen/generators/bigpemu/bigpemuGenerator.py
@@ -34,9 +34,9 @@ class BigPEmuGenerator(Generator):
         # install windows libraries required
         if not os.path.exists(wineprefix + "/d3dcompiler_43.done"):
             cmd = ["/usr/wine/winetricks", "d3dcompiler_43"]
-            env = {"LD_LIBRARY_PATH": "/lib:/usr/lib:/lib32:/usr/wine/lutris/lib/wine", "WINEPREFIX": wineprefix }
+            env = {"LD_LIBRARY_PATH": "/lib:/usr/lib:/usr/wine/proton/lib/wine", "WINEPREFIX": wineprefix }
             env.update(os.environ)
-            env["PATH"] = "/usr/wine/lutris/bin:/bin:/usr/bin"
+            env["PATH"] = "/usr/wine/proton/bin:/bin:/usr/bin"
             eslog.debug(f"command: {str(cmd)}")
             proc = subprocess.Popen(cmd, env=env, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             out, err = proc.communicate()
@@ -48,9 +48,9 @@ class BigPEmuGenerator(Generator):
 
         if not os.path.exists(wineprefix + "/d3dx9_43.done"):
             cmd = ["/usr/wine/winetricks", "d3dx9_43"]
-            env = {"LD_LIBRARY_PATH": "/lib:/usr/lib:/lib32:/usr/wine/lutris/lib/wine", "WINEPREFIX": wineprefix }
+            env = {"LD_LIBRARY_PATH": "/lib:/usr/lib:/usr/wine/proton/lib/wine", "WINEPREFIX": wineprefix }
             env.update(os.environ)
-            env["PATH"] = "/usr/wine/lutris/bin:/bin:/usr/bin"
+            env["PATH"] = "/usr/wine/proton/bin:/bin:/usr/bin"
             eslog.debug(f"command: {str(cmd)}")
             proc = subprocess.Popen(cmd, env=env, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             out, err = proc.communicate()
@@ -62,9 +62,9 @@ class BigPEmuGenerator(Generator):
     
         if not os.path.exists(wineprefix + "/d3dx9.done"):
             cmd = ["/usr/wine/winetricks", "d3dx9"]
-            env = {"LD_LIBRARY_PATH": "/lib:/usr/lib:/lib32:/usr/wine/lutris/lib/wine", "WINEPREFIX": wineprefix }
+            env = {"LD_LIBRARY_PATH": "/lib:/usr/lib:/usr/wine/proton/lib/wine", "WINEPREFIX": wineprefix }
             env.update(os.environ)
-            env["PATH"] = "/usr/wine/lutris/bin:/bin:/usr/bin"
+            env["PATH"] = "/usr/wine/proton/bin:/bin:/usr/bin"
             eslog.debug(f"command: {str(cmd)}")
             proc = subprocess.Popen(cmd, env=env, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             out, err = proc.communicate()
@@ -78,14 +78,15 @@ class BigPEmuGenerator(Generator):
         # /userdata/saves/bigpemu-bottle/drive_c/users/root/AppData/Roaming/BigPEmu/BigPEmuConfig.bigpcfg
 
         # now run the emulator
-        commandArray = ["/usr/wine/lutris/bin/wine", "/userdata/saves/bigpemu-bottle/bigpemu/BigPEmu.exe", rom]
+        commandArray = ["/usr/wine/proton/bin/wine64", "/userdata/saves/bigpemu-bottle/bigpemu/BigPEmu.exe", rom]
         # we use a 64-bit wine bottle
         return Command.Command(
             array=commandArray,
             env={
                 "WINEPREFIX": wineprefix,
-                "LD_LIBRARY_PATH": "/lib:/usr/lib:/lib32:/usr/wine/lutris/lib/wine",
-                "LIBGL_DRIVERS_PATH": "/usr/lib/dri:/lib32/dri",
-                "SPA_PLUGIN_DIR": "/usr/lib/spa-0.2:/lib32/spa-0.2",
-                "PIPEWIRE_MODULE_DIR": "/usr/lib/pipewire-0.3:/lib32/pipewire-0.3"
+                "LD_LIBRARY_PATH": "/lib:/usr/lib:/usr/wine/proton/lib/wine",
+                "LIBGL_DRIVERS_PATH": "/usr/lib/dri",
+                "SPA_PLUGIN_DIR": "/usr/lib/spa-0.2",
+                "PIPEWIRE_MODULE_DIR": "/usr/lib/pipewire-0.3",
+                "SDL_GAMECONTROLLERCONFIG": controllersConfig.generateSdlGameControllerConfig(playersControllers)
             })
