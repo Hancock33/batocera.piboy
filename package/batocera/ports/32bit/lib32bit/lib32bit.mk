2739a1c4cc392d92afe9a5798f58708f4ffa1835
Lee <lee.hancock4@ntlworld.com>
2023-01-21 08:04:57 +0000

    update 32bit install

--- package/batocera/ports/32bit/lib32bit/lib32bit.mk
+++ package/batocera/ports/32bit/lib32bit/lib32bit.mk
@@ -7,9 +7,12 @@ LIB32BIT_VERSION = 20230121
 LIB32BIT_SOURCE = batocera-rpi4-32bit-libs-$(LIB32BIT_VERSION).tar.xz
 LIB32BIT_SITE = https://github.com/Hancock33/batocera-rp4-32bit-libs/releases/download/$(LIB32BIT_VERSION)
 
-define LIB32BIT_INSTALL_TARGET_CMDS
+define LIB32BIT_EXTRACT_CMDS
 	tar xf $(DL_DIR)/$(LIB32BIT_DL_SUBDIR)/$(LIB32BIT_SOURCE) -C $(TARGET_DIR)/usr
 	ln -sf /usr/lib32/ld-linux-armhf.so.3 $(TARGET_DIR)/lib
+endef
+
+define LIB32BIT_INSTALL_TARGET_CMDS
 	# evmap config
 	mkdir -p $(TARGET_DIR)/usr/share/evmapy/piboy
 	cp $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/ports/32bit/lib32bit/sorr.keys $(TARGET_DIR)/usr/share/evmapy/sorr.keys
