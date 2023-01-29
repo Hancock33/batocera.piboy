f71ce51507efef9d9be7266c98c139b445cdcfc5
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2023-01-29 14:57:05 +0000

    update libraries

--- package/batocera/libraries/libopenmpt/libopenmpt.mk
+++ package/batocera/libraries/libopenmpt/libopenmpt.mk
@@ -3,8 +3,8 @@
 # libopenmpt
 #
 ################################################################################
-# Version: Commits on Sept 25, 2022
-LIBOPENMPT_VERSION = 0.6.6
+# Version: Commits on Jan 01, 2023
+LIBOPENMPT_VERSION = 0.6.7
 LIBOPENMPT_SITE =  https://lib.openmpt.org/files/libopenmpt/src
 LIBOPENMPT_SOURCE = libopenmpt-$(LIBOPENMPT_VERSION)+release.autotools.tar.gz
 LIBOPENMPT_LICENSE = LGPLv3
--- package/batocera/libraries/python-hidapi/python-hidapi.mk
+++ package/batocera/libraries/python-hidapi/python-hidapi.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-PYTHON_HIDAPI_VERSION = 0.12.0.post2
+PYTHON_HIDAPI_VERSION = 0.13.1
 PYTHON_HIDAPI_SOURCE = hidapi-$(PYTHON_HIDAPI_VERSION).tar.gz
 PYTHON_HIDAPI_SETUP_TYPE = setuptools
-PYTHON_HIDAPI_SITE = https://pypi.python.org/packages/source/h/hidapi
+PYTHON_HIDAPI_SITE = https://files.pythonhosted.org/packages/78/0a/d71f35a8dcbe88dab21cd668a62b688ea6dd45872feba45a97efd0452c19
 PYTHON_HIDAPI_LICENSE = GPLv3
 PYTHON_HIDAPI_LICENSE_FILES = LICENSE-gpl3.txt
 PYTHON_HIDAPI_DEPENDENCIES = libusb udev
