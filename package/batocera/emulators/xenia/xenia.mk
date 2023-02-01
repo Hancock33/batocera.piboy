fa4000d5e135d16bff4f223fa7f6c1f1a835855d
Lee <lee.hancock4@ntlworld.com>
2023-02-01 06:18:57 +0000

    build fixes

--- package/batocera/emulators/xenia/xenia.mk
+++ package/batocera/emulators/xenia/xenia.mk
@@ -3,8 +3,8 @@
 # xenia
 #
 ################################################################################
-# Version: Commits on Jan 24, 2022
-XENIA_VERSION = cba4ca0e31b8bed3e0f05f176be42948b80c2671
+# Version: Commits on Jan 30, 2022
+XENIA_VERSION = v1.0.2786-master
 XENIA_SOURCE = xenia_master.zip
 XENIA_SITE = https://github.com/xenia-project/release-builds-windows/releases/download/$(XENIA_VERSION)
 XENIA_LICENSE = BSD
