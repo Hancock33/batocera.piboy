55a6f639491613c7e107c5fefd6c4ebeb081be2b
Lee <lee.hancock4@ntlworld.com>
2023-02-03 06:28:41 +0000

    update locales

--- package/batocera/ports/ecwolf/ecwolf.mk
+++ package/batocera/ports/ecwolf/ecwolf.mk
@@ -3,8 +3,8 @@
 # ecwolf
 #
 ################################################################################
-# Version: Commits on Feb 01, 2023
-ECWOLF_VERSION = 72c5f2210986842f406639f1b477caca40ed68ed
+# Version: Commits on Feb 02, 2023 (1.4.1)
+ECWOLF_VERSION = 8cf8d23a6da3bc0f21a03bf8bb6efadd613902f4
 ECWOLF_SITE = https://bitbucket.org/ecwolf/ecwolf.git
 ECWOLF_SITE_METHOD=git
 ECWOLF_GIT_SUBMODULES=YES
