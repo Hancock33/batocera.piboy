d9ca244c8607e78e16af23ad6670ca14c2fe9dbd
Lee <lee.hancock4@ntlworld.com>
2023-02-05 10:28:36 +0000

    patch gzdoom gles rendering

--- package/batocera/ports/doom/gzdoom/gzdoom.mk
+++ package/batocera/ports/doom/gzdoom/gzdoom.mk
@@ -11,7 +11,7 @@ GZDOOM_DEPENDENCIES = sdl2 bzip2 fluidsynth openal mesa3d libglu libglew host-zm
 
 GZDOOM_SUPPORTS_IN_SOURCE_BUILD = NO
 GZDOOM_CONF_OPTS += -DCMAKE_BUILD_TYPE=Release
-GZDOOM_CONF_OPTS += -DCMAKE_RULE_MESSAGES=OFF
+GZDOOM_CONF_OPTS += -DBUILD_SHARED_LIBS=OFF
 GZDOOM_CONF_OPTS += -DNO_GTK=ON
 GZDOOM_CONF_OPTS += -DFORCE_CROSSCOMPILE=ON
 GZDOOM_CONF_OPTS += -DIMPORT_EXECUTABLES=$(HOST_GZDOOM_BUILDDIR)/ImportExecutables.cmake
@@ -26,18 +26,11 @@ endif
 
 ifeq ($(BR2_PACKAGE_BATOCERA_GLES2)$(BR2_PACKAGE_BATOCERA_GLES3),y)
     GZDOOM_CONF_OPTS += -DHAVE_GLES2=ON
-	GZDOOM_POST_PATCH_HOOKS += GZDOOM_PATCH_USE_GLES2
 else
     GZDOOM_CONF_OPTS += -DHAVE_GLES2=OFF
 endif
 
-define GZDOOM_PATCH_USE_GLES2
-	$(SED) 's%#define USE_GLES2 0%#define USE_GLES2 1%' $(@D)/src/common/rendering/gles/gles_system.h
-	$(SED) '1i #define __ANDROID__' $(@D)/src/common/rendering/gles/gles_system.cpp
-endef
-
 define GZDOOM_INSTALL
-	cp $(@D)/buildroot-build/libraries/discordrpc/src/libdiscord-rpc.so $(TARGET_DIR)/usr/lib
 	$(TARGET_STRIP) $(TARGET_DIR)/usr/bin/gzdoom
 	mv $(TARGET_DIR)/usr/bin/gzdoom $(TARGET_DIR)/usr/share/gzdoom/gzdoom
 endef
