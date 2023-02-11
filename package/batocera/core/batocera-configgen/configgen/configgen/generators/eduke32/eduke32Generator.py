864fbbc06493ade62c9a7e24413ba382d511d2c1
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2023-02-11 14:46:09 +0000

    update eduke generator

--- package/batocera/core/batocera-configgen/configgen/configgen/generators/eduke32/eduke32Generator.py
+++ package/batocera/core/batocera-configgen/configgen/configgen/generators/eduke32/eduke32Generator.py
@@ -1,15 +1,26 @@
 #!/usr/bin/env python
+import batoceraFiles
+import Command
+from configparser import ConfigParser
+import controllersConfig
+from generators.Generator import Generator
 import os
 from os import path
-import Command
-from generators.Generator import Generator
-import controllersConfig
+from utils.buildargs import parse_args
 
 class Eduke32Generator(Generator):
 
     def generate(self, system, rom, playersControllers, guns, gameResolution):
-        commandArray = ["eduke32", rom, "-game_dir", os.path.dirname(os.path.abspath(rom)), "-nologo" if system.getOptBoolean("nologo") else ""]
-        os.chdir(os.path.dirname(os.path.abspath(rom)))
+        os.chdir("/userdata/roms/ports/eduke32")
+
+        rtsfile = rom.replace('.GRP', '.RTS').replace('.grp', '.rts').replace('.EDUKE', '.RTS').replace('.eduke', '.rts')
+        if (rom.lower()).endswith('eduke'):
+            edukegroup=open(rom).readline().rstrip()
+            edukerom=rom.replace('.eduke', '.GRP').replace('.EDUKE', '.GRP')
+
+            commandArray = ["eduke32", edukerom, "-game_dir", os.path.dirname(os.path.abspath(rom)), "-g", edukegroup, "-rts", rtsfile, "-nologo" if system.getOptBoolean("nologo") else ""]
+        else:
+            commandArray = ["eduke32", rom, "-game_dir", os.path.dirname(os.path.abspath(rom)), "-rts", rtsfile, "-nologo" if system.getOptBoolean("nologo") else ""]
 
         if os.path.isfile('/tmp/piboy') and not os.path.isfile('/tmp/piboy_xrs'):
             os.system('piboy_keys eduke32.keys')
