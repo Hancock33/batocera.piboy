bf94c8743c4ad137c9bed694bf3031f629d42fe6
Lee <lee.hancock4@ntlworld.com>
2023-02-18 10:36:40 +0000

    add fallout 1

--- package/batocera/core/batocera-configgen/configgen/configgen/generators/fallout2/fallout2Generator.py
+++ package/batocera/core/batocera-configgen/configgen/configgen/generators/fallout2/fallout2Generator.py
@@ -5,11 +5,21 @@ from generators.Generator import Generator
 import controllersConfig
 import os
 
+from utils.logger import get_logger
+eslog = get_logger(__name__)
+
 class Fallout2Generator(Generator):
 
     def generate(self, system, rom, playersControllers, guns, gameResolution):
-        os.chdir("/userdata/roms/ports/fallout2")
-        commandArray = ["fallout2-ce"]
+        romdir = os.path.dirname(os.path.abspath(rom))
+        eslog.info(f"Game directory {str(romdir)}")
+
+        if (romdir.__contains__("/fallout1")):
+            os.chdir("/userdata/roms/ports/fallout1")
+            commandArray = ["fallout-ce"]
+        else:
+            os.chdir("/userdata/roms/ports/fallout2")
+            commandArray = ["fallout2-ce"]
 
         return Command.Command(
             array=commandArray,
