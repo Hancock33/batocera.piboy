07eae7169080df36acb2974cbc4c59c6456f5802
Lee <lee.hancock4@ntlworld.com>
2023-02-20 20:22:38 +0000

    revert cemu + raze

--- package/batocera/ports/raze/raze.mk
+++ package/batocera/ports/raze/raze.mk
@@ -3,8 +3,8 @@
 # raze
 #
 ################################################################################
-# Version: Commits on Feb 20, 2023
-RAZE_VERSION = 07d87940f220e1013fd561ba0fff68b279021f16
+# Version: Commits on Feb 19, 2023
+RAZE_VERSION = 7b05353326519ca974da96ebf0780ba30dc03c76
 RAZE_SITE = $(call github,ZDoom,Raze,$(RAZE_VERSION))
 RAZE_LICENSE = GPLv2
 RAZE_DEPENDENCIES = sdl2 bzip2 fluidsynth openal mesa3d libglu libglew zmusic gzdoom
