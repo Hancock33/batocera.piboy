fcb6a950581bbb2c2419ae31e901e77713672f0e
KeyserSoze1 <6797509+KeyserSoze1@users.noreply.github.com>
2023-03-04 05:03:01 -0500

    Added a music path for the Bermuda port. (#48)

--- package/batocera/core/batocera-configgen/configgen/configgen/generators/bermuda/bermudaGenerator.py
+++ package/batocera/core/batocera-configgen/configgen/configgen/generators/bermuda/bermudaGenerator.py
@@ -9,7 +9,7 @@ class BermudaGenerator(Generator):
 
     def generate(self, system, rom, playersControllers, guns, gameResolution):
         os.chdir("/userdata/roms/ports/bermuda")
-        commandArray = ["bermuda", "--datapath=/userdata/roms/ports/bermuda", "--savepath=/userdata/saves/bermuda", "--fullscreen"]
+        commandArray = ["bermuda", "--datapath=/userdata/roms/ports/bermuda", "--musicpath=/userdata/roms/ports/bermuda/MUSIC", "--savepath=/userdata/saves/bermuda", "--fullscreen"]
 
         if system.isOptSet("bermuda_aspect"):
             if system.config['bermuda_aspect'] == '1':
--- package/batocera/emulationstation/batocera-es-system/roms/ports/bermuda/_Readme.txt
+++ package/batocera/emulationstation/batocera-es-system/roms/ports/bermuda/_Readme.txt
@@ -1,2 +1,2 @@
 This port requires a copy of the original datafiles from the Windows release of Bermuda Syndrome.
-You will need to copy all files from the DATA directory into this directory.
+You will need to copy all files from the DATA directory into this directory. Optional music files should be copied to a MUSIC subfolder of this directory.
