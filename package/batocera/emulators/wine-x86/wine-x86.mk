622f92f1a8c8d42e374c85eefce7249f471f1e1f
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2023-03-19 11:46:03 +0000

    update download location

--- package/batocera/emulators/wine-x86/wine-x86.mk
+++ package/batocera/emulators/wine-x86/wine-x86.mk
@@ -5,7 +5,7 @@
 ################################################################################
 WINE_X86_VERSION = 20230318
 WINE_X86_SOURCE = batocera-x86-32bit-libs-$(WINE_X86_VERSION).tar.xz
-WINE_X86_SITE = https://github.com/Hancock33/batocera-rp4-32bit-libs/releases/download/$(WINE_X86_VERSION)
+WINE_X86_SITE = https://github.com/Hancock33/batocera-32bit-libs/releases/download/$(WINE_X86_VERSION)
 
 define WINE_X86_EXTRACT_CMDS
 	tar xf $(DL_DIR)/$(WINE_X86_DL_SUBDIR)/$(WINE_X86_SOURCE) -C $(TARGET_DIR)
