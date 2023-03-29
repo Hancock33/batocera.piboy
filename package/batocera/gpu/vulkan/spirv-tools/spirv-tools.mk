e3724bf589d3cf49573404d4f8d3351d7155af46
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2023-03-29 21:25:34 +0100

    update spirv

--- package/batocera/gpu/vulkan/spirv-tools/spirv-tools.mk
+++ package/batocera/gpu/vulkan/spirv-tools/spirv-tools.mk
@@ -3,8 +3,8 @@
 # spirv-tools
 #
 ################################################################################
-# Version: Commits on Mar 28, 2023
-SPIRV_TOOLS_VERSION = a0fcd06f8fcc65c4c340e1a72492dc89017af8f5
+# Version: Commits on Mar 29, 2023
+SPIRV_TOOLS_VERSION = 6b72fe20c5d261f9698a82742ae08d9930375981
 SPIRV_TOOLS_SITE = $(call github,KhronosGroup,SPIRV-Tools,$(SPIRV_TOOLS_VERSION))
 SPIRV_TOOLS_DEPENDENCIES = spirv-headers
 SPIRV_TOOLS_INSTALL_STAGING = YES
