6468c73f56e01acd00c6cc814de3d2ca34ca93aa
Lee <lee.hancock4@ntlworld.com>
2023-04-01 08:00:56 +0100

    build fixes

--- package/batocera/libraries/libopenmpt/libopenmpt.mk
+++ package/batocera/libraries/libopenmpt/libopenmpt.mk
@@ -3,9 +3,13 @@
 # libopenmpt
 #
 ################################################################################
-# Version: Commits on Mar 05, 2023
+
+# needed for kodi20-audiodecoder-openmpt
 LIBOPENMPT_VERSION = 0.6.9
-LIBOPENMPT_SITE =  https://lib.openmpt.org/files/libopenmpt/src
-LIBOPENMPT_SOURCE = libopenmpt-$(LIBOPENMPT_VERSION)+release.autotools.tar.gz
-LIBOPENMPT_LICENSE = LGPLv3
+LIBOPENMPT_SOURCE = libopenmpt-${LIBOPENMPT_VERSION}+release.autotools.tar.gz
+LIBOPENMPT_SITE = https://lib.openmpt.org/files/libopenmpt/src
 LIBOPENMPT_INSTALL_STAGING = YES
+LIBOPENMPT_AUTORECONF = YES
+LIBOPENMPT_DEPENDENCIES = host-pkgconf zlib host-doxygen
+
+$(eval $(autotools-package))
