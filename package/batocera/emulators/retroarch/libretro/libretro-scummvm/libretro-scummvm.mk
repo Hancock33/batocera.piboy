b40cab61b3b7f20477cd299db13167834dfe1263
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2023-04-02 15:07:12 +0100

    last up for the weekend, sun and beer time :-)

--- package/batocera/emulators/retroarch/libretro/libretro-scummvm/libretro-scummvm.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-scummvm/libretro-scummvm.mk
@@ -3,8 +3,8 @@
 # libretro-scummvm
 #
 ################################################################################
-# Version: Commits on Apr 01, 2023
-LIBRETRO_SCUMMVM_VERSION = 1af55ed1dc3169042ef15d961b6d3fe77f34ede5
+# Version: Commits on Apr 02, 2023
+LIBRETRO_SCUMMVM_VERSION = 89c64611907368d8625855f378f0f15e9f17d5d9
 LIBRETRO_SCUMMVM_SOURCE = scummvm-$(LIBRETRO_SCUMMVM_VERSION).tar.gz
 LIBRETRO_SCUMMVM_SITE = $(call github,scummvm,scummvm,$(LIBRETRO_SCUMMVM_VERSION))
 LIBRETRO_SCUMMVM_LICENSE = GPLv2
