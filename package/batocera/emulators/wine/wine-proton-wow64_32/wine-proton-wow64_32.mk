c1af47091fc592623e3a674d9116d59623656fae
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2023-04-14 09:44:23 +0100

    some strange happened

--- package/batocera/emulators/wine/wine-proton-wow64_32/wine-proton-wow64_32.mk
+++ package/batocera/emulators/wine/wine-proton-wow64_32/wine-proton-wow64_32.mk
@@ -3,8 +3,8 @@
 # wine-proton-wow64_32
 #
 ################################################################################
-# Version: Commits on Mar 22, 2023 (branch@experimental_7.0)
-WINE_PROTON_WOW64_32_VERSION = 6f2e9039c530927b37c49e10a963af17ca36095d
+# Version: Commits on Apr 13, 2023 (branch@experimental_7.0)
+WINE_PROTON_WOW64_32_VERSION = c24405ee2ef130b7bbc1968da784c03b608cd9f7
 WINE_PROTON_WOW64_32_SOURCE = wine-proton-$(WINE_PROTON_WOW64_32_VERSION).tar.gz
 WINE_PROTON_WOW64_32_SITE = $(call github,ValveSoftware,wine,$(WINE_PROTON_WOW64_32_VERSION))
 WINE_PROTON_WOW64_32_LICENSE = LGPL-2.1+
