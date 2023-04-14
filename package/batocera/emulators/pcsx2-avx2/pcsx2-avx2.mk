3e368b3850545645d1b165b418f9be8f8e9fd4f4
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2023-04-14 21:40:49 +0100

    pcsx2-avx2 use the same source tarball as pcsx2

--- package/batocera/emulators/pcsx2-avx2/pcsx2-avx2.mk
+++ package/batocera/emulators/pcsx2-avx2/pcsx2-avx2.mk
@@ -5,6 +5,7 @@
 ################################################################################
 # Version: Commits on Apr 14, 2023
 PCSX2_AVX2_VERSION = v1.7.4381
+PCSX2_AVX2_SOURCE = pcsx2-$(PCSX2_AVX2_VERSION)-br1.tar.gz
 PCSX2_AVX2_SITE = https://github.com/pcsx2/pcsx2.git
 PCSX2_AVX2_SITE_METHOD = git
 PCSX2_AVX2_GIT_SUBMODULES = YES
