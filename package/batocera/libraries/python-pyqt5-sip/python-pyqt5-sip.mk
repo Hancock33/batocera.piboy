c639cbccdf308aa190e3c0db43cb845c82bfc4b0
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2023-04-14 02:51:53 +0100

    update pyqt

--- package/batocera/libraries/python-pyqt5-sip/python-pyqt5-sip.mk
+++ package/batocera/libraries/python-pyqt5-sip/python-pyqt5-sip.mk
@@ -3,9 +3,9 @@
 # python-pyqt5-sip
 #
 ################################################################################
-PYTHON_PYQT5_SIP_VERSION = 12.11.1
+PYTHON_PYQT5_SIP_VERSION = 12.12.0
 PYTHON_PYQT5_SIP_SOURCE = PyQt5_sip-$(PYTHON_PYQT5_SIP_VERSION).tar.gz
-PYTHON_PYQT5_SIP_SITE = https://files.pythonhosted.org/packages/d2/f4/4c3e5fc1584f5a2964cfc89bf420cfcc59aba0b1f2d08446e2b491a21ed5
+PYTHON_PYQT5_SIP_SITE = https://files.pythonhosted.org/packages/2a/f1/dde13ef47f3075737af449bf833ed4d56a16bd1762c66c674c3cceacd973
 
 PYTHON_PYQT5_SIP_SETUP_TYPE = setuptools
 
