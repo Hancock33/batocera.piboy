e9c974eca201bac94813f5b8d93b88be17bc0b36
Lee <lee.hancock4@ntlworld.com>
2023-04-15 15:09:47 +0100

    melonds use qt6

--- package/batocera/emulators/melonds/melonds.mk
+++ package/batocera/emulators/melonds/melonds.mk
@@ -9,13 +9,14 @@ MELONDS_SITE = https://github.com/melonDS-emu/melonDS.git
 MELONDS_SITE_METHOD=git
 MELONDS_GIT_SUBMODULES=YES
 MELONDS_LICENSE = GPLv2
-MELONDS_DEPENDENCIES = sdl2 qt5base qt5multimedia slirp libepoxy libarchive
+MELONDS_DEPENDENCIES = sdl2 qt6base qt6multimedia slirp libepoxy libarchive
 
 MELONDS_SUPPORTS_IN_SOURCE_BUILD = NO
 
 MELONDS_CONF_OPTS += -DCMAKE_BUILD_TYPE=Release
 MELONDS_CONF_OPTS += -DCMAKE_INSTALL_PREFIX="/usr"
 MELONDS_CONF_OPTS += -DBUILD_SHARED_LIBS=OFF
+MELONDS_CONF_OPTS += -DUSE_QT6=ON
 
 define MELONDS_INSTALL_TARGET_CMDS
     $(INSTALL) -D $(@D)/buildroot-build/melonDS \
