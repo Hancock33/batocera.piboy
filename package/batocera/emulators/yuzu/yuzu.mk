a4121e4a65a51e3d9e0b25ab972328cfc4be9569
Lee <lee.hancock4@ntlworld.com>
2023-04-15 13:41:11 +0100

    yuze - use qt6

--- package/batocera/emulators/yuzu/yuzu.mk
+++ package/batocera/emulators/yuzu/yuzu.mk
@@ -9,8 +9,8 @@ YUZU_SITE = https://github.com/yuzu-emu/yuzu.git
 YUZU_SITE_METHOD=git
 YUZU_GIT_SUBMODULES=YES
 YUZU_LICENSE = GPLv2
-YUZU_DEPENDENCIES = qt5base qt5tools qt5multimedia fmt boost ffmpeg \
-                    zstd zlib libzip lz4 catch2 sdl2 opus
+YUZU_DEPENDENCIES += fmt boost ffmpeg zstd zlib libzip lz4 catch2 sdl2 opus
+YUZU_DEPENDENCIES += qt6base qt6svg qt6tools
 
 YUZU_SUPPORTS_IN_SOURCE_BUILD = NO
 
@@ -23,6 +23,8 @@ YUZU_CONF_OPTS += -DENABLE_SDL2=ON
 YUZU_CONF_OPTS += -DYUZU_USE_EXTERNAL_SDL2=OFF
 YUZU_CONF_OPTS += -DUSE_DISCORD_PRESENCE=OFF
 YUZU_CONF_OPTS += -DYUZU_TESTS=OFF
+YUZU_CONF_OPTS += -DENABLE_QT6=ON
+YUZU_CONF_OPTS += -DYUZU_USE_EXTERNAL_VULKAN_HEADERS=OFF
 
 ifeq ($(BR2_PACKAGE_VULKAN_HEADERS)$(BR2_PACKAGE_VULKAN_LOADER),yy)
     YUZU_DEPENDENCIES += host-glslang
@@ -30,7 +32,6 @@ endif
 
 define YUZU_INSTALL_TARGET_CMDS
     mkdir -p $(TARGET_DIR)/usr/bin
-    mkdir -p $(TARGET_DIR)/usr/lib/yuzu
     $(INSTALL) -D $(@D)/buildroot-build/bin/yuzu $(TARGET_DIR)/usr/bin/
     $(INSTALL) -D $(@D)/buildroot-build/bin/yuzu-cmd $(TARGET_DIR)/usr/bin/
     $(INSTALL) -D $(@D)/buildroot-build/bin/yuzu-room $(TARGET_DIR)/usr/bin/
