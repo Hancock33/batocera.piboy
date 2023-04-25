f24e6dfee0bb083aba77d084cd5aa9b840a5893d
Lee <lee.hancock4@ntlworld.com>
2023-04-25 20:16:07 +0100

    switch libretro cores to ninja bs

--- package/batocera/emulators/retroarch/libretro/libretro-easyrpg/libretro-easyrpg.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-easyrpg/libretro-easyrpg.mk
@@ -12,11 +12,15 @@ LIBRETRO_EASYRPG_LICENSE = GPLv3
 LIBRETRO_EASYRPG_SUPPORTS_IN_SOURCE_BUILD = NO
 
 LIBRETRO_EASYRPG_DEPENDENCIES = sdl2 zlib fmt libpng freetype mpg123 libvorbis \
-    opusfile pixman speexdsp libxmp wildmidi liblcf fluidsynth
+    opusfile pixman speexdsp libxmp wildmidi liblcf fluidsynth host-ninja
 
-LIBRETRO_EASYRPG_CONF_OPTS += -DCMAKE_BUILD_TYPE=Release \
+LIBRETRO_EASYRPG_CONF_OPTS += -DCMAKE_BUILD_TYPE=Release -GNinja \
     -DPLAYER_TARGET_PLATFORM=libretro -DBUILD_SHARED_LIBS=ON
 LIBRETRO_EASYRPG_CONF_ENV += LDFLAGS="-lpthread -fPIC" CFLAGS="-fPIC" CXX_FLAGS="-fPIC"
+
+define LIBRETRO_EASYRPG_BUILD_CMDS
+	$(TARGET_MAKE_ENV) $(BR2_CMAKE) --build $(LIBRETRO_EASYRPG_BUILDDIR)
+endef
 
 define LIBRETRO_EASYRPG_INSTALL_TARGET_CMDS
     $(INSTALL) -D $(@D)/buildroot-build/easyrpg_libretro.so \
