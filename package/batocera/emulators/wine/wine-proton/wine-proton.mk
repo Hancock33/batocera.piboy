dd844434448609624739af4cfdfa4150b92b71c5
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2023-04-28 18:46:34 +0100

    update wine

--- package/batocera/emulators/wine/wine-proton/wine-proton.mk
+++ package/batocera/emulators/wine/wine-proton/wine-proton.mk
@@ -3,8 +3,8 @@
 # wine-proton
 #
 ################################################################################
-# Version: Commits on Apr 26, 2023 (branch@proton_8.0)
-WINE_PROTON_VERSION = fc24c322de5824be41e7359ac8560fe98a26d69b
+# Version: Commits on Apr 28, 2023 (branch@proton_8.0)
+WINE_PROTON_VERSION = b801fdb67070b5c86560ca515bd8ac33750970d2
 WINE_PROTON_SOURCE = wine-proton-$(WINE_PROTON_VERSION).tar.gz
 WINE_PROTON_SITE = $(call github,ValveSoftware,wine,$(WINE_PROTON_VERSION))
 WINE_PROTON_LICENSE = LGPL-2.1+
