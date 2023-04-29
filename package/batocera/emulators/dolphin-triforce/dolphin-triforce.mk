a854606060687b22885436cb3c6f5de578a83b84
Lee <lee.hancock4@ntlworld.com>
2023-04-29 15:46:27 +0100

    update wii triforce both mario karts are working other game wip

--- package/batocera/emulators/dolphin-triforce/dolphin-triforce.mk
+++ package/batocera/emulators/dolphin-triforce/dolphin-triforce.mk
@@ -3,32 +3,41 @@
 # dolphin-triforce
 #
 ################################################################################
-# This is a build that is no longer receiving updates. AppImage format ensures compatibility into the future.
-# Site https://dolphin-emu.org/download/list/Triforce/1/
-# Source https://github.com/EIGHTFINITE/dolphin-triforce
-# Dolphin version: 4.0-315
-DOLPHIN_TRIFORCE_SOURCE =
-DOLPHIN_TRIFORCE_VERSION = 1.0.0-001
+# Version: Commits on Mar 15, 2023
+DOLPHIN_TRIFORCE_VERSION = 6b7664dd230de299bae22150be77f4766cddde83
+DOLPHIN_TRIFORCE_SITE = https://crediar.dev/crediar/dolphin.git
+DOLPHIN_TRIFORCE_SITE_METHOD = git
 DOLPHIN_TRIFORCE_LICENSE = GPLv2+
+DOLPHIN_TRIFORCE_GIT_SUBMODULES = YES
+DOLPHIN_TRIFORCE_SUPPORTS_IN_SOURCE_BUILD = NO
 
-ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_X86_64_ANY),y)
-	DOLPHIN_TRIFORCE_DEPENDENCIES = xserver_xorg-server libfuse
-endif
+DOLPHIN_TRIFORCE_DEPENDENCIES = libevdev ffmpeg zlib libpng lzo libusb libcurl
+DOLPHIN_TRIFORCE_DEPENDENCIES += bluez5_utils hidapi xz host-xz sdl2 host-ninja
+DOLPHIN_TRIFORCE_DEPENDENCIES += xserver_xorg-server qt6base
 
-# Includes custom game configs required to successfully launch and play them.
-define DOLPHIN_TRIFORCE_INSTALL_TARGET_CMDS
-	mkdir -p $(TARGET_DIR)/usr/bin
-	$(INSTALL) -D -m 0555 "$(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/emulators/dolphin-triforce/dolphin-triforce.AppImage" "${TARGET_DIR}/usr/bin/dolphin-triforce"
+DOLPHIN_TRIFORCE_CONF_OPTS += -DDISTRIBUTOR='batocera.linux'
+DOLPHIN_TRIFORCE_CONF_OPTS += -DBUILD_SHARED_LIBS=OFF
+DOLPHIN_TRIFORCE_CONF_OPTS += -DUSE_UPNP=OFF
+DOLPHIN_TRIFORCE_CONF_OPTS += -DENABLE_TESTS=OFF
+DOLPHIN_TRIFORCE_CONF_OPTS += -DUSE_DISCORD_PRESENCE=OFF
+DOLPHIN_TRIFORCE_CONF_OPTS += -DUSE_MGBA=OFF
+DOLPHIN_TRIFORCE_CONF_OPTS += -DENABLE_AUTOUPDATE=OFF
+DOLPHIN_TRIFORCE_CONF_OPTS += -DCMAKE_BUILD_TYPE=Release -GNinja
+DOLPHIN_TRIFORCE_CONF_OPTS += -DENABLE_ANALYTICS=OFF
+
+define DOLPHIN_TRIFORCE_BUILD_CMDS
+	$(TARGET_MAKE_ENV) $(BR2_CMAKE) --build $(DOLPHIN_TRIFORCE_BUILDDIR)
 endef
 
-# Hotkeys (non-functional at the moment)
-define DOLPHIN_TRIFORCE_EVMAP
-	mkdir -p $(TARGET_DIR)/usr/share/evmapy
+#define DOLPHIN_TRIFORCE_INSTALL_TARGET_CMDS
+#	$(TARGET_MAKE_ENV) DESTDIR=$(TARGET_DIR) $(BR2_CMAKE) --install $(DOLPHIN_TRIFORCE_BUILDDIR)
+#endef
 
-	cp -prn $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/emulators/dolphin-triforce/triforce.dolphin_triforce.keys \
-		$(TARGET_DIR)/usr/share/evmapy
+define DOLPHIN_TRIFORCE_EVMAPY
+    mkdir -p $(TARGET_DIR)/usr/share/evmapy
+    cp $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/emulators/dolphin-triforce/*.keys $(TARGET_DIR)/usr/share/evmapy
 endef
 
-DOLPHIN_TRIFORCE_POST_INSTALL_TARGET_HOOKS = DOLPHIN_TRIFORCE_EVMAP
+DOLPHIN_TRIFORCE_POST_INSTALL_TARGET_HOOKS += DOLPHIN_TRIFORCE_EVMAPY
 
-$(eval $(generic-package))
+$(eval $(cmake-package))
