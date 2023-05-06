aee199aa467cc6843ccb3b9ceb3c821a6f4ffdd8
Lee <lee.hancock4@ntlworld.com>
2023-05-06 07:58:10 +0100

    revert libretro-picodrive

--- package/batocera/emulators/retroarch/libretro/libretro-picodrive/libretro-picodrive.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-picodrive/libretro-picodrive.mk
@@ -3,8 +3,8 @@
 # libretro-picodrive
 #
 ################################################################################
-# Version: Commits on May 02, 2023
-LIBRETRO_PICODRIVE_VERSION = 3a77090514c3ba3c1a0286a046bc9fe323e07f27
+# Version: Commits on May 01, 2023
+LIBRETRO_PICODRIVE_VERSION = 83a9e3050807584bc7ee2ea79519ecd3b8a94c80
 LIBRETRO_PICODRIVE_SITE = https://github.com/irixxxx/picodrive.git
 LIBRETRO_PICODRIVE_SITE_METHOD=git
 LIBRETRO_PICODRIVE_GIT_SUBMODULES=YES
@@ -40,8 +40,7 @@ define LIBRETRO_PICODRIVE_BUILD_CMDS
 	$(SED) 's+-I platform/libretro/libretro-common/include+-I $(@D)/platform/libretro/libretro-common/include+' $(@D)/Makefile.libretro
 
 	$(MAKE) -C $(@D)/cpu/cyclone CONFIG_FILE=$(@D)/cpu/cyclone_config.h
-	# force -j 1 to avoid parallel issues in the makefile
-	cd $(@D) && $(TARGET_CONFIGURE_OPTS) $(MAKE) -j 1 CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C  $(@D) -f Makefile.libretro platform="$(LIBRETRO_PICODRIVE_PLATFORM)" \
+	cd $(@D) && $(TARGET_CONFIGURE_OPTS) $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C  $(@D) -f Makefile.libretro platform="$(LIBRETRO_PICODRIVE_PLATFORM)" \
         GIT_VERSION=" $(shell echo $(LIBRETRO_PICODRIVE_VERSION) | cut -c 1-7)"
 endef
 
