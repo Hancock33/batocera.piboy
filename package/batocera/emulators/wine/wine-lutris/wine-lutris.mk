e96663f9e58495b5b365f7cfc16ef3f6394feeac
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2023-05-12 23:35:30 +0100

    update wine

--- package/batocera/emulators/wine/wine-lutris/wine-lutris.mk
+++ package/batocera/emulators/wine/wine-lutris/wine-lutris.mk
@@ -3,8 +3,8 @@
 # wine-lutris
 #
 ################################################################################
-# Version: Commits on May 09, 2023
-WINE_LUTRIS_VERSION = 19eab9c0e59201277f359d55ee5747383e014e87
+# Version: Commits on May 12, 2023
+WINE_LUTRIS_VERSION = 5662d85e0e95a2715ab3b81c25c89121fc8db34d
 WINE_LUTRIS_SOURCE = wine-lutris-$(WINE_LUTRIS_VERSION).tar.gz
 WINE_LUTRIS_SITE = $(call github,wine-mirror,wine,$(WINE_LUTRIS_VERSION))
 WINE_LUTRIS_LICENSE = LGPL-2.1+
