1bffdf24176acdb3432c00e953abc5b4c726089f
Lee <lee.hancock4@ntlworld.com>
2023-05-25 23:01:52 +0100

    update spirv-tools

--- package/batocera/gpu/vulkan/spirv-tools/spirv-tools.mk
+++ package/batocera/gpu/vulkan/spirv-tools/spirv-tools.mk
@@ -5,10 +5,13 @@
 ################################################################################
 # Version: Commits on May 25, 2023
 SPIRV_TOOLS_VERSION = e0936b646cdd2c7bb930501a26f2b95b2f684455
-SPIRV_TOOLS_SITE = $(call github,KhronosGroup,SPIRV-Tools,$(SPIRV_TOOLS_VERSION))
+SPIRV_TOOLS_SITE = https://github.com/KhronosGroup/SPIRV-Tools.git
+SPIRV_TOOLS_SITE_METHOD = git
+SPIRV_TOOLS_GIT_SUBMODULES = YES
+
 SPIRV_TOOLS_DEPENDENCIES = spirv-headers
 SPIRV_TOOLS_INSTALL_STAGING = YES
 
-SPIRV_TOOLS_CONF_OPTS += -DSPIRV-Headers_SOURCE_DIR="$(BUILD_DIR)/spirv-headers-$(SPIRV_HEADERS_VERSION)"
+SPIRV_TOOLS_CONF_OPTS += -DSPIRV-Headers_SOURCE_DIR="$(BUILD_DIR)/spirv-headers-$(SPIRV_HEADERS_VERSION)" -DSPIRV_SKIP_TESTS=ON
 
 $(eval $(cmake-package))
