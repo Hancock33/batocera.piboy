5e9be27a5347eddf4b3de465a025859053ae240a
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2023-05-31 23:10:23 +0100

    Update xenia.mk

--- package/batocera/emulators/xenia/xenia.mk
+++ package/batocera/emulators/xenia/xenia.mk
@@ -3,8 +3,8 @@
 # xenia
 #
 ################################################################################
-# Version: Commits on May 05, 2023
-XENIA_VERSION = v1.0.2798-master
+# Version: Commits on May 30, 2023
+XENIA_VERSION = v1.0.2801-master
 XENIA_SOURCE = xenia_master.zip
 XENIA_SITE = https://github.com/xenia-project/release-builds-windows/releases/download/$(XENIA_VERSION)
 XENIA_LICENSE = BSD
