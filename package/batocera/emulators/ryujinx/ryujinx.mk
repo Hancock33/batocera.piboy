d0d69adf2ad506a69b3fcd3fd737352b83ddd157
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2023-06-11 08:02:02 +0100

    Update ryujinx.mk

--- package/batocera/emulators/ryujinx/ryujinx.mk
+++ package/batocera/emulators/ryujinx/ryujinx.mk
@@ -3,8 +3,8 @@
 # ryujinx
 #
 ################################################################################
-# Version: Commits on Jun 09, 2023
-RYUJINX_VERSION = 1.1.881
+# Version: Commits on Jun 10, 2023
+RYUJINX_VERSION = 1.1.883
 RYUJINX_SOURCE = ryujinx-$(RYUJINX_VERSION)-linux_x64.tar.gz
 RYUJINX_SITE = https://github.com/Ryujinx/release-channel-master/releases/download/$(RYUJINX_VERSION)
 RYUJINX_LICENSE = MIT
