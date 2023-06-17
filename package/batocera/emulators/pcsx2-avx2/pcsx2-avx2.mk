14430d7e968e39fc3c7b7e75b4c024991ce8bcf3
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2023-06-17 11:03:00 +0100

    updates b4 rebuild

--- package/batocera/emulators/pcsx2-avx2/pcsx2-avx2.mk
+++ package/batocera/emulators/pcsx2-avx2/pcsx2-avx2.mk
@@ -3,8 +3,8 @@
 # pcsx2-avx2
 #
 ################################################################################
-# Version: Commits on Jun 16, 2023
-PCSX2_AVX2_VERSION = 0cf4cb6e4f00b650b2212d7e3be2ff118fa3ae26
+# Version: Commits on Jun 13, 2023
+PCSX2_AVX2_VERSION = 36c27188a4bc05fa93bd6b21b1c62c1d034610c0
 PCSX2_AVX2_SOURCE = pcsx2-$(PCSX2_AVX2_VERSION)-br1.tar.gz
 PCSX2_AVX2_SITE = https://github.com/pcsx2/pcsx2.git
 PCSX2_AVX2_SITE_METHOD = git
--- package/batocera/ports/doom/doomretro/doomretro.mk
+++ package/batocera/ports/doom/doomretro/doomretro.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 # Version: Commits on Jun 17, 2023
-DOOMRETRO_VERSION = e2d5dc93a7a3936b6bec6a09cc458298ced744af
+DOOMRETRO_VERSION = 449b13c811f493b8f1ec2cce9ee39badf2f4c2e7
 DOOMRETRO_SITE = $(call github,bradharding,doomretro,$(DOOMRETRO_VERSION))
 
 DOOMRETRO_DEPENDENCIES = sdl2 sdl2_mixer sdl2_image host-ninja
