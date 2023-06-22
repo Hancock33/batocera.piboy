67f136b0b891c7aa42442a7bb8d5183d6500ec4f
Lee <lee.hancock4@ntlworld.com>
2023-06-22 06:16:50 +0100

    build fixes

--- package/batocera/emulators/retroarch/libretro/libretro-boom3/libretro-boom3.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-boom3/libretro-boom3.mk
@@ -8,11 +8,9 @@ LIBRETRO_BOOM3_VERSION = 0bea79abf5ec8262dfe9af73cb8c54ea6e2aeb98
 LIBRETRO_BOOM3_SITE = $(call github,libretro,boom3,$(LIBRETRO_BOOM3_VERSION))
 LIBRETRO_BOOM3_LICENSE = GPLv3
 LIBRETRO_BOOM3_LICENSE_FILES = COPYING.txt
-LIBRETRO_BOOM3_SUBDIR = neo
-
 LIBRETRO_BOOM3_DEPENDENCIES = host-libjpeg libcurl libogg libvorbis openal sdl2 zlib 
 
-LIBRETRO_BOOM3_PLATFORM = $(LIBRETRO_PLATFORM)
+define LIBRETRO_BOOM3_BUILD_CMDS
 	$(TARGET_CONFIGURE_OPTS) $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D)/neo -f Makefile platform=unix \
         GIT_VERSION="-$(shell echo $(LIBRETRO_BOOM3_VERSION) | cut -c 1-7)"
 
@@ -26,3 +24,4 @@ define LIBRETRO_BOOM3_INSTALL_TARGET_CMDS
 endef
 
 $(eval $(generic-package))
+
