e7eb88f4a4ce89ea3682be853fcee880d5d428d8
Lee <lee.hancock4@ntlworld.com>
2023-07-02 10:56:56 +0100

    update wine

--- package/batocera/emulators/wine/wine-lutris-wow64_32/wine-lutris-wow64_32.mk
+++ package/batocera/emulators/wine/wine-lutris-wow64_32/wine-lutris-wow64_32.mk
@@ -3,10 +3,10 @@
 # wine-lutris-wow64_32
 #
 ################################################################################
-# Version: Commits on Jun 29, 2023
-WINE_LUTRIS_WOW64_32_VERSION = 884cff821481b4819f9bdba455217bd5a3f97744
+# Version: Commits on Jun 29, 2023 (branch@ge-lol-8.11-1)
+WINE_LUTRIS_WOW64_32_VERSION = 35b413edc488a9cff73ee85fced725894cc75bb0
 WINE_LUTRIS_WOW64_32_SOURCE = wine-lutris-$(WINE_LUTRIS_WOW64_32_VERSION).tar.gz
-WINE_LUTRIS_WOW64_32_SITE = $(call github,wine-mirror,wine,$(WINE_LUTRIS_WOW64_32_VERSION))
+WINE_LUTRIS_WOW64_32_SITE = $(call github,GloriousEggroll,wine,$(WINE_LUTRIS_WOW64_32_VERSION))
 WINE_LUTRIS_WOW64_32_LICENSE = LGPL-2.1+
 WINE_LUTRIS_WOW64_32_LICENSE_FILES = COPYING.LIB LICENSE
 WINE_LUTRIS_WOW64_32_CPE_ID_VENDOR = winehq
--- package/batocera/emulators/wine/wine-proton/wine-proton.mk
+++ package/batocera/emulators/wine/wine-proton/wine-proton.mk
@@ -3,10 +3,10 @@
 # wine-proton
 #
 ################################################################################
-# Version: Commits on Jun 07, 2023 (branch@proton_8.0-rc)
-WINE_PROTON_VERSION = 839b0a77c8d913c18ff3a76d7207078be865df9d
+# Version: Commits on Jun 05, 2023 (branch@Proton8-8)
+WINE_PROTON_VERSION = d48c657a1364248c1aaed099e5cbee30c1d8c09e
 WINE_PROTON_SOURCE = wine-proton-$(WINE_PROTON_VERSION).tar.gz
-WINE_PROTON_SITE = $(call github,ValveSoftware,wine,$(WINE_PROTON_VERSION))
+WINE_PROTON_SITE = $(call github,GloriousEggroll,proton-wine,$(WINE_PROTON_VERSION))
 WINE_PROTON_LICENSE = LGPL-2.1+
 WINE_PROTON_LICENSE_FILES = COPYING.LIB LICENSE
 WINE_PROTON_CPE_ID_VENDOR = winehq
