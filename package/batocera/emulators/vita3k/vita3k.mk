317a14d8f87681293a562690098cf98c3d437962
Lee <lee.hancock4@ntlworld.com>
2023-07-08 06:44:07 +0100

    update vita3k patches

--- package/batocera/emulators/vita3k/vita3k.mk
+++ package/batocera/emulators/vita3k/vita3k.mk
@@ -3,8 +3,8 @@
 # vita3k
 #
 ################################################################################
-# Version: Commits on Jul 07, 2023
-VITA3K_VERSION = 389f079c19e45a9d35426d18c5883f7b46c66f3b
+# Version: Commits on Jul 06, 2023
+VITA3K_VERSION = acb706b92027c0cf86811050299a953073ee2978
 VITA3K_SITE = https://github.com/vita3k/vita3k
 VITA3K_SITE_METHOD=git
 VITA3K_GIT_SUBMODULES=YES
