66e000644689b10eaf2b136c40ac7f486694fe1d
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2023-07-09 16:45:34 +0100

    Update gzdoom.mk

--- package/batocera/ports/doom/gzdoom/gzdoom.mk
+++ package/batocera/ports/doom/gzdoom/gzdoom.mk
@@ -3,8 +3,8 @@
 # gzdoom
 #
 ################################################################################
-# Version: Commits on Jul 06, 2023
-GZDOOM_VERSION = bcfa43b7d6a5ace58649b6ccf42c0d72605e9346
+# Version: Commits on Jul 09, 2023
+GZDOOM_VERSION = 99850ea216ef13d56fb6e65e927aabd76d91b601
 GZDOOM_SITE = $(call github,ZDoom,gzdoom,$(GZDOOM_VERSION))
 GZDOOM_LICENSE = GPL-3.0
 GZDOOM_DEPENDENCIES = sdl2 bzip2 fluidsynth openal mesa3d libglu libglew host-zmusic zmusic host-ninja
