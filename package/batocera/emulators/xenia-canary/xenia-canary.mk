2267a62207a2594211590bd6794bb2e0d0be69f1
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2023-07-10 20:06:28 +0100

    Update xenia-canary.mk

--- package/batocera/emulators/xenia-canary/xenia-canary.mk
+++ package/batocera/emulators/xenia-canary/xenia-canary.mk
@@ -23,14 +23,14 @@ endef
 
 define XENIA_CANARY_POST_PROCESS
 	# get the latest patches
-	mkdir -p $(TARGET_DIR)/usr/xenia-canary/patches
+	mkdir -p $(TARGET_DIR)/usr/bin/xenia-canary/patches
 	mkdir -p $(@D)/temp
 	( cd $(@D)/temp && $(GIT) init && \
 	  $(GIT) remote add origin https://github.com/xenia-canary/game-patches.git && \
 	  $(GIT) config core.sparsecheckout true && \
 	  echo "patches/*.toml" >> .git/info/sparse-checkout && \
 	  $(GIT) pull --depth=1 origin main && \
-	  mv -f patches/*.toml $(TARGET_DIR)/usr/xenia-canary/patches \
+	  mv -f patches/*.toml $(TARGET_DIR)/usr/bin/xenia-canary/patches \
 	)
 	
 	# Clean up the temporary directory
