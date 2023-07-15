c3196e3fe3140653293156a90a8d522dad79c9bb
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2023-07-15 13:31:16 +0100

    tidy up quake3

--- package/batocera/core/batocera-configgen/configgen/configgen/generators/quake3/quake3Generator.py
+++ package/batocera/core/batocera-configgen/configgen/configgen/generators/quake3/quake3Generator.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python
-import os
-from os import path
 import Command
+import batoceraFiles
 from generators.Generator import Generator
+import shutil
+import os.path
+import configparser
 import controllersConfig
+from shutil import copyfile
+from . import quake3Config
 
 class Quake3Generator(Generator):
 
     def generate(self, system, rom, playersControllers, guns, gameResolution):
+        quake3Config.writeCfgFiles(system, rom, playersControllers, gameResolution)
+
         commandArray = ["ioquake3", "+set", "cl_renderer", "vulkan", "+set", "fs_basePath", "/userdata/roms/ports/quake3" ]
         os.chdir("/usr/share/quake3")
 
@@ -16,3 +22,8 @@ class Quake3Generator(Generator):
             env={
                 'SDL_GAMECONTROLLERCONFIG': controllersConfig.generateSdlGameControllerConfig(playersControllers)
             })
+
+    def getInGameRatio(self, config, gameResolution, rom):
+        if gameResolution["width"] / float(gameResolution["height"]) > ((16.0 / 9.0) - 0.1):
+            return 16/9
+        return 4/3
