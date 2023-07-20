a5b9fe283585e3b03f6927c834fbbf79016558a0
Lee <lee.hancock4@ntlworld.com>
2023-07-20 05:32:02 +0100

    update locales

--- package/batocera/emulators/ryujinx/ryujinx.mk
+++ package/batocera/emulators/ryujinx/ryujinx.mk
@@ -3,8 +3,8 @@
 # ryujinx
 #
 ################################################################################
-# Version: Commits on Jul 14, 2023
-RYUJINX_VERSION = 1.1.961
+# Version: Commits on Jul 18, 2023
+RYUJINX_VERSION = 1.1.965
 RYUJINX_SOURCE = ryujinx-$(RYUJINX_VERSION)-linux_x64.tar.gz
 RYUJINX_SITE = https://github.com/Ryujinx/release-channel-master/releases/download/$(RYUJINX_VERSION)
 RYUJINX_LICENSE = MIT
