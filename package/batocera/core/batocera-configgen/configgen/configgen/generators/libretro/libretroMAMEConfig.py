91195a64c1e6ed2da3f4b0b88f3171ca1aa9e37b
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2023-08-05 20:58:52 +0100

    fixes
    - use batocera-cpucore on X86 only
    - mame command line updated again

--- package/batocera/core/batocera-configgen/configgen/configgen/generators/libretro/libretroMAMEConfig.py
+++ package/batocera/core/batocera-configgen/configgen/configgen/generators/libretro/libretroMAMEConfig.py
@@ -249,8 +249,11 @@ def generateMAMEConfigs(playersControllers, system, rom):
                         else:
                             commandLine += [ "-" + messRomType[messMode] ]
                 # Use the full filename for MESS non-softlist ROMs
-                #commandLine += [ f'"{romBasename}"' ]
-                commandLine += [ f'"{rom}"' ]
+                if system.config['core'] != 'same_cdi':
+                    commandLine += [ f'"{romBasename}"' ]
+                else:
+                    commandLine += [ f'"{rom}"' ]
+
                 commandLine += [ "-rompath", romDirname + ";/userdata/bios/" ]
 
                 # Boot disk for Macintosh
