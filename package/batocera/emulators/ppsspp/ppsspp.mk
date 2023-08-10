bcdd0b36c975fdc3382197a02916e40b85f8735d
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2023-08-10 18:53:28 +0100

    some updates

--- package/batocera/emulators/ppsspp/ppsspp.mk
+++ package/batocera/emulators/ppsspp/ppsspp.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 # Version: Commits on Aug 10, 2023
-PPSSPP_VERSION = b7b3e81e2f67f78f33ef82d62ee7b069ef87aa56
+PPSSPP_VERSION = 87f7523f995af4a4d348ad222c02a96c0caf6638
 PPSSPP_SITE = https://github.com/hrydgard/ppsspp.git
 PPSSPP_SITE_METHOD=git
 PPSSPP_GIT_SUBMODULES=YES
--- package/batocera/ports/fheroes2/fheroes2.mk
+++ package/batocera/ports/fheroes2/fheroes2.mk
@@ -3,8 +3,8 @@
 # fheroes2
 #
 ################################################################################
-# Version: Commits on Aug 09, 2023
-FHEROES2_VERSION = 72e1479f1b043c1c6f6ffce1b242f1ca038c038a
+# Version: Commits on Aug 10, 2023
+FHEROES2_VERSION = 901cd9cfbe3cf18ee7152e987ee090f2f69b92c5
 FHEROES2_SITE = $(call github,ihhub,fheroes2,$(FHEROES2_VERSION))
 FHEROES2_DEPENDENCIES = sdl2 sdl2_image
 
