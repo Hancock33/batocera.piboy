3110fa6a8b834caa15a6fc23353a4e7f90b81f6a
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2023-08-13 11:24:52 +0100

    update fallout 1 & 2 to batocera naming

--- package/batocera/ports/fallout1-ce/fallout1-ce.mk
+++ package/batocera/ports/fallout1-ce/fallout1-ce.mk
@@ -3,13 +3,16 @@
 # fallout1-ce
 #
 ################################################################################
-
-FALLOUT1_CE_VERSION = v1.0.0
+# Version: Commits on Aug 07, 2023
+FALLOUT1_CE_VERSION = 6be823cf7c614a693aca3ab6a4a8aa1df2537793
 FALLOUT1_CE_SITE = $(call github,alexbatalov,fallout1-ce,$(FALLOUT1_CE_VERSION))
 FALLOUT1_CE_DEPENDENCIES = sdl2
+FALLOUT1_CE_CMAKE_BACKEND = ninja
+
+FALLOUT1_CE_CONF_OPTS += -DCMAKE_BUILD_TYPE=Release
 
 define FALLOUT1_CE_INSTALL_TARGET_CMDS
-	cp $(@D)/fallout-ce $(TARGET_DIR)/usr/bin/fallout1-ce
+    cp $(@D)/fallout-ce $(TARGET_DIR)/usr/bin/fallout-ce
 endef
 
 $(eval $(cmake-package))
