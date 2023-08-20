616388ea7d7466c3fd52039174ecb85a54c389ad
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2023-08-20 09:18:14 +0100

    update wine

--- package/batocera/emulators/wine/wine-proton-wow64_32/wine-proton-wow64_32.mk
+++ package/batocera/emulators/wine/wine-proton-wow64_32/wine-proton-wow64_32.mk
@@ -3,8 +3,8 @@
 # wine-proton-wow64_32
 #
 ################################################################################
-# Version: Commits on Aug 02, 2023 (branch@Proton8-13)
-WINE_PROTON_WOW64_32_VERSION = 62b54703e5d0b81d623e65bfff877588ee29d3ac
+# Version: Commits on Aug 20, 2023 (branch@Proton8-14)
+WINE_PROTON_WOW64_32_VERSION = 0bee1bf20f5ae0f0548bbf0e1882aad6be377887
 WINE_PROTON_WOW64_32_SOURCE = wine-proton-$(WINE_PROTON_WOW64_32_VERSION).tar.gz
 WINE_PROTON_WOW64_32_SITE = $(call github,GloriousEggroll,proton-wine,$(WINE_PROTON_WOW64_32_VERSION))
 WINE_PROTON_WOW64_32_LICENSE = LGPL-2.1+
