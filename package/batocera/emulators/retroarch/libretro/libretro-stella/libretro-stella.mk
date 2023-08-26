cba2c75861cbf85309bbb545518c9f6cac3e9a82
Lee <lee.hancock4@ntlworld.com>
2023-08-26 15:50:17 +0100

    update locales

--- package/batocera/emulators/retroarch/libretro/libretro-stella/libretro-stella.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-stella/libretro-stella.mk
@@ -3,8 +3,8 @@
 # libretro-stella
 #
 ################################################################################
-# Version: Commits on Aug 26, 2023
-LIBRETRO_STELLA_VERSION = df34de46ab92fcfa3b4e3f8583696c86b5969d44
+# Version: Commits on Aug 25, 2023
+LIBRETRO_STELLA_VERSION = 432660f67931c0400843d4f6ee6b23a88e309fcb
 LIBRETRO_STELLA_SITE = $(call github,stella-emu,stella,$(LIBRETRO_STELLA_VERSION))
 LIBRETRO_STELLA_LICENSE = GPLv2
 
