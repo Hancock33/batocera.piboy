24fbb493b47c2aa748acdc9240f5a87d5cc77690
Lee <lee.hancock4@ntlworld.com>
2023-09-02 21:08:54 +0100

    remove wget + cache downloaded packages

--- package/batocera/emulators/wine/wine-mono/wine-mono-lutris.mk
+++ package/batocera/emulators/wine/wine-mono/wine-mono-lutris.mk
@@ -17,7 +17,6 @@ define WINE_MONO_EXTRACT_CMDS
 	mkdir -p $(@D)/target/usr/wine/lutris/share/wine/mono/ && cd $(@D)/target/usr/wine/lutris/share/wine/mono/ && tar xf $(DL_DIR)/$(WINE_MONO_DL_SUBDIR)/$(WINE_MONO_SOURCE)
 	mkdir -p $(TARGET_DIR)/usr/wine/proton/share/wine
 	ln -sf /usr/wine/lutris/share/wine/mono $(TARGET_DIR)/usr/wine/proton/share/wine
-	#mkdir -p $(@D)/target/usr/share/wine/gecko && cd $(@D)/target/usr/share/wine/gecko && wget -qO- $(WINE_GECKO_SITE) | tar -xjf -
 endef
 
 define WINE_MONO_INSTALL_TARGET_CMDS
