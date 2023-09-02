d15daeb87cbfec9a4195c98f4704515f48476c89
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2023-09-02 22:30:18 +0100

    Update wine-lutris-wow64_32.mk

--- package/batocera/emulators/wine/wine-lutris-wow64_32/wine-lutris-wow64_32.mk
+++ package/batocera/emulators/wine/wine-lutris-wow64_32/wine-lutris-wow64_32.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-WINE_LUTRIS_WOW64_32_VERSION=$(WINE_LUTRIS_VERSION)
+WINE_LUTRIS_WOW64_32_VERSION = $(WINE_LUTRIS_VERSION)
 WINE_LUTRIS_WOW64_32_SOURCE = wine-$(WINE_LUTRIS_VERSION).tar.gz
 WINE_LUTRIS_WOW64_32_SITE = https://github.com/wine-mirror/wine/archive/refs/tags
 WINE_LUTRIS_WOW64_32_LICENSE = LGPL-2.1+
