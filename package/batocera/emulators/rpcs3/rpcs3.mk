c0dcafd6adda0fe49881f82b477ddbf2cd52bcaf
Lee <lee.hancock4@ntlworld.com>
2023-09-02 15:24:38 +0100

    update rpcs3

--- package/batocera/emulators/rpcs3/rpcs3.mk
+++ package/batocera/emulators/rpcs3/rpcs3.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 # Version: Commits on Sept 02, 2023
-RPCS3_VERSION = 11006dac35544f3fd990fb7c6c6fc1a0e313d26a
+RPCS3_VERSION = ea579849126426a38467a27473f47e5a250bd5b6
 RPCS3_SITE = https://github.com/RPCS3/rpcs3.git
 RPCS3_SITE_METHOD=git
 RPCS3_GIT_SUBMODULES=YES
