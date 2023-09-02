6760fd3ce1f6636ad2aa75eb8a1ea434f0b22f05
Lee <lee.hancock4@ntlworld.com>
2023-09-02 11:19:36 +0100

    update dosbox-x

--- package/batocera/emulators/dosbox-x/dosbox-x.mk
+++ package/batocera/emulators/dosbox-x/dosbox-x.mk
@@ -3,8 +3,8 @@
 # dosbox-x
 #
 ################################################################################
-# Version: Commits on Sept 02, 2023
-DOSBOX_X_VERSION = c340222b0a7ee58dac8b4edab9395f93e981aa37
+# Version: Commits on Sept 01, 2023
+DOSBOX_X_VERSION = dosbox-x-v2023.09.01
 DOSBOX_X_SITE = $(call github,joncampbell123,dosbox-x,$(DOSBOX_X_VERSION))
 DOSBOX_X_DEPENDENCIES = sdl2 sdl2_net fluidsynth zlib libpng libogg libvorbis linux-headers
 DOSBOX_X_LICENSE = GPLv2
