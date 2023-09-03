9902a6d6c74519e011c337b1c918d69c053bfba7
Lee <lee.hancock4@ntlworld.com>
2023-09-03 11:47:38 +0100

    allow libretro-hatarib to cross-compile

--- package/batocera/emulators/retroarch/libretro/libretro-hatarib/libretro-hatarib.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-hatarib/libretro-hatarib.mk
@@ -13,16 +13,24 @@ LIBRETRO_HATARIB_DEPENDENCIES = libcapsimage libpng libzlib sdl2
 LIBRETRO_HATARIB_GIT_SUBMODULES = YES
 
 LIBRETRO_HATARIB_CONF_ENV += \
-	SHORTHASH='"$(shell echo $(LIBRETRO_HATARIB_VERSION) | cut -c 1-7)"' \
+    SHORTHASH='"$(shell echo $(LIBRETRO_HATARIB_VERSION) | cut -c 1-7)"' \
 	SDL2_INCLUDE="$(STAGING_DIR)/usr/include/SDL2" \
 	SDL2_LIB="$(STAGING_DIR)/usr/lib" \
 	SDL2_LINK="$(STAGING_DIR)/usr/lib/libSDL2.so" \
 	ZLIB_INCLUDE="$(STAGING_DIR)/usr/include" \
 	ZLIB_LIB="$(STAGING_DIR)/usr/lib" \
-	ZLIB_LINK="$(STAGING_DIR)/usr/lib/libz.so"
+	ZLIB_LINK="$(STAGING_DIR)/usr/lib/libz.so" \
+	CC="$(TARGET_CC)" \
+	AR="$(TARGET_AR)" \
+	RANLIB="$(TARGET_RANLIB)"
+
+LIBRETRO_HATARIB_REPLACE_CCFG =-DCMAKE_TOOLCHAIN_FILE=$(HOST_DIR)/share/buildroot/toolchainfile.cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_FLAGS_RELEASE="$(TARGET_OPTIMIZATION) -fPIC"
 
 define LIBRETRO_HATARIB_BUILD_CMDS
-    CC="$(TARGET_CC)" AR="$(TARGET_AR)" RANLIB="$(TARGET_RANLIB)" \
+	$(SED) "s|-O3|$(TARGET_OPTIMIZATION)|g" $(@D)/makefile
+	$(SED) 's|cmake |$(HOST_DIR)/bin/cmake |g' $(@D)/makefile
+	$(SED) 's|CMAKEFLAGS += |CMAKEFLAGS += $(LIBRETRO_HATARIB_REPLACE_CCFG) |g' $(@D)/makefile
+
 	cd $(@D) && $(MAKE) -f makefile \
 	$(LIBRETRO_HATARIB_CONF_ENV)
 endef
