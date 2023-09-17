deebd88693240d5f079460b873ee97ac818d2789
Lee <lee.hancock4@ntlworld.com>
2023-09-17 08:03:20 +0100

    build fixes

--- package/batocera/emulators/retroarch/libretro/libretro-hatarib/libretro-hatarib.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-hatarib/libretro-hatarib.mk
@@ -3,8 +3,8 @@
 # libretro-hatarib
 #
 ################################################################################
-# Version: Commits on Sept 15, 2023
-LIBRETRO_HATARIB_VERSION = 0179d463885ca5dd653f0eb4c2feca9858027b41
+# Version: Commits on Sept 13, 2023
+LIBRETRO_HATARIB_VERSION = 364d38a55ba9ce16bc7bb90bde83ad1fdd0dee7b
 LIBRETRO_HATARIB_SITE = https://github.com/bbbradsmith/hatariB
 LIBRETRO_HATARIB_SITE_METHOD=git
 LIBRETRO_HATARIB_LICENSE = GPLv2
