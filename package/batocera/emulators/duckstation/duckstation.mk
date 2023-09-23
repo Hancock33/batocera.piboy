859eeb60ebb0cc3a67525a2c67956fd1eeda009f
Lee <lee.hancock4@ntlworld.com>
2023-09-23 14:13:45 +0100

    revert duckstation

--- package/batocera/emulators/duckstation/duckstation.mk
+++ package/batocera/emulators/duckstation/duckstation.mk
@@ -3,8 +3,8 @@
 # duckstation
 #
 ################################################################################
-# Version: Commits on Sept 23, 2023
-DUCKSTATION_VERSION = 610a40490aa849782866c90616de6a9c42ef57a3
+# Version: Commits on Aug 23, 2023
+DUCKSTATION_VERSION = 82cdef45b377eae34180af01cdd329cfd957d507
 DUCKSTATION_SITE = https://github.com/stenzek/duckstation.git
 DUCKSTATION_SITE_METHOD=git
 DUCKSTATION_GIT_SUBMODULES=YES
@@ -22,55 +22,54 @@ ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_RK3588),y)
 endif
 
 ifeq ($(BR2_PACKAGE_WAYLAND),y)
-	DUCKSTATION_CONF_OPTS += -DUSE_WAYLAND=ON
+    DUCKSTATION_CONF_OPTS += -DUSE_WAYLAND=ON
 else
-	DUCKSTATION_CONF_OPTS += -DUSE_WAYLAND=OFF
+    DUCKSTATION_CONF_OPTS += -DUSE_WAYLAND=OFF
 endif
 
 ifeq ($(BR2_PACKAGE_XORG7),y)
-	DUCKSTATION_CONF_OPTS += -DUSE_X11=ON
+    DUCKSTATION_CONF_OPTS += -DUSE_X11=ON
 else
-	DUCKSTATION_CONF_OPTS += -DUSE_X11=OFF
+    DUCKSTATION_CONF_OPTS += -DUSE_X11=OFF
 endif
 
 ifeq ($(BR2_PACKAGE_VULKAN_HEADERS)$(BR2_PACKAGE_VULKAN_LOADER),yy)
-	DUCKSTATION_CONF_OPTS += -DENABLE_VULKAN=ON
+    DUCKSTATION_CONF_OPTS += -DENABLE_VULKAN=ON
 else
-	DUCKSTATION_CONF_OPTS += -DENABLE_VULKAN=OFF
+    DUCKSTATION_CONF_OPTS += -DENABLE_VULKAN=OFF
 endif
 
 #ifeq ($(BR2_PACKAGE_QT6),y)
-#	DUCKSTATION_CONF_OPTS += -DBUILD_QT_FRONTEND=ON -DBUILD_SDL_FRONTEND=OFF
-#	DUCKSTATION_DEPENDENCIES += qt6base qt6svg qt6tools qt6multimedia
+#    DUCKSTATION_CONF_OPTS += -DBUILD_QT_FRONTEND=ON -DBUILD_SDL_FRONTEND=OFF
+#    DUCKSTATION_DEPENDENCIES += qt6base qt6svg qt6tools qt6multimedia
 #else
-	DUCKSTATION_CONF_OPTS += -DBUILD_QT_FRONTEND=OFF -DBUILD_SDL_FRONTEND=OFF -DBUILD_NOGUI_FRONTEND=ON
-	DUCKSTATION_DEPENDENCIES += libdrm sdl2 libevdev
+    DUCKSTATION_CONF_OPTS += -DBUILD_QT_FRONTEND=OFF -DBUILD_SDL_FRONTEND=OFF -DBUILD_NOGUI_FRONTEND=ON
+    DUCKSTATION_DEPENDENCIES += libdrm sdl2 libevdev
 #endif
 
 ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_X86_64_ANY),y)
-	DUCKSTATION_CONF_OPTS += -DUSE_GLX=ON
+    DUCKSTATION_CONF_OPTS += -DUSE_GLX=ON
 endif
 
 ifeq ($(BR2_PACKAGE_HAS_LIBEGL),y)
-	DUCKSTATION_CONF_OPTS += -DUSE_EGL=ON
-	ifeq ($(BR2_PACKAGE_HAS_LIBMALI),y)
-		DUCKSTATION_CONF_OPTS += -DUSE_MALI=ON
-	endif
+    DUCKSTATION_CONF_OPTS += -DUSE_EGL=ON
+    ifeq ($(BR2_PACKAGE_HAS_LIBMALI),y)
+        DUCKSTATION_CONF_OPTS += -DUSE_MALI=ON
+    endif
 else
-	DUCKSTATION_CONF_OPTS += -DUSE_EGL=OFF
+    DUCKSTATION_CONF_OPTS += -DUSE_EGL=OFF
 endif
 
 define DUCKSTATION_INSTALL_TARGET_CMDS
-	mkdir -p $(TARGET_DIR)/usr/bin/duckstation
-#	@if [ "$(X86_INSTALL)" = "n" ]; then  $(INSTALL) -D $(@D)/buildroot-build/bin/duckstation-nogui $(TARGET_DIR)/usr/duckstation/bin/ ; fi
-#	@if [ "$(X86_INSTALL)" = "y" ]; then  cp -pr  $(@D)/buildroot-build/bin/translations			$(TARGET_DIR)/usr/duckstation/bin/ ; fi
-#	@if [ "$(X86_INSTALL)" = "y" ]; then  $(INSTALL) -D $(@D)/buildroot-build/bin/duckstation-qt	$(TARGET_DIR)/usr/duckstation/bin/duckstation-nogui ; fi
-	$(INSTALL) -D $(@D)/buildroot-build/bin/duckstation*	$(TARGET_DIR)/usr/bin/duckstation
-	cp -pr  $(@D)/data/resources							$(TARGET_DIR)/usr/bin/duckstation/
+    mkdir -p $(TARGET_DIR)/usr/bin/duckstation
+#    @if [ "$(X86_INSTALL)" = "n" ]; then  $(INSTALL) -D $(@D)/buildroot-build/bin/duckstation-nogui $(TARGET_DIR)/usr/duckstation/bin/ ; fi
+#    @if [ "$(X86_INSTALL)" = "y" ]; then  cp -pr  $(@D)/buildroot-build/bin/translations            $(TARGET_DIR)/usr/duckstation/bin/ ; fi
+#    @if [ "$(X86_INSTALL)" = "y" ]; then  $(INSTALL) -D $(@D)/buildroot-build/bin/duckstation-qt    $(TARGET_DIR)/usr/duckstation/bin/duckstation-nogui ; fi
+    $(INSTALL) -D $(@D)/buildroot-build/bin/duckstation* $(TARGET_DIR)/usr/bin/duckstation
+    cp -pr  $(@D)/buildroot-build/bin/resources          $(TARGET_DIR)/usr/bin/duckstation/
 
-	mkdir -p $(TARGET_DIR)/usr/share/evmapy
-	cp $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/emulators/duckstation/psx.duckstation.keys $(TARGET_DIR)/usr/share/evmapy
+    mkdir -p $(TARGET_DIR)/usr/share/evmapy
+    cp $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/emulators/duckstation/psx.duckstation.keys $(TARGET_DIR)/usr/share/evmapy
 endef
 
 $(eval $(cmake-package))
-
