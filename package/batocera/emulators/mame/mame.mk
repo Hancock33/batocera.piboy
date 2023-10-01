d59a1629181c77a206fc104e6205a93f98587620
Lee <lee.hancock4@ntlworld.com>
2023-10-01 18:48:25 +0100

    update locales

--- package/batocera/emulators/mame/mame.mk
+++ package/batocera/emulators/mame/mame.mk
@@ -11,12 +11,10 @@ MAME_LICENSE = MAME
 
 MAME_CROSS_ARCH = unknown
 MAME_CROSS_OPTS = PRECOMPILE=0
-MAME_CFLAGS = $(TARGET_OPTIMIZATION)
 MAME_LDFLAGS = -fuse-ld=mold
 
 # Limit number of jobs not to eat too much RAM....
 MAME_JOBS = $(shell expr $(shell nproc))
-
 # x86_64 is desktop linux based on X11 and OpenGL
 ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_X86_64_ANY),y)
 MAME_CROSS_ARCH = x86_64
@@ -73,8 +71,6 @@ define MAME_BUILD_CMDS
 	cd $(@D); \
 	PATH="$(HOST_DIR)/bin:$$PATH" \
 	SYSROOT="$(STAGING_DIR)" \
-	CFLAGS="$(MAME_CFLAGS)" \
-	CXXFLAGS="$(MAME_CFLAGS)" \
 	LDFLAGS="--sysroot=$(STAGING_DIR) $(MAME_LDFLAGS)" \
 	PKG_CONFIG="$(HOST_DIR)/usr/bin/pkg-config --define-prefix" \
 	PKG_CONFIG_PATH="$(STAGING_DIR)/usr/lib/pkgconfig" \
@@ -105,7 +101,7 @@ define MAME_BUILD_CMDS
 	LDOPTS="-lasound -lfontconfig" \
 	SYMBOLS=0 \
 	STRIP_SYMBOLS=1 \
-	TOOLS=1 NOWERROR=1 OPTIMIZE=s
+	TOOLS=1 NOWERROR=1 OPTIMIZE=s OPT_FLAGS="$(TARGET_OPTIMIZATION)"
 endef
 
 define MAME_INSTALL_TARGET_CMDS
