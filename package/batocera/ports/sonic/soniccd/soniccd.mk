25b2236b76d54ea5b21367b1848a3e33b4367b76
Lee <lee.hancock4@ntlworld.com>
2023-10-07 20:37:41 +0100

    use cmake for sonic ports

--- package/batocera/ports/sonic/soniccd/soniccd.mk
+++ package/batocera/ports/sonic/soniccd/soniccd.mk
@@ -8,23 +8,15 @@ SONICCD_VERSION = 97a2c60381de9e18467d351e9f12fb00f75732b5
 SONICCD_SITE = https://github.com/Rubberduckycooly/Sonic-CD-11-Decompilation.git
 SONICCD_SITE_METHOD = git
 SONICCD_GIT_SUBMODULES = YES
+SONICCD_SUPPORTS_IN_SOURCE_BUILD = NO
 
 SONICCD_DEPENDENCIES = sdl2 libogg libvorbis libtheora
 SONICCD_LICENSE = Custom
 
-ifeq ($(BR2_PACKAGE_XORG7),y)
-	SONICCD_BINNAME = RSDKv3
-else
-	SONICCD_VERSION = 222caf6
-	SONICCD_BINNAME = soniccd
-endif
-
-define SONICCD_BUILD_CMDS
-	$(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D) -f Makefile VERBOSE=1
-endef
+SONICCD_CONF_OPTS += -DRETRO_SDL_VERSION=2
 
 define SONICCD_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 0755 $(@D)/bin/$(SONICCD_BINNAME) $(TARGET_DIR)/usr/bin/soniccd
+	$(INSTALL) -D -m 0755 $(@D)/buildroot-build/RSDKv3 $(TARGET_DIR)/usr/bin/soniccd
 endef
 
-$(eval $(generic-package))
+$(eval $(cmake-package))
