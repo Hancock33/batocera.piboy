b74a5a46346e357b28f84e5f8f9a2f951020c193
Lee <lee.hancock4@ntlworld.com>
2023-10-08 21:56:20 +0100

    update stella to latest git vesion

--- package/batocera/emulators/batocera-stella/batocera-stella.mk
+++ package/batocera/emulators/batocera-stella/batocera-stella.mk
@@ -5,8 +5,7 @@
 ################################################################################
 # Version: Commits on Oct 02, 2023
 BATOCERA_STELLA_VERSION = bc109182a01714113f2613c2e8f3814ff4e7e7d8
-BATOCERA_STELLA_SOURCE = stella-$(BATOCERA_STELLA_VERSION)-src.tar.xz
-BATOCERA_STELLA_SITE = https://github.com/stella-emu/stella/releases/download/$(BATOCERA_STELLA_VERSION)
+BATOCERA_STELLA_SITE = $(call github,stella-emu,stella,$(BATOCERA_STELLA_VERSION))
 BATOCERA_STELLA_LICENSE = GPL-2.0+
 BATOCERA_STELLA_LICENSE_FILES = Copyright.txt License.txt
 
