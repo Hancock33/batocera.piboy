c6dea1e66177dd98df9937210b4b75f3a3bd45f5
Lee <lee.hancock4@ntlworld.com>
2023-10-14 13:32:47 +0100

    revert devilutionx

--- package/batocera/ports/devilutionx/devilutionx.mk
+++ package/batocera/ports/devilutionx/devilutionx.mk
@@ -3,8 +3,8 @@
 # devilutionx
 #
 ################################################################################
-# Version: Commits on Oct 14, 2023
-DEVILUTIONX_VERSION = 88bbf4f913270ffbdb9482ce48cf733730f68282
+# Version: Commits on Oct 03, 2023
+DEVILUTIONX_VERSION = e18b37a7df6a2328bbe53f85c7fe7d222562f1ee
 DEVILUTIONX_SITE = https://github.com/diasurgical/devilutionX.git
 DEVILUTIONX_SITE_METHOD=git
 DEVILUTIONX_SUBDIR = dist-src
