78b328eb1df24754431d9018aa5f61670179201d
Lee <lee.hancock4@ntlworld.com>
2023-10-15 14:16:54 +0100

    fix abuse assets

--- package/batocera/ports/abuse/abuse.mk
+++ package/batocera/ports/abuse/abuse.mk
@@ -13,14 +13,13 @@ ABUSE_SUPPORTS_IN_SOURCE_BUILD = NO
 ABUSE_CONF_OPTS += -DASSETDIR=/usr/share/abuse
 
 define ABUSE_INSTALL_TARGET_CMDS
-	mkdir -p $(TARGET_DIR)/usr/share/abuse
+	# install game data
+	mkdir -p $(TARGET_DIR)/usr/share/abuse/
+	tar -xf $(ABUSE_DL_DIR)/abuse-data-2.00.tar.gz -C $(TARGET_DIR)/usr/share/abuse/
 	$(INSTALL) -D -m 0755 $(@D)/buildroot-build/src/abuse $(TARGET_DIR)/usr/bin/abuse
 	cp -pvr $(@D)/data/* $(TARGET_DIR)/usr/share/abuse
 	rm $(TARGET_DIR)/usr/share/abuse/CMakeLists.txt
 	rm $(TARGET_DIR)/usr/share/abuse/READMD.md
-	# install game data
-	mkdir -p $(TARGET_DIR)/usr/share/abuse/
-	tar -xf $(ABUSE_DL_DIR)/abuse-data-2.00.tar.gz -C $(TARGET_DIR)/usr/share/abuse/
 	# install media
 	mkdir -p $(TARGET_DIR)/usr/share/emulationstation/ports/abuse
 	cp -a  $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/ports/abuse/media/* $(TARGET_DIR)/usr/share/emulationstation/ports/abuse/
