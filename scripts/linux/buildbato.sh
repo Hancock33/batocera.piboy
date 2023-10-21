d6b7c1db4b52f9ad543f8ea13fc423088325b9fe
Lee <lee.hancock4@ntlworld.com>
2023-10-21 10:42:53 +0100

    add personal crude build scripts

--- scripts/linux/buildbato.sh
+++ scripts/linux/buildbato.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "1 - Generating configurations"
+echo "Generating configurations"
 cd $HOME/batocera.se && 
 git pull
 git submodule init
@@ -8,21 +8,33 @@ git submodule update
 ./build.sh $HOME/build-dir/batocera.rpi4 piboy4 > /dev/null 2>&1
 ./build.sh $HOME/build-dir/batocera.x86_64 x86_64 > /dev/null 2>&1
 ./build.sh $HOME/build-dir/batocera.x86_wow64 x86_wow64 > /dev/null 2>&1
+rm -r $HOME/build-dir/batocera.*/build/batocera-system-* > /dev/null 2>&1
+rm -r $HOME/build-dir/batocera.*/build/batocera-splash-* > /dev/null 2>&1
+rm -r $HOME/build-dir/batocera.*/build/batocera-es-system-* > /dev/null 2>&1
+rm -r $HOME/build-dir/batocera.*/build/batocera-configgen-* > /dev/null 2>&1
 
-echo "2 - Cleaning batocera packages"
-rm -r $HOME/build-dir/batocera.*/build/batocera-a* > /dev/null 2>&1
-rm -r $HOME/build-dir/batocera.*/build/batocera-b* > /dev/null 2>&1
-rm -r $HOME/build-dir/batocera.*/build/batocera-c* > /dev/null 2>&1
-rm -r $HOME/build-dir/batocera.*/build/batocera-d* > /dev/null 2>&1
-rm -r $HOME/build-dir/batocera.*/build/batocera-e* > /dev/null 2>&1
-rm -r $HOME/build-dir/batocera.*/build/batocera-g* > /dev/null 2>&1
-rm -r $HOME/build-dir/batocera.*/build/batocera-i* > /dev/null 2>&1
-rm -r $HOME/build-dir/batocera.*/build/batocera-notice* > /dev/null 2>&1
-rm -r $HOME/build-dir/batocera.*/build/batocera-p* > /dev/null 2>&1
-rm -r $HOME/build-dir/batocera.*/build/batocera-r* > /dev/null 2>&1
-rm -r $HOME/build-dir/batocera.*/build/batocera-s* > /dev/null 2>&1
-rm -r $HOME/build-dir/batocera.*/build/batocera-t* > /dev/null 2>&1
-rm -r $HOME/build-dir/batocera.*/build/batocera-w* > /dev/null 2>&1
+read -p "Clean Batocera packages? (y/n)" yn
+case $yn in 
+	y )
+	    echo "Clean Batocera packages"
+        rm -r $HOME/build-dir/batocera.*/build/batocera-a* > /dev/null 2>&1
+        rm -r $HOME/build-dir/batocera.*/build/batocera-b* > /dev/null 2>&1
+        rm -r $HOME/build-dir/batocera.*/build/batocera-c* > /dev/null 2>&1
+        rm -r $HOME/build-dir/batocera.*/build/batocera-d* > /dev/null 2>&1
+        rm -r $HOME/build-dir/batocera.*/build/batocera-e* > /dev/null 2>&1
+        rm -r $HOME/build-dir/batocera.*/build/batocera-g* > /dev/null 2>&1
+        rm -r $HOME/build-dir/batocera.*/build/batocera-i* > /dev/null 2>&1
+        rm -r $HOME/build-dir/batocera.*/build/batocera-notice* > /dev/null 2>&1
+        rm -r $HOME/build-dir/batocera.*/build/batocera-p* > /dev/null 2>&1
+        rm -r $HOME/build-dir/batocera.*/build/batocera-r* > /dev/null 2>&1
+        rm -r $HOME/build-dir/batocera.*/build/batocera-s* > /dev/null 2>&1
+        rm -r $HOME/build-dir/batocera.*/build/batocera-t* > /dev/null 2>&1
+        rm -r $HOME/build-dir/batocera.*/build/batocera-w* > /dev/null 2>&1
+        ;;
+	n )
+        echo "Moving on ..."
+        ;;
+esac
 
 # Variables
 BUILD_SUB=0
@@ -32,7 +44,7 @@ make_sub_sys=""
 make_distro=""
 
 # Build subsystems
-read -p "3 - Build subsystems? (y/n)" yn
+read -p "Build subsystems? (y/n)" yn
 case $yn in 
 	y )
         BUILD_SUB=1;;
@@ -41,7 +53,7 @@ case $yn in
 esac
 
 # Build RPI subsystems + distro
-read -p "4 - Build RPI? (y/n)" yn
+read -p "Build RPI? (y/n)" yn
 case $yn in 
 	y )
         BUILD_RPI=1;; 
@@ -50,7 +62,7 @@ case $yn in
 esac
 
 # Build X86 subsystems + distro
-read -p "5 - Build X86? (y/n)" yn
+read -p "Build X86? (y/n)" yn
 case $yn in 
 	y )
         BUILD_X86=1;;
@@ -58,14 +70,14 @@ case $yn in
         BUILD_X86=0;;
 esac
 
-if [ $BUILD_RPI = '1' ]; then
-	make_sub_sys="aarch32"
-	make_distro=" rpi4"
+if [ $BUILD_X86 = '1' ]; then
+	make_sub_sys="x86_wow64"
+	make_distro="x86_64"
 fi
 
-if [ $BUILD_X86 = '1' ]; then
-	make_sub_sys=$make_sub_sys" x86_wow64"
-	make_distro=$make_distro" x86_64"
+if [ $BUILD_RPI = '1' ]; then
+	make_sub_sys=$make_sub_sys" aarch32"
+	make_distro=$make_distro" rpi4"
 fi
 
 # Build subsystems
@@ -74,7 +86,7 @@ if [ $BUILD_SUB = '1' ]; then
     do
         echo "Building subsystem: "$i
         cd $HOME/build-dir/batocera.$i
-        make > /dev/null
+        make -j33 > /dev/null
     done
 fi
 
@@ -83,7 +95,7 @@ for i in $make_distro
 do
     echo "Building: "$i
     cd $HOME/build-dir/batocera.$i
-    make > /dev/null
+    make -j33 > /dev/null
 done
 
 # Cleanup
@@ -94,8 +106,8 @@ x86_64"
 
 for i in $cleanup
 do
-    echo "Cleaning subsystem: "$i
+    echo "Cleaning: "$i
 	find $HOME/build-dir/batocera.$i/build -maxdepth 1 -type d -printf '%T@ %p %f\n' | sed -r 's:\-[0-9a-f\.]+$$::' | sort -k3 -k1 | uniq -f 2 -d | cut -d' ' -f2 | xargs rm -rf
 done
 
-find $HOME/dl -maxdepth 2 -type f -name "*.zip" -o -name "*.tar.*" -printf '%T@ %p %f\n' | sed -r 's:\-[0-9a-f\.]+(\.zip|\.tar\.[2a-z]+)$$::' | sort -k3 -k1 | uniq -f 2 -d | cut -d' ' -f2 | xargs rm -rf
+#find $HOME/dl -maxdepth 2 -type f -name "*.zip" -o -name "*.tar.*" -printf '%T@ %p %f\n' | sed -r 's:\-[0-9a-f\.]+(\.zip|\.tar\.[2a-z]+)$$::' | sort -k3 -k1 | uniq -f 2 -d | cut -d' ' -f2 | xargs rm -rf
