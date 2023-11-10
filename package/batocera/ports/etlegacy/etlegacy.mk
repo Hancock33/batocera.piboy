47358a6c428ed768150687455854c83135179a75
Lee <lee.hancock4@ntlworld.com>
2023-11-10 20:57:19 +0000

    updates

--- package/batocera/ports/etlegacy/etlegacy.mk
+++ package/batocera/ports/etlegacy/etlegacy.mk
@@ -12,7 +12,7 @@ ETLEGACY_LICENSE = GPL-3.0
 ETLEGACY_LICENSE_FILE = COPYING.txt
 
 ETLEGACY_DEPENDENCIES += freetype libcurl libglew libpng libtheora
-ETLEGACY_DEPENDENCIES += libvorbis lua openal openssl sdl2
+ETLEGACY_DEPENDENCIES += libvorbis luajit openal openssl sdl2
 
 ETLEGACY_SUPPORTS_IN_SOURCE_BUILD = NO
 
