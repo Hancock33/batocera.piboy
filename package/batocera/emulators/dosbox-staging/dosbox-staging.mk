9d233d13813eb8ac93c90a7b00602d08af9f2046
Lee <lee.hancock4@ntlworld.com>
2023-12-16 09:25:29 +0000

    tryout libmt32 on dosbox-staging

--- package/batocera/emulators/dosbox-staging/dosbox-staging.mk
+++ package/batocera/emulators/dosbox-staging/dosbox-staging.mk
@@ -12,7 +12,7 @@ DOSBOX_STAGING_LICENSE = GPLv2
 DOSBOX_STAGING_CPPFLAGS = -DNDEBUG
 DOSBOX_STAGING_CFLAGS   = -O3 -fstrict-aliasing -fno-signed-zeros -fno-trapping-math -fassociative-math -frename-registers -ffunction-sections -fdata-sections
 DOSBOX_STAGING_CXXFLAGS = -O3 -fstrict-aliasing -fno-signed-zeros -fno-trapping-math -fassociative-math -frename-registers -ffunction-sections -fdata-sections
-DOSBOX_STAGING_CONF_OPTS = -Duse_mt32emu=false
+DOSBOX_STAGING_CONF_OPTS += -Duse_zlib_ng=system
 
 ifneq ($(BR2_PACKAGE_BATOCERA_TARGET_X86_64_ANY),y)
 DOSBOX_STAGING_CONF_OPTS += -Duse_opengl=false
@@ -88,6 +88,8 @@ define DOSBOX_STAGING_DL_DEPENDENCIES
 	mkdir -p  $(DOSBOX_STAGING_DL_DIR)/sumodules
 	curl -L https://github.com/glennrp/libpng/archive/v1.6.40.tar.gz					-o $(DOSBOX_STAGING_DL_DIR)/sumodules/libpng-1.6.40.tar.gz
 	curl -L https://wrapdb.mesonbuild.com/v2/libpng_1.6.40-1/get_patch					-o $(DOSBOX_STAGING_DL_DIR)/sumodules/libpng_1.6.40-1_patch.zip
+	curl -L https://github.com/munt/munt/archive/libmt32emu_2_7_1.tar.gz				-o $(DOSBOX_STAGING_DL_DIR)/sumodules/libmt32emu_2_7_1.tar.gz
+	curl -L https://wrapdb.mesonbuild.com/v2/mt32emu_2.7.1-1/get_patch					-o $(DOSBOX_STAGING_DL_DIR)/sumodules/mt32emu_2.7.1-1_patch.zip
 	mkdir -p $(@D)/subprojects/packagecache
 	cp  $(DOSBOX_STAGING_DL_DIR)/sumodules/* $(@D)/subprojects/packagecache
 endef
