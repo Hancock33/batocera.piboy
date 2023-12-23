fa3063a80bdea923fa4f5e17d2fb9c1121295238
Lee <lee.hancock4@ntlworld.com>
2023-12-23 21:50:52 +0000

    add cubeb for cemu

---
+++ package/batocera/audio/cubeb/cubeb.mk
@@ -0,0 +1,19 @@
+################################################################################
+#
+# cubeb
+#
+################################################################################
+# Version: Commits on Dec 12, 2023
+CUBEB_VERSION = ed2efe22cb803f022b5a38fbbc53eec250056874
+CUBEB_SITE = $(call github,mozilla,cubeb,$(CUBEB_VERSION))
+
+CUBEB_LICENSE = ISC
+CUBEB_SUPPORTS_IN_SOURCE_BUILD = NO
+CUBEB_INSTALL_STAGING = YES
+CUBEB_DEPENDENCIES = alsa-lib speex 
+
+CUBEB_CONF_OPTS += -DBUILD_TOOLS=no
+CUBEB_CONF_OPTS += -DLAZY_LOAD_LIBS=no
+CUBEB_CONF_OPTS += -DUSE_SANITIZERS=no
+
+$(eval $(cmake-package))
