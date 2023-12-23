7d672e1b2cab721abe9931766386dbd8596b65d5
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2023-12-23 10:54:08 +0000

    bump glslang

--- package/batocera/gpu/vulkan/glslang/glslang.mk
+++ package/batocera/gpu/vulkan/glslang/glslang.mk
@@ -3,8 +3,8 @@
 # glslang
 #
 ################################################################################
-# Version: Commits on Oct 17, 2023
-GLSLANG_VERSION = 1.3.268.0
+# Version: Commits on Dec 22, 2023
+GLSLANG_VERSION = 14.0.0
 GLSLANG_SITE = $(call github,KhronosGroup,glslang,vulkan-sdk-$(GLSLANG_VERSION))
 GLSLANG_DEPENDENCIES = vulkan-headers vulkan-loader spirv-tools
 GLSLANG_INSTALL_STAGING = YES
@@ -17,13 +17,6 @@ GLSLANG_CONF_ENV += LDFLAGS="--lpthread -ldl"
 ifeq ($(BR2_PACKAGE_MESA3D),y)
 GLSLANG_DEPENDENCIES += mesa3d
 endif
-
-define GLSLANG_INSTALL_OSDEPENENT
-	cp -av $(GLSLANG_BUILDDIR)/glslang/OSDependent/Unix/libOSDependent.a $(STAGING_DIR)/usr/lib
-	cp -av $(GLSLANG_BUILDDIR)/OGLCompilersDLL/libOGLCompiler.a		  $(STAGING_DIR)/usr/lib
-endef
-
-GLSLANG_POST_INSTALL_TARGET_HOOKS = GLSLANG_INSTALL_OSDEPENENT
 
 $(eval $(cmake-package))
 $(eval $(host-cmake-package))
