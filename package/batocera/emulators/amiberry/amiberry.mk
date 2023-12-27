e203723d922fd0619a9f1b8e72a114308e20a6b7
Lee <lee.hancock4@ntlworld.com>
2023-12-27 22:16:50 +0000

    tryout amiberry6 preview

--- package/batocera/emulators/amiberry/amiberry.mk
+++ package/batocera/emulators/amiberry/amiberry.mk
@@ -3,33 +3,26 @@
 # amiberry
 #
 ################################################################################
-# Version: Commits on Nov 19, 2023
-AMIBERRY_VERSION = v5.6.4
+# Version: Commits on Dec 27, 2023 (branch@preview)
+AMIBERRY_VERSION = e8c1e40657b84c6c49e7d19966e52717d651e618
 AMIBERRY_SITE = $(call github,BlitterStudio,amiberry,$(AMIBERRY_VERSION))
 AMIBERRY_LICENSE = GPLv3
 AMIBERRY_DEPENDENCIES = sdl2 sdl2_image sdl2_ttf mpg123 libxml2 libmpeg2 flac libpng libserialport portmidi
 
 define AMIBERRY_INSTALL_TARGET_CMDS
-	$(INSTALL) -D $(@D)/amiberry $(TARGET_DIR)/usr/bin/amiberry
-		mkdir -p $(TARGET_DIR)/usr/share/amiberry
+	$(INSTALL) -D $(@D)/amiberry 						$(TARGET_DIR)/usr/bin/amiberry
+	mkdir -p											$(TARGET_DIR)/usr/share/amiberry
+	cp -rf $(@D)/data									$(TARGET_DIR)/usr/share/amiberry
+	ln -sf /userdata/system/configs/amiberry/whdboot	$(TARGET_DIR)/usr/share/amiberry/whdboot
+	ln -sf /usr/lib/libcapsimage.so.5.1 				$(TARGET_DIR)/usr/share/amiberry/capsimg.so
 
-	ln -sf /userdata/system/configs/amiberry/whdboot $(TARGET_DIR)/usr/share/amiberry/whdboot
-		mkdir -p $(TARGET_DIR)/usr/share/batocera/datainit/system/configs/amiberry
-	ln -sf /usr/lib/libcapsimage.so.5.1 $(TARGET_DIR)/usr/share/amiberry/capsimg.so
-
-	cp -pr $(@D)/whdboot $(TARGET_DIR)/usr/share/batocera/datainit/system/configs/amiberry/
-	cp -rf $(@D)/data $(TARGET_DIR)/usr/share/amiberry
+	mkdir -p 											$(TARGET_DIR)/usr/share/batocera/datainit/system/configs/amiberry
+	cp -pvr $(@D)/whdboot 								$(TARGET_DIR)/usr/share/batocera/datainit/system/configs/amiberry
 endef
 
 define AMIBERRY_EVMAP
 	mkdir -p $(TARGET_DIR)/usr/share/evmapy
-
-	cp -prn $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/emulators/amiberry/controllers/amiga500.amiberry.keys \
-		$(TARGET_DIR)/usr/share/evmapy
-	cp -prn $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/emulators/amiberry/controllers/amiga1200.amiberry.keys \
-		$(TARGET_DIR)/usr/share/evmapy
-	cp -prn $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/emulators/amiberry/controllers/amigacd32.amiberry.keys \
-		$(TARGET_DIR)/usr/share/evmapy
+	cp -pvr $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/emulators/amiberry/controllers/*.amiberry.keys $(TARGET_DIR)/usr/share/evmapy
 endef
 
 AMIBERRY_POST_INSTALL_TARGET_HOOKS = AMIBERRY_EVMAP
