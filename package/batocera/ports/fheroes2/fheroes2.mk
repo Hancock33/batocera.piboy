cd010f6a085b69ba8b5923dbc3a5d1c8fd3795d5
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2023-12-27 18:58:07 +0000

    evening updates

--- package/batocera/ports/fheroes2/fheroes2.mk
+++ package/batocera/ports/fheroes2/fheroes2.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 # Version: Commits on Dec 27, 2023
-FHEROES2_VERSION = c0e6ce80d26e14c154c49c18bf946485f69e0a59
+FHEROES2_VERSION = 05171cfe671dfb2d205eb7345e4b7d183721954a
 FHEROES2_SITE = $(call github,ihhub,fheroes2,$(FHEROES2_VERSION))
 FHEROES2_DEPENDENCIES = sdl2 sdl2_image
 
