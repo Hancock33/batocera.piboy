2e45cfa49a880954bf224a9744ae9a8b325c57fa
Lee <lee.hancock4@ntlworld.com>
2023-12-31 20:42:32 +0000

    enable libretro-pcsx2

--- package/batocera/emulators/retroarch/libretro/libretro-pcsx2/libretro-pcsx2.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-pcsx2/libretro-pcsx2.mk
@@ -13,13 +13,13 @@ LIBRETRO_PCSX2_SUPPORTS_IN_SOURCE_BUILD = NO
 LIBRETRO_PCSX2_LICENSE = GPLv2
 LIBRETRO_PCSX2_DEPENDENCIES = libaio xz host-xxd
 
-LIBRETRO_PCSX2_CONF_OPTS += -DQT_BUILD=OFF
-LIBRETRO_PCSX2_CONF_OPTS += -DUSE_VULKAN=ON
+LIBRETRO_PCSX2_CONF_OPTS += -DBUILD_SHARED_LIBS=OFF
 LIBRETRO_PCSX2_CONF_OPTS += -DLIBRETRO=ON
+LIBRETRO_PCSX2_CONF_OPTS += -DUSE_VULKAN=ON
 
 define LIBRETRO_PCSX2_INSTALL_TARGET_CMDS
-	$(INSTALL) -D $(@D)/pcsx2/pcsx2_libretro.so \
-		$(TARGET_DIR)/usr/lib/libretro/pcsx2_libretro.so
+	mkdir -p $(TARGET_DIR)/usr/lib/libretro
+	$(INSTALL) -D $(@D)/buildroot-build/bin/pcsx2_libretro.so $(TARGET_DIR)/usr/lib/libretro/pcsx2_libretro.so
 endef
 
 $(eval $(cmake-package))
