069b1258cf753082ba044a0723bab1149b33ae42
Lee <lee.hancock4@ntlworld.com>
2024-01-14 10:58:06 +0000

    bump vpinball

--- package/batocera/audio/libaltsound/libaltsound.mk
+++ package/batocera/audio/libaltsound/libaltsound.mk
@@ -3,15 +3,19 @@
 # libaltsound
 #
 ################################################################################
-# Version: Commits on Dec 26, 2023
-LIBALTSOUND_VERSION = 816cc987db61c428c61746b65ab30aa765c87116
+# Version: Commits on Jan 13, 2024
+LIBALTSOUND_VERSION = cc1b66f4f8784acd028565c79ebdc335da3c6749
 LIBALTSOUND_SITE = $(call github,vpinball,libaltsound,$(LIBALTSOUND_VERSION))
-LIBALTSOUND_LICENSE = GPLv3+
+LIBALTSOUND_LICENSE = BSD-3-Clause
 LIBALTSOUND_LICENSE_FILES = LICENSE
-LIBALTSOUND_DEPENDENCIES = libfreeimage libpinmame libserum libzedmd sdl2 sdl2_image sdl2_ttf
+LIBALTSOUND_DEPENDENCIES = host-libcurl
 LIBALTSOUND_SUPPORTS_IN_SOURCE_BUILD = NO
-LIBALTSOUND_INSTALL_STAGING = YES
 LIBALTSOUND_EXTRA_DOWNLOADS = https://www.un4seen.com/files/bass24-linux.zip
+
+LIBALTSOUND_CONF_OPTS += -DCMAKE_BUILD_TYPE=Release
+LIBALTSOUND_CONF_OPTS += -DBUILD_STATIC=OFF
+LIBALTSOUND_CONF_OPTS += -DPLATFORM=linux
+LIBALTSOUND_CONF_OPTS += -DARCH=$(PKG_ARCH)
 
 # handle supported target platforms
 ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_RK3588),y)
@@ -29,18 +33,18 @@ ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_X86_64_ANY),y)
 	BASS_ALT_ARCH = x86_64
 endif
 
-
-LIBALTSOUND_CONF_OPTS += -DPLATFORM=linux -DBUILD_ARCH=$(PKG_ARCH) -DBUILD_STATIC=OFF
-
-define LIBALTSOUND_CMAKE_HACKS
-	sed -i -e s+" 3.25"+" 3.22"+								$(@D)/CMakeLists.txt
-	rm -rf $(@D)/tmp && mkdir									$(@D)/tmp
-	$(UNZIP) -o $(VPINBALL_DL_DIR)/bass24-linux.zip -d			$(@D)/tmp
-	cp $(@D)/tmp/bass.h											$(@D)/third-party/include
-	cp $(@D)/tmp/libs/$(BASS_ALT_ARCH)/libbass.so 				$(@D)/third-party/runtime-libs/linux/$(PKG_ARCH)
-	cp $(@D)/tmp/bass.h 										$(@D)/third-party/include
+define LIBALTSOUND_BASS_HACKS
+	sed -i -e s+" 3.25"+" 3.22"+						$(@D)/CMakeLists.txt
+	rm -rf $(@D)/tmp && mkdir							$(@D)/tmp
+	$(UNZIP) -o $(VPINBALL_DL_DIR)/bass24-linux.zip -d	$(@D)/tmp
+	cp $(@D)/tmp/bass.h									$(@D)/third-party/include
+	cp $(@D)/tmp/libs/$(BASS_ALT_ARCH)/libbass.so 		$(@D)/third-party/runtime-libs/linux/$(PKG_ARCH)
+	cp $(@D)/tmp/bass.h 								$(@D)/third-party/include
 endef
 
-LIBALTSOUND_PRE_CONFIGURE_HOOKS += LIBALTSOUND_CMAKE_HACKS
+# Install to staging to build Visual Pinball Standalone
+LIBALTSOUND_INSTALL_STAGING = YES
+
+LIBALTSOUND_PRE_CONFIGURE_HOOKS += LIBALTSOUND_BASS_HACKS
 
 $(eval $(cmake-package))
---
+++ package/batocera/libraries/libdmdutil/libdmdutil.mk
@@ -0,0 +1,44 @@
+################################################################################
+#
+# libdmdutil
+#
+################################################################################
+# Version: Commits on Jan 13, 2024
+LIBDMDUTIL_VERSION = 8b1dca4900885aaa38b07f7d2e5a0f6370ba5658
+LIBDMDUTIL_SITE = $(call github,vpinball,libdmdutil,$(LIBDMDUTIL_VERSION))
+LIBDMDUTIL_LICENSE = BSD-3-Clause
+LIBDMDUTIL_LICENSE_FILES = LICENSE
+LIBDMDUTIL_DEPENDENCIES = libserialport libzedmd libserum
+LIBDMDUTIL_SUPPORTS_IN_SOURCE_BUILD = NO
+
+LIBDMDUTIL_CONF_OPTS += -DCMAKE_BUILD_TYPE=Release
+LIBDMDUTIL_CONF_OPTS += -DBUILD_STATIC=OFF
+LIBDMDUTIL_CONF_OPTS += -DPLATFORM=linux
+LIBDMDUTIL_CONF_OPTS += -DARCH=$(DMDUTIL_ARCH)
+
+# handle supported target platforms
+ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_RK3588),y)
+	DMDUTIL_ARCH = aarch64
+endif
+
+ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_BCM2711)$(BR2_PACKAGE_BATOCERA_TARGET_BCM2712),y)
+	DMDUTIL_ARCH = aarch64
+endif
+
+ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_X86_64_ANY),y)
+	DMDUTIL_ARCH = x64
+endif
+
+define LIBDMDUTIL_CMAKE_HACKS
+	## derived from platforms/${PLATFORM}/${DMDUTIL_ARCH}/external.sh and CMakeLists.txt ##
+	$(SED) 's:third-party/include$$:$(STAGING_DIR)/usr/include/\n	third-party/include:g'						$(@D)/CMakeLists.txt
+	$(SED) 's:$${CMAKE_SOURCE_DIR}/third-party/runtime-libs/$${PLATFORM}/$${ARCH}/:$(STAGING_DIR)/usr/lib/:g'	$(@D)/CMakeLists.txt
+	$(SED) 's:third-party/runtime-libs/$${PLATFORM}/$${ARCH}:$(STAGING_DIR)/usr/lib/:g'							$(@D)/CMakeLists.txt
+endef
+
+# Install to staging to build Visual Pinball Standalone
+LIBDMDUTIL_INSTALL_STAGING = YES
+
+LIBDMDUTIL_PRE_CONFIGURE_HOOKS += LIBDMDUTIL_CMAKE_HACKS
+
+$(eval $(cmake-package))
