5acfe7a8838cf98a482de3fa71dae468a8c93d83
Lee <lee.hancock4@ntlworld.com>
2024-01-14 13:34:43 +0000

    use wine-staging patches

--- package/batocera/emulators/wine/wine-lutris-wow64_32/wine-lutris-wow64_32.mk
+++ package/batocera/emulators/wine/wine-lutris-wow64_32/wine-lutris-wow64_32.mk
@@ -14,7 +14,7 @@ WINE_LUTRIS_WOW64_32_CPE_ID_VENDOR = winehq
 WINE_LUTRIS_WOW64_32_SELINUX_MODULES = wine
 WINE_LUTRIS_WOW64_32_DEPENDENCIES = host-bison host-flex host-wine-lutris
 HOST_WINE_LUTRIS_WOW64_32_DEPENDENCIES = host-bison host-flex host-clang host-lld
-#HOST_WINE_LUTRIS_EXTRA_DOWNLOADS = https://github.com/wine-staging/wine-staging/archive/refs/tags/v$(WINE_LUTRIS_WOW64_32_STAGING_VERSION).tar.gz
+HOST_WINE_LUTRIS_EXTRA_DOWNLOADS = https://github.com/wine-staging/wine-staging/archive/refs/tags/v$(WINE_LUTRIS_WOW64_32_STAGING_VERSION).tar.gz
 
 # Configure Lutris
 define WINE_LUTRIS_WOW64_32_AUTOGEN
@@ -26,9 +26,9 @@ define WINE_LUTRIS_WOW64_32_AUTOGEN
 	# Create folder for install
 	mkdir -p $(TARGET_DIR)/usr/wine/lutris
 	# Use Staging Patches
-	#printf "%s\n" "$(TERM_BOLD)>>> $($(PKG)_NAME) $($(PKG)_VERSION) Patching wine-staging" >&2
-	#tar -xf $(WINE_LUTRIS_DL_DIR)/v$(WINE_LUTRIS_WOW64_32_STAGING_VERSION).tar.gz -C $(@D)
-	#cd $(@D); ./wine-staging-$(WINE_LUTRIS_WOW64_32_STAGING_VERSION)/staging/patchinstall.py --all
+	printf "%s\n" "$(TERM_BOLD)>>> $($(PKG)_NAME) $($(PKG)_VERSION) Patching wine-staging" >&2
+	tar -xf $(WINE_LUTRIS_DL_DIR)/v$(WINE_LUTRIS_WOW64_32_STAGING_VERSION).tar.gz -C $(@D)
+	cd $(@D); ./wine-staging-$(WINE_LUTRIS_WOW64_32_STAGING_VERSION)/staging/patchinstall.py --all
 	# Autotools generation
 	cd $(@D); autoreconf -fiv
 	cd $(@D); ./tools/make_requests
