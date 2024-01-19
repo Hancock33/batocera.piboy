bcb3833aec5a0776b7eecdf58a7754fa0a099820
Lee <lee.hancock4@ntlworld.com>
2024-01-19 23:35:28 +0000

    bump ryujinx

--- package/batocera/emulators/ryujinx/ryujinx.mk
+++ package/batocera/emulators/ryujinx/ryujinx.mk
@@ -3,8 +3,8 @@
 # ryujinx
 #
 ################################################################################
-# Version: Commits on Jan 16, 2024
-RYUJINX_VERSION = 1.1.1111
+# Version: Commits on Jan 18, 2024
+RYUJINX_VERSION = 1.1.1114
 RYUJINX_SOURCE = ryujinx-$(RYUJINX_VERSION)-linux_x64.tar.gz
 RYUJINX_SITE = https://github.com/Ryujinx/release-channel-master/releases/download/$(RYUJINX_VERSION)
 RYUJINX_LICENSE = MIT
