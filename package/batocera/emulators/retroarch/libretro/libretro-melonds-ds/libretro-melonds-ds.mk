c5f5bb6c2a146538ff4a9689aa6231aebde83d97
Lee <lee.hancock4@ntlworld.com>
2024-01-25 21:09:25 +0000

    update libretro-melons-ds

--- package/batocera/emulators/retroarch/libretro/libretro-melonds-ds/libretro-melonds-ds.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-melonds-ds/libretro-melonds-ds.mk
@@ -11,15 +11,14 @@ LIBRETRO_MELONDS_DS_DEPENDENCIES = libpcap
 
 LIBRETRO_MELONDS_DS_SUPPORTS_IN_SOURCE_BUILD = NO
 
-LIBRETRO_MELONDS_DS_CONF_OPTS += -DCMAKE_BUILD_TYPE=Release
+LIBRETRO_MELONDS_DS_CONF_OPTS += -DCMAKE_C_COMPILER=$(HOST_DIR)/bin/$(GNU_TARGET_NAME)-gcc
+LIBRETRO_MELONDS_DS_CONF_OPTS += -DCMAKE_CXX_COMPILER=$(HOST_DIR)/bin/$(GNU_TARGET_NAME)-g++
 LIBRETRO_MELONDS_DS_CONF_OPTS += -DBUILD_SHARED_LIBS=OFF
 
 define LIBRETRO_MELONDS_DS_INSTALL_TARGET_CMDS
-	$(INSTALL) -D $(@D)/buildroot-build/src/libretro/melondsds_libretro.so \
-		$(TARGET_DIR)/usr/lib/libretro/melondsds_libretro.so
 	mkdir -p $(TARGET_DIR)/usr/share/libretro/info
-	cp $(@D)/buildroot-build/melondsds_libretro.info \
-	    $(TARGET_DIR)/usr/share/libretro/info/melondsds_libretro.info
+	$(INSTALL) -D $(@D)/buildroot-build/src/libretro/melondsds_libretro.so	$(TARGET_DIR)/usr/lib/libretro/melondsds_libretro.so
+	cp $(@D)/buildroot-build/melondsds_libretro.info						$(TARGET_DIR)/usr/share/libretro/info/melondsds_libretro.info
 endef
 
 $(eval $(cmake-package))
