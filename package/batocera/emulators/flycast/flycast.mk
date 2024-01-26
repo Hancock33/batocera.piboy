cdbd3085702ce9625e555513b0f06dcec25e7d3c
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2024-01-26 22:25:13 +0000

    some more updates

--- package/batocera/emulators/flycast/flycast.mk
+++ package/batocera/emulators/flycast/flycast.mk
@@ -3,8 +3,8 @@
 # flycast
 #
 ################################################################################
-# Version: Commits on Jan 22, 2024
-FLYCAST_VERSION = 07d6ca6c846ec40ff7f78cbf9f669e70afc956d1
+# Version: Commits on Jan 26, 2024
+FLYCAST_VERSION = e1537ddbda8d6f77c3789026e49e41138e459845
 FLYCAST_SITE = https://github.com/flyinghead/flycast.git
 FLYCAST_SITE_METHOD=git
 FLYCAST_GIT_SUBMODULES=YES
