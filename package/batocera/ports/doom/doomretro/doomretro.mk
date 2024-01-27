c3b202d61200d5d6affcffcc98e9138bb9624999
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2024-01-27 00:12:06 +0000

    some updates

--- package/batocera/ports/doom/doomretro/doomretro.mk
+++ package/batocera/ports/doom/doomretro/doomretro.mk
@@ -3,8 +3,8 @@
 # doomretro
 #
 ################################################################################
-# Version: Commits on Jan 26, 2024
-DOOMRETRO_VERSION = 422c84ae12bb6d68d1bc10d5486e376a9cb13041
+# Version: Commits on Jan 27, 2024
+DOOMRETRO_VERSION = ec57f9d700f966acb71090b730ccb0d0b369efad
 DOOMRETRO_SITE = $(call github,bradharding,doomretro,$(DOOMRETRO_VERSION))
 
 DOOMRETRO_DEPENDENCIES = sdl2 sdl2_mixer sdl2_image
--- package/batocera/ports/fheroes2/fheroes2.mk
+++ package/batocera/ports/fheroes2/fheroes2.mk
@@ -3,8 +3,8 @@
 # fheroes2
 #
 ################################################################################
-# Version: Commits on Jan 25, 2024
-FHEROES2_VERSION = 3bfc40c8a9d1c3534b22b2a20da034c2aaacd821
+# Version: Commits on Jan 26, 2024
+FHEROES2_VERSION = 79d53bee02426972539527c5292ee5d7637900ce
 FHEROES2_SITE = $(call github,ihhub,fheroes2,$(FHEROES2_VERSION))
 FHEROES2_DEPENDENCIES = sdl2 sdl2_image
 
