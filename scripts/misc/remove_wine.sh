6ddeab1b5055b894a2fd15a421312023643ac46d
Lee <lee.hancock4@ntlworld.com>
2024-01-28 17:54:34 +0000

    bump wine-9.1 + staging

---
+++ scripts/misc/remove_wine.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+GREEN="\e[1;32m"
+RED="\e[1;31m"
+ENDCOLOR="\e[0m"
+STARTLINE="\e[1;44m--------------------------------------------------\n"
+ENDLINE="\n--------------------------------------------------"${ENDCOLOR}
+clear
+echo -e ${STARTLINE}"Removing Batocera Packages                        "${ENDLINE}
+
+batowine86=( $(find $HOME/batocera.se/package/batocera/emulators/wine-x86 -type f -name "*.mk" | sort -k3 -k1 ) )
+batowine=( $(find $HOME/batocera.se/package/batocera/emulators/wine -type f -name "*.mk" | sort -k3 -k1 ) )
+
+# Variables
+BUILD_X86=0
+
+
+# Build X86 image
+read -p "Clean Wine Build X86? (y/n)" yn
+case $yn in 
+	y )
+        BUILD_X86=1;;
+	n )
+        BUILD_X86=0;;
+esac
+
+if [ $BUILD_X86 = '1' ]; then
+	for ((i=0; i<${#batowine86[@]}; i++)); do
+		f=( $(basename "${batowine86[$i]}" .mk ) )
+		echo -e "removing x86_64 package: "${RED}$f${ENDCOLOR}
+		rm -rf $HOME/build-dir/batocera.x86_64/build/$f-*
+		rm -rf $HOME/build-dir/batocera.x86_64/build/host-$f-*
+		rm -rf $HOME/build-dir/batocera.x86_wow64/build/$f-*
+		rm -rf $HOME/build-dir/batocera.x86_wow64/build/host-$f-*
+	done
+	for ((i=0; i<${#batowine[@]}; i++)); do
+		f=( $(basename "${batowine[$i]}" .mk ) )
+		echo -e "removing x86_64 package: "${RED}$f${ENDCOLOR}
+		rm -rf $HOME/build-dir/batocera.x86_64/build/$f-*
+		rm -rf $HOME/build-dir/batocera.x86_64/build/host-$f-*
+		rm -rf $HOME/build-dir/batocera.x86_wow64/build/$f-*
+		rm -rf $HOME/build-dir/batocera.x86_wow64/build/host-$f-*
+	done
+	
+	rm -rf $HOME/build-dir/batocera.x86_64/build/wine*
+	rm -rf $HOME/build-dir/batocera.x86_64/build/batocera-wine*
+	rm -rf $HOME/build-dir/batocera.x86_64/target/usr/wine
+	rm -rf $HOME/build-dir/batocera.x86_wow64/target/usr/wine
+fi
+
+# Cleanup
+cleanup="x86_wow64
+x86_64"
+
+for i in $cleanup
+do
+    echo -e "Cleaning: "${RED}$i${ENDCOLOR}
+	find $HOME/build-dir/batocera.$i/build -maxdepth 1 -type d -printf '%T@ %p %f\n' | sed -r 's:\-[0-9a-f\.]+$$::' | sort -k3 -k1 | uniq -f 2 -d | cut -d' ' -f2
+	find $HOME/build-dir/batocera.$i/build -maxdepth 1 -type d -printf '%T@ %p %f\n' | sed -r 's:\-[0-9a-f\.]+$$::' | sort -k3 -k1 | uniq -f 2 -d | cut -d' ' -f2 | xargs rm -rf
+done
+
+#find $HOME/dl -maxdepth 2 -type f -name "*.zip" -o -name "*.tar.*" -printf '%T@ %p %f\n' | sed -r 's:\-[0-9a-f\.]+(\.zip|\.tar\.[2a-z]+)$$::' | sort -k3 -k1 | uniq -f 2 -d | cut -d' ' -f2 | xargs rm -rf
+
