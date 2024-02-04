0847e79e0fad1def15a3b2147503ee23798b97ad
Lee <lee.hancock4@ntlworld.com>
2024-02-04 18:31:45 +0000

    bump linux-6.8-rc3 patches

---
+++ scripts/misc/patches.sh
@@ -0,0 +1,30 @@
+GREEN="\e[1;32m"
+RED="\e[1;31m"
+BLUE="\e[1;44m"
+ENDCOLOR="\e[0m"
+
+clear
+echo ${BLUE}"----------------------------------------------------------------------------------------"
+echo "Making Patches..."
+echo "----------------------------------------------------------------------------------------"${ENDCOLOR}
+echo ""
+
+echo '*.rej' >> .gitignore
+echo '*.orig' >> .gitignore
+git add .
+git commit -m gitignore
+
+for file in /tmp/patches/*.patch; do
+    find . -type f -name "*.orig" -exec rm -v {} \;
+    find . -type f -name "*.rej" -exec rm -v {} \;
+    echo ${GREEN}"----------------------------------------------------------------------------------------"
+    echo ${RED}"Using file $file"${GREEN}
+    echo "----------------------------------------------------------------------------------------"${ENDCOLOR}
+    git stash &&
+    patch -Np1 -i $file &&
+	rm $file &&
+    git add . &&
+	git diff --cached > $file &&
+	git commit -m $file
+done
+git stash
