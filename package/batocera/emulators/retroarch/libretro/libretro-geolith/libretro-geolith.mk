d390c664a3789d0bb68aaf6cd34cd24461753247
Lee <lee.hancock4@ntlworld.com>
2024-02-09 06:21:55 +0000

    libretro-geolith inital checkin

---
+++ package/batocera/emulators/retroarch/libretro/libretro-geolith/libretro-geolith.mk
@@ -0,0 +1,22 @@
+################################################################################
+#
+# libretro-geolith
+#
+################################################################################
+# Version: Commits on Feb 09, 2024
+LIBRETRO_GEOLITH_VERSION = 1d77e46b2c25cc61f01cead7707bfd7717e93da9
+LIBRETRO_GEOLITH_SITE = $(call github,libretro,geolith-libretro,$(LIBRETRO_GEOLITH_VERSION))
+LIBRETRO_GEOLITH_LICENSE = GPL-3.0
+
+define LIBRETRO_GEOLITH_BUILD_CMDS
+	$(SED) "s|-O2|$(TARGET_OPTIMIZATION)|g" $(@D)/libretro/Makefile
+	$(SED) "s|-O3|$(TARGET_OPTIMIZATION)|g" $(@D)/libretro/Makefile
+	$(TARGET_CONFIGURE_OPTS) $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D)/libretro
+endef
+
+define LIBRETRO_GEOLITH_INSTALL_TARGET_CMDS
+	mkdir -p $(TARGET_DIR)/usr/lib/libretro
+	$(INSTALL) -D $(@D)/libretro/geolith_libretro.so $(TARGET_DIR)/usr/lib/libretro/geolith_libretro.so
+endef
+
+$(eval $(generic-package))
