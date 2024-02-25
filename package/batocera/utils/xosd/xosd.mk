0c4dd5cb6c215366021d59582f119c43526ddb90
Lee <lee.hancock4@ntlworld.com>
2024-02-25 16:08:38 +0000

    update xosd

--- package/batocera/utils/xosd/xosd.mk
+++ package/batocera/utils/xosd/xosd.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-XOSD_VERSION = 2.2.14
+XOSD_VERSION = 1.1.0
 XOSD_SOURCE = xosd-$(XOSD_VERSION).tar.gz
-XOSD_SITE = https://freefr.dl.sourceforge.net/project/libxosd/libxosd/xosd-$(XOSD_VERSION)
+XOSD_SITE = $(call github,kdmurthy,libxosd-xft,$(XOSD_VERSION))
 
 XOSD_DEPENDENCIES = xserver_xorg-server
 
