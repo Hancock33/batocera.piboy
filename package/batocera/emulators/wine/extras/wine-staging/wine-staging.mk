b1ea55310bf56527738a05525a0d548c0dd6ede9
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2024-03-09 09:25:20 +0000

    bump wine extras

--- package/batocera/emulators/wine/extras/wine-staging/wine-staging.mk
+++ package/batocera/emulators/wine/extras/wine-staging/wine-staging.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-WINE_STAGING_VERSION = wine-9.3
+WINE_STAGING_VERSION = wine-9.4
 WINE_STAGING_SOURCE = wine-$(WINE_STAGING_VERSION).tar.gz
 WINE_STAGING_SITE = $(call github,wine-mirror,wine,$(WINE_STAGING_VERSION))
 WINE_STAGING_LICENSE = LGPL-2.1+
@@ -12,7 +12,7 @@ WINE_STAGING_LICENSE_FILES = COPYING.LIB LICENSE
 WINE_STAGING_SELINUX_MODULES = wine
 WINE_STAGING_DEPENDENCIES = host-bison host-flex host-wine-staging
 HOST_WINE_STAGING_DEPENDENCIES = host-bison host-flex
-WINE_STAGING_STAGING_VERSION = v9.3
+WINE_STAGING_STAGING_VERSION = v9.4
 HOST_WINE_STAGING_EXTRA_DOWNLOADS = https://github.com/wine-staging/wine-staging/archive/$(WINE_STAGING_STAGING_VERSION).tar.gz
 
 ifeq ($(BR_CMAKE_USE_CLANG),y)
