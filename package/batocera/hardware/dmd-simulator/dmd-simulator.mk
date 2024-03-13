c365dd04be5616dc4c042ada59ca70c0c06b5414
Lee <lee.hancock4@ntlworld.com>
2024-03-13 22:36:39 +0000

    build fixes

--- package/batocera/hardware/dmd-simulator/dmd-simulator.mk
+++ package/batocera/hardware/dmd-simulator/dmd-simulator.mk
@@ -16,12 +16,12 @@ define DMD_SIMULATOR_INSTALL_DMD_SIMULATOR_PLAYER
 	$(INSTALL) -D -m 0755 $(@D)/dmd-play.py      $(TARGET_DIR)/usr/bin/dmd-play
 
         mkdir -p $(TARGET_DIR)/usr/share/dmd-simulator/scripts
-        $(INSTALL) -m 0755 -D $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/screens/dmd-simulator/scripts/*.sh $(TARGET_DIR)/usr/share/dmd-simulator/scripts/
+        $(INSTALL) -m 0755 -D $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/hardware/dmd-simulator/scripts/*.sh $(TARGET_DIR)/usr/share/dmd-simulator/scripts/
 
         mkdir -p $(TARGET_DIR)/usr/share/batocera/services
-	install -m 0755 $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/screens/dmd-simulator/dmd_simulator.service $(TARGET_DIR)/usr/share/batocera/services/dmd_simulator
+	install -m 0755 $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/hardware/dmd-simulator/dmd_simulator.service $(TARGET_DIR)/usr/share/batocera/services/dmd_simulator
 
-	$(INSTALL) -m 0644 -D $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/screens/dmd-simulator/batocera.png $(TARGET_DIR)/usr/share/dmd-simulator/images/system/batocera.png
+	$(INSTALL) -m 0644 -D $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/hardware/dmd-simulator/batocera.png $(TARGET_DIR)/usr/share/dmd-simulator/images/system/batocera.png
 endef
 
 ifeq ($(BR2_PACKAGE_DMD_SIMULATOR),y)
