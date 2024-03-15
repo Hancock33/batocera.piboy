51a56fb5115ad1cd4449aa8ed0dd35579ff032f8
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2024-03-15 17:56:23 +0000

    Update dxvk-nvapi.mk

--- package/batocera/emulators/wine/dxvk-nvapi/dxvk-nvapi.mk
+++ package/batocera/emulators/wine/dxvk-nvapi/dxvk-nvapi.mk
@@ -3,8 +3,8 @@
 # dxvk-nvapi
 #
 ################################################################################
-# Version: Commits on Sept 09, 2023
-DXVK_NVAPI_VERSION = 0.6.4
+# Version: Commits on Mar 15, 2024
+DXVK_NVAPI_VERSION = 0.7.0
 DXVK_NVAPI_SOURCE = dxvk-nvapi-v$(DXVK_NVAPI_VERSION).tar.gz
 DXVK_NVAPI_SITE = https://github.com/jp7677/dxvk-nvapi/releases/download/v$(DXVK_NVAPI_VERSION)
 DXVK_NVAPI_LICENSE = zlib/libpng
