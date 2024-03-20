5d422284f8a3b2f2c0ea865de06919aa410fd3d8
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2024-03-20 22:59:48 +0000

    Update dxvk.mk

--- package/batocera/emulators/wine/dxvk/dxvk.mk
+++ package/batocera/emulators/wine/dxvk/dxvk.mk
@@ -3,8 +3,8 @@
 # dxvk
 #
 ################################################################################
-# Version: Commits on Sept 04, 2023
-DXVK_VERSION = 2.3
+# Version: Commits on Mar 20, 2024
+DXVK_VERSION = 2.3.1
 DXVK_SOURCE = dxvk-$(DXVK_VERSION).tar.gz
 DXVK_SITE = https://github.com/doitsujin/dxvk/releases/download/v$(DXVK_VERSION)
 DXVK_LICENSE = zlib/libpng
