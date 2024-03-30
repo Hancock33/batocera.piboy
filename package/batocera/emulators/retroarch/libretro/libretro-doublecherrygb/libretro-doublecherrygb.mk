d7f971f969ba906247f18139a34ca5aa9f3435a6
Lee <lee.hancock4@ntlworld.com>
2024-03-30 10:54:46 +0000

    add libretro-doublecherrygb

---
+++ package/batocera/emulators/retroarch/libretro/libretro-doublecherrygb/libretro-doublecherrygb.mk
@@ -0,0 +1,23 @@
+################################################################################
+#
+# libretro-doublecherrygb
+#
+################################################################################
+# Version: Commits on Mar 07, 2024
+LIBRETRO_DOUBLECHERRYGB_VERSION = abb98f44e416c06ee193506046664da340a641f8
+LIBRETRO_DOUBLECHERRYGB_SITE = https://github.com/TimOelrichs/doublecherryGB-libretro.git
+LIBRETRO_DOUBLECHERRYGB_SITE_METHOD=git
+LIBRETRO_DOUBLECHERRYGB_GIT_SUBMODULES=YES
+LIBRETRO_DOUBLECHERRYGB_LICENSE = GPLv2
+
+define LIBRETRO_DOUBLECHERRYGB_BUILD_CMDS
+	$(SED) "s|\-O[23]|$(TARGET_OPTIMIZATION)|g" $(@D)/Makefile
+	$(TARGET_CONFIGURE_OPTS) $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D)
+endef
+
+define LIBRETRO_DOUBLECHERRYGB_INSTALL_TARGET_CMDS
+	mkdir -p $(TARGET_DIR)/usr/lib/libretro
+	$(INSTALL) -D $(@D)/DoubleCherryGB_libretro.so $(TARGET_DIR)/usr/lib/libretro/doublecherrygb_libretro.so
+endef
+
+$(eval $(generic-package))
