f886236ca66911da13c573045a174ffa0b263b4a
Lee <lee.hancock4@ntlworld.com>
2024-03-31 23:44:44 +0100

    correct libretro-squirreljme

--- package/batocera/emulators/retroarch/libretro/libretro-squirreljme/libretro-squirreljme.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-squirreljme/libretro-squirreljme.mk
@@ -7,23 +7,13 @@
 LIBRETRO_SQUIRRELJME_VERSION = 83c10ac08a481c1c3ed5df61e20bbe6993a87388
 LIBRETRO_SQUIRRELJME_SITE = $(call github,SquirrelJME,SquirrelJME,$(LIBRETRO_SQUIRRELJME_VERSION))
 LIBRETRO_SQUIRRELJME_LICENSE = GPL-3.0
-
-ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_BCM2711),y)
-LIBRETRO_SQUIRRELJME_MAKE_FILE=Makefile.linux_rpi4
-else ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_BCM2837),y)
-LIBRETRO_SQUIRRELJME_MAKE_FILE=Makefile.linux_rpi3
-else
-LIBRETRO_SQUIRRELJME_MAKE_FILE=Makefile.linux_x86_64
-endif
-
-define LIBRETRO_SQUIRRELJME_BUILD_CMDS
-	$(SED) "s|\-O[23]|$(TARGET_OPTIMIZATION)|g" $(@D)/ratufacoat/libretro/$(LIBRETRO_SQUIRRELJME_MAKE_FILE)
-	$(TARGET_CONFIGURE_OPTS) $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D)/ratufacoat/libretro -f "$(LIBRETRO_SQUIRRELJME_MAKE_FILE)"
-endef
+LIBRETRO_SQUIRRELJME_SUPPORTS_IN_SOURCE_BUILD = NO
+LIBRETRO_SQUIRRELJME_SUBDIR = nanocoat
 
 define LIBRETRO_SQUIRRELJME_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/usr/lib/libretro
-	$(INSTALL) -D $(@D)/ratufacoat/libretro/squirreljme_libretro.so $(TARGET_DIR)/usr/lib/libretro/squirreljme_libretro.so
+	$(INSTALL) -D $(@D)/nanocoat/buildroot-build/squirreljme_libretro.so	$(TARGET_DIR)/usr/lib/libretro/squirreljme_libretro.so
+	$(INSTALL) -D $(@D)/nanocoat/squirreljme_libretro.info					$(TARGET_DIR)/usr/share/libretro/info/
 endef
 
-$(eval $(generic-package))
+$(eval $(cmake-package))
