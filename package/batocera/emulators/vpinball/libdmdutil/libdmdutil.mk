7fb6952403e7076a7b2ff3c395339826f5f6a410
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2024-04-03 18:59:46 +0100

    cleanup vpinball libs

--- package/batocera/emulators/vpinball/libdmdutil/libdmdutil.mk
+++ package/batocera/emulators/vpinball/libdmdutil/libdmdutil.mk
@@ -11,36 +11,24 @@ LIBDMDUTIL_LICENSE_FILES = LICENSE
 LIBDMDUTIL_DEPENDENCIES = cargs libpupdmd libserialport libzedmd libserum sockpp
 LIBDMDUTIL_SUPPORTS_IN_SOURCE_BUILD = NO
 
-LIBDMDUTIL_CONF_OPTS += -DCMAKE_BUILD_TYPE=Release
-LIBDMDUTIL_CONF_OPTS += -DBUILD_STATIC=OFF
 LIBDMDUTIL_CONF_OPTS += -DPLATFORM=linux
-LIBDMDUTIL_CONF_OPTS += -DARCH=$(DMDUTIL_ARCH)
 LIBDMDUTIL_CONF_OPTS += -DPOST_BUILD_COPY_EXT_LIBS=OFF
 
 # handle supported target platforms
 ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_RK3588),y)
-	DMDUTIL_ARCH = aarch64
+    LIBDMDUTIL_CONF_OPTS += -DARCH=aarch64
 endif
 
 ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_BCM2711)$(BR2_PACKAGE_BATOCERA_TARGET_BCM2712),y)
-	DMDUTIL_ARCH = aarch64
+    LIBDMDUTIL_CONF_OPTS += -DARCH=aarch64
 endif
 
 ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_X86_64_ANY),y)
-	DMDUTIL_ARCH = x64
+    LIBDMDUTIL_CONF_OPTS += -DARCH=x64
 endif
-
-define LIBDMDUTIL_CMAKE_HACKS
-	## derived from platforms/${PLATFORM}/${BUILD_ARCH}/external.sh and CMakeLists.txt ##
-	$(SED) 's:third-party/include$$:$(STAGING_DIR)/usr/include/\n   third-party/include:g' $(@D)/CMakeLists.txt
-	$(SED) 's:$${CMAKE_SOURCE_DIR}/third-party/runtime-libs/$${PLATFORM}/$${ARCH}/:$(STAGING_DIR)/usr/lib/:g' $(@D)/CMakeLists.txt
-	$(SED) 's:third-party/runtime-libs/$${PLATFORM}/$${ARCH}:$(STAGING_DIR)/usr/lib/:g' $(@D)/CMakeLists.txt
-endef
 
 # Install to staging to build Visual Pinball Standalone
 LIBDMDUTIL_INSTALL_STAGING = YES
-
-LIBDMDUTIL_PRE_CONFIGURE_HOOKS += LIBDMDUTIL_CMAKE_HACKS
 
 define LIBDMDUTIL_INSTALL_SERVER
 	$(INSTALL) -D -m 0755 $(LIBDMDUTIL_BUILDDIR)/dmdserver $(TARGET_DIR)/usr/bin/dmdserver
@@ -49,7 +37,5 @@ define LIBDMDUTIL_INSTALL_SERVER
 	install -m 0755 $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/emulators/vpinball/libdmdutil/dmd_server.service  $(TARGET_DIR)/usr/share/batocera/services/dmd_real
 	install -m 0755 $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/emulators/vpinball/libdmdutil/dmdserver-config.py $(TARGET_DIR)/usr/bin/dmdserver-config
 endef
-
-LIBDMDUTIL_POST_INSTALL_TARGET_HOOKS += LIBDMDUTIL_INSTALL_SERVER
 
 $(eval $(cmake-package))
