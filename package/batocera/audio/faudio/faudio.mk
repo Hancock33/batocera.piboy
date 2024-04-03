a4d8009e51623a5d08a816dd1ba8f00e41a5ea5a
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2024-04-03 07:57:08 +0100

    Update faudio.mk

--- package/batocera/audio/faudio/faudio.mk
+++ package/batocera/audio/faudio/faudio.mk
@@ -3,8 +3,8 @@
 # faudio
 #
 ################################################################################
-# Version: Commits on Mar 01, 2024
-FAUDIO_VERSION = 24.03
+# Version: Commits on Apr 01, 2024
+FAUDIO_VERSION = 24.04
 FAUDIO_SITE = $(call github,FNA-XNA,FAudio,$(FAUDIO_VERSION))
 FAUDIO_LICENSE = ZLIB
 FAUDIO_LICENSE_FILES = LICENSE
