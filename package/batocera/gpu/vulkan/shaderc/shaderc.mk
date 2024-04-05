355a1e45f8652cdde71d66e6051810d1781eed71
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2024-04-05 09:53:18 +0100

    Update shaderc.mk

--- package/batocera/gpu/vulkan/shaderc/shaderc.mk
+++ package/batocera/gpu/vulkan/shaderc/shaderc.mk
@@ -12,10 +12,11 @@ SHADERC_SUPPORTS_IN_SOURCE_BUILD = NO
 
 SHADERC_CONF_OPTS += -DCMAKE_EXE_LINKER_FLAGS=""
 SHADERC_CONF_OPTS += -DSHADERC_SKIP_TESTS=ON
+SHADERC_CONF_OPTS += -DSHADERC_SKIP_EXAMPLES=ON
 SHADERC_CONF_OPTS += -Dglslang_SOURCE_DIR=$(STAGING_DIR)/usr/include/glslang
 
 define SHADERC_THIRDPARTY
-	$(SED) '/examples/d;/third_party/d' -i $(@D)/CMakeLists.txt
+	$(SED) '/third_party/d' -i $(@D)/CMakeLists.txt
 	$(SED) '/build-version/d' -i $(@D)/glslc/CMakeLists.txt
 	$(@D)/utils/update_build_version.py $(@D) $(SPIRV_TOOLS_BUILDDIR) $(GLSLANG_BUILDDIR) $(@D)/glslc/src/build-version.inc
 endef
