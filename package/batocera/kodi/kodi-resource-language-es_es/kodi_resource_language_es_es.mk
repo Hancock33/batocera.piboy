298fb3fc9de5648b492dd3f43ea373e33067b01e
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2024-04-06 17:44:17 +0100

    update kodi-21
    now lets see what ffmpeg 6 breaks

--- package/batocera/kodi/kodi-resource-language-es_es/kodi_resource_language_es_es.mk
+++ package/batocera/kodi/kodi-resource-language-es_es/kodi_resource_language_es_es.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-KODI_RESOURCE_LANGUAGE_ES_ES_VERSION = 10.0.69
+KODI_RESOURCE_LANGUAGE_ES_ES_VERSION = 11.0.78
 KODI_RESOURCE_LANGUAGE_ES_ES_SOURCE = resource.language.es_es-$(KODI_RESOURCE_LANGUAGE_ES_ES_VERSION).zip
-KODI_RESOURCE_LANGUAGE_ES_ES_SITE = http://mirrors.kodi.tv/addons/nexus/resource.language.es_es
+KODI_RESOURCE_LANGUAGE_ES_ES_SITE = http://mirrors.kodi.tv/addons/omega/resource.language.es_es
 KODI_RESOURCE_LANGUAGE_ES_ES_PLUGINNAME=resource.language.es_es
 
 KODI_PLUGIN_TARGET_DIR=$(TARGET_DIR)/usr/share/kodi/addons
