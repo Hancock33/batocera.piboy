52f47f20e26779961e7f733453b6af9644fb519a
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2024-04-17 18:15:01 +0100

    Update rpi-eeprom.mk

--- package/batocera/firmwares/rpi-eeprom/rpi-eeprom.mk
+++ package/batocera/firmwares/rpi-eeprom/rpi-eeprom.mk
@@ -3,15 +3,15 @@
 # rpi-eeprom
 #
 ################################################################################
-# Version: Commits on Apr 05, 2024
-RPI_EEPROM_VERSION = 61023cbd32725a07e094f9b2d01df302f4ddabba
+# Version: Commits on Apr 17, 2024
+RPI_EEPROM_VERSION = c0a207f4524418d4f7d7266934dc7a1ae92110bf
 RPI_EEPROM_SITE = $(call github,raspberrypi,rpi-eeprom,$(RPI_EEPROM_VERSION))
 RPI_EEPROM_DEPENDENCIES = rpi-userland-batocera
 
 RPI_EEPROM_LICENSE = BSD-3-Clause
 
-RPI4_EEPROM_VERSION=2024-01-18
-RPI5_EEPROM_VERSION=2024-04-05
+RPI4_EEPROM_VERSION=2024-04-17
+RPI5_EEPROM_VERSION=2024-04-17
 
 RPI4_FW_DIR=$(TARGET_DIR)/lib/firmware/raspberrypi/bootloader-2711/default
 RPI5_FW_DIR=$(TARGET_DIR)/lib/firmware/raspberrypi/bootloader-2712/default
