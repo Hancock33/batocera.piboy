3a4387e25ced8c9adb466a0990d325c8a9e72fee
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2024-04-28 21:53:08 +0100

    cleanup

--- package/batocera/hardware/batocera-backglass/batocera-backglass.mk
+++ package/batocera/hardware/batocera-backglass/batocera-backglass.mk
@@ -8,7 +8,7 @@ BATOCERA_BACKGLASS_VERSION = 1.0
 BATOCERA_BACKGLASS_LICENSE = GPL
 BATOCERA_BACKGLASS_SOURCE=
 
-BACKGLASS_PATH = $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/screens/batocera-backglass
+BACKGLASS_PATH = $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/hardware/batocera-backglass
 
 define BATOCERA_BACKGLASS_INSTALL_TARGET_CMDS
 	# script
@@ -18,11 +18,11 @@ define BATOCERA_BACKGLASS_INSTALL_TARGET_CMDS
 
 	# hooks
         mkdir -p $(TARGET_DIR)/usr/share/batocera-backglass/scripts
-        $(INSTALL) -m 0755 -D $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/screens/batocera-backglass/scripts/*.sh $(TARGET_DIR)/usr/share/batocera-backglass/scripts/
+        $(INSTALL) -m 0755 -D $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/hardware/batocera-backglass/scripts/*.sh $(TARGET_DIR)/usr/share/batocera-backglass/scripts/
 
 	# default web page
         mkdir -p $(TARGET_DIR)/usr/share/batocera-backglass/www/backglass-default
-	cp -pr $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/screens/batocera-backglass/www/backglass-default/*.{js,css,htm} $(TARGET_DIR)/usr/share/batocera-backglass/www/backglass-default/
+	cp -pr $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/hardware/batocera-backglass/www/backglass-default/*.{js,css,htm} $(TARGET_DIR)/usr/share/batocera-backglass/www/backglass-default/
 endef
 
 $(eval $(generic-package))
