2a8656caf7136f8306b4c71e7eb385abb0037aa5
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2024-05-18 14:08:37 +0100

    misc version updates

--- package/batocera/hardware/ayn-platform/ayn-platform.mk
+++ package/batocera/hardware/ayn-platform/ayn-platform.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 # Version: Commits on Apr 16, 2024
-AYN_PLATFORM_VERSION = 62032caddc25920f1f8ef256808d5a0cfc252f39
+AYN_PLATFORM_VERSION = v0.5.1
 AYN_PLATFORM_SITE = $(call github,ShadowBlip,ayn-platform,$(AYN_PLATFORM_VERSION))
 AYN_PLATFORM_LICENSE = GPL-3.0
 AYN_PLATFORM_LICENSE_FILES = LICENSE
