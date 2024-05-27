bfeddc4571900e66ea05ec75681d7e07fda9c90c
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2024-05-27 15:56:32 +0100

    Update ffmpeg-python.mk

--- package/batocera/libraries/ffmpeg-python/ffmpeg-python.mk
+++ package/batocera/libraries/ffmpeg-python/ffmpeg-python.mk
@@ -3,7 +3,7 @@
 # ffmpeg-python
 #
 ################################################################################
-
+# Version: Commits on Jul 11, 2022
 FFMPEG_PYTHON_VERSION = df129c7ba30aaa9ffffb81a48f53aa7253b0b4e6
 FFMPEG_PYTHON_SITE = $(call github,kkroening,ffmpeg-python,$(FFMPEG_PYTHON_VERSION))
 FFMPEG_PYTHON_SETUP_TYPE = setuptools
