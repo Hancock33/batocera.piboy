566fecac473b57cf4f923da43f25c7221d8e131d
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2024-06-04 20:23:04 +0100

    Update syncthing.mk

--- package/batocera/utils/syncthing/syncthing.mk
+++ package/batocera/utils/syncthing/syncthing.mk
@@ -3,8 +3,8 @@
 # syncthing
 #
 ################################################################################
-# Version: Commits on May 08, 2024
-SYNCTHING_VERSION = v1.27.7
+# Version: Commits on Jun 04, 2024
+SYNCTHING_VERSION = v1.27.8
 SYNCTHING_SITE = $(call github,syncthing,syncthing,$(SYNCTHING_VERSION))
 SYNCTHING_LICENSE = MPLv2
 SYNCTHING_LICENSE_FILES = LICENSE
