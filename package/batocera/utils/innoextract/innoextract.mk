14d015d5da880d72c049455b9a2c4bb988d6cf6a
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2024-06-06 06:03:46 +0100

    misc updates

--- package/batocera/utils/innoextract/innoextract.mk
+++ package/batocera/utils/innoextract/innoextract.mk
@@ -3,6 +3,7 @@
 # innoextract
 #
 ################################################################################
+# Version: Commits on Aug 09, 2020
 INNOEXTRACT_VERSION = 1.9
 INNOEXTRACT_SITE = $(call github,dscharrer,innoextract,$(INNOEXTRACT_VERSION))
 INNOEXTRACT_LICENSE = MIT
