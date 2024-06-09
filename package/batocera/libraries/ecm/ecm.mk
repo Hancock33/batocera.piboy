0f04fe71f062e6c362944d9c4ee965e43f405cf0
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2024-06-09 21:38:50 +0100

    bump libraries

--- package/batocera/libraries/ecm/ecm.mk
+++ package/batocera/libraries/ecm/ecm.mk
@@ -3,8 +3,8 @@
 # ecm
 #
 ################################################################################
-
-ECM_VERSION = v5.114.0
+# Version: Commits on May 19, 2024
+ECM_VERSION = v5.116.0
 ECM_SITE =  $(call github,KDE,extra-cmake-modules,$(ECM_VERSION))
 ECM_INSTALL_STAGING = YES
 ECM_INSTALL_TARGET = NO
