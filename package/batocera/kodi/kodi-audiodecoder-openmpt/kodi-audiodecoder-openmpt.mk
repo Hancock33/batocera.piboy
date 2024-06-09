c025c6a3c5e14905d02d9a564a99ef304582e0e8
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2024-06-09 21:27:13 +0100

    bump kodi plugins

--- package/batocera/kodi/kodi-audiodecoder-openmpt/kodi-audiodecoder-openmpt.mk
+++ package/batocera/kodi/kodi-audiodecoder-openmpt/kodi-audiodecoder-openmpt.mk
@@ -5,6 +5,7 @@
 ################################################################################
 # Version: Commits on Feb 19, 2024
 KODI_AUDIODECODER_OPENMPT_VERSION = f7cb103de96404fe26938f93cd277a066f77cb49
+KODI_AUDIODECODER_OPENMPT_BRANCH = Omega
 KODI_AUDIODECODER_OPENMPT_SITE = $(call github,xbmc,audiodecoder.openmpt,$(KODI_AUDIODECODER_OPENMPT_VERSION))
 KODI_AUDIODECODER_OPENMPT_LICENSE = GPL-2.0+
 KODI_AUDIODECODER_OPENMPT_LICENSE_FILES = LICENSE.md
