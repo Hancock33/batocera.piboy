a2d9b816b67571052e62c4851a930ce131ad4555
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2024-06-10 22:00:28 +0100

    use br autogen variable instead of ./autogen.sh

--- package/batocera/emulators/dosbox/dosbox.mk
+++ package/batocera/emulators/dosbox/dosbox.mk
@@ -8,15 +8,12 @@ DOSBOX_VERSION = e6b88ad03202d1f74e329f54f213d3b070bd6202
 DOSBOX_SITE = $(call github,duganchen,dosbox,$(DOSBOX_VERSION))
 DOSBOX_DEPENDENCIES = sdl2 sdl2_net fluidsynth zlib libpng libogg libvorbis
 DOSBOX_LICENSE = GPLv2
+DOSBOX_AUTORECONF = YES
 
-define DOSBOX_CONFIGURE_CMDS
-	cd $(@D); ./autogen.sh; $(TARGET_CONFIGURE_OPTS) CROSS_COMPILE="$(HOST_DIR)/usr/bin/" LIBS="-lvorbisfile -lvorbis -logg" \
-		./configure --host="$(GNU_TARGET_NAME)" \
-					--enable-core-inline \
+DOSBOX_CONF_OPTS =	--enable-core-inline \
 					--enable-dynrec \
 					--enable-unaligned_memory \
 					--prefix=/usr \
-					--with-sdl-prefix="$(STAGING_DIR)/usr";
-endef
+					--with-sdl-prefix="$(STAGING_DIR)/usr"
 
 $(eval $(autotools-package))
--- package/batocera/utils/l3afpad/l3afpad.mk
+++ package/batocera/utils/l3afpad/l3afpad.mk
@@ -10,10 +10,6 @@ L3AFPAD_SITE = $(call github,stevenhoneyman,l3afpad,$(L3AFPAD_VERSION))
 L3AFPAD_DEPENDENCIES = libgtk3 host-intltool harfbuzz
 L3AFPAD_LICENSE = GPL-2.0+
 L3AFPAD_LICENSE_FILES = COPYING
-
-define L3AFPAD_CONFIGURE_CMDS
-	cd $(@D); PATH=$(HOST_DIR)/bin:$(PATH) ./autogen.sh ; \
-	$(TARGET_CONFIGURE_OPTS) PREFIX=$(STAGING_DIR) CFLAGS="-I$(STAGING_DIR)/usr/include" LDFLAGS="-L$(STAGING_DIR)/usr/lib" ./configure --host --disable-nls --prefix=/usr
-endef
+L3AFPAD_AUTORECONF = YES
 
 $(eval $(autotools-package))
