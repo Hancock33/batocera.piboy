ef25e3afa3189c42608a6c12cba6f4c204499aed
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2024-06-10 22:00:28 +0100

    use br autogen variable instead of ./autogen.sh

--- package/batocera/utils/re2c/re2c.mk
+++ package/batocera/utils/re2c/re2c.mk
@@ -7,12 +7,7 @@
 RE2C_VERSION = 671fc0bc08af8f06df3ed59c3294f0920d5b5184
 RE2C_SITE =  $(call github,skvadrik,re2c,$(RE2C_VERSION))
 
-define RE2C_AUTOGEN
-	cd $(@D); ./autogen.sh
-endef
-
-RE2C_PRE_CONFIGURE_HOOKS += RE2C_AUTOGEN
-HOST_RE2C_PRE_CONFIGURE_HOOKS += RE2C_AUTOGEN
+RE2C_AUTORECONF = YES
 
 $(eval $(autotools-package))
 $(eval $(host-autotools-package))
