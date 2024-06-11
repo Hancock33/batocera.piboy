5852fa4929958b4fa76f37b8577aa778c11b6cd2
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2024-06-11 22:06:10 +0100

    update dates

--- package/batocera/libraries/python-pyftdi/python-pyftdi.mk
+++ package/batocera/libraries/python-pyftdi/python-pyftdi.mk
@@ -3,10 +3,10 @@
 # python-pyftdi
 #
 ################################################################################
-
-PYTHON_PYFTDI_VERSION = 0.54.0
+# Version: Commits on Apr 10, 2024
+PYTHON_PYFTDI_VERSION = 0.55.4
 PYTHON_PYFTDI_SOURCE = pyftdi-$(PYTHON_PYFTDI_VERSION).tar.gz
-PYTHON_PYFTDI_SITE = https://files.pythonhosted.org/packages/49/a3/6cd09c0493662b285b2ba87a08b1378a5b13e5cab44eb6a3f740c801c804
+PYTHON_PYFTDI_SITE = https://files.pythonhosted.org/packages/8f/b4/8578b8e4c1e69faa8297cb1e1fd0c86de03bb7eeb3e7cb63157e1e72abfb
 PYTHON_PYFTDI_SETUP_TYPE = setuptools
 PYTHON_PYFTDI_LICENSE = BSD
 PYTHON_PYFTDI_LICENSE_FILES = pyftdi/doc/license.rst
