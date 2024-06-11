077fd77eacfbaf53930f35e8233470e66af54b5d
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2024-06-11 21:49:36 +0100

    bump rpi python-adafruit modules

--- package/batocera/libraries/python-adafruit-pureio/python-adafruit-pureio.mk
+++ package/batocera/libraries/python-adafruit-pureio/python-adafruit-pureio.mk
@@ -3,10 +3,10 @@
 # python-adafruit-pureio
 #
 ################################################################################
-
+# Version: Commits on May 25, 2023
 PYTHON_ADAFRUIT_PUREIO_VERSION = 1.1.10
 PYTHON_ADAFRUIT_PUREIO_SOURCE = Adafruit_PureIO-$(PYTHON_ADAFRUIT_PUREIO_VERSION).tar.gz
-PYTHON_ADAFRUIT_PUREIO_SITE = https://files.pythonhosted.org/packages/bc/0c/ceb6d01615504190ad4061af51bf46093c8e3cb3cf942d840cf95b8ba4ad
+PYTHON_ADAFRUIT_PUREIO_SITE = https://files.pythonhosted.org/packages/e5/b7/f1672435116822079bbdab42163f9e6424769b7db778873d95d18c085230
 PYTHON_ADAFRUIT_PUREIO_SETUP_TYPE = setuptools
 PYTHON_ADAFRUIT_PUREIO_LICENSE = MIT
 PYTHON_ADAFRUIT_PUREIO_LICENSE_FILES = LICENSE
