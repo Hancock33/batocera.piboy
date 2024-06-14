4769b7ab1a58199017d4bc61fa229689447681dd
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2024-06-14 04:44:24 +0100

    Update wsdd.mk

--- package/batocera/utils/wsdd/wsdd.mk
+++ package/batocera/utils/wsdd/wsdd.mk
@@ -9,9 +9,9 @@ WSDD_SITE = $(call github,christgau,wsdd,$(WSDD_VERSION))
 WSDD_LICENSE = MIT
 
 define WSDD_INSTALL_TARGET_CMDS
-        $(INSTALL) -Dm755 $(@D)/src/wsdd.py $(TARGET_DIR)/usr/bin/wsdd
+	$(INSTALL) -Dm755 $(@D)/src/wsdd.py $(TARGET_DIR)/usr/bin/wsdd
 	mkdir -p $(TARGET_DIR)/etc/init.d
-        $(INSTALL) -Dm755 $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/utils/wsdd/S93wsdd $(TARGET_DIR)/etc/init.d
+	$(INSTALL) -Dm755 $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/utils/wsdd/S93wsdd $(TARGET_DIR)/etc/init.d
 endef
 
 $(eval $(generic-package))
