7f7f4927b91115da1f8d7326af6d618b3c2a7fd3
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2024-06-26 14:23:25 +0100

    mangohud

--- package/batocera/utils/mangohud/mangohud.mk
+++ package/batocera/utils/mangohud/mangohud.mk
@@ -3,12 +3,11 @@
 # mangohud
 #
 ################################################################################
-# Version: Commits on Nov 28, 2021
-MANGOHUD_VERSION = 12620c91eaca0917a7939a92ec33915cadf24475
-MANGOHUD_BRANCH = simple-image-loader
+# Version: Commits on May 28, 2022
+MANGOHUD_VERSION = a8a0a245e69fbbca5263d2436fd1c04289375498
 MANGOHUD_SITE =  $(call github,flightlessmango,MangoHud,$(MANGOHUD_VERSION))
 
-MANGOHUD_DEPENDENCIES = host-libcurl host-python-mako host-glslang
+MANGOHUD_DEPENDENCIES = host-libcurl host-python-mako host-glslang dbus
 
 ifeq ($(BR2_PACKAGE_LIBDRM),y)
 	MANGOHUD_DEPENDENCIES += libdrm
@@ -18,13 +17,15 @@ ifeq ($(BR2_PACKAGE_XSERVER_XORG_SERVER),y)
 	MANGOHUD_DEPENDENCIES += xserver_xorg-server
 endif
 
-MANGOHUD_CONF_OPTS = -Dwith_xnvctrl=disabled -Dtests=disabled
-
 ifeq ($(BR2_PACKAGE_VULKAN_HEADERS),y)
 	MANGOHUD_DEPENDENCIES += vulkan-headers
-	MANGOHUD_CONF_OPTS += -Duse_vulkan=true
+endif
+
+MANGOHUD_CONF_OPTS = -Dwith_xnvctrl=disabled
+ifeq ($(BR2_PACKAGE_VULKAN_HEADERS),y)
+	MANGOHUD_CONF_OPTS += -Duse_vulkan=true -Duse_system_vulkan=disabled
 else
-	MANGOHUD_CONF_OPTS += -Duse_vulkan=false
+	MANGOHUD_CONF_OPTS += -Duse_vulkan=false -Duse_system_vulkan=disabled
 endif
 
 ifeq ($(BR2_PACKAGE_XSERVER_XORG_SERVER),y)
@@ -34,6 +35,7 @@ else
 endif
 
 ifeq ($(BR2_PACKAGE_WAYLAND),y)
+	MANGOHUD_DEPENDENCIES += wayland
 	MANGOHUD_CONF_OPTS += -Dwith_wayland=enabled
 else
 	MANGOHUD_CONF_OPTS += -Dwith_wayland=disabled
@@ -41,17 +43,16 @@ endif
 
 # this is a not nice workaround
 # i don't know why meson uses bad ssl certificates and doesn't manage to download them
-# use submodule vulkan headers - https://github.com/flightlessmango/MangoHud/issues/968
 define MANGOHUD_DWD_DEPENDENCIES
+	mkdir -p  $(MANGOHUD_DL_DIR)/sumodules
+	curl -L https://github.com/ocornut/imgui/archive/v1.81.tar.gz					-o $(MANGOHUD_DL_DIR)/sumodules/imgui-1.81.tar.gz
+	curl -L https://wrapdb.mesonbuild.com/v2/imgui_1.81-1/get_patch					-o $(MANGOHUD_DL_DIR)/sumodules/imgui-1.81-1-wrap.zip
+	curl -L https://github.com/gabime/spdlog/archive/v1.8.5.tar.gz					-o $(MANGOHUD_DL_DIR)/sumodules/v1.8.5.tar.gz
+	curl -L https://wrapdb.mesonbuild.com/v2/spdlog_1.8.5-1/get_patch				-o $(MANGOHUD_DL_DIR)/sumodules/spdlog-1.8.5-1-wrap.zip
+	curl -L https://github.com/KhronosGroup/Vulkan-Headers/archive/v1.2.158.tar.gz	-o $(MANGOHUD_DL_DIR)/sumodules/vulkan-headers-1.2.158.tar.gz
+	curl -L https://wrapdb.mesonbuild.com/v2/vulkan-headers_1.2.158-2/get_patch		-o $(MANGOHUD_DL_DIR)/sumodules/vulkan-headers-1.2.158-2-wrap.zip
 	mkdir -p $(@D)/subprojects/packagecache
-	$(HOST_DIR)/bin/curl -L https://github.com/ocornut/imgui/archive/refs/tags/v1.89.9.tar.gz		-o $(@D)/subprojects/packagecache/imgui-1.89.9.tar.gz
-	$(HOST_DIR)/bin/curl -L https://wrapdb.mesonbuild.com/v2/imgui_1.89.9-1/get_patch				-o $(@D)/subprojects/packagecache/imgui_1.89.9-1_patch.zip
-	$(HOST_DIR)/bin/curl -L https://github.com/gabime/spdlog/archive/refs/tags/v1.14.1.tar.gz		-o $(@D)/subprojects/packagecache/spdlog-1.14.1.tar.gz
-	$(HOST_DIR)/bin/curl -L https://wrapdb.mesonbuild.com/v2/spdlog_1.14.1-1/get_patch				-o $(@D)/subprojects/packagecache/spdlog_1.14.1-1_patch.zip
-	$(HOST_DIR)/bin/curl -L https://github.com/KhronosGroup/Vulkan-Headers/archive/v1.2.158.tar.gz	-o $(@D)/subprojects/packagecache/vulkan-headers-1.2.158.tar.gz
-	$(HOST_DIR)/bin/curl -L https://wrapdb.mesonbuild.com/v2/vulkan-headers_1.2.158-2/get_patch		-o $(@D)/subprojects/packagecache/vulkan-headers-1.2.158-2-wrap.zip
-	$(HOST_DIR)/bin/curl -L https://github.com/epezent/implot/archive/refs/tags/v0.16.zip			-o $(@D)/subprojects/packagecache/implot-0.16.zip
-	$(HOST_DIR)/bin/curl -L https://wrapdb.mesonbuild.com/v2/implot_0.16-1/get_patch				-o $(@D)/subprojects/packagecache/implot_0.16-1_patch.zip
+	cp  $(MANGOHUD_DL_DIR)/sumodules/* $(@D)/subprojects/packagecache
 endef
 MANGOHUD_PRE_CONFIGURE_HOOKS += MANGOHUD_DWD_DEPENDENCIES
 
