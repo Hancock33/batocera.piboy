7435cf365bb9e33e55a3dfefcf9be6ddc5ec0551
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2024-06-28 22:06:54 +0100

    bump wine version 9.12

--- package/batocera/emulators/wine/extras/wine-native/wine-native.mk
+++ package/batocera/emulators/wine/extras/wine-native/wine-native.mk
@@ -3,8 +3,8 @@
 # wine-native
 #
 ################################################################################
-# Version: Commits on Jun 14, 2024
-WINE_NATIVE_VERSION = wine-9.11
+# Version: Commits on Jun 28, 2024
+WINE_NATIVE_VERSION = wine-9.12
 WINE_NATIVE_SOURCE = wine-$(WINE_NATIVE_VERSION).tar.gz
 WINE_NATIVE_SITE = $(call github,wine-mirror,wine,$(WINE_NATIVE_VERSION))
 WINE_NATIVE_LICENSE = LGPL-2.1+
