c76c5166ad7ad01a3a5288245953b0d0f6a6a7cf
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2024-07-06 12:51:49 +0100

    Updates on Jul 06, 2024 12:45
    ----------------------------------------------------------------------------------------
    doomretro.mk 760b592a1ef13f8a99600fd9fd90430c16fbaae7 # Version: Commits on Jul 06, 2024
    ----------------------------------------------------------------------------------------
    Minor tweaks,
    
    -----------------------------------------------------------------------------------------
    sonic3-air.mk e36ed6abb0ae456488a2d130d7f76a5bab23664b # Version: Commits on Jul 06, 2024
    -----------------------------------------------------------------------------------------
    Fix for a potential crash in Mods menu,

--- package/batocera/ports/doom/doomretro/doomretro.mk
+++ package/batocera/ports/doom/doomretro/doomretro.mk
@@ -3,8 +3,8 @@
 # doomretro
 #
 ################################################################################
-# Version: Commits on Jul 05, 2024
-DOOMRETRO_VERSION = 13f7ba1ea42b32be3505e9816fe0f24a27487543
+# Version: Commits on Jul 06, 2024
+DOOMRETRO_VERSION = 760b592a1ef13f8a99600fd9fd90430c16fbaae7
 DOOMRETRO_SITE = $(call github,bradharding,doomretro,$(DOOMRETRO_VERSION))
 
 DOOMRETRO_DEPENDENCIES = sdl2 sdl2_mixer sdl2_image
