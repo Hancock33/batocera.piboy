Hancock33/batocera.piboy

commit f0d2567a3e85fa6aa09aed583413bc30f5fa53e4

Fix Mugen sound (for real, new approach)

package/batocera/core/batocera-configgen/configgen/configgen/generators/mugen/mugenGenerator.py
@@ -1,55 +1,32 @@
 #!/usr/bin/env python
-
 from generators.Generator import Generator
 import Command
 import os
 import controllersConfig
-import configparser
 import re
 
 class MugenGenerator(Generator):
 
-    @staticmethod
-    def cleanMugenCfg(path):
-        with open(path, 'r', encoding='utf-8-sig') as f:
-            contents = f.read()
-
-        contents = re.sub(r'^[ ]*;', ';', contents, 0, re.MULTILINE)
-        with open(path, 'w') as f:
-            f.write(contents)
-
     def generate(self, system, rom, playersControllers, guns, gameResolution):
 
-        settings = configparser.ConfigParser(interpolation=None, strict=False) # strict=False to allow to read duplicates set by users
-        # To prevent ConfigParser from converting to lower case
-        settings.optionxform = str
         settings_path = rom + "/data/mugen.cfg"
-        if os.path.exists(settings_path):
-            MugenGenerator.cleanMugenCfg(settings_path)
-            settings.read(settings_path)
+        with open(settings_path, 'r', encoding='utf-8-sig') as f:
+            contents = f.read()
 
-        if not settings.has_section("Video"):
-            settings.add_section("Video")
-        settings.set("Video", "FullScreen", "1")
-        settings.set("Video", "Width",  str(gameResolution["width"]))
-        settings.set("Video", "Height", str(gameResolution["height"]))
-
-        if not settings.has_section("Config"):
-            settings.add_section("Config")
-        settings.set("Config", "GameWidth",  str(gameResolution["width"]))
-        settings.set("Config", "GameHeight", str(gameResolution["height"]))
-        settings.set("Config", "Language", "en")
-
-        if not settings.has_section("Music"):
-            settings.add_section("Music")
-        settings.set("Music", "plugin", "mpg123")
+        #clean up
+        contents = re.sub(r'^[ ]*;', ';', contents, 0, re.MULTILINE)
+        contents = re.sub(r'^[ ;]*FullScreen[ ]*=.*', 'FullScreen = 1', contents, 0, re.MULTILINE)
+        contents = re.sub(r'^[ ;]*GameWidth[ ]*=.*', 'Width = '+str(gameResolution["width"]), contents, 0, re.MULTILINE)
+        contents = re.sub(r'^[ ;]*GameHeight[ ]*=.*', 'Height = '+str(gameResolution["height"]), contents, 0, re.MULTILINE)
+        contents = re.sub(r'^[ ;]*Width[ ]*=.*', 'GameWidth = '+str(gameResolution["width"]), contents, 0, re.MULTILINE)
+        contents = re.sub(r'^[ ;]*Height[ ]*=.*', 'GameHeight = '+str(gameResolution["height"]), contents, 0, re.MULTILINE)
+        contents = re.sub(r'^[ ;]*Language[ ]*=.*', 'Language = "en"', contents, 0, re.MULTILINE)
+        with open(settings_path, 'w') as f:
+            f.write(contents)
 
         # Save config
         if not os.path.exists(os.path.dirname(settings_path)):
             os.makedirs(os.path.dirname(settings_path))
-
-        with open(settings_path, 'w') as configfile:
-            settings.write(configfile)
 
         commandArray = ["batocera-wine", "mugen", "play", rom]
         return Command.Command(array=commandArray, env={ "SDL_GAMECONTROLLERCONFIG": controllersConfig.generateSdlGameControllerConfig(playersControllers) })

commit 057145c43fd55b67b81d4f0e8d43aae5306665ae

fix mugen generator config rewrite
config rewrite for older mugen is broken since
https://github.com/batocera-linux/batocera.linux/commit/f0d2567a3e85fa6aa09aed583413bc30f5fa53e4

Width/Height are wrongly rewritten to GameWidth/GameHeight

newer version support both Width/Heigh  && newer GameWidth/GameHeight, so it was not breaking it

Older mugen support only Width= && Height=,  and crash at start with GameWidth/GameHeight, throwing
a video config error.

package/batocera/core/batocera-configgen/configgen/configgen/generators/mugen/mugenGenerator.py
@@ -16,10 +16,10 @@ class MugenGenerator(Generator):
         #clean up
         contents = re.sub(r'^[ ]*;', ';', contents, 0, re.MULTILINE)
         contents = re.sub(r'^[ ;]*FullScreen[ ]*=.*', 'FullScreen = 1', contents, 0, re.MULTILINE)
-        contents = re.sub(r'^[ ;]*GameWidth[ ]*=.*', 'Width = '+str(gameResolution["width"]), contents, 0, re.MULTILINE)
-        contents = re.sub(r'^[ ;]*GameHeight[ ]*=.*', 'Height = '+str(gameResolution["height"]), contents, 0, re.MULTILINE)
-        contents = re.sub(r'^[ ;]*Width[ ]*=.*', 'GameWidth = '+str(gameResolution["width"]), contents, 0, re.MULTILINE)
-        contents = re.sub(r'^[ ;]*Height[ ]*=.*', 'GameHeight = '+str(gameResolution["height"]), contents, 0, re.MULTILINE)
+        contents = re.sub(r'^[ ;]*GameWidth[ ]*=.*', 'GameWidth = '+str(gameResolution["width"]), contents, 0, re.MULTILINE)
+        contents = re.sub(r'^[ ;]*GameHeight[ ]*=.*', 'GameHeight = '+str(gameResolution["height"]), contents, 0, re.MULTILINE)
+        contents = re.sub(r'^[ ;]*Width[ ]*=.*', 'Width = '+str(gameResolution["width"]), contents, 0, re.MULTILINE)
+        contents = re.sub(r'^[ ;]*Height[ ]*=.*', 'Height = '+str(gameResolution["height"]), contents, 0, re.MULTILINE)
         contents = re.sub(r'^[ ;]*Language[ ]*=.*', 'Language = "en"', contents, 0, re.MULTILINE)
         with open(settings_path, 'w') as f:
             f.write(contents)