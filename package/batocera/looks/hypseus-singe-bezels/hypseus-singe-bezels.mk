b075cc5d5c27f6678801ccc923425d563ddfa323
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2024-07-13 16:02:59 +0100

    Update hypseus-singe-bezels.mk

--- package/batocera/looks/hypseus-singe-bezels/hypseus-singe-bezels.mk
+++ package/batocera/looks/hypseus-singe-bezels/hypseus-singe-bezels.mk
@@ -11,11 +11,15 @@ HYPSEUS_SINGE_BEZELS_LICENSE_FILES = LICENSE
 HYPSEUS_SINGE_BEZELS_DEPENDENCIES = ffmpeg-python
 
 define HYPSEUS_SINGE_BEZELS_INSTALL_TARGET_CMDS
-	mkdir -p $(TARGET_DIR)/usr/share/hypseus-singe/bezels
-	cp -f $(@D)/default.png $(TARGET_DIR)/usr/share/hypseus-singe/bezels
-	cp -f $(@D)/Daphne/*.png $(TARGET_DIR)/usr/share/hypseus-singe/bezels
-	cp -f $(@D)/Singe/*.png $(TARGET_DIR)/usr/share/hypseus-singe/bezels
-	cp -f $(@D)/Kimmy/*.png $(TARGET_DIR)/usr/share/hypseus-singe/bezels
+	rm -rf   $(TARGET_DIR)/usr/share/hypseus-singe/bezels
+	#mkdir -p $(TARGET_DIR)/usr/share/hypseus-singe/bezels
+	#cp -f $(@D)/default.png $(TARGET_DIR)/usr/share/hypseus-singe/bezels
+	#cp -f $(@D)/Daphne/*.png $(TARGET_DIR)/usr/share/hypseus-singe/bezels
+	#cp -f $(@D)/Singe/*.png $(TARGET_DIR)/usr/share/hypseus-singe/bezels
+	#cp -f $(@D)/Kimmy/*.png $(TARGET_DIR)/usr/share/hypseus-singe/bezels
+	mkdir -p $(TARGET_DIR)/usr/share/batocera/datainit/decorations/hypseus-singe
+	mkdir -p $(TARGET_DIR)/usr/share/hypseus-singe
+	ln -sf /userdata/decorations/hypseus-singe $(TARGET_DIR)/usr/share/hypseus-singe/bezels
 endef
 
 $(eval $(generic-package))
