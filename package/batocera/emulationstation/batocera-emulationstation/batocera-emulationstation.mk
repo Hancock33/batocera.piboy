b8f78a545c8b97e3f885291bca766201852c8324
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2024-07-18 21:51:15 +0100

    update emulationstation

--- package/batocera/emulationstation/batocera-emulationstation/batocera-emulationstation.mk
+++ package/batocera/emulationstation/batocera-emulationstation/batocera-emulationstation.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 # Version: Commits on Jul 18, 2024
-BATOCERA_EMULATIONSTATION_VERSION = bf03903230faf3208ab3bd94ca8c1ceb1945a4e2
+BATOCERA_EMULATIONSTATION_VERSION = 0261e73a172fcc6e01f73c9d5988f60bc0cb40fd
 BATOCERA_EMULATIONSTATION_SITE = https://github.com/batocera-linux/batocera-emulationstation
 BATOCERA_EMULATIONSTATION_SITE_METHOD = git
 BATOCERA_EMULATIONSTATION_LICENSE = MIT
