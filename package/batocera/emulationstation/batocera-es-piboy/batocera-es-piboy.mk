b2a4a75caa710cda76fbefa664c4a3b985352842
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2024-07-18 21:51:15 +0100

    update emulationstation

--- package/batocera/emulationstation/batocera-es-piboy/batocera-es-piboy.mk
+++ package/batocera/emulationstation/batocera-es-piboy/batocera-es-piboy.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 # Version: Commits on Jul 18, 2024
-BATOCERA_ES_PIBOY_VERSION = bf03903230faf3208ab3bd94ca8c1ceb1945a4e2
+BATOCERA_ES_PIBOY_VERSION = 0261e73a172fcc6e01f73c9d5988f60bc0cb40fd
 BATOCERA_ES_PIBOY_SITE = https://github.com/batocera-linux/batocera-emulationstation
 BATOCERA_ES_PIBOY_SITE_METHOD = git
 BATOCERA_ES_PIBOY_LICENSE = MIT
