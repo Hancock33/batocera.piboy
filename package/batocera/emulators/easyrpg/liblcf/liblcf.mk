52cdb03f47f7255f074f9b64f6857475519d9ecb
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2024-08-02 09:07:36 +0100

    cleanups + revert gcc

--- package/batocera/emulators/easyrpg/liblcf/liblcf.mk
+++ package/batocera/emulators/easyrpg/liblcf/liblcf.mk
@@ -10,8 +10,6 @@ LIBLCF_LICENSE = MIT
 LIBLCF_SITE = $(call github,EasyRPG,liblcf,$(LIBLCF_VERSION))
 
 LIBLCF_CONF_ENV += LDFLAGS="-lpthread -fPIC" CFLAGS="-fPIC" CXXFLAGS="-fPIC"
-LIBLCF_CONF_OPTS += -DCMAKE_C_COMPILER=$(HOST_DIR)/bin/$(GNU_TARGET_NAME)-gcc
-LIBLCF_CONF_OPTS += -DCMAKE_CXX_COMPILER=$(HOST_DIR)/bin/$(GNU_TARGET_NAME)-g++
 
 # Should be set when the package cannot be built inside the source tree but needs a separate build directory.
 LIBLCF_SUPPORTS_IN_SOURCE_BUILD = NO
