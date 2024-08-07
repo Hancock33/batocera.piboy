6f02614e542f7c13d40c60d502a18644c9a7bea8
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2024-08-07 21:24:32 +0100

    bump 32bit libs

--- package/batocera/emulators/wine/wine-x86/wine-x86.mk
+++ package/batocera/emulators/wine/wine-x86/wine-x86.mk
@@ -3,8 +3,8 @@
 # wine-x86
 #
 ################################################################################
-# Version: Commits on Aug 06, 2024
-WINE_X86_VERSION = 20240806
+# Version: Commits on Aug 07, 2024
+WINE_X86_VERSION = 20240807
 WINE_X86_SOURCE = batocera-x86-32bit-libs-$(WINE_X86_VERSION).tar.xz
 WINE_X86_SITE = https://github.com/Hancock33/batocera-32bit-libs/releases/download/$(WINE_X86_VERSION)
 
