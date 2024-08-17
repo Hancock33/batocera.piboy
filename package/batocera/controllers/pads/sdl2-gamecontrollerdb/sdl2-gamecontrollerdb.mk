8254287bf283e5dd1510fa26faf4e93304d45440
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2024-08-17 21:55:53 +0100

    Update sdl2-gamecontrollerdb.mk

--- package/batocera/controllers/pads/sdl2-gamecontrollerdb/sdl2-gamecontrollerdb.mk
+++ package/batocera/controllers/pads/sdl2-gamecontrollerdb/sdl2-gamecontrollerdb.mk
@@ -11,7 +11,7 @@ SDL2_GAMECONTROLLERDB_DEPENDENCIES = sdl2
 define SDL2_GAMECONTROLLERDB_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/usr/share/sdlcontrollerdb
 	cp $(@D)/gamecontrollerdb.txt $(TARGET_DIR)/usr/share/sdlcontrollerdb
-	$(INSTALL) -D -m 0755 $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/controllers/pads/pads/sdl2-gamecontrollerdb/S98sdlctl_db $(TARGET_DIR)/etc/init.d/S98sdlctl_db
+	$(INSTALL) -D -m 0755 $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/controllers/pads/sdl2-gamecontrollerdb/S98sdlctl_db $(TARGET_DIR)/etc/init.d/S98sdlctl_db
 endef
 
 $(eval $(generic-package))
