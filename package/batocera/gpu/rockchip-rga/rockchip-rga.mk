9d65d1a2f6bc679ea85344bee920c50551817ee2
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2024-08-25 17:21:57 +0100

    fix nvidia merge commit issues

--- package/batocera/gpu/rockchip-rga/rockchip-rga.mk
+++ package/batocera/gpu/rockchip-rga/rockchip-rga.mk
@@ -10,10 +10,10 @@ ROCKCHIP_RGA_BRANCH = linux-rga-multi
 
 # RK3326 needs the older rockchip-rga for it's 4.4 kernel
 ifeq ($(BR2_PACKAGE_HOST_LINUX_HEADERS_CUSTOM_4_4),y)
-    # Version.: Commits on Aug 5, 2021
-    ROCKCHIP_RGA_VERSION = df26244eb0c3991df319d4276ab80e8c0bf91f64
-    ROCKCHIP_RGA_SOURCE = rockchip-rga-$(ROCKCHIP_RGA_VERSION).tar.gz
-    ROCKCHIP_RGA_SITE = https://github.com/batocera-linux/rockchip-packages/releases/download/20220303
+	# Version.: Commits on Aug 5, 2021
+	ROCKCHIP_RGA_VERSION = df26244eb0c3991df319d4276ab80e8c0bf91f64
+	ROCKCHIP_RGA_SOURCE = rockchip-rga-$(ROCKCHIP_RGA_VERSION).tar.gz
+	ROCKCHIP_RGA_SITE = https://github.com/batocera-linux/rockchip-packages/releases/download/20220303
 endif
 
 ROCKCHIP_RGA_LICENSE = Apache License 2.0
