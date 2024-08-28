e8ad2272c10040de5810fd8777b6d75e8a5b97fe
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2024-08-28 06:09:38 +0100

    update br

--- package/batocera/ports/alephone/alephone.mk
+++ package/batocera/ports/alephone/alephone.mk
@@ -6,7 +6,7 @@
 # Version: Commits on Aug 03, 2024
 ALEPHONE_VERSION = 2825da5e84e06390f0038f1e3037ad825df40c6f
 ALEPHONE_SITE = $(call github,Aleph-One-Marathon,alephone,$(ALEPHONE_VERSION))
-ALEPHONE_DEPENDENCIES = boost ffmpeg4 sdl2 sdl2_image sdl2_ttf sdl2_net
+ALEPHONE_DEPENDENCIES = boost ffmpeg sdl2 sdl2_image sdl2_ttf sdl2_net
 ALEPHONE_LICENSE = GPLv3
 ALEPHONE_CONF_OPTS = \
 					--disable-sdltest \
@@ -19,10 +19,6 @@ define ALEPHONE_AUTOCONFIG
 	cd $(@D) && autoreconf -f -i
 endef
 ALEPHONE_PRE_CONFIGURE_HOOKS += ALEPHONE_AUTOCONFIG
-
-ALEPHONE_CONF_ENV += PKG_CONFIG_PATH="$(STAGING_DIR)/usr/lib/ffmpeg4.4/pkgconfig"
-ALEPHONE_CONF_ENV += CFLAGS="-I$(STAGING_DIR)/usr/include/ffmpeg4.4:$(TARGET_CFLAGS)"
-ALEPHONE_CONF_ENV += LDFLAGS="-L$(STAGING_DIR)/usr/lib/ffmpeg4.4"
 
 define ALEPHONE_INSTALL_TARGET_CMDS
 	$(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D) DESTDIR=$(TARGET_DIR) install
