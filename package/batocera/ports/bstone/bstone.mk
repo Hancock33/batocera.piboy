10356ece6b393c09954b196b7ec854d2fa82c15c
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2024-08-29 19:42:14 +0100

    bstone updates

--- package/batocera/ports/bstone/bstone.mk
+++ package/batocera/ports/bstone/bstone.mk
@@ -14,7 +14,7 @@ BSTONE_SUPPORTS_IN_SOURCE_BUILD = NO
 BSTONE_CONF_OPTS += -DBSTONE_CI_GIT_HASH="$(shell echo $(BSTONE_VERSION) | cut -c 1-7)"
 
 define BSTONE_INSTALL_TARGET_CMDS
-	cp -pvr $(@D)/buildroot-build/src/bstone $(TARGET_DIR)/usr/bin
+	cp -pvr $(@D)/buildroot-build/src/bstone/bstone $(TARGET_DIR)/usr/bin
 	# evmap config
 	mkdir -p $(TARGET_DIR)/usr/share/evmapy
 	cp $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/ports/bstone/bstone.keys $(TARGET_DIR)/usr/share/evmapy
