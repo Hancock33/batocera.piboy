e90ab9e4a4ac8c70a09d6b5d6d8c28055b87f001
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2024-08-30 17:39:35 +0100

    iconv tweaks

--- package/batocera/libraries/libiconv-kodi/libiconv-kodi.mk
+++ package/batocera/libraries/libiconv-kodi/libiconv-kodi.mk
@@ -4,27 +4,26 @@
 #
 ################################################################################
 
-LIBICONV_KODI_VERSION = 1.15
+LIBICONV_KODI_VERSION = 1.17
 LIBICONV_KODI_SITE = $(BR2_GNU_MIRROR)/libiconv
 LIBICONV_KODI_SOURCE = libiconv-$(LIBICONV_KODI_VERSION).tar.gz
 LIBICONV_KODI_INSTALL_STAGING = YES
 LIBICONV_KODI_LICENSE = GPL-3.0+ (iconv program), LGPL-2.0+ (library)
 LIBICONV_KODI_LICENSE_FILES = COPYING COPYING.LIB
+LIBICONV_KODI_LIBTOOL_PATCH = NO
 
 LIBICONV_KODI_CONF_OPTS += \
 	--prefix=/usr/lib/libiconv-kodi \
 	--libdir=/usr/lib/libiconv-kodi/lib \
+	--bindir=/usr/iib/libiconv-kodi/bin \
+	--disable-shared \
 	--enable-extra-encodings
 
-define LIBICONV_KODI_DISABLE_PRELOAD
-	$(SED) '/preload/d' $(@D)/Makefile.in
-endef
-LIBICONV_KODI_PRE_CONFIGURE_HOOKS += LIBICONV_KODI_DISABLE_PRELOAD
-
-define LIBICONV_KODI_MOVE_LIBS
-	$(MV) -f  $(TARGET_DIR)/usr/lib/libiconv-kodi/lib/*  $(TARGET_DIR)/usr/lib
+define LIBICONV_KODI_INSTALL_TARGET_CMDS
+	$(RM) -rf $(TARGET_DIR)/usr/lib/libiconv-kodi/include
+	cp -av  $(TARGET_DIR)/usr/lib/libiconv-kodi/*  $(TARGET_DIR)/usr/
 	$(RM) -rf $(TARGET_DIR)/usr/lib/libiconv-kodi
 endef
-LIBICONV_KODI_POST_INSTALL_TARGET_HOOKS += LIBICONV_KODI_MOVE_LIBS
+
 
 $(eval $(autotools-package))
