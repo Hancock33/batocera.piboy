d3d61c3196696f113b6b996e5ac8ed428528d434
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2024-08-31 07:32:16 +0100

    Update wine-mono.mk

--- package/batocera/emulators/wine/wine-mono/wine-mono.mk
+++ package/batocera/emulators/wine/wine-mono/wine-mono.mk
@@ -5,8 +5,8 @@
 ################################################################################
 # Version: Commits on Aug 16, 2024
 WINE_MONO_VERSION = wine-mono-9.3.0
-WINE_MONO_SOURCE = wine-mono-$(WINE_MONO_VERSION)-x86.tar.xz
-WINE_MONO_SITE = https://github.com/madewokherd/wine-mono/releases/download/wine-mono-$(WINE_MONO_VERSION)
+WINE_MONO_SOURCE = $(WINE_MONO_VERSION)-x86.tar.xz
+WINE_MONO_SITE = https://github.com/madewokherd/wine-mono/releases/download/$(WINE_MONO_VERSION)
 
 define WINE_MONO_EXTRACT_CMDS
 	mkdir -p $(@D)/target/usr/wine/ge-custom/share/wine/mono/
