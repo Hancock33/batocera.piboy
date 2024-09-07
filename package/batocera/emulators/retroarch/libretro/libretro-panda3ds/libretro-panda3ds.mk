ba7ad60c05951703000a05ccf6f497787781c512
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2024-09-07 10:59:10 +0100

    inital libretro-panda3ds build

---
+++ package/batocera/emulators/retroarch/libretro/libretro-panda3ds/libretro-panda3ds.mk
@@ -0,0 +1,24 @@
+################################################################################
+#
+# libretro-panda3ds
+#
+################################################################################
+# Version: Commits on Sept 05, 2024
+LIBRETRO_PANDA3DS_VERSION = f1b7830952e98299a62d325333cbe83b7bf81e83
+LIBRETRO_PANDA3DS_SITE = https://github.com/wheremyfoodat/Panda3DS.git
+LIBRETRO_PANDA3DS_SITE_METHOD=git
+LIBRETRO_PANDA3DS_GIT_SUBMODULES=YES
+LIBRETRO_PANDA3DS_LICENSE = GPLv3
+LIBRETRO_PANDA3DS_DEPENDENCIES += fdk-aac ffmpeg fmt lua
+LIBRETRO_PANDA3DS_SUPPORTS_IN_SOURCE_BUILD = NO
+
+LIBRETRO_PANDA3DS_CONF_OPTS += -DBUILD_LIBRETRO_CORE=ON
+LIBRETRO_PANDA3DS_CONF_OPTS += -DENABLE_DISCORD_RPC=OFF
+LIBRETRO_PANDA3DS_CONF_OPTS += -DENABLE_USER_BUILD=ON 
+
+define LIBRETRO_PANDA3DS_INSTALL_TARGET_CMDS
+	mkdir -p $(TARGET_DIR)/usr/lib/libretro
+	$(INSTALL) -D $(@D)/buildroot-build/panda3ds_libretro.so $(TARGET_DIR)/usr/lib/libretro/panda3ds_libretro.so
+endef
+
+$(eval $(cmake-package))
