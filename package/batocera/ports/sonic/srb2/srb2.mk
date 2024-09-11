afb877b1b7613ae5de2a025f2636b1e2f22dca58
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2024-09-11 20:28:25 +0100

    change srb2 to cmake

--- package/batocera/ports/sonic/srb2/srb2.mk
+++ package/batocera/ports/sonic/srb2/srb2.mk
@@ -6,7 +6,6 @@
 # Version: Commits on Feb 19, 2024
 SRB2_VERSION = dd54f749f2371804ca08a574801cd88a951a0e1e
 SRB2_SITE = $(call github,STJr,SRB2,$(SRB2_VERSION))
-
 SRB2_DEPENDENCIES = sdl2 sdl2_mixer libgme libopenmpt
 SRB2_LICENSE = GPL-2.0
 SRB2_SUPPORTS_IN_SOURCE_BUILD = NO
@@ -18,16 +17,11 @@ endef
 
 SRB2_POST_EXTRACT_HOOKS += SRB2_ASSETS
 
-define SRB2_BUILD_CMDS
-	$(SED) "s|^comptime\.c ::|comptime.c :|" $(@D)/src/Makefile
-	$(SED) "s|-DCOMPVERSION||g" $(@D)/src/Makefile
-	$(SED) "s|illegal|BATOCERA|g" $(@D)/src/comptime.c
-	$(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D) -f Makefile DEBUGMODE=0 LINUX64=1 NOHW=1
-endef
+SRB2_CONF_OPTS += -DSRB2_SDL2_EXE_NAME=srb2
+SRB2_CONF_OPTS += -DSRB2_CONFIG_ENABLE_TESTS=FALSE
 
 define SRB2_INSTALL_TARGET_CMDS
-	cp -L $(@D)/bin/debug/lsdl2srb2.debug $(TARGET_DIR)/usr/bin/srb2
-	chmod 754 $(TARGET_DIR)/usr/bin/srb2
+	cp -L $(@D)/buildroot-build/bin/srb2 $(TARGET_DIR)/usr/bin/srb2
 	mkdir -p $(TARGET_DIR)/usr/share/batocera/datainit/system/.srb2
 	cp $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/ports/sonic/srb2/config.cfg $(TARGET_DIR)/usr/share/batocera/datainit/system/.srb2/
 	# evmap config
@@ -35,4 +29,4 @@ define SRB2_INSTALL_TARGET_CMDS
 	cp $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/ports/sonic/srb2/srb2.keys $(TARGET_DIR)/usr/share/evmapy
 endef
 
-$(eval $(generic-package))
+$(eval $(cmake-package))
