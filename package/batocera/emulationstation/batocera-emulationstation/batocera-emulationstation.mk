5d793b6cc8c422e2d0570c84e7b62c7b7c061e35
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2024-09-15 18:29:47 +0100

    update emulationstatopm

--- package/batocera/emulationstation/batocera-emulationstation/batocera-emulationstation.mk
+++ package/batocera/emulationstation/batocera-emulationstation/batocera-emulationstation.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 # Version: Commits on Sept 15, 2024
-BATOCERA_EMULATIONSTATION_VERSION = 708f3c5ec72d9e2774856d8fc5d60bd44cd87c13
+BATOCERA_EMULATIONSTATION_VERSION = 0029fdfe75a1a762438ddcb6ac8a0fa17f2cfeb8
 BATOCERA_EMULATIONSTATION_SITE = https://github.com/batocera-linux/batocera-emulationstation
 BATOCERA_EMULATIONSTATION_SITE_METHOD = git
 BATOCERA_EMULATIONSTATION_GIT_SUBMODULES = YES
