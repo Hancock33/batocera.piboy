755a3e4fe29ddb9af8537a8841eece5110da33e1
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2024-09-20 20:17:00 +0100

    Update winetricks.mk

--- package/batocera/emulators/wine/winetricks/winetricks.mk
+++ package/batocera/emulators/wine/winetricks/winetricks.mk
@@ -8,6 +8,7 @@ WINETRICKS_VERSION = b8d145a95171e3d9a58ed0bd37bf2bae64d8230f
 WINETRICKS_SITE = $(call github,Winetricks,winetricks,$(WINETRICKS_VERSION))
 
 define WINETRICKS_INSTALL_TARGET_CMDS
+	$(SED) "s|    winetricks_latest_version_check|    #winetricks_latest_version_check|g" $(@D)/src/winetricks
 	$(INSTALL) -D $(@D)/src/winetricks $(TARGET_DIR)/usr/wine/winetricks
 endef
 
