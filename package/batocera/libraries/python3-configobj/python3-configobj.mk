a17fe7903cfea4187b8272b2d8e78143ec0aa8c0
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2024-09-21 22:28:43 +0100

    fix python3-configobj download

--- package/batocera/libraries/python3-configobj/python3-configobj.mk
+++ package/batocera/libraries/python3-configobj/python3-configobj.mk
@@ -5,8 +5,7 @@
 ################################################################################
 # Version: Commits on Sept 21, 2024
 PYTHON3_CONFIGOBJ_VERSION = v5.0.9
-PYTHON3_CONFIGOBJ_SOURCE = configobj-$(subst v,,$(PYTHON3_CONFIGOBJ_VERSION)).tar.gz
-PYTHON3_CONFIGOBJ_SITE = https://github.com/DiffSK/configobj/releases/download/$(PYTHON3_CONFIGOBJ_VERSION)
+PYTHON3_CONFIGOBJ_SITE = $(call github,DiffSK,configobj,$(PYTHON3_CONFIGOBJ_VERSION))
 PYTHON3_CONFIGOBJ_SETUP_TYPE = setuptools
 PYTHON3_CONFIGOBJ_LICENSE_FILES = LICENSE
 
