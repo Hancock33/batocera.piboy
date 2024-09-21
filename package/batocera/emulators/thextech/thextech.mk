2ea4c246e76b033dff026b952dee7369d88e9a07
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2024-09-21 20:41:46 +0100

    update thextech patches

--- package/batocera/emulators/thextech/thextech.mk
+++ package/batocera/emulators/thextech/thextech.mk
@@ -14,7 +14,9 @@ THEXTECH_DEPENDENCIES = sdl2 sdl2_mixer sdl2_ttf
 THEXTECH_CONF_ENV = GIT_DISCOVERY_ACROSS_FILESYSTEM=1
 THEXTECH_CONF_OPTS += -DUSE_SYSTEM_LIBS_DEFAULT=ON
 THEXTECH_CONF_OPTS += -DUSE_SYSTEM_SDL2=ON
+THEXTECH_CONF_OPTS += -DUSE_SYSTEM_ZLIB=ON
 THEXTECH_CONF_OPTS += -DTHEXTECH_ENABLE_TTF_SUPPORT=ON
+THEXTECH_CONF_OPTS += -DTHEXTECH_FORCE_FULLSCREEN=ON
 
 ifneq ($(BR2_PACKAGE_HAS_LIBGL),y)
     THEXTECH_CONF_OPTS += -DTHEXTECH_BUILD_GL_DESKTOP_MODERN=OFF
@@ -29,16 +31,15 @@ endif
 endif
 
 ifeq ($(BR2_PACKAGE_HAS_LIBMALI),y)
-    THEXTECH_CONF_OPTS += -DCMAKE_EXE_LINKER_FLAGS=-lmali
-    THEXTECH_CONF_OPTS += -DCMAKE_SHARED_LINKER_FLAGS=-lmali
+	THEXTECH_CONF_OPTS += -DCMAKE_EXE_LINKER_FLAGS=-lmali
+	THEXTECH_CONF_OPTS += -DCMAKE_SHARED_LINKER_FLAGS=-lmali
 endif
 
 define THEXTECH_INSTALL_TARGET_CMDS
 	$(INSTALL) -D $(@D)/output/bin/thextech $(TARGET_DIR)/usr/bin/
 	cp -avf $(@D)/output/lib/libSDL2_mixer_ext.so* $(TARGET_DIR)/usr/lib/
 	mkdir -p $(TARGET_DIR)/usr/share/evmapy
-	cp -avf $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/emulators/thextech/thextech.keys \
-	    $(TARGET_DIR)/usr/share/evmapy/
+	cp -avf $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/emulators/thextech/thextech.keys $(TARGET_DIR)/usr/share/evmapy/
 endef
 
 $(eval $(cmake-package))
