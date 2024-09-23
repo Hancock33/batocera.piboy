3ae1f0de7e43d94b9b31259eed7c2c845186e68a
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2024-09-23 07:20:31 +0100

    bump scummvm builds

--- package/batocera/emulators/scummvm/scummvm.mk
+++ package/batocera/emulators/scummvm/scummvm.mk
@@ -3,8 +3,8 @@
 # scummvm
 #
 ################################################################################
-# Version: Commits on Sept 21, 2024
-SCUMMVM_VERSION = c6afb82b9effe61e71ab6da92e1d756d49c97476
+# Version: Commits on Sept 23, 2024
+SCUMMVM_VERSION = 4259a3ed4f11e844e05e3a101d668002317290bc
 SCUMMVM_SITE = $(call github,scummvm,scummvm,$(SCUMMVM_VERSION))
 SCUMMVM_LICENSE = GPLv2
 SCUMMVM_DEPENDENCIES += sdl2 zlib libmpeg2 libogg libvorbis flac libmad libpng libtheora faad2 freetype jpeg fluidsynth
