cf18301e5d229312985df0cb91a1f16e3f82f6cb
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2024-09-25 09:33:11 +0100

    bump some python libs

--- package/batocera/libraries/python-hidapi/python-hidapi.mk
+++ package/batocera/libraries/python-hidapi/python-hidapi.mk
@@ -3,11 +3,11 @@
 # python-hidapi
 #
 ################################################################################
-# Version: Commits on May 28, 2023
-PYTHON_HIDAPI_VERSION = 0.14.0
+# Version: Commits on Jul 23, 2024
+PYTHON_HIDAPI_VERSION = 0.14.0.post2
 PYTHON_HIDAPI_SOURCE = hidapi-$(PYTHON_HIDAPI_VERSION).tar.gz
 PYTHON_HIDAPI_SETUP_TYPE = setuptools
-PYTHON_HIDAPI_SITE = https://files.pythonhosted.org/packages/95/0e/c106800c94219ec3e6b483210e91623117bfafcf1decaff3c422e18af349
+PYTHON_HIDAPI_SITE = https://files.pythonhosted.org/packages/bf/6f/90c536b020a8e860f047a2839830a1ade3e1490e67336ecf489b4856eb7b/hidapi-0.14.0.post2.tar.gz
 PYTHON_HIDAPI_LICENSE = GPLv3
 PYTHON_HIDAPI_LICENSE_FILES = LICENSE-gpl3.txt
 PYTHON_HIDAPI_DEPENDENCIES = libusb udev hidapi host-python-cython
--- package/batocera/libraries/python3-gpiod/python3-gpiod.mk
+++ package/batocera/libraries/python3-gpiod/python3-gpiod.mk
@@ -3,10 +3,10 @@
 # python3-gpiod
 #
 ################################################################################
-# Version: Commits on Jun 18, 2024
-PYTHON3_GPIOD_VERSION = 2.2.0
+# Version: Commits on Aug 01, 2024
+PYTHON3_GPIOD_VERSION = 2.2.1
 PYTHON3_GPIOD_SOURCE = gpiod-$(PYTHON3_GPIOD_VERSION).tar.gz
-PYTHON3_GPIOD_SITE = https://files.pythonhosted.org/packages/aa/b7/12bda3ba884c7299ac461f84732583048d962145ad230447115758c32e6e
+PYTHON3_GPIOD_SITE = https://files.pythonhosted.org/packages/e6/6a/a8120a6c582099ea7fd3b46a7071f0b9dd32808d337dc9e3da05ef046e67
 PYTHON3_GPIOD_SETUP_TYPE = setuptools
 PYTHON3_GPIOD_LICENSE_FILES = LICENSE
 
