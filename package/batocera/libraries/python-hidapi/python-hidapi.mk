f81e569cbda72dfbaf7f08c3bef1c5c59a8c4c42
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2024-09-25 12:07:27 +0100

    Update python-hidapi.mk

--- package/batocera/libraries/python-hidapi/python-hidapi.mk
+++ package/batocera/libraries/python-hidapi/python-hidapi.mk
@@ -7,7 +7,7 @@
 PYTHON_HIDAPI_VERSION = 0.14.0.post2
 PYTHON_HIDAPI_SOURCE = hidapi-$(PYTHON_HIDAPI_VERSION).tar.gz
 PYTHON_HIDAPI_SETUP_TYPE = setuptools
-PYTHON_HIDAPI_SITE = https://files.pythonhosted.org/packages/bf/6f/90c536b020a8e860f047a2839830a1ade3e1490e67336ecf489b4856eb7b/hidapi-0.14.0.post2.tar.gz
+PYTHON_HIDAPI_SITE = https://files.pythonhosted.org/packages/bf/6f/90c536b020a8e860f047a2839830a1ade3e1490e67336ecf489b4856eb7b
 PYTHON_HIDAPI_LICENSE = GPLv3
 PYTHON_HIDAPI_LICENSE_FILES = LICENSE-gpl3.txt
 PYTHON_HIDAPI_DEPENDENCIES = libusb udev hidapi host-python-cython
