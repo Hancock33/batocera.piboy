42783d14fb23702e0d6ea0c643ff8d9a6d998c2e
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2024-09-25 08:51:13 +0100

    bump cabextract version 1.11

--- package/batocera/utils/cabextract/cabextract.mk
+++ package/batocera/utils/cabextract/cabextract.mk
@@ -4,8 +4,13 @@
 #
 ################################################################################
 # Version: Commits on Feb 24, 2023
-CABEXTRACT_VERSION = 1.9.1
-CABEXTRACT_SITE = https://www.cabextract.org.uk
+CABEXTRACT_VERSION = 1.11
+CABEXTRACT_SITE =  $(call github,kyz,libmspack,v$(LIBMSPACK_VERSION))
+CABEXTRACT_LICENSE = LGPL-2.1
+CABEXTRACT_LICENSE_FILES = COPYING.LIB
+CABEXTRACT_SUBDIR = cabextract
+CABEXTRACT_AUTORECONF = YES
+CABEXTRACT_INSTALL_STAGING = YES
 
 CABEXTRACT_CONF_OPTS = --with-external-libmspack
 
