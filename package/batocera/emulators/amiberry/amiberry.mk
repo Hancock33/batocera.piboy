39cd864d56f2b8932b4c272e89608d9fbc5025f7
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2024-10-13 17:54:07 +0100

    update amiberry

--- package/batocera/emulators/amiberry/amiberry.mk
+++ package/batocera/emulators/amiberry/amiberry.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 # Version: Commits on Oct 13, 2024
-AMIBERRY_VERSION = b85279830a7c7250e007e0584bf8d428bd3c4ba6
+AMIBERRY_VERSION = 8ff8a1886102ac35645ada4cd2ac2299a8fc903a
 AMIBERRY_SITE = $(call github,BlitterStudio,amiberry,$(AMIBERRY_VERSION))
 AMIBERRY_LICENSE = GPLv3
 AMIBERRY_DEPENDENCIES = sdl2 sdl2_image sdl2_ttf mpg123 libxml2 libmpeg2 flac libpng libserialport portmidi
