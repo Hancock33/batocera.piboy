a2efa84dfb507d309835946002230a645db0cb0a
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2024-10-18 20:23:11 +0100

    Updates on Oct 18, 2024 20:15
    --------------------------------------------------------------------------------------
    flycast.mk 54e8589f6379c34cf50b1651e04ef9655a472d47 # Version: Commits on Oct 18, 2024
    --------------------------------------------------------------------------------------
    android: disable import/export home when SAF is disabled. Code clean up
    
    Disable import/export home folder buttons when not using SAF file
    
    picker.
    
    Get rid of unused home migration feature when onboarding.
    
    Android TV screenshots and banner.,
    
    ----------------------------------------------------
    pcsx2.mk v2.1.211 # Version: Commits on Oct 18, 2024
    ----------------------------------------------------
    - [Debugger: Add settings to control the symbol analysis passes](https://github.com/PCSX2/pcsx2/pull/11901)
    
    -------------------------------------------------------------------------------------------
    xenia-canary.mk d2921a09ebab899f62f7a0e44161f871828b36f6 # Version: Commits on Oct 18, 2024
    -------------------------------------------------------------------------------------------
    [UI] Fixed inconsistency in slot id,
    
    --------------------------------------------------------------------------------------
    stalker.mk bd73c34fd1c3eb59c1cef018114a7b2bb9995cdf # Version: Commits on Oct 18, 2024
    --------------------------------------------------------------------------------------
    Partially revert \xrGame/ActorCameras.cpp: don't update same camera two times\
    
    This effectively reverts 84f09c511d8d21535b47d3fe2e011abab9417d34,
    
    ----------------------------------------------------------------------------------------
    doomretro.mk 2d6dea1b6c3a3d3d13f599b6c3ffdbfcd49f7f8f # Version: Commits on Oct 18, 2024
    ----------------------------------------------------------------------------------------
    Fix obituary when player explodes barrel and `playername` CVAR empty,
    
    -----------------------------------------------------------------------------------------------
    libretro-flycast.mk 54e8589f6379c34cf50b1651e04ef9655a472d47 # Version: Commits on Oct 18, 2024
    -----------------------------------------------------------------------------------------------
    android: disable import/export home when SAF is disabled. Code clean up
    
    Disable import/export home folder buttons when not using SAF file
    
    picker.
    
    Get rid of unused home migration feature when onboarding.
    
    Android TV screenshots and banner.,

--- package/batocera/emulators/xenia-canary/xenia-canary.mk
+++ package/batocera/emulators/xenia-canary/xenia-canary.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 # Version: Commits on Oct 18, 2024
-XENIA_CANARY_VERSION = 58098aa6bf292269481c6680c329f7d86ac67b01
+XENIA_CANARY_VERSION = d2921a09ebab899f62f7a0e44161f871828b36f6
 XENIA_CANARY_SOURCE = xenia_canary.zip
 XENIA_CANARY_SITE = https://github.com/xenia-canary/xenia-canary/releases/download/"$(shell echo $(XENIA_CANARY_VERSION) | cut -c 1-7)"
 XENIA_CANARY_LICENSE = BSD
