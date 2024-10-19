c74a86041e6012c87719d67a1185ffd5daee605b
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2024-10-19 08:25:54 +0100

    bump 32bit libs

--- package/batocera/emulators/wine/wine-custom/wine-custom.mk
+++ package/batocera/emulators/wine/wine-custom/wine-custom.mk
@@ -3,8 +3,8 @@
 # wine-custom
 #
 ################################################################################
-# Version: Commits on Oct 18, 2024
-WINE_CUSTOM_VERSION = ntsynch-9.20
+# Version: Commits on Oct 19, 2024
+WINE_CUSTOM_VERSION = ntsynch-9.20.r0.g5a1b9d50
 WINE_CUSTOM_SOURCE = wine-$(WINE_CUSTOM_VERSION).tar.gz
 WINE_CUSTOM_SITE = $(call github,Hancock33,wine-tkg-batocera,$(WINE_CUSTOM_VERSION))
 WINE_CUSTOM_LICENSE = LGPL-2.1+
