bad7f56ecb7f0bda2a374834460fe72c292c014e
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2024-10-26 09:58:43 +0100

    Update clevis.mk

--- package/batocera/utils/clevis/clevis.mk
+++ package/batocera/utils/clevis/clevis.mk
@@ -5,7 +5,7 @@
 ################################################################################
 # Version: Commits on Oct 17, 2024
 CLEVIS_VERSION = ea01ad0c954ac478e72881504d8b7b0486431526
-CLEVIS_SITE = $(call github,latchset,clevis,v$(CLEVIS_VERSION))
+CLEVIS_SITE = $(call github,latchset,clevis,$(CLEVIS_VERSION))
 CLEVIS_LICENSE = GPLv3
 CLEVIS_LICENSE_FILES = COPYING
 
