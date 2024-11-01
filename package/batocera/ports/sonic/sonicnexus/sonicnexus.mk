2845e4882035802194da24256a5f5bc67ee83d10
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2024-11-01 23:00:50 +0000

    Update sonicnexus.mk

--- package/batocera/ports/sonic/sonicnexus/sonicnexus.mk
+++ package/batocera/ports/sonic/sonicnexus/sonicnexus.mk
@@ -8,14 +8,10 @@ SONICNEXUS_VERSION = 17540a395182aeca0570f489daf83fa4899da91d
 SONICNEXUS_SITE = $(call github,RSDKModding,RSDKv2-Decompilation,$(SONICNEXUS_VERSION))
 SONICNEXUS_DEPENDENCIES = sdl2 libogg libvorbis
 SONICNEXUS_LICENSE = Custom
-
-define SONICNEXUS_BUILD_CMDS
-	$(TARGET_CONFIGURE_OPTS) CFLAGS="$(TARGET_CFLAGS) -flto" CXXFLAGS="$(TARGET_CXXFLAGS) -flto" \
-	$(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D) -f Makefile USE_HW_REN=1
-endef
+SONICNEXUS_SUPPORTS_IN_SOURCE_BUILD = NO
 
 define SONICNEXUS_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 0755 $(@D)/bin/nexus  $(TARGET_DIR)/usr/bin/sonicnexus
+	$(INSTALL) -D -m 0755 $(@D)/buildroot-build/Nexus  $(TARGET_DIR)/usr/bin/sonicnexus
 endef
 
 define SONICNEXUS_EVMAPY
@@ -25,4 +21,5 @@ endef
 
 SONICNEXUS_POST_INSTALL_TARGET_HOOKS += SONICNEXUS_EVMAPY
 
-$(eval $(generic-package))
+$(eval $(cmake-package))
+
