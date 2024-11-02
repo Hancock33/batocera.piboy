cba5170b1def0166ea05311d97d147ac58932be7
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2024-11-02 11:42:34 +0000

    Update rottGenerator.py

--- package/batocera/core/batocera-configgen/configgen/configgen/generators/rott/rottGenerator.py
+++ package/batocera/core/batocera-configgen/configgen/configgen/generators/rott/rottGenerator.py
@@ -2,18 +2,32 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-from ... import Command, controllersConfig
+from ... import Command
+from ...batoceraPaths import CONFIGS
+from ...controller import generate_sdl_game_controller_config
 from ..Generator import Generator
+
+if TYPE_CHECKING:
+    from ...types import HotkeysContext
 
 class RottGenerator(Generator):
 
-    def generate(self, system, rom, playersControllers, metadata, guns, wheels, gameResolution):
-        commandArray = ["rott-darkwar"]
-
-        return Command.Command(array=commandArray)
-
     def getHotkeysContext(self) -> HotkeysContext:
         return {
-            "name": "rott-darkwar",
+            "name": "rott",
             "keys": { "exit": ["KEY_LEFTALT", "KEY_F4"] }
         }
+
+    def generate(self, system, rom, playersControllers, metadata, guns, wheels, gameResolution):
+
+        commandArray = [ "rott-darkwar" ]
+
+        return Command.Command(
+            array=commandArray,
+            env={
+                "XDG_DATA_HOME": CONFIGS,
+                "XDG_DATA_DIRS": "/userdata/roms/port/rott",
+                "SDL_GAMECONTROLLERCONFIG": generate_sdl_game_controller_config(playersControllers),
+                "SDL_JOYSTICK_HIDAPI": "0"
+            }
+        )
