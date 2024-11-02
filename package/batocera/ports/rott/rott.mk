de67942617bcf5703a7ae70c9426a95acf1f9131
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2024-11-02 12:00:21 +0000

    cleanup rott

--- package/batocera/ports/rott/rott.mk
+++ package/batocera/ports/rott/rott.mk
@@ -3,26 +3,22 @@
 # rott
 #
 ################################################################################
-# Version: Commits on Jan 04, 2021
-ROTT_VERSION = 407e3d8b07eaa968e8ed7d52a03fd3199dccdddf
-ROTT_SITE = $(call github,LTCHIPS,rottexpr,$(ROTT_VERSION))
+# Version: Commits on Oct 31, 2024
+ROTT_VERSION = 7fc2dac0dd895514b84063dc99aaaa93d2df69ed
+ROTT_SITE = $(call github,fabiangreffrath,taradino,$(ROTT_VERSION))
+ROTT_LICENSE = GPLv2
+ROTT_LICENSE_FILE = README.md
 
 ROTT_DEPENDENCIES = sdl2 sdl2_mixer
-ROTT_LICENSE = GPLv2
 
-define ROTT_BUILD_CMDS
-	$(SED) "s|-Og|$(TARGET_OPTIMIZATION)|g" $(@D)/src/Makefile
-	$(TARGET_CONFIGURE_OPTS) $(MAKE) \
-	CPP="$(TARGET_CPP)" CXX="$(TARGET_CXX)" CC="$(TARGET_CC) -fPIC" \
-	AS="$(TARGET_CC)" LD="$(TARGET_LD)" STRIP="$(TARGET_STRIP)" \
-	-C $(@D)/src -f Makefile DATADIR="/userdata/roms/ports/rott"
-endef
+ROTT_SUPPORTS_IN_SOURCE_BUILD = NO
+
+ROTT_CONF_OPTS += -DBUILD_SHARED_LIBS=OFF
 
 define ROTT_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 0755 $(@D)/src/rott -D $(TARGET_DIR)/usr/bin/rott-darkwar
-	# evmap config
+	cp $(@D)/buildroot-build/taradino $(TARGET_DIR)/usr/bin/rott-darkwar
 	mkdir -p $(TARGET_DIR)/usr/share/evmapy
-	cp $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/ports/rott/rott.keys $(TARGET_DIR)/usr/share/evmapy/rott.keys
+	cp -f $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/ports/rott/rott.keys $(TARGET_DIR)/usr/share/evmapy
 endef
 
-$(eval $(generic-package))
+$(eval $(cmake-package))
