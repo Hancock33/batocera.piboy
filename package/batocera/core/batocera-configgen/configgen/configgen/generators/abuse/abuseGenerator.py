c47d3b6c52eb052dd20c6d13786a75b50b52ec3e
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2024-11-15 21:20:38 +0000

    generator cleanups

--- package/batocera/core/batocera-configgen/configgen/configgen/generators/abuse/abuseGenerator.py
+++ package/batocera/core/batocera-configgen/configgen/configgen/generators/abuse/abuseGenerator.py
@@ -1,9 +1,7 @@
-
 from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-import os
 from ... import Command
 from ...controller import generate_sdl_game_controller_config
 from ..Generator import Generator
@@ -11,9 +9,8 @@ from ..Generator import Generator
 if TYPE_CHECKING:
     from ...types import HotkeysContext
 
+
 class AbuseGenerator(Generator):
-    if os.path.isfile('/tmp/piboy'):
-        os.system('piboy_keys abuse.keys')
 
     def generate(self, system, rom, playersControllers, metadata, guns, wheels, gameResolution):
         commandArray = ["abuse", "-datadir", "/usr/share/abuse"]
