0809a3cf3ec508049cbe6e160936980aa1556a49
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2024-11-18 20:26:34 +0000

    flatpak use libcurl

--- package/batocera/utils/flatpak/flatpak.mk
+++ package/batocera/utils/flatpak/flatpak.mk
@@ -9,8 +9,8 @@ FLATPAK_SOURCE = flatpak-$(FLATPAK_VERSION).tar.xz
 FLATPAK_SITE = https://github.com/flatpak/flatpak/releases/download/$(FLATPAK_VERSION)
 
 FLATPAK_DEPENDENCIES += appstream glib-networking host-pkgconf host-python-pyparsing
-FLATPAK_DEPENDENCIES += json-glib libarchive libcap libfuse libglib2 libgpgme libostree
-FLATPAK_DEPENDENCIES += libseccomp libsoup libsoup3 pkgconf polkit python-pyparsing yaml-cpp
+FLATPAK_DEPENDENCIES += json-glib libarchive libcap libcurl libfuse libglib2 libgpgme libostree
+FLATPAK_DEPENDENCIES += libseccomp pkgconf polkit python-pyparsing yaml-cpp
 FLATPAK_DEPENDENCIES += hicolor-icon-theme adwaita-icon-theme adwaita-icon-theme-light
 
 FLATPAK_CONF_OPTS += --with-sysroot="$(STAGING_DIR)"
@@ -22,18 +22,13 @@ FLATPAK_CONF_OPTS += --disable-selinux-module
 FLATPAK_CONF_ENV += LDFLAGS=-lpthread
 
 define FLATPAK_INSTALL_SCRIPTS
-	install -m 0755 \
-	    $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/utils/flatpak/batocera-flatpak-update \
-		$(TARGET_DIR)/usr/bin/
+	install -m 0755  $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/utils/flatpak/batocera-flatpak-update $(TARGET_DIR)/usr/bin/
 	mkdir -p $(TARGET_DIR)/usr/share/emulationstation/hooks
-	ln -sf /usr/bin/batocera-flatpak-update \
-	    $(TARGET_DIR)/usr/share/emulationstation/hooks/preupdate-gamelists-flatpak
-	ln -sf /usr/bin/batocera-steam-update \
-	    $(TARGET_DIR)/usr/share/emulationstation/hooks/preupdate-gamelists-steam
+	ln -sf /usr/bin/batocera-flatpak-update $(TARGET_DIR)/usr/share/emulationstation/hooks/preupdate-gamelists-flatpak
+	ln -sf /usr/bin/batocera-steam-update   $(TARGET_DIR)/usr/share/emulationstation/hooks/preupdate-gamelists-steam
 	#evmap config
 	mkdir -p $(TARGET_DIR)/usr/share/evmapy
-	cp -f $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/utils/flatpak/*.keys \
-	    $(TARGET_DIR)/usr/share/evmapy
+	cp -f $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/utils/flatpak/*.keys $(TARGET_DIR)/usr/share/evmapy
 endef
 
 FLATPAK_POST_INSTALL_TARGET_HOOKS += FLATPAK_INSTALL_SCRIPTS
