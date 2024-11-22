b2ff8d7b60196e26b6deb5b4fb1fe05b2ee4a468
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2024-11-22 16:20:08 +0000

    update python modules

--- package/batocera/libraries/python-gpiod/python-gpiod.mk
+++ package/batocera/libraries/python-gpiod/python-gpiod.mk
@@ -3,11 +3,10 @@
 # python-gpiod
 #
 ################################################################################
-# Version: Commits on Aug 01, 2024
-PYTHON_GPIOD_VERSION = 2.2.1
+# Version: Commits on Nov 12, 2024
+PYTHON_GPIOD_VERSION = 2.2.2
 PYTHON_GPIOD_SOURCE = gpiod-$(PYTHON_GPIOD_VERSION).tar.gz
-PYTHON_GPIOD_SITE = https://files.pythonhosted.org/packages/e6/6a/a8120a6c582099ea7fd3b46a7071f0b9dd32808d337dc9e3da05ef046e67
+PYTHON_GPIOD_SITE = https://files.pythonhosted.org/packages/5a/be/9876843be7d8d3e05582de8919077b9084beac5d9791b139653e3048e4fd
 PYTHON_GPIOD_SETUP_TYPE = setuptools
-PYTHON_GPIOD_LICENSE_FILES = LICENSE
 
 $(eval $(python-package))
--- package/batocera/libraries/python-proxy-tools/python-proxy-tools.mk
+++ package/batocera/libraries/python-proxy-tools/python-proxy-tools.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 # Version: Commits on May 06, 2014
-PYTHON_PROXY_TOOLS_VERSION = db43f1e35d4f90a65c5a4d56d9e9af88212ec6e6
-PYTHON_PROXY_TOOLS_SITE = $(call github,jtushman,proxy_tools,$(PYTHON_PROXY_TOOLS_VERSION))
-PYTHON_PROXY_TOOLS_LICENSE = BSD
+PYTHON_PROXY_TOOLS_VERSION = 0.1.0
+PYTHON_PROXY_TOOLS_SOURCE = proxy_tools-$(PYTHON_PROXY_TOOLS_VERSION).tar.gz
+PYTHON_PROXY_TOOLS_SITE = https://files.pythonhosted.org/packages/f2/cf/77d3e19b7fabd03895caca7857ef51e4c409e0ca6b37ee6e9f7daa50b642
 PYTHON_PROXY_TOOLS_SETUP_TYPE = setuptools
 
 $(eval $(python-package))
