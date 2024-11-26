bfb378179c4812312511f8409602805bc04b61bf
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2024-11-26 15:10:50 +0000

    update some dates

--- package/batocera/controllers/wheels/hid-t150/hid-t150.mk
+++ package/batocera/controllers/wheels/hid-t150/hid-t150.mk
@@ -3,7 +3,7 @@
 # hid-t150
 #
 ################################################################################
-# Version: Commits on Dec 19, 2023
+# Version: Commits on Dec 18, 2023
 HID_T150_VERSION = 580b79b7b479076ba470fcc21fbd8484f5328546
 HID_T150_SITE = $(call github,scarburato,t150_driver,$(HID_T150_VERSION))
 HID_T150_MODULE_SUBDIRS = hid-t150
