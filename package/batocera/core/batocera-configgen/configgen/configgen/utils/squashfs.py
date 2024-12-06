Hancock33/batocera.piboy

commit 8a69c3976b74e64d1b4730b171f731a0ae8c1208

improve configgen squashfs handling with context manager

package/batocera/core/batocera-configgen/configgen/configgen/utils/squashfs.py
@@ -1,0 +1,75 @@
+from __future__ import annotations
+
+import logging
+import subprocess
+from contextlib import contextmanager
+from pathlib import Path
+from typing import TYPE_CHECKING, Final
+
+from ..batoceraPaths import mkdir_if_not_exists
+
+if TYPE_CHECKING:
+    from collections.abc import Iterator
+
+eslog = logging.getLogger(__name__)
+
+_SQUASHFS_DIR: Final = Path("/var/run/squashfs/")
+
+
+@contextmanager
+def squashfs_rom(rom: str | Path, /) -> Iterator[str]:
+    rom = Path(rom)
+
+    eslog.debug(f"squashfs_rom({rom})")
+    mount_point = _SQUASHFS_DIR / rom.stem
+
+    mkdir_if_not_exists(_SQUASHFS_DIR)
+
+    # first, try to clean an empty remaining directory (for example because of a crash)
+    if mount_point.exists() and mount_point.is_dir():
+        eslog.debug(f"squashfs_rom: {mount_point} already exists")
+        # try to remove an empty directory, else, run the directory, ignoring the .squashfs
+        try:
+            mount_point.rmdir()
+        except:
+            eslog.debug(f"squashfs_rom: failed to rmdir {mount_point}")
+            yield str(mount_point)
+            # No cleanup is necessary
+            return
+
+    # ok, the base directory doesn't exist, let's create it and mount the squashfs on it
+    mount_point.mkdir()
+
+    return_code = subprocess.call(["mount", rom, mount_point])
+    if return_code != 0:
+        eslog.debug(f"squashfs_rom: mounting {mount_point} failed")
+        try:
+            mount_point.rmdir()
+        except:
+            pass
+        raise Exception(f"unable to mount the file {rom}")
+
+    try:
+        # if the squashfs contains a single file with the same name, take it as the rom file
+        rom_single = mount_point / rom.stem
+        if len(list(mount_point.iterdir())) == 1 and rom_single.exists():
+            eslog.debug(f"squashfs: single rom {rom_single}")
+            yield str(rom_single)
+        else:
+            try:
+                rom_linked = (mount_point / ".ROM").resolve(strict=True)
+                eslog.debug(f"squashfs: linked rom {rom_linked}")
+                yield str(rom_linked)
+            except:
+                yield str(mount_point)
+    finally:
+        eslog.debug(f"squashfs_rom: cleaning up {mount_point}")
+
+        # unmount
+        return_code = subprocess.call(["unmount", mount_point])
+        if return_code != 0:
+            eslog.debug(f"squashfs_rom: unmounting {mount_point} failed")
+            raise Exception(f"unable to umount the file {mount_point}")
+
+        # cleaning the empty directory
+        mount_point.rmdir()

package/batocera/core/batocera-configgen/configgen/configgen/emulatorlauncher.py
@@ -17,96 +17,29 @@ if os.path.exists("/var/run/emulatorlauncher.perf"):
 
 ### import always needed ###
 import argparse
-import signal
-import time
-from sys import exit
-import subprocess
 import json
 import logging
-from pathlib import Path
+import signal
+import subprocess
+import time
+from sys import exit
 
-from . import controllersConfig as controllers
-from . import GeneratorImporter
+from . import GeneratorImporter, controllersConfig as controllers
 from .batoceraPaths import SAVES
 from .Emulator import Emulator
-from .utils import bezels as bezelsUtil
-from .utils import videoMode
-from .utils import gunsUtils
-from .utils import wheelsUtils
-from .utils.logger import setup_logging
+from .utils import bezels as bezelsUtil, gunsUtils, videoMode, wheelsUtils
 from .utils.hotkeygen import set_hotkeygen_context
+from .utils.logger import setup_logging
+from .utils.squashfs import squashfs_rom
 
 eslog = logging.getLogger(__name__)
-
-def squashfs_begin(rom):
-    eslog.debug(f"squashfs_begin({rom})")
-    rommountpoint = "/var/run/squashfs/" + os.path.basename(rom)[:-9]
-
-    if not os.path.exists("/var/run/squashfs"):
-        os.mkdir("/var/run/squashfs")
-
-    # first, try to clean an empty remaining directory (for example because of a crash)
-    if os.path.exists(rommountpoint) and os.path.isdir(rommountpoint):
-        eslog.debug(f"squashfs_begin: {rommountpoint} already exists")
-        # try to remove an empty directory, else, run the directory, ignoring the .squashfs
-        try:
-            os.rmdir(rommountpoint)
-        except:
-            eslog.debug(f"squashfs_begin: failed to rmdir {rommountpoint}")
-            return False, None, rommountpoint
-
-    # ok, the base directory doesn't exist, let's create it and mount the squashfs on it
-    os.mkdir(rommountpoint)
-    return_code = subprocess.call(["mount", rom, rommountpoint])
-    if return_code != 0:
-        eslog.debug(f"squashfs_begin: mounting {rommountpoint} failed")
-        try:
-            os.rmdir(rommountpoint)
-        except:
-            pass
-        raise Exception(f"unable to mount the file {rom}")
-
-    # if the squashfs contains a single file with the same name, take it as the rom file
-    romsingle = rommountpoint + "/" + os.path.basename(rom)[:-9]
-    if len(os.listdir(rommountpoint)) == 1 and  os.path.exists(romsingle):
-        eslog.debug(f"squashfs: single rom {romsingle}")
-        return True, rommountpoint, romsingle
-
-    # If a .ROM symlink is present, use the linked file as the ROM.
-    try:
-        romlinked = os.path.realpath(os.path.join(rommountpoint, ".ROM"), strict=True)
-        eslog.debug(f"squashfs: linked rom {romlinked}")
-        return True, rommountpoint, romlinked
-    except:
-        pass
-
-    return True, rommountpoint, rommountpoint
-
-def squashfs_end(rommountpoint):
-    eslog.debug(f"squashfs_end({rommountpoint})")
-
-    # umount
-    return_code = subprocess.call(["umount", rommountpoint])
-    if return_code != 0:
-        eslog.debug(f"squashfs_begin: unmounting {rommountpoint} failed")
-        raise Exception(f"unable to umount the file {rommountpoint}")
-
-    # cleaning the empty directory
-    os.rmdir(rommountpoint)
 
 def main(args, maxnbplayers):
     # squashfs roms if squashed
     extension = os.path.splitext(args.rom)[1][1:].lower()
     if extension == "squashfs":
-        exitCode = 0
-        need_end = False
-        try:
-            need_end, rommountpoint, rom = squashfs_begin(args.rom)
-            exitCode = start_rom(args, maxnbplayers, rom, args.rom)
-        finally:
-            if need_end:
-                squashfs_end(rommountpoint)
-        return exitCode
+        with squashfs_rom(args.rom) as rom:
+            return start_rom(args, maxnbplayers, rom, args.rom)
     else:
         return start_rom(args, maxnbplayers, args.rom, args.rom)
 
@@ -627,8 +560,6 @@ def launch():
             eslog.error("configgen exception: ", exc_info=True)
 
         if profiler:
-            import io
-            import pstats
             profiler.disable()
             profiler.dump_stats('/var/run/emulatorlauncher.prof')
 

commit 57e195f5f84f2c2d4ba6d9be4a431de26f290cfa

configgen: squashfs_rom: Call "umount" instead of "unmount".
The squashfs refactor in 8a69c3976b74e64d1b4730b171f731a0ae8c1208
mistakenly calls an "unmount" command to unmount the squashfs volume,
leading to an emulatorlauncher failure (on exit) with the squashfs volume
remaining mounted.  This now calls the correct umount command.

package/batocera/core/batocera-configgen/configgen/configgen/utils/squashfs.py
@@ -67,10 +67,10 @@ def squashfs_rom(rom: str | Path, /) -> Iterator[str]:
         eslog.debug(f"squashfs_rom: cleaning up {mount_point}")
 
         # unmount
-        return_code = subprocess.call(["unmount", mount_point])
+        return_code = subprocess.call(["umount", mount_point])
         if return_code != 0:
             eslog.debug(f"squashfs_rom: unmounting {mount_point} failed")
-            raise Exception(f"unable to umount the file {mount_point}")
+            raise Exception(f"unable to unmount the file {mount_point}")
 
         # cleaning the empty directory
         mount_point.rmdir()