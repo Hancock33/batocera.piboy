ae9203d7fe63e410e0cd715e35c782707c43ce46
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2024-12-08 07:44:41 +0000

    tidyup vita3k

--- package/batocera/emulators/vita3k/vita3k.mk
+++ package/batocera/emulators/vita3k/vita3k.mk
@@ -18,39 +18,18 @@ VITA3K_CONF_OPTS += -DBUILD_SHARED_LIBS=OFF
 VITA3K_CONF_OPTS += -DUSE_DISCORD_RICH_PRESENCE=OFF
 VITA3K_CONF_OPTS += -DUSE_VITA3K_UPDATE=OFF
 VITA3K_CONF_OPTS += -DBUILD_EXTERNAL=ON
-VITA3K_CONF_OPTS += -DXXH_X86DISPATCH_ALLOW_AVX=ON
 VITA3K_CONF_OPTS += -DCMAKE_C_COMPILER=$(HOST_DIR)/bin/$(GNU_TARGET_NAME)-gcc
 VITA3K_CONF_OPTS += -DCMAKE_CXX_COMPILER=$(HOST_DIR)/bin/$(GNU_TARGET_NAME)-g++
 
-ifeq ($(BR2_x86_64),y)
-    VITA3K_FFMPEG_NAME=ffmpeg-linux-x64.zip
-else ifeq ($(BR2_aarch64),y)
-    VITA3K_FFMPEG_NAME=ffmpeg-linux-arm64.zip
-endif
-
-VITA3K_FFMPEG_VER=$(shell cd "$(DL_DIR)/$(VITA3K_DL_SUBDIR)/git/external/ffmpeg" \
-	&& git rev-parse --short HEAD)
-
-ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_ZEN3),y)
+ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_X86_64),y)
     VITA3K_CONF_OPTS += -DXXH_X86DISPATCH_ALLOW_AVX=ON
 else
     VITA3K_CONF_OPTS += -DXXH_X86DISPATCH_ALLOW_AVX=OFF
 endif
 
-define VITA3K_GET_SUBMODULE
-	mkdir -p $(@D)/external
-	cd $(@D)/external && git clone https://github.com/Vita3K/nativefiledialog-cmake
-endef
-
-define VITA3K_FFMPEG_ZIP
-	mkdir -p $(@D)/buildroot-build/external
-	$(HOST_DIR)/bin/curl -L \
-		https://github.com/Vita3K/ffmpeg-core/releases/download/$(VITA3K_FFMPEG_VER)/$(VITA3K_FFMPEG_NAME) \
-		-o $(@D)/buildroot-build/external/ffmpeg.zip
-endef
-
 define VITA3K_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/usr/bin/vita3k/
+	$(TARGET_STRIP) $(@D)/buildroot-build/bin/Vita3K
 	cp -R $(@D)/buildroot-build/bin/* $(TARGET_DIR)/usr/bin/vita3k/
 endef
 
@@ -59,8 +38,6 @@ define VITA3K_INSTALL_EVMAPY
 	cp $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/emulators/vita3k/psvita.vita3k.keys $(TARGET_DIR)/usr/share/evmapy
 endef
 
-VITA3K_POST_EXTRACT_HOOKS = VITA3K_GET_SUBMODULE
-VITA3K_POST_EXTRACT_HOOKS += VITA3K_FFMPEG_ZIP
 VITA3K_POST_INSTALL_TARGET_HOOKS = VITA3K_INSTALL_EVMAPY
 
 $(eval $(cmake-package))
