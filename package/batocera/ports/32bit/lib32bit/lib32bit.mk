43fbb6081927cd8517f243bb1fdbfc24995b0b8c
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2024-12-11 20:11:44 +0000

    bump 32lib libs

--- package/batocera/ports/32bit/lib32bit/lib32bit.mk
+++ package/batocera/ports/32bit/lib32bit/lib32bit.mk
@@ -3,8 +3,8 @@
 # lib32bit
 #
 ################################################################################
-# Version: Commits on Dec 07, 2024
-LIB32BIT_VERSION = 20241207
+# Version: Commits on Dec 11, 2024
+LIB32BIT_VERSION = 20241211
 LIB32BIT_SOURCE = batocera-rpi4-32bit-libs-$(LIB32BIT_VERSION).tar.xz
 LIB32BIT_SITE = https://github.com/Hancock33/batocera-32bit-libs/releases/download/$(LIB32BIT_VERSION)
 
