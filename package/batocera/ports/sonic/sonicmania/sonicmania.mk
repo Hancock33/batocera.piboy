fdaae4c1aff09f2ba1e460512d2409b80bc8d25d
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2024-12-14 15:12:08 +0000

    tidyup sonic ports

--- package/batocera/ports/sonic/sonicmania/sonicmania.mk
+++ package/batocera/ports/sonic/sonicmania/sonicmania.mk
@@ -11,23 +11,23 @@ SONICMANIA_GIT_SUBMODULES = YES
 SONICMANIA_LICENSE = Proprietary
 SONICMANIA_LICENSE_FILE = LICENSE.md
 
-SONICMANIA_DEPENDENCIES += libglu libglew libglfw libogg libtheora portaudio
+SONICMANIA_DEPENDENCIES += libglu libglew libglfw libogg libtheora portaudio sdl2
 
 SONICMANIA_SUPPORTS_IN_SOURCE_BUILD = NO
 
 SONICMANIA_CONF_OPTS += -DBUILD_SHARED_LIBS=OFF
 SONICMANIA_CONF_OPTS += -DGAME_STATIC=ON
+SONICMANIA_CONF_OPTS += -DGAME_INCLUDE_EDITOR=OFF
 SONICMANIA_CONF_OPTS += -DRETRO_OUTPUT_NAME=sonicmania
+SONICMANIA_CONF_OPTS += -DRETRO_SUBSYSTEM=SDL2
 
 define SONICMANIA_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 0755 $(@D)/buildroot-build/dependencies/RSDKv5/sonicmania \
-		$(TARGET_DIR)/usr/bin/sonicmania
+	$(INSTALL) -D -m 0755 $(@D)/buildroot-build/dependencies/RSDKv5/sonicmania $(TARGET_DIR)/usr/bin/sonicmania
 endef
 
 define SONICMANIA_EVMAPY
 	mkdir -p $(TARGET_DIR)/usr/share/evmapy
-	cp $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/ports/sonic/sonicmania/sonicmania.keys \
-		$(TARGET_DIR)/usr/share/evmapy
+	cp $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/ports/sonic/sonicmania/sonicmania.keys $(TARGET_DIR)/usr/share/evmapy
 endef
 
 SONICMANIA_POST_INSTALL_TARGET_HOOKS += SONICMANIA_EVMAPY
