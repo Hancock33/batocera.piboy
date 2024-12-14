fb26eff48b634b84880d9c4ff0da65917d63ea96
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2024-12-14 09:36:51 +0000

    refactor configgen invocation of batocera-vulkan into module #13064

--- package/batocera/core/batocera-configgen/configgen/configgen/generators/xenia/xeniaGenerator.py
+++ package/batocera/core/batocera-configgen/configgen/configgen/generators/xenia/xeniaGenerator.py
@@ -15,6 +15,7 @@ import toml
 from ... import Command
 from ...batoceraPaths import CACHE, CONFIGS, HOME, SAVES, mkdir_if_not_exists
 from ...controller import generate_sdl_game_controller_config
+from ...utils import vulkan
 from ..Generator import Generator
 
 if TYPE_CHECKING:
@@ -56,28 +57,21 @@ class XeniaGenerator(Generator):
         core = system.config['core']
 
         # check Vulkan first before doing anything
-        try:
-            have_vulkan = subprocess.check_output(["/usr/bin/batocera-vulkan", "hasVulkan"], text=True).strip()
-            if have_vulkan == "true":
-                eslog.debug("Vulkan driver is available on the system.")
-                try:
-                    vulkan_version = subprocess.check_output(["/usr/bin/batocera-vulkan", "vulkanVersion"], text=True).strip()
-                    if vulkan_version > "1.3":
-                        eslog.debug("Using Vulkan version: {}".format(vulkan_version))
-                    else:
-                        if system.isOptSet('xenia_api') and system.config['xenia_api'] == "D3D12":
-                            eslog.debug("Vulkan version: {} is not compatible with Xenia when using D3D12".format(vulkan_version))
-                            eslog.debug("You may have performance & graphical errors, switching to native Vulkan".format(vulkan_version))
-                            system.config['xenia_api'] = "Vulkan"
-                        else:
-                            eslog.debug("Vulkan version: {} is not recommended with Xenia".format(vulkan_version))
-                except subprocess.CalledProcessError:
-                    eslog.debug("Error checking for Vulkan version.")
+        if vulkan.is_available():
+            eslog.debug("Vulkan driver is available on the system.")
+            vulkan_version = vulkan.get_verson()
+            if vulkan_version > "1.3":
+                eslog.debug("Using Vulkan version: {}".format(vulkan_version))
             else:
-                eslog.debug("*** Vulkan driver required is not available on the system!!! ***")
-                sys.exit()
-        except subprocess.CalledProcessError:
-            eslog.debug("Error executing batocera-vulkan script.")
+                if system.isOptSet('xenia_api') and system.config['xenia_api'] == "D3D12":
+                    eslog.debug("Vulkan version: {} is not compatible with Xenia when using D3D12".format(vulkan_version))
+                    eslog.debug("You may have performance & graphical errors, switching to native Vulkan".format(vulkan_version))
+                    system.config['xenia_api'] = "Vulkan"
+                else:
+                    eslog.debug("Vulkan version: {} is not recommended with Xenia".format(vulkan_version))
+        else:
+            eslog.debug("*** Vulkan driver required is not available on the system!!! ***")
+            sys.exit()
 
         # set to 64bit environment by default
         os.environ['WINEARCH'] = 'win64'
