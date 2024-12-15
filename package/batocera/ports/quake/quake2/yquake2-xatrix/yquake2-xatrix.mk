079bf807526bbfa1be93de8078cee12941feb2c4
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2024-12-15 12:14:17 +0000

    tidyup quake2

--- package/batocera/ports/quake/quake2/yquake2-xatrix/yquake2-xatrix.mk
+++ package/batocera/ports/quake/quake2/yquake2-xatrix/yquake2-xatrix.mk
@@ -1,17 +1,16 @@
 ################################################################################
 #
-# xatrix
+# yquake2-xatrix
 #
 ################################################################################
 # Version: Commits on Sept 21, 2024
-XATRIX_VERSION = 1957253b256180f9bbb5b52c8d63cc1578170211
-XATRIX_SITE = $(call github,yquake2,xatrix,$(XATRIX_VERSION))
+YQUAKE2_XATRIX_VERSION = 1957253b256180f9bbb5b52c8d63cc1578170211
+YQUAKE2_XATRIX_SITE = $(call github,yquake2,xatrix,$(YQUAKE2_XATRIX_VERSION))
+YQUAKE2_XATRIX_DEPENDENCIES = sdl2
+YQUAKE2_XATRIX_SUPPORTS_IN_SOURCE_BUILD = NO
 
-XATRIX_DEPENDENCIES = sdl2 sdl2_mixer yquake2
-XATRIX_LICENSE = GPL-2.0
-XATRIX_SUPPORTS_IN_SOURCE_BUILD = NO
-
-define XATRIX_INSTALL_TARGET_CMDS
+# Install game library
+define YQUAKE2_XATRIX_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/usr/share/game_assets/quake2/xatrix
 	cp -pvr $(@D)/buildroot-build/Release/* $(TARGET_DIR)/usr/share/game_assets/quake2/xatrix
 endef
