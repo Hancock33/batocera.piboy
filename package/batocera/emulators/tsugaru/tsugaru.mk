e962aad859259f3939a4b45eba4fc51ff2a37624
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2024-12-15 13:56:12 +0000

    Update tsugaru.mk

--- package/batocera/emulators/tsugaru/tsugaru.mk
+++ package/batocera/emulators/tsugaru/tsugaru.mk
@@ -9,16 +9,14 @@ TSUGARU_SITE = $(call github,captainys,TOWNSEMU,$(TSUGARU_VERSION))
 TSUGARU_DEPENDENCIES = libglu
 TSUGARU_LICENSE = GPLv2
 TSUGARU_SUPPORTS_IN_SOURCE_BUILD = NO
-# CMakeLists.txt in src subfolder
 TSUGARU_SUBDIR = src
 
-TSUGARU_CONF_OPTS = -DBUILD_SHARED_LIBS=FALSE
+TSUGARU_CONF_OPTS += -DBUILD_SHARED_LIBS=FALSE
 TSUGARU_CONF_ENV += LDFLAGS=-lpthread
 
 define TSUGARU_INSTALL_TARGET_CMDS
-		mkdir -p $(TARGET_DIR)/usr/bin
-		$(INSTALL) -D $(@D)/src/buildroot-build/main_cui/Tsugaru_CUI \
-				$(TARGET_DIR)/usr/bin/
+	mkdir -p $(TARGET_DIR)/usr/bin
+	$(INSTALL) -D $(@D)/src/buildroot-build/main_cui/Tsugaru_CUI $(TARGET_DIR)/usr/bin/
 
 	mkdir -p $(TARGET_DIR)/usr/share/evmapy
 	cp $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/emulators/tsugaru/fmtowns.keys $(TARGET_DIR)/usr/share/evmapy
