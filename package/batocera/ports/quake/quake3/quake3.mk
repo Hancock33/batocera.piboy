47aa6928dfbbe9546b8599f731e4a0389a118c05
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2024-12-15 11:42:21 +0000

    tidyup quake3

--- package/batocera/ports/quake/quake3/quake3.mk
+++ package/batocera/ports/quake/quake3/quake3.mk
@@ -18,33 +18,39 @@ QUAKE3_BUILD_ARGS += BUILD_MISSIONPACK=1
 QUAKE3_BUILD_ARGS += BUILD_GAME_SO=0
 QUAKE3_BUILD_ARGS += BUILD_GAME_QVM=0
 QUAKE3_BUILD_ARGS += CROSS_COMPILING=1
-QUAKE3_BUILD_ARGS += USE_RENDERER_DLOPEN=0
+QUAKE3_BUILD_ARGS += USE_RENDERER_DLOPEN=1
 QUAKE3_BUILD_ARGS += OPTIMIZE="$(TARGET_OPTIMIZATION)"
 
-ifeq ($(BR2_PACKAGE_GL4ES),y)
-QUAKE3_DEPENDENCIES += gl4es
-endif
-
 ifeq ($(BR2_aarch64),y)
-QUAKE3_BUILD_ARGS += COMPILE_ARCH=arm64
+    IOQUAKE3_BUILD_ARGS += COMPILE_ARCH=arm64
+    IOQUAKE3_ARCH = arm64
 else ifeq ($(BR2_arm),y)
-QUAKE3_BUILD_ARGS += COMPILE_ARCH=armv7l
+    IOQUAKE3_BUILD_ARGS += COMPILE_ARCH=armv7l
+    IOQUAKE3_ARCH = armv7l
 else ifeq ($(BR2_x86_64),y)
-QUAKE3_BUILD_ARGS += COMPILE_ARCH=x86_64
+    IOQUAKE3_BUILD_ARGS += COMPILE_ARCH=x86_64
+    IOQUAKE3_ARCH = x86_64
+else ifeq ($(BR2_RISCV_64),y)
+    IOQUAKE3_BUILD_ARGS += COMPILE_ARCH=riscv64
+    IOQUAKE3_ARCH = riscv64
 endif
 
 define QUAKE3_BUILD_CMDS
-	$(TARGET_CONFIGURE_OPTS) $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D)/ -f Makefile \
-	DEFAULT_BASEDIR=/userdata/roms/ports/quake3 \
-	$(QUAKE3_BUILD_ARGS) -C $(@D) -f Makefile
+	$(TARGET_CONFIGURE_OPTS) $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" DEFAULT_BASEDIR=/userdata/roms/ports/quake3 $(IOQUAKE3_BUILD_ARGS) SYSROOT=$(STAGING_DIR) -C $(@D) -f Makefile
 endef
 
 define QUAKE3_INSTALL_TARGET_CMDS
-	mkdir -p $(TARGET_DIR)/usr/share/quake3
-	cp -pvr $(@D)/build/release-linux-*/ioquake3_opengl2.* $(TARGET_DIR)/usr/bin/ioquake3
-	# evmap config
+	mkdir -p $(TARGET_DIR)/usr/bin/quake3
+	$(INSTALL) -D $(@D)/build/release-linux-$(IOQUAKE3_ARCH)/ioquake3.$(IOQUAKE3_ARCH) $(TARGET_DIR)/usr/bin/quake3/ioquake3
+	$(INSTALL) -D $(@D)/build/release-linux-$(IOQUAKE3_ARCH)/renderer_opengl1_$(IOQUAKE3_ARCH).so $(TARGET_DIR)/usr/bin/quake3/
+	$(INSTALL) -D $(@D)/build/release-linux-$(IOQUAKE3_ARCH)/renderer_opengl2_$(IOQUAKE3_ARCH).so $(TARGET_DIR)/usr/bin/quake3/
+endef
+
+define QUAKE3_EVMAPY
 	mkdir -p $(TARGET_DIR)/usr/share/evmapy
 	cp $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/ports/quake/quake3/quake3.keys $(TARGET_DIR)/usr/share/evmapy
 endef
+
+IOQUAKE3_POST_INSTALL_TARGET_HOOKS += QUAKE3_EVMAPY
 
 $(eval $(generic-package))
