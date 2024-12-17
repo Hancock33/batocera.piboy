bryanforbes/batocera.linux

refactor configgen invocation of batocera-vulkan into module

package/batocera/core/batocera-configgen/configgen/configgen/generators/shadps4/shadps4Generator.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 
 import logging
 import os
-import subprocess
 import sys
 from pathlib import Path
 from typing import TYPE_CHECKING
@@ -12,6 +11,7 @@ import toml
 from ... import Command
 from ...batoceraPaths import CONFIGS
 from ...controller import generate_sdl_game_controller_config
+from ...utils import vulkan
 from ..Generator import Generator
 
 if TYPE_CHECKING:
@@ -40,40 +40,26 @@ class shadPS4Generator(Generator):
 
         # Check Vulkan first before doing anything
         discrete_index = 0
-        try:
-            have_vulkan = subprocess.check_output(["/usr/bin/batocera-vulkan", "hasVulkan"], text=True).strip()
-            if have_vulkan == "true":
-                eslog.debug("Vulkan driver is available on the system.")
-                try:
-                    vulkan_version = subprocess.check_output(["/usr/bin/batocera-vulkan", "vulkanVersion"], text=True).strip()
-                    if vulkan_version > "1.3":
-                        eslog.debug(f"Using Vulkan version: {vulkan_version}")
-                        try:
-                            have_discrete = subprocess.check_output(["/usr/bin/batocera-vulkan", "hasDiscrete"], text=True).strip()
-                            if have_discrete == "true":
-                                eslog.debug("A discrete GPU is available on the system. We will use that for performance")
-                                try:
-                                    discrete_index = subprocess.check_output(["/usr/bin/batocera-vulkan", "discreteIndex"], text=True).strip()
-                                    if discrete_index != "":
-                                        eslog.debug(f"Using Discrete GPU Index: {discrete_index} for shadPS4")
-                                    else:
-                                        eslog.debug("Couldn't get discrete GPU index")
-                                except subprocess.CalledProcessError:
-                                    eslog.debug("Error getting discrete GPU index")
-                            else:
-                                eslog.debug("Discrete GPU is not available on the system. Using default.")
-                                discrete_index = 0
-                        except subprocess.CalledProcessError:
-                            eslog.debug("Error checking for discrete GPU.")
+        if vulkan.is_available():
+            eslog.debug("Vulkan driver is available on the system.")
+            vulkan_version = vulkan.get_verson()
+            if vulkan_version > "1.3":
+                eslog.debug(f"Using Vulkan version: {vulkan_version}")
+                if vulkan.has_discrete_gpu():
+                    eslog.debug("A discrete GPU is available on the system. We will use that for performance")
+                    discrete_index = vulkan.get_discrete_gpu_index()
+                    if discrete_index:
+                        eslog.debug(f"Using Discrete GPU Index: {discrete_index} for shadPS4")
                     else:
-                        eslog.debug(f"Vulkan version: {vulkan_version} is not compatible with shadPS4")
-                except subprocess.CalledProcessError:
-                    eslog.debug("Error checking for Vulkan version.")
+                        eslog.debug("Couldn't get discrete GPU index")
+                        discrete_index = 0
+                else:
+                    eslog.debug("Discrete GPU is not available on the system. Using default.")
             else:
-                eslog.debug("*** Vulkan driver required is not available on the system!!! ***")
-                sys.exit(1)
-        except subprocess.CalledProcessError:
-            eslog.debug("Error executing batocera-vulkan script.")
+                eslog.debug(f"Vulkan version: {vulkan_version} is not compatible with shadPS4")
+        else:
+            eslog.debug("*** Vulkan driver required is not available on the system!!! ***")
+            sys.exit(1)
 
         # Adjust the config.toml file
         config = {}

package/batocera/core/batocera-configgen/configgen/configgen/generators/xenia/xeniaGenerator.py
@@ -15,6 +15,7 @@ import toml
 from ... import Command
 from ...batoceraPaths import CACHE, CONFIGS, HOME, SAVES, mkdir_if_not_exists
 from ...controller import generate_sdl_game_controller_config
+from ...utils import vulkan
 from ..Generator import Generator
 
 if TYPE_CHECKING:
@@ -56,28 +57,21 @@ class XeniaGenerator(Generator):
         core = system.config['core']
 
         # check Vulkan first before doing anything
-        try:
-            have_vulkan = subprocess.check_output(["/usr/bin/batocera-vulkan", "hasVulkan"], text=True).strip()
-            if have_vulkan == "true":
-                eslog.debug("Vulkan driver is available on the system.")
-                try:
-                    vulkan_version = subprocess.check_output(["/usr/bin/batocera-vulkan", "vulkanVersion"], text=True).strip()
-                    if vulkan_version > "1.3":
-                        eslog.debug("Using Vulkan version: {}".format(vulkan_version))
-                    else:
-                        if system.isOptSet('xenia_api') and system.config['xenia_api'] == "D3D12":
-                            eslog.debug("Vulkan version: {} is not compatible with Xenia when using D3D12".format(vulkan_version))
-                            eslog.debug("You may have performance & graphical errors, switching to native Vulkan".format(vulkan_version))
-                            system.config['xenia_api'] = "Vulkan"
-                        else:
-                            eslog.debug("Vulkan version: {} is not recommended with Xenia".format(vulkan_version))
-                except subprocess.CalledProcessError:
-                    eslog.debug("Error checking for Vulkan version.")
+        if vulkan.is_available():
+            eslog.debug("Vulkan driver is available on the system.")
+            vulkan_version = vulkan.get_verson()
+            if vulkan_version > "1.3":
+                eslog.debug("Using Vulkan version: {}".format(vulkan_version))
             else:
-                eslog.debug("*** Vulkan driver required is not available on the system!!! ***")
-                sys.exit()
-        except subprocess.CalledProcessError:
-            eslog.debug("Error executing batocera-vulkan script.")
+                if system.isOptSet('xenia_api') and system.config['xenia_api'] == "D3D12":
+                    eslog.debug("Vulkan version: {} is not compatible with Xenia when using D3D12".format(vulkan_version))
+                    eslog.debug("You may have performance & graphical errors, switching to native Vulkan".format(vulkan_version))
+                    system.config['xenia_api'] = "Vulkan"
+                else:
+                    eslog.debug("Vulkan version: {} is not recommended with Xenia".format(vulkan_version))
+        else:
+            eslog.debug("*** Vulkan driver required is not available on the system!!! ***")
+            sys.exit()
 
         # set to 64bit environment by default
         os.environ['WINEARCH'] = 'win64'
@@ -111,7 +105,7 @@ class XeniaGenerator(Generator):
         if not (canarypath / 'portable.txt').exists():
             with (canarypath / 'portable.txt').open('w'):
                 pass
-        
+
         vcrun2019_done = wineprefix / "vcrun2019.done"
         if not vcrun2019_done.exists():
             cmd = ["/usr/wine/winetricks", "-q", "vcrun2019"]
@@ -126,9 +120,9 @@ class XeniaGenerator(Generator):
             eslog.error(err.decode())
             with vcrun2019_done.open("w") as f:
                 f.write("done")
-        
+
         dll_files = ["d3d12.dll", "d3d12core.dll", "d3d11.dll", "d3d10core.dll", "d3d9.dll", "d3d8.dll", "dxgi.dll"]
-        # Create symbolic links for 64-bit DLLs  
+        # Create symbolic links for 64-bit DLLs
         try:
             for dll in dll_files:
                 src_path = Path("/usr/wine/dxvk/x64") / dll
@@ -151,7 +145,7 @@ class XeniaGenerator(Generator):
                 dest_path.symlink_to(src_path)
         except Exception as e:
             eslog.debug(f"Error creating 32-bit link for {dll}: {e}")
-        
+
         # are we loading a digital title?
         if rom_path.suffix == '.xbox360':
             eslog.debug(f'Found .xbox360 playlist: {rom}')
@@ -283,7 +277,7 @@ class XeniaGenerator(Generator):
             config['Storage']['mount_cache'] = False
         else:
             config['Storage']['mount_cache'] = True
-        
+
         # add node UI
         if 'UI' not in config:
             config['UI'] = {}
@@ -314,7 +308,7 @@ class XeniaGenerator(Generator):
             config['XConfig'] = {'user_language': int(system.config['xenia_language'])}
         else:
             config['XConfig'] = {'user_language': 1}
-        
+
         # now write the updated toml
         with toml_file.open('w') as f:
             toml.dump(config, f)
@@ -369,7 +363,7 @@ class XeniaGenerator(Generator):
                 'VKD3D_SHADER_CACHE_PATH': xeniaCache,
                 'WINEDLLOVERRIDES': "winemenubuilder.exe=;dxgi,d3d8,d3d9,d3d10core,d3d11,d3d12,d3d12core=n",
             }
-        
+
         # ensure nvidia driver used for vulkan
         if Path('/var/tmp/nvidia.prime').exists():
             variables_to_remove = ['__NV_PRIME_RENDER_OFFLOAD', '__VK_LAYER_NV_optimus', '__GLX_VENDOR_LIBRARY_NAME']

package/batocera/core/batocera-configgen/configgen/configgen/utils/vulkan.py
@@ -1,0 +1,64 @@
+from __future__ import annotations
+
+import logging
+import subprocess
+from pathlib import Path
+from typing import Final
+
+_logger: Final = logging.getLogger(__name__)
+
+_BATOCERA_VULKAN: Final = Path('/usr/bin/batocera-vulkan')
+
+
+def is_available() -> bool:
+    try:
+        return subprocess.check_output([_BATOCERA_VULKAN, 'hasVulkan'], text=True).strip() == 'true'
+    except subprocess.CalledProcessError:
+        _logger.exception('Error checking for Vulkan driver')
+
+    return False
+
+
+def has_discrete_gpu() -> bool:
+    try:
+        return subprocess.check_output([_BATOCERA_VULKAN, 'hasDiscrete'], text=True).strip() == 'true'
+    except subprocess.CalledProcessError:
+        _logger.exception('Error checking for discrete GPU.')
+
+    return False
+
+
+def get_discrete_gpu_index() -> str | None:
+    try:
+        return subprocess.check_output([_BATOCERA_VULKAN, 'discreteIndex'], text=True).strip() or None
+    except subprocess.CalledProcessError:
+        _logger.exception('Error getting discrete GPU index')
+
+    return None
+
+
+def get_discrete_gpu_name() -> str | None:
+    try:
+        return subprocess.check_output([_BATOCERA_VULKAN, 'discreteName'], text=True).strip() or None
+    except subprocess.CalledProcessError:
+        _logger.exception('Error getting discrete GPU Name')
+
+    return None
+
+
+def get_discrete_gpu_uuid() -> str | None:
+    try:
+        return subprocess.check_output([_BATOCERA_VULKAN, 'discreteUUID'], text=True).strip() or None
+    except subprocess.CalledProcessError:
+        _logger.exception('Error getting discrete GPU UUID')
+
+    return None
+
+
+def get_verson() -> str:
+    try:
+        return subprocess.check_output([_BATOCERA_VULKAN, 'vulkanVersion'], text=True).strip()
+    except subprocess.CalledProcessError:
+        _logger.exception('Error checking for Vulkan version.')
+
+    return ''

Hancock33/batocera.piboy

package/batocera/core/batocera-configgen/configgen/configgen/generators/shadps4/shadps4Generator.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 
 import logging
 import os
-import subprocess
 import sys
 from pathlib import Path
 from typing import TYPE_CHECKING
@@ -12,6 +11,7 @@ import toml
 from ... import Command
 from ...batoceraPaths import CONFIGS
 from ...controller import generate_sdl_game_controller_config
+from ...utils import vulkan
 from ..Generator import Generator
 
 if TYPE_CHECKING:
@@ -40,40 +40,26 @@ class shadPS4Generator(Generator):
 
         # Check Vulkan first before doing anything
         discrete_index = 0
-        try:
-            have_vulkan = subprocess.check_output(["/usr/bin/batocera-vulkan", "hasVulkan"], text=True).strip()
-            if have_vulkan == "true":
-                eslog.debug("Vulkan driver is available on the system.")
-                try:
-                    vulkan_version = subprocess.check_output(["/usr/bin/batocera-vulkan", "vulkanVersion"], text=True).strip()
-                    if vulkan_version > "1.3":
-                        eslog.debug(f"Using Vulkan version: {vulkan_version}")
-                        try:
-                            have_discrete = subprocess.check_output(["/usr/bin/batocera-vulkan", "hasDiscrete"], text=True).strip()
-                            if have_discrete == "true":
-                                eslog.debug("A discrete GPU is available on the system. We will use that for performance")
-                                try:
-                                    discrete_index = subprocess.check_output(["/usr/bin/batocera-vulkan", "discreteIndex"], text=True).strip()
-                                    if discrete_index != "":
-                                        eslog.debug(f"Using Discrete GPU Index: {discrete_index} for shadPS4")
-                                    else:
-                                        eslog.debug("Couldn't get discrete GPU index")
-                                except subprocess.CalledProcessError:
-                                    eslog.debug("Error getting discrete GPU index")
-                            else:
-                                eslog.debug("Discrete GPU is not available on the system. Using default.")
-                                discrete_index = 0
-                        except subprocess.CalledProcessError:
-                            eslog.debug("Error checking for discrete GPU.")
+        if vulkan.is_available():
+            eslog.debug("Vulkan driver is available on the system.")
+            vulkan_version = vulkan.get_version()
+            if vulkan_version > "1.3":
+                eslog.debug(f"Using Vulkan version: {vulkan_version}")
+                if vulkan.has_discrete_gpu():
+                    eslog.debug("A discrete GPU is available on the system. We will use that for performance")
+                    discrete_index = vulkan.get_discrete_gpu_index()
+                    if discrete_index:
+                        eslog.debug(f"Using Discrete GPU Index: {discrete_index} for shadPS4")
                     else:
-                        eslog.debug(f"Vulkan version: {vulkan_version} is not compatible with shadPS4")
-                except subprocess.CalledProcessError:
-                    eslog.debug("Error checking for Vulkan version.")
+                        eslog.debug("Couldn't get discrete GPU index")
+                        discrete_index = 0
+                else:
+                    eslog.debug("Discrete GPU is not available on the system. Using default.")
             else:
-                eslog.debug("*** Vulkan driver required is not available on the system!!! ***")
-                sys.exit(1)
-        except subprocess.CalledProcessError:
-            eslog.debug("Error executing batocera-vulkan script.")
+                eslog.debug(f"Vulkan version: {vulkan_version} is not compatible with shadPS4")
+        else:
+            eslog.debug("*** Vulkan driver required is not available on the system!!! ***")
+            sys.exit(1)
 
         # Adjust the config.toml file
         config = {}

package/batocera/core/batocera-configgen/configgen/configgen/generators/xenia/xeniaGenerator.py
@@ -15,6 +15,7 @@ import toml
 from ... import Command
 from ...batoceraPaths import CACHE, CONFIGS, HOME, SAVES, mkdir_if_not_exists
 from ...controller import generate_sdl_game_controller_config
+from ...utils import vulkan
 from ..Generator import Generator
 
 if TYPE_CHECKING:
@@ -56,28 +57,21 @@ class XeniaGenerator(Generator):
         core = system.config['core']
 
         # check Vulkan first before doing anything
-        try:
-            have_vulkan = subprocess.check_output(["/usr/bin/batocera-vulkan", "hasVulkan"], text=True).strip()
-            if have_vulkan == "true":
-                eslog.debug("Vulkan driver is available on the system.")
-                try:
-                    vulkan_version = subprocess.check_output(["/usr/bin/batocera-vulkan", "vulkanVersion"], text=True).strip()
-                    if vulkan_version > "1.3":
-                        eslog.debug("Using Vulkan version: {}".format(vulkan_version))
-                    else:
-                        if system.isOptSet('xenia_api') and system.config['xenia_api'] == "D3D12":
-                            eslog.debug("Vulkan version: {} is not compatible with Xenia when using D3D12".format(vulkan_version))
-                            eslog.debug("You may have performance & graphical errors, switching to native Vulkan".format(vulkan_version))
-                            system.config['xenia_api'] = "Vulkan"
-                        else:
-                            eslog.debug("Vulkan version: {} is not recommended with Xenia".format(vulkan_version))
-                except subprocess.CalledProcessError:
-                    eslog.debug("Error checking for Vulkan version.")
+        if vulkan.is_available():
+            eslog.debug("Vulkan driver is available on the system.")
+            vulkan_version = vulkan.get_version()
+            if vulkan_version > "1.3":
+                eslog.debug("Using Vulkan version: {}".format(vulkan_version))
             else:
-                eslog.debug("*** Vulkan driver required is not available on the system!!! ***")
-                sys.exit()
-        except subprocess.CalledProcessError:
-            eslog.debug("Error executing batocera-vulkan script.")
+                if system.isOptSet('xenia_api') and system.config['xenia_api'] == "D3D12":
+                    eslog.debug("Vulkan version: {} is not compatible with Xenia when using D3D12".format(vulkan_version))
+                    eslog.debug("You may have performance & graphical errors, switching to native Vulkan".format(vulkan_version))
+                    system.config['xenia_api'] = "Vulkan"
+                else:
+                    eslog.debug("Vulkan version: {} is not recommended with Xenia".format(vulkan_version))
+        else:
+            eslog.debug("*** Vulkan driver required is not available on the system!!! ***")
+            sys.exit()
 
         # set to 64bit environment by default
         os.environ['WINEARCH'] = 'win64'
@@ -111,7 +105,7 @@ class XeniaGenerator(Generator):
         if not (canarypath / 'portable.txt').exists():
             with (canarypath / 'portable.txt').open('w'):
                 pass
-        
+
         vcrun2019_done = wineprefix / "vcrun2019.done"
         if not vcrun2019_done.exists():
             cmd = ["/usr/wine/winetricks", "-q", "vcrun2019"]
@@ -126,9 +120,9 @@ class XeniaGenerator(Generator):
             eslog.error(err.decode())
             with vcrun2019_done.open("w") as f:
                 f.write("done")
-        
+
         dll_files = ["d3d12.dll", "d3d12core.dll", "d3d11.dll", "d3d10core.dll", "d3d9.dll", "d3d8.dll", "dxgi.dll"]
-        # Create symbolic links for 64-bit DLLs  
+        # Create symbolic links for 64-bit DLLs
         try:
             for dll in dll_files:
                 src_path = Path("/usr/wine/dxvk/x64") / dll
@@ -151,7 +145,7 @@ class XeniaGenerator(Generator):
                 dest_path.symlink_to(src_path)
         except Exception as e:
             eslog.debug(f"Error creating 32-bit link for {dll}: {e}")
-        
+
         # are we loading a digital title?
         if rom_path.suffix == '.xbox360':
             eslog.debug(f'Found .xbox360 playlist: {rom}')
@@ -283,7 +277,7 @@ class XeniaGenerator(Generator):
             config['Storage']['mount_cache'] = False
         else:
             config['Storage']['mount_cache'] = True
-        
+
         # add node UI
         if 'UI' not in config:
             config['UI'] = {}
@@ -314,7 +308,7 @@ class XeniaGenerator(Generator):
             config['XConfig'] = {'user_language': int(system.config['xenia_language'])}
         else:
             config['XConfig'] = {'user_language': 1}
-        
+
         # now write the updated toml
         with toml_file.open('w') as f:
             toml.dump(config, f)
@@ -369,7 +363,7 @@ class XeniaGenerator(Generator):
                 'VKD3D_SHADER_CACHE_PATH': xeniaCache,
                 'WINEDLLOVERRIDES': "winemenubuilder.exe=;dxgi,d3d8,d3d9,d3d10core,d3d11,d3d12,d3d12core=n",
             }
-        
+
         # ensure nvidia driver used for vulkan
         if Path('/var/tmp/nvidia.prime').exists():
             variables_to_remove = ['__NV_PRIME_RENDER_OFFLOAD', '__VK_LAYER_NV_optimus', '__GLX_VENDOR_LIBRARY_NAME']

package/batocera/core/batocera-configgen/configgen/configgen/utils/vulkan.py
@@ -1,0 +1,64 @@
+from __future__ import annotations
+
+import logging
+import subprocess
+from pathlib import Path
+from typing import Final
+
+_logger: Final = logging.getLogger(__name__)
+
+_BATOCERA_VULKAN: Final = Path('/usr/bin/batocera-vulkan')
+
+
+def is_available() -> bool:
+    try:
+        return subprocess.check_output([_BATOCERA_VULKAN, 'hasVulkan'], text=True).strip() == 'true'
+    except subprocess.CalledProcessError:
+        _logger.exception('Error checking for Vulkan driver')
+
+    return False
+
+
+def has_discrete_gpu() -> bool:
+    try:
+        return subprocess.check_output([_BATOCERA_VULKAN, 'hasDiscrete'], text=True).strip() == 'true'
+    except subprocess.CalledProcessError:
+        _logger.exception('Error checking for discrete GPU.')
+
+    return False
+
+
+def get_discrete_gpu_index() -> str | None:
+    try:
+        return subprocess.check_output([_BATOCERA_VULKAN, 'discreteIndex'], text=True).strip() or None
+    except subprocess.CalledProcessError:
+        _logger.exception('Error getting discrete GPU index')
+
+    return None
+
+
+def get_discrete_gpu_name() -> str | None:
+    try:
+        return subprocess.check_output([_BATOCERA_VULKAN, 'discreteName'], text=True).strip() or None
+    except subprocess.CalledProcessError:
+        _logger.exception('Error getting discrete GPU Name')
+
+    return None
+
+
+def get_discrete_gpu_uuid() -> str | None:
+    try:
+        return subprocess.check_output([_BATOCERA_VULKAN, 'discreteUUID'], text=True).strip() or None
+    except subprocess.CalledProcessError:
+        _logger.exception('Error getting discrete GPU UUID')
+
+    return None
+
+
+def get_version() -> str:
+    try:
+        return subprocess.check_output([_BATOCERA_VULKAN, 'vulkanVersion'], text=True).strip()
+    except subprocess.CalledProcessError:
+        _logger.exception('Error checking for Vulkan version.')
+
+    return ''