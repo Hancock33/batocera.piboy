7bce6901a63e0d9b77685e01caa2062d0523680a
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2024-12-31 11:45:13 +0000

    Update retroarch-assets.mk
    Delete menu_widgets directory
    These files can go now, they are no longer used since 2021

--- package/batocera/emulators/retroarch/retroarch-assets/retroarch-assets.mk
+++ package/batocera/emulators/retroarch/retroarch-assets/retroarch-assets.mk
@@ -10,7 +10,6 @@ RETROARCH_ASSETS_LICENSE = GPL
 
 define RETROARCH_ASSETS_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/usr/share/libretro/assets/xmb
-	cp -r $(@D)/menu_widgets $(TARGET_DIR)/usr/share/libretro/assets
 	cp -r $(@D)/ozone $(TARGET_DIR)/usr/share/libretro/assets
 	cp -r $(@D)/rgui $(TARGET_DIR)/usr/share/libretro/assets
 	cp -r $(@D)/glui $(TARGET_DIR)/usr/share/libretro/assets
