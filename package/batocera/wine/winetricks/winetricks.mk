95f1491e3781cee435f52a2e9100a09b6e2b0226
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-01-01 22:20:31 +0000

    revert winetricks

--- package/batocera/wine/winetricks/winetricks.mk
+++ package/batocera/wine/winetricks/winetricks.mk
@@ -3,8 +3,8 @@
 # winetricks
 #
 ################################################################################
-# Version: Commits on Dec 26, 2024
-WINETRICKS_VERSION = 98078f5d2e5901d2524e6b16930451c42aff8c18
+# Version: Commits on Dec 19, 2024
+WINETRICKS_VERSION = cede5047101299802a9756f0885e24a5b6f2fbb5
 WINETRICKS_SITE = $(call github,Winetricks,winetricks,$(WINETRICKS_VERSION))
 
 define WINETRICKS_INSTALL_TARGET_CMDS
