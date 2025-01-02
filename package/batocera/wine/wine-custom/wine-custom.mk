8398443528c6d9c07a830a5cca71e948e11dd702
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-01-02 19:26:34 +0000

    bump wine

--- package/batocera/wine/wine-custom/wine-custom.mk
+++ package/batocera/wine/wine-custom/wine-custom.mk
@@ -3,8 +3,8 @@
 # wine-custom
 #
 ################################################################################
-# Version: Commits on Dec 30, 2024
-WINE_CUSTOM_VERSION = ntsync7-10.0rc3.r9
+# Version: Commits on Jan 01, 2024
+WINE_CUSTOM_VERSION = 10.0rc3.r14
 WINE_CUSTOM_SOURCE = wine-$(WINE_CUSTOM_VERSION).tar.gz
 WINE_CUSTOM_SITE = $(call github,Hancock33,wine-tkg-batocera,$(WINE_CUSTOM_VERSION))
 WINE_CUSTOM_LICENSE = LGPL-2.1+
