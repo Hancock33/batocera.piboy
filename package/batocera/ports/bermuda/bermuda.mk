758e03bb98d172f358626a88515fbddfaf766758
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-01-18 10:17:22 +0000

    bermuda makefile tweak

--- package/batocera/ports/bermuda/bermuda.mk
+++ package/batocera/ports/bermuda/bermuda.mk
@@ -10,14 +10,6 @@ BERMUDA_SITE = $(call github,cyxx,bermuda,$(BERMUDA_VERSION))
 BERMUDA_DEPENDENCIES = sdl2 sdl2_mixer
 BERMUDA_LICENSE = GPLv2
 
-define BERMUDA_BUILD_CMDS
-	$(SED) "s|-O |$(TARGET_OPTIMIZATION) |g" $(@D)/Makefile
-	$(TARGET_CONFIGURE_OPTS) $(MAKE) \
-	CPP="$(TARGET_CPP)" CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" \
-	AS="$(TARGET_CC)" LD="$(TARGET_LD)" STRIP="$(TARGET_STRIP)" \
-	-C $(@D)
-endef
-
 define BERMUDA_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/bs -D $(TARGET_DIR)/usr/bin/bermuda
 
@@ -26,4 +18,4 @@ define BERMUDA_INSTALL_TARGET_CMDS
 	cp $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/ports/bermuda/bermuda.keys $(TARGET_DIR)/usr/share/evmapy/bermuda.keys
 endef
 
-$(eval $(generic-package))
+$(eval $(cmake-package))
