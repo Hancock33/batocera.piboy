6eb6d01be25e1f4651e1f61c08149e6f327f1af3
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-01-18 09:51:56 +0000

    switch awgl fork

--- package/batocera/ports/awgl/awgl.mk
+++ package/batocera/ports/awgl/awgl.mk
@@ -3,20 +3,12 @@
 # awgl
 #
 ################################################################################
-# Version: Commits on Mar 12, 2022
-AWGL_VERSION = 1eeaa530c859bd6e37b3d3bd7d3136bc16afcc89
-AWGL_SITE = $(call github,cyxx,rawgl,$(AWGL_VERSION))
+# Version: Commits on Jun 13, 2024
+AWGL_VERSION = df249583a87d5c2f73cb362f7163799e01cd0c4c
+AWGL_SITE = $(call github,M-HT,rawgl,$(AWGL_VERSION))
 
 AWGL_DEPENDENCIES = sdl2 sdl2_mixer freepats
 AWGL_LICENSE = GPLv2
-
-define AWGL_BUILD_CMDS
-	$(SED) "s|-O |$(TARGET_OPTIMIZATION) |g" $(@D)/Makefile
-	$(TARGET_CONFIGURE_OPTS) $(MAKE) \
-	CPP="$(TARGET_CPP)" CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" \
-	AS="$(TARGET_CC)" LD="$(TARGET_LD)" STRIP="$(TARGET_STRIP)" \
-	-C $(@D)
-endef
 
 define AWGL_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/rawgl -D $(TARGET_DIR)/usr/bin/awgl
@@ -26,4 +18,4 @@ define AWGL_INSTALL_TARGET_CMDS
 	cp $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/ports/awgl/awgl.keys $(TARGET_DIR)/usr/share/evmapy/awgl.keys
 endef
 
-$(eval $(generic-package))
+$(eval $(cmake-package))
