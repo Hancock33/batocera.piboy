67b5832791e8b7055cfe6e5c98da58c328738023
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-01-18 14:20:52 +0000

    remove freepats

--- package/batocera/ports/awgl/awgl.mk
+++ package/batocera/ports/awgl/awgl.mk
@@ -7,7 +7,7 @@
 AWGL_VERSION = df249583a87d5c2f73cb362f7163799e01cd0c4c
 AWGL_SITE = $(call github,M-HT,rawgl,$(AWGL_VERSION))
 
-AWGL_DEPENDENCIES = sdl2 sdl2_mixer freepats
+AWGL_DEPENDENCIES = sdl2 sdl2_mixer
 AWGL_LICENSE = GPLv2
 
 define AWGL_INSTALL_TARGET_CMDS
