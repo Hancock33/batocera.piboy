1f19d1fb7aad284461d8b173a7b00a33feb4ceba
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-01-18 10:06:08 +0000

    update f2bgl patch

--- package/batocera/ports/f2bgl/f2bgl.mk
+++ package/batocera/ports/f2bgl/f2bgl.mk
@@ -10,13 +10,6 @@ F2BGL_SITE = $(call github,cyxx,f2bgl,$(F2BGL_VERSION))
 F2BGL_DEPENDENCIES = sdl2 sdl2_mixer freepats
 F2BGL_LICENSE = GPLv2
 
-define F2BGL_BUILD_CMDS
-	$(TARGET_CONFIGURE_OPTS) $(MAKE) \
-	CPP="$(TARGET_CPP)" CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" \
-	AS="$(TARGET_CC)" LD="$(TARGET_LD)" STRIP="$(TARGET_STRIP)" \
-	-C $(@D)
-endef
-
 define F2BGL_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/f2bgl -D $(TARGET_DIR)/usr/bin/f2bgl
 
@@ -25,4 +18,4 @@ define F2BGL_INSTALL_TARGET_CMDS
 	cp $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/ports/f2bgl/f2bgl.keys $(TARGET_DIR)/usr/share/evmapy/f2bgl.keys
 endef
 
-$(eval $(generic-package))
+$(eval $(cmake-package))
