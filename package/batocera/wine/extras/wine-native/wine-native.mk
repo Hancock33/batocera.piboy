054f6a382d0fcbbd5c23dc072d23ccc05097d235
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-01-21 21:26:28 +0000

    bump wine extras

--- package/batocera/wine/extras/wine-native/wine-native.mk
+++ package/batocera/wine/extras/wine-native/wine-native.mk
@@ -3,8 +3,8 @@
 # wine-native
 #
 ################################################################################
-# Version: Commits on Jan 17, 2025
-WINE_NATIVE_VERSION = wine-10.0-rc6
+# Version: Commits on Jan 21, 2025
+WINE_NATIVE_VERSION = wine-10.0
 WINE_NATIVE_SOURCE = wine-$(WINE_NATIVE_VERSION).tar.gz
 WINE_NATIVE_SITE = $(call github,wine-mirror,wine,$(WINE_NATIVE_VERSION))
 WINE_NATIVE_LICENSE = LGPL-2.1+
