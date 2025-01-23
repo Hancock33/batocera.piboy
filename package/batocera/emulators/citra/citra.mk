f7808114a0b275c31161a0dd24059f248accf92f
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-01-23 19:34:51 +0000

    update citra patches

--- package/batocera/emulators/citra/citra.mk
+++ package/batocera/emulators/citra/citra.mk
@@ -3,8 +3,8 @@
 # citra
 #
 ################################################################################
-# Version: Commits on Jan 23, 2025
-CITRA_VERSION = 1ebf659b7489bb8d714590a2ced4da0044754972
+# Version: Commits on Dec 12, 2024
+CITRA_VERSION = v2024.12.12
 CITRA_SITE = https://github.com/Borked3DS/Borked3DS.git
 CITRA_SITE_METHOD=git
 CITRA_GIT_SUBMODULES=YES
