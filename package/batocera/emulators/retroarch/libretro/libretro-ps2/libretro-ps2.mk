371e559e014d2d36a4765b425e1e5bdce438a6af
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-01-25 09:36:34 +0000

    try to make libretro cmake consistant

--- package/batocera/emulators/retroarch/libretro/libretro-ps2/libretro-ps2.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-ps2/libretro-ps2.mk
@@ -8,14 +8,9 @@ LIBRETRO_PS2_VERSION = df5cc7cb8829c9f7297bb2f24b5fb49590ef1eee
 LIBRETRO_PS2_SITE = https://github.com/libretro/ps2.git
 LIBRETRO_PS2_SITE_METHOD = git
 LIBRETRO_PS2_GIT_SUBMODULES = YES
-LIBRETRO_PS2_LICENSE = GPLv2
-LIBRETRO_PS2_DEPENDENCIES = libaio xz host-xxd retroarch
 LIBRETRO_PS2_SUPPORTS_IN_SOURCE_BUILD = NO
-
-LIBRETRO_PS2_DEPENDENCIES = libaio xz host-xxd
-LIBRETRO_PS2_DEPENDENCIES += alsa-lib fmt freetype libaio libbacktrace libcurl libgtk3 libpcap libpng libsamplerate libsoundtouch
-LIBRETRO_PS2_DEPENDENCIES += portaudio sdl2 shaderc webp wxwidgets xorgproto yaml-cpp zlib
-LIBRETRO_PS2_DEPENDENCIES += host-clang host-libcurl
+LIBRETRO_PS2_LICENSE = GPLv2
+LIBRETRO_PS2_DEPENDENCIES = pcsx2 retroarch
 
 LIBRETRO_PS2_CONF_OPTS += -DBUILD_SHARED_LIBS=OFF
 LIBRETRO_PS2_CONF_OPTS += -DUSE_SYSTEM_LIBS=AUTO
