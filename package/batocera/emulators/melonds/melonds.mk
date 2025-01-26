1a9718876177057df2136fe65aae4b636ff94a85
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-01-26 10:21:11 +0000

    correct melonds deps

--- package/batocera/emulators/melonds/melonds.mk
+++ package/batocera/emulators/melonds/melonds.mk
@@ -8,7 +8,7 @@ MELONDS_VERSION = 15c3faa26e879bdcff615558ded6dd886681ccae
 MELONDS_SITE = $(call github,melonDS-emu,melonDS,$(MELONDS_VERSION))
 MELONDS_LICENSE = GPLv2
 MELONDS_SUPPORTS_IN_SOURCE_BUILD = NO
-MELONDS_DEPENDENCIES += ecm libarchive libenet libepoxy sdl2 slirp
+MELONDS_DEPENDENCIES += ecm enet libarchive  libepoxy sdl2 slirp
 MELONDS_DEPENDENCIES += qt6base qt6multimedia qt6svg
 
 MELONDS_CONF_OPTS += -DCMAKE_INSTALL_PREFIX="/usr"
