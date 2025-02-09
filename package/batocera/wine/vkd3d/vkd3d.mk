66bbad6eed0e34f71c297db7ce5429ec4a92d19b
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-02-09 00:45:27 +0000

    update config.in

--- package/batocera/wine/vkd3d/vkd3d.mk
+++ package/batocera/wine/vkd3d/vkd3d.mk
@@ -8,7 +8,7 @@ VKD3D_VERSION = vkd3d-1.13
 VKD3D_SITE = https://source.winehq.org/git/vkd3d.git
 VKD3D_LICENSE = LGPL-2.1+
 VKD3D_LICENSE_FILES = COPYING.LIB LICENSE
-VKD3D_DEPENDENCIES = host-bison host-flex host-wine-custom spirv-headers host-libtool vulkan-headers vulkan-loader spirv-tools
+VKD3D_DEPENDENCIES = host-bison host-flex host-libtool host-wine-custom spirv-headers spirv-tools vulkan-headers vulkan-loader
 VKD3D_CONF_ENV += WIDL="$(BUILD_DIR)/host-wine-custom-$(WINE_CUSTOM_VERSION)/tools/widl/widl"
 
 VKD3D_CONF_OPTS = --disable-tests --with-sysroot=$(STAGING_DIR)
