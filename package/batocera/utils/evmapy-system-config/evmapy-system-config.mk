8716d474640ae7c04a08f3d89453d6083e553ec8
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-02-11 07:21:31 +0000

    correct merge errors

--- package/batocera/utils/evmapy-system-config/evmapy-system-config.mk
+++ package/batocera/utils/evmapy-system-config/evmapy-system-config.mk
@@ -9,6 +9,7 @@ EVMAPY_SYSTEM_CONFIG_SOURCE =
 define EVMAPY_SYSTEM_CONFIG_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/usr/share/evmapy
 	cp $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/utils/evmapy-system-config/mouse.keys $(TARGET_DIR)/usr/share/evmapy/windows_installers.keys
+
 	cp -avr $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/utils/evmapy-system-config/keys/* $(TARGET_DIR)/usr/share/evmapy
 
 	@if [ "$(BR2_PACKAGE_XPI_GAMECON_RPI)" = "y" ]; then \
