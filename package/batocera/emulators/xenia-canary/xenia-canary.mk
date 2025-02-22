4060418f8255784e6a4df459dcec76d99791581e
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-02-22 09:20:00 +0000

    Update xenia-canary.mk

--- package/batocera/emulators/xenia-canary/xenia-canary.mk
+++ package/batocera/emulators/xenia-canary/xenia-canary.mk
@@ -5,8 +5,8 @@
 ################################################################################
 # Version: Commits on Feb 17, 2025
 XENIA_CANARY_VERSION = b3d345610adaf700f169034df976fa111660dd57
-XENIA_CANARY_SOURCE = xenia_canary.zip
-XENIA_CANARY_SITE = https://github.com/xenia-canary/xenia-canary/releases/download/"$(shell echo $(XENIA_CANARY_VERSION) | cut -c 1-7)"
+XENIA_CANARY_SOURCE = xenia_canary_windows.zip
+XENIA_CANARY_SITE = https://github.com/xenia-canary/xenia-canary-releases/releases/download/canary_experimental
 XENIA_CANARY_LICENSE = BSD
 XENIA_CANARY_LICENSE_FILE = LICENSE
 XENIA_CANARY_DEPENDENCIES = python-toml
