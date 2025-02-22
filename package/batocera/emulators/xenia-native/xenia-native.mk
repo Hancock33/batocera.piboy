baed2e066eb61998bc89b8eac9d55f043f89e097
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-02-22 13:27:56 +0000

    Update xenia-native.mk

--- package/batocera/emulators/xenia-native/xenia-native.mk
+++ package/batocera/emulators/xenia-native/xenia-native.mk
@@ -15,15 +15,15 @@ XENIA_NATIVE_SUBDIR = build
 
 XENIA_NATIVE_DEPENDENCIES = xserver_xorg-server alsa-lib fmt freetype libgtk3 libpng lz4 sdl2 zlib
 
-define XENIA_NATIVE_CMAKE_FILES
+define XENIA_NATIVE_BUILD_CMDS
 	sed -i "s|pkg-config --|PKG_CONFIG_PATH=$(STAGING_DIR)/usr/lib/pkgconfig pkg-config --|g" $(@D)/tools/build/scripts/pkg_config.lua
 	cd $(@D) && ./xb premake --devenv=cmake
 	sed -i "s|/usr/|$(STAGING_DIR)/usr/|g" $(@D)/build/*.cmake
+	cd $(@D) && $(TARGET_CONFIGURE_OPTS) CXX="$(HOST_DIR)/bin/clang++" CC="$(HOST_DIR)/bin/clang" ./xb build --config=Release
 endef
-XENIA_NATIVE_PRE_CONFIGURE_HOOKS +=  XENIA_NATIVE_CMAKE_FILES
 
 define XENIA_NATIVE_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 -D $(@D)/build/bin/Linux/Release/xenia_canary $(TARGET_DIR)/usr/bin/xenia-native
 endef
 
-$(eval $(cmake-package))
+$(eval $(generic-package))
