ce66386e8b85da29554663121663d34c437ffbdb
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-02-23 11:35:31 +0000

    fix vice

--- package/batocera/emulators/vice/vice.mk
+++ package/batocera/emulators/vice/vice.mk
@@ -8,24 +8,26 @@ VICE_VERSION = r45502
 VICE_SITE = $(call github,VICE-Team,svn-mirror,$(VICE_VERSION))
 VICE_LICENSE = GPLv2
 VICE_SUBDIR  = vice
-VICE_DEPENDENCIES =  alsa-lib giflib host-dos2unix host-xa jpeg libcurl libpng zlib
+VICE_DEPENDENCIES = alsa-lib giflib host-dos2unix host-xa jpeg libcurl sdl2 sdl2_image zlib
+VICE_CONF_ENV += LDFLAGS=-lSDL2
 
 VICE_CONF_OPTS += --disable-debug-gtk3ui
+VICE_CONF_OPTS += --disable-html-docs
 VICE_CONF_OPTS += --disable-option-checking
 VICE_CONF_OPTS += --disable-pdf-docs
-VICE_CONF_OPTS += --with-alsa
-VICE_CONF_OPTS += --with-png
-VICE_CONF_OPTS += --with-fastsid
-VICE_CONF_OPTS += --without-pulse
-VICE_CONF_OPTS += --enable-x64
 VICE_CONF_OPTS += --enable-arch=yes
 VICE_CONF_OPTS += --enable-midi
-
-ifeq ($(BR2_PACKAGE_SDL2),y)
 VICE_CONF_OPTS += --enable-sdl2ui
+VICE_CONF_OPTS += --enable-x64
+VICE_CONF_OPTS += --with-alsa
+VICE_CONF_OPTS += --with-fastsid
+VICE_CONF_OPTS += --with-gif
 VICE_CONF_OPTS += --with-sdlsound
-VICE_CONF_ENV += LDFLAGS=-lSDL2
-VICE_DEPENDENCIES += sdl2 sdl2_image
+VICE_CONF_OPTS += --without-pulse
+
+ifeq ($(BR2_PACKAGE_MPG123),y)
+VICE_CONF_OPTS += --with-mpg123
+VICE_DEPENDENCIES += mpg123
 endif
 
 ifeq ($(BR2_PACKAGE_FLAC),y)
@@ -41,6 +43,11 @@ endif
 ifeq ($(BR2_PACKAGE_LAME),y)
 VICE_CONF_OPTS += --with-lame
 VICE_DEPENDENCIES += lame
+endif
+
+ifeq ($(BR2_PACKAGE_LIBPNG),y)
+VICE_CONF_OPTS += --with-png
+VICE_DEPENDENCIES += libpng
 endif
 
 define VICE_SRC_AUTOCONFIGURE
