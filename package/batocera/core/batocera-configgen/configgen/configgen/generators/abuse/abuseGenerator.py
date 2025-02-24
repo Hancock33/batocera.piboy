99f8b09e26c4ef6f4022cf5516d2c635d8ac28d0
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-02-24 21:04:11 +0000

    switch abuse port

--- package/batocera/core/batocera-configgen/configgen/configgen/generators/abuse/abuseGenerator.py
+++ package/batocera/core/batocera-configgen/configgen/configgen/generators/abuse/abuseGenerator.py
@@ -13,12 +13,15 @@ if TYPE_CHECKING:
 class AbuseGenerator(Generator):
 
     def generate(self, system, rom, playersControllers, metadata, guns, wheels, gameResolution):
-        commandArray = ["abuse", "-datadir", "/usr/share/abuse"]
+        commandArray = ["abuse"]
 
         return Command.Command(
             array=commandArray,
             env={
-                'SDL_GAMECONTROLLERCONFIG': generate_sdl_game_controller_config(playersControllers)
+                'SDL_GAMECONTROLLERCONFIG': generate_sdl_game_controller_config(playersControllers),
+                'SDL_JOYSTICK_HIDAPI': "0",
+                'ABUSE_PATH': '/userdata/roms/ports/abuse',
+                'ABUSE_SAVE_PATH': '/userdata/saves/abuse'
             })
 
     def getHotkeysContext(self) -> HotkeysContext:
