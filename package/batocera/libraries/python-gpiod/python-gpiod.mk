dbe5fce74e2cf674d1333215485d5791f572815e
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-02-25 23:08:05 +0000

    update python libs

--- package/batocera/libraries/python-gpiod/python-gpiod.mk
+++ package/batocera/libraries/python-gpiod/python-gpiod.mk
@@ -3,10 +3,10 @@
 # python-gpiod
 #
 ################################################################################
-# Version: Commits on Dec 20, 2024
-PYTHON_GPIOD_VERSION = 2.2.3
+# Version: Commits on Feb 19, 2025
+PYTHON_GPIOD_VERSION = 2.3.0
 PYTHON_GPIOD_SOURCE = gpiod-$(PYTHON_GPIOD_VERSION).tar.gz
-PYTHON_GPIOD_SITE = https://files.pythonhosted.org/packages/17/3f/774043a7328048e506a80425d8c114f8faf445cfc958f7a59dedc0fcaf36
+PYTHON_GPIOD_SITE = https://files.pythonhosted.org/packages/8f/74/cb43c6e2fe74cf1567160ccbf54db176f72481e5ac58567684a262672c7c
 PYTHON_GPIOD_SETUP_TYPE = setuptools
 
 $(eval $(python-package))
