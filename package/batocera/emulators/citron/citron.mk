15277bbd778107375598632caf68c09f0cb44581
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-03-16 19:52:04 +0000

    migrate suyu > citron

---
+++ package/batocera/emulators/citron/citron.mk
@@ -0,0 +1,37 @@
+################################################################################
+#
+# suyu
+#
+################################################################################
+# Version: Commits on Mar 16, 2025
+CITRON_VERSION = 51800e249bc44bd13b528220a8e064c3744c05d1
+CITRON_SITE = https://git.citron-emu.org/Citron/Citron.git
+CITRON_SITE_METHOD=git
+CITRON_GIT_SUBMODULES=YES
+CITRON_LICENSE = GPLv2
+CITRON_DEPENDENCIES += fmt boost ffmpeg zstd zlib libzip lz4 catch2 sdl2 opus
+CITRON_DEPENDENCIES += qt6base qt6svg qt6tools vulkan-headers vulkan-loader vulkan-validationlayers
+CITRON_SUPPORTS_IN_SOURCE_BUILD = NO
+
+CITRON_CONF_ENV += LDFLAGS=-lpthread ARCHITECTURE_x86_64=1
+
+CITRON_CONF_OPTS += -DARCHITECTURE_x86_64=ON
+CITRON_CONF_OPTS += -DBUILD_SHARED_LIBS=OFF
+CITRON_CONF_OPTS += -DCITRON_TESTS=OFF
+CITRON_CONF_OPTS += -DCITRON_USE_EXTERNAL_SDL2=OFF
+CITRON_CONF_OPTS += -DENABLE_QT6=ON
+CITRON_CONF_OPTS += -DENABLE_SDL2=ON
+CITRON_CONF_OPTS += -DENABLE_WEB_SERVICE=OFF
+CITRON_CONF_OPTS += -DUSE_DISCORD_PRESENCE=OFF
+CITRON_CONF_OPTS += -DUSE_SANITIZERS=OFF
+
+ifeq ($(BR2_PACKAGE_BATOCERA_VULKAN),y)
+    CITRON_DEPENDENCIES += host-glslang vulkan-headers vulkan-loader
+endif
+
+define CITRON_INSTALL_TARGET_CMDS
+	mkdir -p $(TARGET_DIR)/usr/bin
+	cp $(@D)/buildroot-build/bin/{citron,tzdb2nx} $(TARGET_DIR)/usr/bin/
+endef
+
+$(eval $(cmake-package))
