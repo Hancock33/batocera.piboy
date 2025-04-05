efdebaa6efb39cb5d202410805b0d627d5067a17
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-04-05 08:14:16 +0100

    Update libretro-dice.mk

--- package/batocera/emulators/retroarch/libretro/libretro-dice/libretro-dice.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-dice/libretro-dice.mk
@@ -12,7 +12,7 @@ LIBRETRO_DICE_DEPENDENCIES += retroarch
 LIBRETRO_DICE_PLATFORM = $(LIBRETRO_PLATFORM)
 
 define LIBRETRO_DICE_BUILD_CMDS
-	$(SED) "s|\-O[23]|$(TARGET_OPTIMIZATION)|g" $(@D)/Makefile
+	$(SED) "s|\-O[23]|$(TARGET_OPTIMIZATION)|g" $(@D)/Makefile.libretro
 	$(TARGET_CONFIGURE_OPTS) $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C \
 		$(@D)/ -f Makefile platform="$(LIBRETRO_DICE_PLATFORM)" \
 		GIT_VERSION="-$(shell echo $(LIBRETRO_DICE_VERSION) | cut -c 1-7)"
