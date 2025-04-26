e55bc4981db39922e2b42179338c985c0ce9b325
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-04-26 13:44:23 +0100

    Update bigpemu.mk

--- package/batocera/emulators/bigpemu/bigpemu.mk
+++ package/batocera/emulators/bigpemu/bigpemu.mk
@@ -9,8 +9,9 @@ BIGPEMU_SOURCE = BigPEmu_Linux64_$(BIGPEMU_VERSION).tar.gz
 BIGPEMU_SITE = https://www.richwhitehouse.com/jaguar/builds
 
 define BIGPEMU_INSTALL_TARGET_CMDS
+	rm -rf $(TARGET_DIR)/usr/bin/bigpemu
 	mkdir -p $(TARGET_DIR)/usr/bin/bigpemu
-	cp -pr $(@D)/* $(TARGET_DIR)/usr/bin/bigpemu/
+	cp -av $(@D)/* $(TARGET_DIR)/usr/bin/bigpemu/
 endef
 
 $(eval $(generic-package))
