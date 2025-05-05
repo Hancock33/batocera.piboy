06fb4f3907a8a294767a81160cdac4585b4ee579
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-05-05 16:21:35 +0100

    Update l3afpad.mk

--- package/batocera/utils/l3afpad/l3afpad.mk
+++ package/batocera/utils/l3afpad/l3afpad.mk
@@ -10,6 +10,5 @@ L3AFPAD_SITE = $(call github,stevenhoneyman,l3afpad,$(L3AFPAD_VERSION))
 L3AFPAD_DEPENDENCIES = libgtk3 host-intltool harfbuzz
 L3AFPAD_LICENSE = GPL-2.0+
 L3AFPAD_LICENSE_FILES = COPYING
-L3AFPAD_AUTORECONF = YES
 
 $(eval $(autotools-package))
