1462d8ae70df86beee934e5ad4f8a6c0a418fa85
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-05-24 22:22:44 +0100

    fallout2-ce switch port

--- package/batocera/ports/fallout2-ce/fallout2-ce.mk
+++ package/batocera/ports/fallout2-ce/fallout2-ce.mk
@@ -3,9 +3,9 @@
 # fallout2-ce
 #
 ################################################################################
-# Version: Commits on Feb 16, 2025
-FALLOUT2_CE_VERSION = e97087b9582f37075db347a89898887320753f8b
-FALLOUT2_CE_SITE = $(call github,alexbatalov,fallout2-ce,$(FALLOUT2_CE_VERSION))
+# Version: Commits on May 24, 2025
+FALLOUT2_CE_VERSION = 547cabc42dacc1e89a258c2c0c5875f66582bb1b
+FALLOUT2_CE_SITE = $(call github,fallout2-ce,fallout2-ce,$(FALLOUT2_CE_VERSION))
 FALLOUT2_CE_DEPENDENCIES = sdl2
 
 FALLOUT2_CE_CONF_OPTS += -DFALLOUT_VENDORED=OFF
