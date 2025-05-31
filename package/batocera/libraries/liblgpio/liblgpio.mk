5b3505505b69660a2a9c5e4465e0af74cd5959da
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-05-31 22:28:16 +0100

    somemore tweaking

--- package/batocera/libraries/liblgpio/liblgpio.mk
+++ package/batocera/libraries/liblgpio/liblgpio.mk
@@ -10,16 +10,17 @@ LIBLGPIO_INSTALL_STAGING = YES
 
 define LIBLGPIO_BUILD_CMDS
 	$(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) \
+	CFLAGS="$(TARGET_CFLAGS) -std=gnu17" \
 	CROSS_PREFIX="$(TARGET_CROSS)" \
 	$(MAKE) CC="$(TARGET_CC)" STRIP="$(TARGET_STRIP)" -C $(@D) -f Makefile lib
 endef
 
 define LIBLGPIO_INSTALL_STAGING_CMDS
-	cp -av $(@D)/liblgpio* $(STAGING_DIR)/usr/lib/
+	cp -av $(@D)/lib*gpio.so* $(STAGING_DIR)/usr/lib/
 endef
 
 define LIBLGPIO_INSTALL_TARGET_CMDS
-	cp -av $(@D)/liblgpio* $(TARGET_DIR)/usr/lib/
+	cp -av $(@D)/lib*gpio.so* $(TARGET_DIR)/usr/lib/
 endef
 
 $(eval $(generic-package))
