f981a21f8a267bf2b57308bc6ea9906e3b62e142
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-06-08 09:10:37 +0100

    update adafruit libs

--- package/batocera/libraries/python-adafruit-platformdetect/python-adafruit-platformdetect.mk
+++ package/batocera/libraries/python-adafruit-platformdetect/python-adafruit-platformdetect.mk
@@ -3,10 +3,10 @@
 # python-adafruit-platformdetect
 #
 ################################################################################
-# Version: Commits on Jan 16, 2025
-PYTHON_ADAFRUIT_PLATFORMDETECT_VERSION = 3.77.0
+# Version: Commits on May 14, 2025
+PYTHON_ADAFRUIT_PLATFORMDETECT_VERSION = 3.79.0
 PYTHON_ADAFRUIT_PLATFORMDETECT_SOURCE = adafruit_platformdetect-$(PYTHON_ADAFRUIT_PLATFORMDETECT_VERSION).tar.gz
-PYTHON_ADAFRUIT_PLATFORMDETECT_SITE = https://files.pythonhosted.org/packages/6b/4e/2b2ca031227de47e2aab6cf092b78934c9c0033a685075ddab3e0c0b55fe
+PYTHON_ADAFRUIT_PLATFORMDETECT_SITE = https://files.pythonhosted.org/packages/dc/fe/74c3cdba24f06e8748819dbbc4912e3e2193f1a900c96b659e5794dee3a8
 PYTHON_ADAFRUIT_PLATFORMDETECT_SETUP_TYPE = setuptools
 PYTHON_ADAFRUIT_PLATFORMDETECT_LICENSE = MIT
 PYTHON_ADAFRUIT_PLATFORMDETECT_LICENSE_FILES = LICENSE
