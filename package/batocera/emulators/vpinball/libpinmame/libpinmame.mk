bfbad949687bd815e9e94e4eff7ce82eaa972cd0
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-06-20 23:10:23 +0100

    vpinball corrections

--- package/batocera/emulators/vpinball/libpinmame/libpinmame.mk
+++ package/batocera/emulators/vpinball/libpinmame/libpinmame.mk
@@ -11,17 +11,19 @@ LIBPINMAME_LICENSE_FILES = LICENSE
 LIBPINMAME_DEPENDENCIES = zlib
 LIBPINMAME_SUPPORTS_IN_SOURCE_BUILD = NO
 
+LIBPINMAME_CONF_OPTS += $(VPINBALL_COMMON_CONF_OPTS)
+
 define LIBPINMAME_RENAME_CMAKE
-	cp $(@D)/cmake/libpinmame/CMakeLists_linux-x64.txt $(@D)/CMakeLists.txt
+	cp $(@D)/cmake/libpinmame/CMakeLists.txt $(@D)/CMakeLists.txt
 	rm $(@D)/makefile
 endef
 
 define LIBPINMAME_INSTALL_TARGET_CMDS
 	# staging files
-	$(INSTALL) -D -m 0755 $(@D)/buildroot-build/libpinmame.so.3.6 $(STAGING_DIR)/usr/lib
+	$(INSTALL) -D -m 0755 $(@D)/buildroot-build/libpinmame.so.* $(STAGING_DIR)/usr/lib
 	cp $(@D)/src/libpinmame/libpinmame.h $(STAGING_DIR)/usr/include
 	# copy to target
-	$(INSTALL) -D -m 0755 $(@D)/buildroot-build/libpinmame.so.3.6 $(TARGET_DIR)/usr/lib
+	$(INSTALL) -D -m 0755 $(@D)/buildroot-build/libpinmame.so.* $(TARGET_DIR)/usr/lib
 endef
 
 LIBPINMAME_PRE_CONFIGURE_HOOKS += LIBPINMAME_RENAME_CMAKE
