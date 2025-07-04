f291049bbf1f538732e546871648a1bef632fefe
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-07-04 21:51:47 +0100

    Update evsieve.mk

--- package/batocera/utils/evsieve/evsieve.mk
+++ package/batocera/utils/evsieve/evsieve.mk
@@ -14,11 +14,10 @@ EVSIEVE_DEPENDENCIES = host-rustc libevdev
 EVSIEVE_ARGS_FOR_BUILD = -L $(STAGING_DIR) -Wl,-rpath,$(STAGING_DIR)
 EVSIEVE_CARGO_ENV = CARGO_HOME=$(HOST_DIR)/share/cargo RUSTFLAGS='$(addprefix -C linker=$(TARGET_CC) -C link-args=,$(EVSIEVE_ARGS_FOR_BUILD))'
 
-EVSIEVE_CARGO_MODE = $(if $(BR2_ENABLE_DEBUG),debug,release)
 EVSIEVE_BIN_DIR = target/$(RUSTC_TARGET_NAME)/$(EVSIEVE_CARGO_MODE)
 
 EVSIEVE_CARGO_OPTS = \
-	$(if $(BR2_ENABLE_DEBUG),,--release) \
+	--release \
 	--target=$(RUSTC_TARGET_NAME) \
 	--manifest-path=$(@D)/Cargo.toml
 
