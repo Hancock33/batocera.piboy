e74d1b262382e83b01651d97b78a64980212c333
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-07-25 23:35:08 +0100

    try mame with 03 optimisation

--- package/batocera/sources/libretro-mame-src/libretro-mame-src.mk
+++ package/batocera/sources/libretro-mame-src/libretro-mame-src.mk
@@ -3,9 +3,9 @@
 # libretro-mame-src
 #
 ################################################################################
-# Version: Commits on Jul 12, 2025
-LIBRETRO_MAME_SRC_VERSION = d6423328d57857c05ef3513999fe4ff4917db197
-LIBRETRO_MAME_SRC_SITE = $(call github,libretro,mame,$(LIBRETRO_MAME_SRC_VERSION))
+# Version: Commits on Jul 25, 2025
+LIBRETRO_MAME_SRC_VERSION = d2a9e5908365a06fcd5d7e066806663abc2bdf20
+LIBRETRO_MAME_SRC_SITE = $(call github,hancock33,lr-mame,$(LIBRETRO_MAME_SRC_VERSION))
 LIBRETRO_MAME_SRC_DEPENDENCIES = sdl2 sdl2_ttf zlib libpng fontconfig sqlite jpeg flac rapidjson expat glm pulseaudio
 LIBRETRO_MAME_SRC_LICENSE = MAME
 
@@ -29,7 +29,7 @@ define LIBRETRO_MAME_SRC_BUILD_CMDS
 	$(TARGET_CONFIGURE_OPTS) $(MAKE) CXX="$(HOST_DIR)/bin/ccache $(TARGET_CXX)" CC="$(HOST_DIR)/bin/ccache $(TARGET_CC)" -C $(@D)/ -f Makefile.libretro \
 		$(LIBRETRO_MAME_SRC_EXTRA_ARGS) \
 		FORCE_DRC_C_BACKEND=0 \
-		OPTIMIZE=s LTO=1 OPT_FLAGS=$(BR2_TARGET_OPTIMIZATION)
+		OPTIMIZE=3 LTO=1 OPT_FLAGS=$(BR2_TARGET_OPTIMIZATION)
 endef
 
 define LIBRETRO_MAME_SRC_INSTALL_TARGET_CMDS
--- package/batocera/sources/mame-src/mame-src.mk
+++ package/batocera/sources/mame-src/mame-src.mk
@@ -3,8 +3,8 @@
 # mame-src
 #
 ################################################################################
-# Version: Commits on Jun 30, 2025
-MAME_SRC_VERSION = mame0278
+# Version: Commits on Jul 25, 2025
+MAME_SRC_VERSION = b90b916e5d644b45182602c213bc444ba1461fac
 MAME_SRC_SOURCE = mame-src-$(MAME_SRC_VERSION).tar.gz
 MAME_SRC_SITE = $(call github,mamedev,mame,$(MAME_SRC_VERSION))
 MAME_SRC_DEPENDENCIES = expat flac fontconfig glm jpeg libpng rapidjson sdl2 sdl2_ttf sqlite zlib
@@ -101,7 +101,7 @@ define MAME_SRC_BUILD_CMDS
 	USE_SYSTEM_LIB_SQLITE3=1 \
 	USE_SYSTEM_LIB_ZLIB=1 \
 	USE_SYSTEM_LIB_ZSTD=1 \
-	OPTIMIZE=s LTO=1 OPT_FLAGS=$(BR2_TARGET_OPTIMIZATION) PTR64=1
+	OPTIMIZE=3 LTO=1 OPT_FLAGS=$(BR2_TARGET_OPTIMIZATION) PTR64=1
 endef
 
 define MAME_SRC_INSTALL_TARGET_CMDS
