23f2cfadebd9402680fdd720932364e89718d65d
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-07-27 12:26:00 +0100

    tidyup xemu

--- package/batocera/emulators/xemu/xemu.mk
+++ package/batocera/emulators/xemu/xemu.mk
@@ -18,59 +18,8 @@ XEMU_CONF_ENV += PATH="/$(BR2_ARCH)/host/$(BR2_ARCH)-buildroot-linux-gnu/sysroot
 
 XEMU_CONF_OPTS += --target-list=i386-softmmu
 XEMU_CONF_OPTS += --cross-prefix="$(STAGING_DIR)"
-XEMU_CONF_OPTS += --extra-cflags="-DXBOX=1 $(TARGET_OPTIMIZATION) -Wno-error=redundant-decls -Wno-error=unused-but-set-variable"
-XEMU_CONF_OPTS += --extra-ldflags=""
-XEMU_CONF_OPTS += --enable-sdl
-XEMU_CONF_OPTS += --enable-opengl
-XEMU_CONF_OPTS += --enable-trace-backends="nop"
-XEMU_CONF_OPTS += --disable-kvm
-XEMU_CONF_OPTS += --disable-xen
+XEMU_CONF_OPTS += --extra-cflags="-DXBOX=1"
 XEMU_CONF_OPTS += --disable-werror
-XEMU_CONF_OPTS += --disable-curl
-XEMU_CONF_OPTS += --disable-vnc
-XEMU_CONF_OPTS += --disable-vnc-sasl
-XEMU_CONF_OPTS += --disable-docs
-XEMU_CONF_OPTS += --disable-tools
-XEMU_CONF_OPTS += --disable-guest-agent
-XEMU_CONF_OPTS += --disable-tpm
-XEMU_CONF_OPTS += --disable-rdma
-XEMU_CONF_OPTS += --disable-replication
-XEMU_CONF_OPTS += --disable-capstone
-XEMU_CONF_OPTS += --disable-libiscsi
-XEMU_CONF_OPTS += --disable-spice
-XEMU_CONF_OPTS += --disable-user
-XEMU_CONF_OPTS += --disable-stack-protector
-XEMU_CONF_OPTS += --disable-glusterfs
-XEMU_CONF_OPTS += --disable-curses
-XEMU_CONF_OPTS += --disable-gnutls
-XEMU_CONF_OPTS += --disable-nettle
-XEMU_CONF_OPTS += --disable-gcrypt
-XEMU_CONF_OPTS += --disable-crypto-afalg
-XEMU_CONF_OPTS += --disable-virglrenderer
-XEMU_CONF_OPTS += --disable-vhost-net
-XEMU_CONF_OPTS += --disable-vhost-crypto
-XEMU_CONF_OPTS += --disable-vhost-user
-XEMU_CONF_OPTS += --disable-virtfs
-XEMU_CONF_OPTS += --disable-snappy
-XEMU_CONF_OPTS += --disable-bzip2
-XEMU_CONF_OPTS += --disable-vde
-XEMU_CONF_OPTS += --disable-seccomp
-XEMU_CONF_OPTS += --disable-numa
-XEMU_CONF_OPTS += --disable-lzo
-XEMU_CONF_OPTS += --disable-smartcard
-XEMU_CONF_OPTS += --disable-usb-redir
-XEMU_CONF_OPTS += --disable-bochs
-XEMU_CONF_OPTS += --disable-cloop
-XEMU_CONF_OPTS += --disable-dmg
-XEMU_CONF_OPTS += --disable-vdi
-XEMU_CONF_OPTS += --disable-vvfat
-XEMU_CONF_OPTS += --disable-qcow1
-XEMU_CONF_OPTS += --disable-qed
-XEMU_CONF_OPTS += --disable-parallels
-XEMU_CONF_OPTS += --disable-hvf
-XEMU_CONF_OPTS += --disable-whpx
-XEMU_CONF_OPTS += --with-default-devices
-XEMU_CONF_OPTS += --disable-renderdoc
 
 # Vulkan
 ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_X86),y)
