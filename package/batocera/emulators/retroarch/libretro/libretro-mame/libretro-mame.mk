240b5763b56a10de51426bf4108d42a5a33f1711
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-08-01 19:02:42 +0100

    mame0279

--- package/batocera/emulators/retroarch/libretro/libretro-mame/libretro-mame.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-mame/libretro-mame.mk
@@ -3,8 +3,8 @@
 # libretro-mame
 #
 ################################################################################
-# Version: Commits on Jul 01, 2025
-LIBRETRO_MAME_VERSION = mame0278
+# Version: Commits on Aug 01, 2025
+LIBRETRO_MAME_VERSION = mame0279
 LIBRETRO_MAME_SITE = https://github.com/Hancock33/batocera-mame-builds/releases/download/$(LIBRETRO_MAME_VERSION)
 LIBRETRO_MAME_LICENSE = MAME
 LIBRETRO_MAME_SOURCE = libretro-mame-$(MAME_CROSS_ARCH)-$(subst mame,,$(LIBRETRO_MAME_VERSION)).tar.xz
