a06165e59244f31594c3e48d48844bf55c88bd4a
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-08-02 09:06:29 +0100

    Update retrocrisis.mk

--- package/batocera/emulators/retroarch/shaders/retrocrisis/retrocrisis.mk
+++ package/batocera/emulators/retroarch/shaders/retrocrisis/retrocrisis.mk
@@ -17,7 +17,7 @@ endef
 
 define RETROCRISIS_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/usr/share/batocera/shaders
-	cp -ar $(@D)/shaders $(TARGET_DIR)/usr/share/batocera
+	cp -ar $(@D)/shaders/shaders_slang/retro* $(TARGET_DIR)/usr/share/batocera/shaders/retrocrisis
 endef
 
 $(eval $(generic-package))
