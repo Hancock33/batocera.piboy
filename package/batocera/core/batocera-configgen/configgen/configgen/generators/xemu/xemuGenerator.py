0f035de06b5da4f5424c9829790343fcee4cd5dd
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-08-10 13:33:27 +0100

    tidy xemu generator

--- package/batocera/core/batocera-configgen/configgen/configgen/generators/xemu/xemuGenerator.py
+++ package/batocera/core/batocera-configgen/configgen/configgen/generators/xemu/xemuGenerator.py
@@ -32,19 +32,19 @@ class XemuGenerator(Generator):
         commandArray: list[str | Path] = [XEMU_BIN]
         commandArray.extend(["-config_path", XEMU_CONFIG])
 
-        if system.isOptSet("xemu_zink") == True:
-            environment = {
-                "XDG_CONFIG_HOME": CONFIGS,
-                "SDL_GAMECONTROLLERCONFIG": generate_sdl_game_controller_config(playersControllers),
-                "MESA_LOADER_DRIVER_OVERRIDE": "zink",
-                "GALLIUM_DRIVER": "zink",
-                "LIBGL_KOPPER_DRI2": "1"
+        environment={
+            "XDG_CONFIG_HOME": CONFIGS,
+            "SDL_GAMECONTROLLERCONFIG": generate_sdl_game_controller_config(playersControllers)
             }
-        else:
-            environment = {
-                "XDG_CONFIG_HOME": CONFIGS,
-                "SDL_GAMECONTROLLERCONFIG": generate_sdl_game_controller_config(playersControllers)
-            }
+
+        if system.config.get_bool("xemu_zink"):
+            environment.update(
+                {
+                    "MESA_LOADER_DRIVER_OVERRIDE": "zink",
+                    "GALLIUM_DRIVER": "zink",
+                    "LIBGL_KOPPER_DRI2": "1"
+                }
+            )
 
         return Command.Command(array=commandArray, env=environment)
 
