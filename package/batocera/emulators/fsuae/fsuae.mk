6037c030a44eb70671b5bcc712848fd131a8eee0
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-08-15 07:21:13 +0100

    fsuae has moved to sdl3

--- package/batocera/emulators/fsuae/fsuae.mk
+++ package/batocera/emulators/fsuae/fsuae.mk
@@ -8,21 +8,16 @@ FSUAE_VERSION = 195e3d43c0ce921f7ddc45f3d1e44fa858823d37
 FSUAE_SITE = $(call github,FrodeSolheim,fs-uae,$(FSUAE_VERSION))
 FSUAE_LICENSE = GPLv2
 FSUAE_DEPENDENCIES += libpng libmpeg2 libglib2 libcapsimage openal
-FSUAE_DEPENDENCIES += sdl2 sdl2_ttf zlib
+FSUAE_DEPENDENCIES += sdl3 sdl3_image sdl3_ttf zlib
+FSUAE_AUTORECONF = YES
 
 FSUAE_CONF_OPTS += --disable-codegen
 
-define FSUAE_HOOK_BOOTSTRAP
-	cd $(@D) && ./bootstrap
-endef
-
-FSUAE_PRE_CONFIGURE_HOOKS += FSUAE_HOOK_BOOTSTRAP
 
 define FSUAE_INSTALL_EVMAPY
 	mkdir -p $(TARGET_DIR)/usr/share/evmapy
-	$(INSTALL) -D -m 0644 \
-		$(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/emulators/fsuae/*.keys \
-			$(TARGET_DIR)/usr/share/evmapy/
+	$(INSTALL) -D -m 0644 $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/emulators/fsuae/*.keys \
+		$(TARGET_DIR)/usr/share/evmapy/
 endef
 
 FSUAE_POST_INSTALL_TARGET_HOOKS = FSUAE_INSTALL_EVMAPY
