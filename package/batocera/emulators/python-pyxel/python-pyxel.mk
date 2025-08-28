148a2f47b5e76eab6826c0404c1d373e879f7f09
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-08-28 23:21:31 +0100

    python-pyxel version 2.5.X fails to build

--- package/batocera/emulators/python-pyxel/python-pyxel.mk
+++ package/batocera/emulators/python-pyxel/python-pyxel.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 # Version: Commits on Aug 28, 2025
-PYTHON_PYXEL_VERSION = v2.5.1
+PYTHON_PYXEL_VERSION = v2.4.10
 PYTHON_PYXEL_SITE =  $(call github,kitao,pyxel,$(PYTHON_PYXEL_VERSION))
 PYTHON_PYXEL_SETUP_TYPE = maturin
 PYTHON_PYXEL_LICENSE = MIT
