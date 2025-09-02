c076e6ebcf0bce021ac6aa76931d088293d8290e
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-09-02 21:02:44 +0100

    bump nvidia driver version 580.82.07

--- package/batocera/gpu/nvidia/nvidia-open-driver/nvidia-open-driver.mk
+++ package/batocera/gpu/nvidia/nvidia-open-driver/nvidia-open-driver.mk
@@ -3,8 +3,8 @@
 # nvidia-open-driver
 #
 ################################################################################
-# Version: Commits on  Aug 12, 2025
-NVIDIA_OPEN_DRIVER_VERSION = 580.76.05
+# Version: Commits on  Sept 02, 2025
+NVIDIA_OPEN_DRIVER_VERSION = 580.82.07
 NVIDIA_OPEN_DRIVER_SUFFIX = $(if $(BR2_x86_64),_64)
 NVIDIA_OPEN_DRIVER_SITE = https://us.download.nvidia.com/XFree86/Linux-x86$(NVIDIA_OPEN_DRIVER_SUFFIX)/$(NVIDIA_OPEN_DRIVER_VERSION)
 NVIDIA_OPEN_DRIVER_SOURCE = NVIDIA-Linux-x86$(NVIDIA_OPEN_DRIVER_SUFFIX)-$(NVIDIA_OPEN_DRIVER_VERSION).run
