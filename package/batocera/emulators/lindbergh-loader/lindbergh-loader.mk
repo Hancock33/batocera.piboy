e136e9220feaad2b7cda38b1d9745682c0e7cd19
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-09-18 07:18:50 +0100

    update lindbergh part 1
    generator needs to be updated to reflect the lindbergh.conf > lindbergh.ini changes

--- package/batocera/emulators/lindbergh-loader/lindbergh-loader.mk
+++ package/batocera/emulators/lindbergh-loader/lindbergh-loader.mk
@@ -15,7 +15,7 @@ endif
 
 ifeq ($(BR2_i386),y)
 LINDBERGH_LOADER_DEPENDENCIES += alsa-lib alsa-plugins alsa-utils faudio libfreeglut pcsc-lite
-LINDBERGH_LOADER_DEPENDENCIES += libglew sdl2 ncurses openal pipewire xlib_libX11 libbsd
+LINDBERGH_LOADER_DEPENDENCIES += libglew sdl3 sdl3_image sdl3_ttf ncurses openal pipewire xlib_libX11 libbsd
 LINDBERGH_LOADER_DEPENDENCIES += xlib_libXcursor xlib_libXext xlib_libXi xlib_libXmu xlib_libXScrnSaver
 
 # match the makefile cflags
@@ -23,7 +23,7 @@ LINDBERGH_LOADER_CFLAGS = -g -pipe -fPIC -I$(STAGING_DIR)/usr/include -flto
 # match the makefile ldflags
 LINDBERGH_LOADER_LDFLAGS += -L$(STAGING_DIR)/usr/lib
 LINDBERGH_LOADER_LDFLAGS += -flto -Wl,-z,defs -rdynamic -static-libgcc -lc -ldl -lGL
-LINDBERGH_LOADER_LDFLAGS += -lglut -lX11 -lXcursor -lSDL2 -lFAudio -lm -lpthread -shared -lgcc_s -lgcc
+LINDBERGH_LOADER_LDFLAGS += -lglut -lX11 -lXcursor -lSDL3 -lSDL3_image -lSDL3_ttf -lFAudio -lm -lpthread -shared -lgcc_s -lgcc
 LINDBERGH_LOADER_LDFLAGS += -nostdlib -lasound -L./src/libxdiff -lxdiff
 
 define LINDBERGH_LOADER_BUILD_CMDS
@@ -42,9 +42,10 @@ define LINDBERGH_LOADER_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/usr/bin/lindbergh
 	mkdir -p $(TARGET_DIR)/usr/lib32/extralibs
 	cp -fv $(@D)/build/* $(TARGET_DIR)/usr/bin/lindbergh/
-	cp -fv $(@D)/docs/lindbergh.conf $(TARGET_DIR)/usr/bin/lindbergh/
 	cp -fv $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/emulators/lindbergh-loader/lib*.so* $(TARGET_DIR)/usr/lib32/extralibs
 	mv $(TARGET_DIR)/usr/bin/lindbergh/lib* $(TARGET_DIR)/usr/lib32/extralibs
+	LD_LIBRARY_PATH=$(TARGET_DIR)/usr/lib32:$(TARGET_DIR)/usr/bin/lindbergh $(TARGET_DIR)/usr/bin/lindbergh/lindbergh --create config $(TARGET_DIR)/usr/bin/lindbergh/lindbergh.ini
+	sed -i "s|FULLSCREEN = false|FULLSCREEN = true|" $(TARGET_DIR)/usr/bin/lindbergh/lindbergh.ini
 endef
 endif
 
