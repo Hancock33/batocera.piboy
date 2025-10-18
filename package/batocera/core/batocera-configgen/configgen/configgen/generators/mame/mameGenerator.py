b2ab0185d61ef96fd3210b3ce32700f9a84695ee
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-10-18 20:07:58 +0100

    Update mameGenerator.py

--- package/batocera/core/batocera-configgen/configgen/configgen/generators/mame/mameGenerator.py
+++ package/batocera/core/batocera-configgen/configgen/configgen/generators/mame/mameGenerator.py
@@ -121,8 +121,7 @@ class MameGenerator(Generator):
         # MAME options used here are explained as it's not always straightforward
         # A lot more options can be configured, just run mame -showusage and have a look
 
-        # set audio to pipewire to fix audio from 0.278
-        commandArray += [ "-sound", "pipewire" ]
+        commandArray += [ "-sound", "sdl" ]
         # skip game info at start
         commandArray += [ "-skip_gameinfo" ]
 
