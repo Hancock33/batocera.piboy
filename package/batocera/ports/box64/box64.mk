27dd88797a28c0dc906fe2c1181fb34dcc531ece
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-10-19 19:04:23 +0100

    Update box64.mk

--- package/batocera/ports/box64/box64.mk
+++ package/batocera/ports/box64/box64.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 # Version: Commits on Oct 19, 2025
-BOX64_VERSION = aa4dbe72c9f7a6297a68ef4edce70a105cbd0f4c
+BOX64_VERSION = 2db2532092029ab62575bbec1fb560d7839ff9cb
 BOX64_SITE = $(call github,ptitseb,box64,$(BOX64_VERSION))
 BOX64_SUPPORTS_IN_SOURCE_BUILD = NO
 
