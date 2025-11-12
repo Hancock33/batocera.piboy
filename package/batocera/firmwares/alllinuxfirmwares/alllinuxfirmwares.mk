53780ca9d244c79561ce14833a7cbb239a2f0465
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-11-12 22:09:36 +0000

    Update alllinuxfirmwares.mk

--- package/batocera/firmwares/alllinuxfirmwares/alllinuxfirmwares.mk
+++ package/batocera/firmwares/alllinuxfirmwares/alllinuxfirmwares.mk
@@ -3,8 +3,8 @@
 # alllinuxfirmwares
 #
 ################################################################################
-# Version: Commits on Oct 21, 2025
-ALLLINUXFIRMWARES_VERSION = 20251021
+# Version: Commits on Nov 11, 2025
+ALLLINUXFIRMWARES_VERSION = 20251111
 ALLLINUXFIRMWARES_SOURCE = linux-firmware-$(ALLLINUXFIRMWARES_VERSION).tar.gz
 ALLLINUXFIRMWARES_SITE = https://www.kernel.org/pub/linux/kernel/firmware
 
