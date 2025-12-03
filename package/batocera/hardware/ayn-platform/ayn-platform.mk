580bf55a044660cfb9db491ece27fa88a084c981
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-12-03 17:46:40 +0000

    update ayaneo and ayn platforms

--- package/batocera/hardware/ayn-platform/ayn-platform.mk
+++ package/batocera/hardware/ayn-platform/ayn-platform.mk
@@ -3,16 +3,11 @@
 # ayn-platform
 #
 ################################################################################
-# Version: Commits on Jul 29, 2024
-AYN_PLATFORM_VERSION = v0.5.2
+# Version: Commits on Sept 16, 2024
+AYN_PLATFORM_VERSION = 9813128ddac097f8e11a92d64b33de7b70154989
 AYN_PLATFORM_SITE = $(call github,ShadowBlip,ayn-platform,$(AYN_PLATFORM_VERSION))
 AYN_PLATFORM_LICENSE = GPL-3.0
 AYN_PLATFORM_LICENSE_FILES = LICENSE
-
-AYN_PLATFORM_MODULE_MAKE_OPTS = \
-	CONFIG_AYN_PLATFORM=m \
-	USER_EXTRA_CFLAGS="-DCONFIG_$(call qstrip,$(BR2_ENDIAN))_ENDIAN \
-		-Wno-error"
 
 $(eval $(kernel-module))
 $(eval $(generic-package))
--- package/batocera/utils/ryzen-smu/ryzen-smu.mk
+++ package/batocera/utils/ryzen-smu/ryzen-smu.mk
@@ -9,7 +9,5 @@ RYZEN_SMU_SITE = $(call github,amkillam,ryzen_smu,$(RYZEN_SMU_VERSION))
 RYZEN_SMU_LICENSE = GPL-2.0
 RYZEN_SMU_LICENSE_FILES = LICENSE
 
-RYZEN_SMU_MODULE_MAKE_OPTS = USER_EXTRA_CFLAGS="-DCONFIG_$(call qstrip,$(BR2_ENDIAN))_ENDIAN -Wno-error"
-
 $(eval $(kernel-module))
 $(eval $(generic-package))
