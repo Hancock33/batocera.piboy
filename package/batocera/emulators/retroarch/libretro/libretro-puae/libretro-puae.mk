42cc222c3da3afed77e08677d638815a833cb461
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-12-03 17:47:24 +0000

    libretro-puae cleanupds

--- package/batocera/emulators/retroarch/libretro/libretro-puae/libretro-puae.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-puae/libretro-puae.mk
@@ -10,7 +10,7 @@ LIBRETRO_PUAE__LICENSE = GPLv2
 LIBRETRO_PUAE_DEPENDENCIES = retroarch
 
 define LIBRETRO_PUAE_BUILD_CMDS
-	$(SED) "s|\-O[23]|$(TARGET_OPTIMIZATION) -Wno-implicit-function-declaration|g" $(@D)/Makefile
+	$(SED) "s|\-O[23]|$(TARGET_OPTIMIZATION)|g" $(@D)/Makefile
 	$(TARGET_CONFIGURE_OPTS) $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D)/ -f Makefile platform="$(LIBRETRO_PLATFORM)" \
 		GIT_VERSION="-$(shell echo $(LIBRETRO_PUAE_VERSION) | cut -c 1-7)"
 endef
