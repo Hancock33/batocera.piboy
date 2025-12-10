1382d52fac56d61f012dafd4e0b2da3ad38ada68
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-12-10 07:15:58 +0000

    Update xemu.mk

--- package/batocera/emulators/xemu/xemu.mk
+++ package/batocera/emulators/xemu/xemu.mk
@@ -3,8 +3,8 @@
 # xemu
 #
 ################################################################################
-# Version: Commits on Nov 29, 2025
-XEMU_VERSION = v0.8.118
+# Version: Commits on Dec 10, 2025
+XEMU_VERSION = v0.8.119
 XEMU_SITE = https://github.com/xemu-project/xemu.git
 XEMU_SITE_METHOD = git
 XEMU_GIT_SUBMODULES = YES
