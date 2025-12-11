84a9a464386466776ca8df28a2dafb6963f2221d
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-12-11 19:58:50 +0000

    add d7vk

---
+++ package/batocera/wine/d7vk/d7vk.mk
@@ -0,0 +1,21 @@
+################################################################################
+#
+# d7vk
+#
+################################################################################
+# Version: Commits on Dec 11, 2025
+D7VK_VERSION = v1.0
+D7VK_SOURCE = d7vk-$(D7VK_VERSION).zip
+D7VK_SITE = https://github.com/WinterSnowfall/d7vk/releases/download/$(D7VK_VERSION)
+D7VK_LICENSE = zlib/libpng
+
+define D7VK_EXTRACT_CMDS
+	mkdir -p $(@D)/target && cd $(@D)/target && unzip $(DL_DIR)/$(D7VK_DL_SUBDIR)/$(D7VK_SOURCE)
+endef
+
+define D7VK_INSTALL_TARGET_CMDS
+	mkdir -p $(TARGET_DIR)/usr/wine/d7vk
+	cp -pr $(@D)/target/d7vk-$(D7VK_VERSION)/x32 $(TARGET_DIR)/usr/wine/d7vk/
+endef
+
+$(eval $(generic-package))
