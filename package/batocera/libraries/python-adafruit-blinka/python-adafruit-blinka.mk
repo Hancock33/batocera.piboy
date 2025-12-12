ff5fcbe73fdf7bda1b0b29cb098618baf5d74366
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-12-12 07:15:59 +0000

    some python updates

--- package/batocera/libraries/python-adafruit-blinka/python-adafruit-blinka.mk
+++ package/batocera/libraries/python-adafruit-blinka/python-adafruit-blinka.mk
@@ -3,10 +3,10 @@
 # python-adafruit-blinka
 #
 ################################################################################
-# Version: Commits on Sept 27, 2025
-PYTHON_ADAFRUIT_BLINKA_VERSION = 8.66.2
+# Version: Commits on Mov 20, 2025
+PYTHON_ADAFRUIT_BLINKA_VERSION = 8.68.0
 PYTHON_ADAFRUIT_BLINKA_SOURCE = adafruit_blinka-$(PYTHON_ADAFRUIT_BLINKA_VERSION).tar.gz
-PYTHON_ADAFRUIT_BLINKA_SITE = https://files.pythonhosted.org/packages/35/2d/7469c26dbeade72709096922db34a75fecb47c6953dba0cd4c14ee2b3b45
+PYTHON_ADAFRUIT_BLINKA_SITE = https://files.pythonhosted.org/packages/38/ca/362177fd0830201fd2a9ae34cecdd5593c9fa9d9d3bc81a6941a54cbe221
 PYTHON_ADAFRUIT_BLINKA_SETUP_TYPE = setuptools
 PYTHON_ADAFRUIT_BLINKA_LICENSE = MIT
 PYTHON_ADAFRUIT_BLINKA_LICENSE_FILES = LICENSE
