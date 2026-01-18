8fdbdb53d216e2704023f75c3d8a2b5db4d3d3b6
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2026-01-18 11:39:46 +0000

    revert vpinball update

--- package/batocera/engines/vpinball/vpinball/vpinball.mk
+++ package/batocera/engines/vpinball/vpinball/vpinball.mk
@@ -3,29 +3,32 @@
 # vpinball
 #
 ################################################################################
-# Version: Commits on Jan 17, 2026
-VPINBALL_VERSION = 32be0a2034ef8448f90c4c5ffe4ba8e6015bdc8a
-VPINBALL_BRANCH = master
+# Version: Commits on Jul 23, 2024
+VPINBALL_VERSION = afc7c38ffb1078076ec523c37b30055061c93ff1
+VPINBALL_BRANCH = standalone
 VPINBALL_SITE = $(call github,vpinball,vpinball,$(VPINBALL_VERSION))
 VPINBALL_LICENSE = GPLv3+
 VPINBALL_LICENSE_FILES = LICENSE
-VPINBALL_DEPENDENCIES = ffmpeg libfreeimage libpinmame libaltsound libserialport libzedmd libserum libdmdutil libdof sdl3 sdl3_image sdl3_ttf
+VPINBALL_DEPENDENCIES = ffmpeg libfreeimage libpinmame libaltsound libserialport libzedmd libserum libdmdutil libdof sdl2 sdl2_image sdl2_ttf
 VPINBALL_SUPPORTS_IN_SOURCE_BUILD = NO
 
 VPINBALL_CONF_OPTS += $(VPINBALL_COMMON_CONF_OPTS)
 VPINBALL_CONF_OPTS += -DBUILD_SHARED_LIBS=OFF
 
 define VPINBALL_CMAKE_HACKS
-	cp $(@D)/make/$(VPINBALL_COMMON_CMAKE) $(@D)/CMakeLists.txt
+	cp $(@D)/standalone/cmake/$(VPINBALL_COMMON_CMAKE) $(@D)/CMakeLists.txt
+	$(SED) '/--copy-dt-needed-entries/d' $(@D)/CMakeLists.txt
+	$(SED) 's:$${CMAKE_SOURCE_DIR}/standalone/$(VPINBALL_COMMON_SRC)/external/include:$(STAGING_DIR)/usr/include/:g' $(@D)/CMakeLists.txt
+	$(SED) 's:$${CMAKE_SOURCE_DIR}/standalone/$(VPINBALL_COMMON_SRC)/external/lib/:$(STAGING_DIR)/usr/lib/:g' $(@D)/CMakeLists.txt
 endef
 
 define VPINBALL_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/usr/bin/vpinball
 	$(INSTALL) -D -m 0755 $(@D)/buildroot-build/VPinballX_GL	$(TARGET_DIR)/usr/bin/vpinball
+	cp -R $(@D)/buildroot-build/flexdmd			$(TARGET_DIR)/usr/bin/vpinball/
 	cp -R $(@D)/buildroot-build/assets			$(TARGET_DIR)/usr/bin/vpinball/
-	cp -R $(@D)/buildroot-build/plugins			$(TARGET_DIR)/usr/bin/vpinball/
 	cp -R $(@D)/buildroot-build/scripts			$(TARGET_DIR)/usr/bin/vpinball/
-	cp -R $(@D)/buildroot-build/shaders-10.8.1	$(TARGET_DIR)/usr/bin/vpinball/
+	cp -R $(@D)/buildroot-build/shader10.8.0	$(TARGET_DIR)/usr/bin/vpinball/
 	$(INSTALL) -D -m 0755 $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/engines/vpinball/vpinball/batocera-vpx-scraper.py $(TARGET_DIR)/usr/bin/batocera-vpx-scraper
 	#evmapy install
 	mkdir -p $(TARGET_DIR)/usr/share/evmapy
