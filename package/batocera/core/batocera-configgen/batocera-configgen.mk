a0252a1f0f05d57a13e521e84e195eda2fa6bc99
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2026-02-05 23:03:47 +0000

    Update batocera-configgen.mk

--- package/batocera/core/batocera-configgen/batocera-configgen.mk
+++ package/batocera/core/batocera-configgen/batocera-configgen.mk
@@ -128,9 +128,6 @@ define BATOCERA_CONFIGGEN_CONFIGS
 	@if [ "$(BATOCERA_CONFIGGEN_SYSTEM)" = "piboy4" ]; then \
 	cp $(BATOCERA_CONFIGGEN_CP_DIR)/configs/configgen-defaults-bcm2711.yml							$(TARGET_DIR)/usr/share/batocera/configgen/configgen-defaults-arch.yml && \
 	cp $(BATOCERA_CONFIGGEN_CP_DIR)/configs/configgen-defaults-piboy4.yml							$(TARGET_DIR)/usr/share/batocera/configgen/configgen-defaults-piboy4.yml ; fi
-
-	rm -rf $(TARGET_DIR)/usr/bin/emulatorlauncher
-	rm -rf $(TARGET_DIR)/usr/lib/python3.14/site-packages/configgen
 endef
 
 define BATOCERA_CONFIGGEN_ES_HOOKS
@@ -146,6 +143,12 @@ define BATOCERA_CONFIGGEN_SCRIPTS
 	install -D -m 0755 $(BATOCERA_CONFIGGEN_CP_DIR)/scripts/batocera-joysticks-hotkeys.py $(TARGET_DIR)/usr/bin/batocera-joysticks-hotkeys
 endef
 
+define BATOCERA_CONFIGGEN_REINSTALL
+	rm -rf $(TARGET_DIR)/usr/bin/emulatorlauncher
+	rm -rf $(TARGET_DIR)/usr/lib/python3.14/site-packages/configgen
+endef
+
+BATOCERA_CONFIGGEN_PRE_EXTRACT_HOOKS = BATOCERA_CONFIGGEN_REINSTALL
 BATOCERA_CONFIGGEN_POST_INSTALL_TARGET_HOOKS = BATOCERA_CONFIGGEN_CONFIGS
 BATOCERA_CONFIGGEN_POST_INSTALL_TARGET_HOOKS += BATOCERA_CONFIGGEN_ES_HOOKS
 BATOCERA_CONFIGGEN_POST_INSTALL_TARGET_HOOKS += BATOCERA_CONFIGGEN_SCRIPTS
