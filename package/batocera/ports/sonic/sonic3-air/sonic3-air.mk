d01dd80ac6cd0c23e8f9ede29c20cf24c3907275
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2026-02-21 11:39:32 +0000

    update sonic3-air missing data

--- package/batocera/ports/sonic/sonic3-air/sonic3-air.mk
+++ package/batocera/ports/sonic/sonic3-air/sonic3-air.mk
@@ -18,8 +18,10 @@ SONIC3_AIR_DEPENDENCIES += xlib_libXcomposite xlib_libXxf86vm
 SONIC3_AIR_SUPPORTS_IN_SOURCE_BUILD = NO
 
 SONIC3_AIR_CONF_OPTS += -DBUILD_SHARED_LIBS=OFF
+SONIC3_AIR_CONF_OPTS += -DUSE_DISCORD=OFF
 
 define SONIC3_AIR_INSTALL_TARGET_CMDS
+	rm -rf $(TARGET_DIR)/usr/bin/sonic3-air
 	mkdir -p $(TARGET_DIR)/usr/bin/sonic3-air
 	# copy binary
 	cp $(@D)/Oxygen/sonic3air/sonic3air_linux $(TARGET_DIR)/usr/bin/sonic3-air
@@ -28,6 +30,7 @@ define SONIC3_AIR_INSTALL_TARGET_CMDS
 	cp $(@D)/Oxygen/sonic3air/oxygenproject.json $(TARGET_DIR)/usr/bin/sonic3-air
 	# copy game resource files
 	cp -r $(@D)/Oxygen/sonic3air/data $(TARGET_DIR)/usr/bin/sonic3-air
+	cp -r $(@D)/Oxygen/oxygenengine/data $(TARGET_DIR)/usr/bin/sonic3-air
 	cp -r $(@D)/Oxygen/sonic3air/scripts $(TARGET_DIR)/usr/bin/sonic3-air
 	cp -r $(@D)/Oxygen/sonic3air/saves $(TARGET_DIR)/usr/bin/sonic3-air
 endef
