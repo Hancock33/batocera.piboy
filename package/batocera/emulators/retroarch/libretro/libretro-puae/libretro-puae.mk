12e1daad9b163defaa86d1a4a9d2843db48733c9
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2026-02-26 20:06:42 +0000

    Update libretro-puae.mk

--- package/batocera/emulators/retroarch/libretro/libretro-puae/libretro-puae.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-puae/libretro-puae.mk
@@ -11,7 +11,7 @@ LIBRETRO_PUAE_DEPENDENCIES += retroarch
 LIBRETRO_PUAE_EMULATOR_INFO = puae.libretro.core.yml
 
 define LIBRETRO_PUAE_BUILD_CMDS
-	$(SED) "s|\-O[23]|$(TARGET_OPTIMIZATION)|g" $(@D)/Makefile
+	$(SED) "s|\-O[23]|$(TARGET_OPTIMIZATION) -Wno-int-conversion|g" $(@D)/Makefile
 	$(TARGET_CONFIGURE_OPTS) $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D)/ -f Makefile platform="$(LIBRETRO_PLATFORM)" \
 		GIT_VERSION="-$(shell echo $(LIBRETRO_PUAE_VERSION) | cut -c 1-7)"
 endef
