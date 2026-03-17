3fb7c15a9be8d25e9a808de28abd5aa71343d71c
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2026-03-17 08:21:51 +0000

    Update libfyaml.mk

--- package/batocera/libraries/libfyaml/libfyaml.mk
+++ package/batocera/libraries/libfyaml/libfyaml.mk
@@ -7,7 +7,9 @@
 LIBFYAML_VERSION = v1.0.0-alpha1
 LIBFYAML_SITE = $(call github,pantoniou,libfyaml,$(LIBFYAML_VERSION))
 LIBFYAML_INSTALL_STAGING = YES
-LIBFYAML_AUTORECONF = YES
 
-$(eval $(autotools-package))
+LIBFYAML_CONF_OPTS += -DENABLE_PYTHON_BINDINGS=OFF
+HOST_LIBFYAML_CONF_OPTS += -DENABLE_PYTHON_BINDINGS=OFF
+
+$(eval $(cmake-package))
 $(eval $(host-cmake-package))
