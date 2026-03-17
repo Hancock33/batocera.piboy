fea0a5bd8cb2967419acb710063906c6a8049efe
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2026-03-17 07:14:41 +0000

    update supertux2 version 0.7.0

--- package/batocera/ports/supertux2/supertux2.mk
+++ package/batocera/ports/supertux2/supertux2.mk
@@ -3,13 +3,11 @@
 # supertux2
 #
 ################################################################################
-# Version: Commits on Mar 16, 2026
-SUPERTUX2_VERSION = ccf3eea315a1fbaa0b83655d97766359456a85a4
-SUPERTUX2_SITE = https://github.com/SuperTux/supertux.git
-SUPERTUX2_SITE_METHOD=git
-SUPERTUX2_GIT_SUBMODULES=YES
+# Version: Commits on Mar 15, 2026
+SUPERTUX2_VERSION = v0.7.0
+SUPERTUX2_SITE =  https://github.com/SuperTux/supertux/releases/download/$(SUPERTUX2_VERSION)
+SUPERTUX2_SOURCE = SuperTux-$(SUPERTUX2_VERSION)-Source.tar.gz
 SUPERTUX2_SUPPORTS_IN_SOURCE_BUILD = NO
-SUPERTUX2_CMAKE_BACKEND = make
 
 # Supertux itself is GPL-3.0+, but it bundles a few libraries with different
 # licenses (sexp-cpp, squirrel, tinygettext) which are linked statically.
@@ -22,8 +20,6 @@ SUPERTUX2_CONF_OPTS += -DBUILD_DOCUMENTATION=OFF
 SUPERTUX2_CONF_OPTS += -DCMAKE_INTERPROCEDURAL_OPTIMIZATION=OFF
 SUPERTUX2_CONF_OPTS += -DINSTALL_SUBDIR_BIN="bin"
 SUPERTUX2_CONF_OPTS += -DINSTALL_SUBDIR_SHARE="share/supertux2"
-SUPERTUX2_CONF_OPTS += -DSUPERTUX_PACKAGE_VERSION="v0.6.3-dev"
-SUPERTUX2_CONF_OPTS += -DSUPERTUX_VERSION_STRING="$(shell echo $(SUPERTUX2_VERSION) | cut -c 1-7)"
 
 define SUPERTUX2_INSTALL_TARGET_CMDS
 	# install media
