e63a4b92d482718a9461382e249f621f1c5fe8ab
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2026-03-29 08:57:36 +0100

    freedroid updates

--- package/batocera/core/batocera-configgen/configgen/configgen/generators/freedroid/freedroidGenerator.py
+++ package/batocera/core/batocera-configgen/configgen/configgen/generators/freedroid/freedroidGenerator.py
@@ -4,12 +4,19 @@ from typing import TYPE_CHECKING
 
 from ... import Command, controllersConfig
 from ..Generator import Generator
+from ...controller import generate_sdl_game_controller_config
 
 class FreedroidGenerator(Generator):
 
     def generate(self, system, rom, playersControllers, metadata, guns, wheels, gameResolution):
         commandArray = ["freedroid", "-f"]
-        return Command.Command(array=commandArray)
+
+        return Command.Command(
+            array=commandArray,
+            env={
+                "SDL_GAMECONTROLLERCONFIG": generate_sdl_game_controller_config(playersControllers)
+            }
+        )
 
     def getHotkeysContext(self) -> HotkeysContext:
         return {
