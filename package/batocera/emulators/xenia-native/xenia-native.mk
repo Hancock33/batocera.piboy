718ee77c5dbe37d1a56c94c8ee14fa69b2ca13b8
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2026-03-29 21:53:43 +0100

    Update xenia-native.mk

--- package/batocera/emulators/xenia-native/xenia-native.mk
+++ package/batocera/emulators/xenia-native/xenia-native.mk
@@ -26,11 +26,30 @@ define XENIA_NATIVE_CROSS_GITVER
 	cat $(@D)/buildroot-build/version.h
 endef
 
+define XENIA_NATIVE_POST_PROCESS
+	# get the latest patches
+	mkdir -p $(TARGET_DIR)/usr/bin/xenia-canary/patches
+	mkdir -p $(@D)/temp
+	( cd $(@D)/temp && git init && \
+	  git remote add origin https://github.com/xenia-canary/game-patches.git && \
+	  git config core.sparsecheckout true && \
+	  echo "patches/*.toml" >> .git/info/sparse-checkout && \
+	  git pull --depth=1 origin main && \
+	  mv -f patches/*.toml $(TARGET_DIR)/usr/bin/xenia-canary/patches \
+	)
+
+	# Clean up the temporary directory
+	rm -rf $(@D)/temp
+endef
+
+
 XENIA_NATIVE_POST_CONFIGURE_HOOKS = XENIA_NATIVE_CROSS_GITVER
+XENIA_NATIVE_POST_INSTALL_TARGET_HOOKS += XENIA_NATIVE_POST_PROCESS
 
 define XENIA_NATIVE_INSTALL_TARGET_CMDS
 	$(TARGET_STRIP) $(@D)/build/bin/Linux/xenia_canary
-	$(INSTALL) -m 0755 -D $(@D)/build/bin/Linux/xenia_canary $(TARGET_DIR)/usr/bin/xenia-native
+	mkdir -p $(TARGET_DIR)/usr/bin/xenia-canary
+	$(INSTALL) -m 0755 -D $(@D)/buildroot-build/Linux/xenia_canary $(TARGET_DIR)/usr/bin/xenia-canary/xenia_canary
 endef
 
 $(eval $(cmake-package))
