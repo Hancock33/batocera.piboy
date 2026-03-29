a71ee08b050f45e483cf129bb706245425fae4df
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2026-03-29 08:57:43 +0100

    Update es-background-musics.mk

--- package/batocera/emulationstation/es-background-musics/es-background-musics.mk
+++ package/batocera/emulationstation/es-background-musics/es-background-musics.mk
@@ -11,7 +11,11 @@ ES_BACKGROUND_MUSICS_SOURCE=
 define ES_BACKGROUND_MUSICS_INSTALL_TARGET_CMDS
 	rm -rf   $(TARGET_DIR)/usr/share/batocera/music
 	mkdir -p $(TARGET_DIR)/usr/share/batocera/music
-	cp -R $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/emulationstation/es-background-musics/music/* $(TARGET_DIR)/usr/share/batocera/music/
+	#cp -R $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/emulationstation/es-background-musics/music/* $(TARGET_DIR)/usr/share/batocera/music/
+	cd $(TARGET_DIR)/usr/share/freedroid/sound && \
+	for f in *.mod; do \
+		ln -srfv -- $(TARGET_DIR)/usr/share/freedroid/sound/"$$(readlink "$$f")"/"$$f" $(TARGET_DIR)/usr/share/batocera/music/"$$f"; \
+	done
 endef
 
 $(eval $(generic-package))
