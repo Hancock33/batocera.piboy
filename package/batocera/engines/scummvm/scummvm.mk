ab07ef71cef970e2d5585e9fb7aa5b3d8b5388e9
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2026-03-31 23:17:59 +0100

    Update scummvm.mk

--- package/batocera/engines/scummvm/scummvm.mk
+++ package/batocera/engines/scummvm/scummvm.mk
@@ -9,8 +9,14 @@ SCUMMVM_SITE = $(call github,scummvm,scummvm,$(SCUMMVM_VERSION))
 SCUMMVM_BRANCH = branch-2026-1-0
 SCUMMVM_LICENSE = GPLv2
 SCUMMVM_EMULATOR_INFO = scummvm.emulator.yml
-SCUMMVM_DEPENDENCIES += faad2 flac fluidsynth freetype jpeg-turbo libmad libmpeg2
+SCUMMVM_DEPENDENCIES += faad2 flac fluidsynth freetype libmad libmpeg2
 SCUMMVM_DEPENDENCIES += libogg libpng libtheora libvorbis sdl2 zlib
+
+ifeq ($(BR2_PACKAGE_JPEG_TURBO),y)
+SCUMMVM_DEPENDENCIES += jpeg-turbo
+else
+SCUMMVM_DEPENDENCIES += jpeg
+endif
 
 SCUMMVM_ADDITIONAL_FLAGS += -I$(STAGING_DIR)/usr/include -lpthread -lm
 SCUMMVM_ADDITIONAL_FLAGS += -L$(STAGING_DIR)/usr/lib -lGLESv2 -lEGL
