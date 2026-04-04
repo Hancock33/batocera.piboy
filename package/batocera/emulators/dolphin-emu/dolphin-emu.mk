9f76bbff34303e93de0db56f19174de37eab4393
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2026-04-04 17:50:48 +0100

    update dolphin and patches

--- package/batocera/emulators/dolphin-emu/dolphin-emu.mk
+++ package/batocera/emulators/dolphin-emu/dolphin-emu.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 # Version: Commits on Apr 04, 2026
-DOLPHIN_EMU_VERSION = 082c6067618de3fd5c5c68a874576ad2641fcde5
+DOLPHIN_EMU_VERSION = 15dee871f36691823177770d4863ddf2375d062f
 DOLPHIN_EMU_VERSION_MAJOR = 2603
-DOLPHIN_EMU_VERSION_MINOR = 219
+DOLPHIN_EMU_VERSION_MINOR = 221
 DOLPHIN_EMU_SITE = https://github.com/dolphin-emu/dolphin
 DOLPHIN_EMU_SITE_METHOD = git
 DOLPHIN_EMU_LICENSE = GPLv2+
