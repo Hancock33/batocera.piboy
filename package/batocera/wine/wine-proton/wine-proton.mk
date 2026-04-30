26c604c43433a50132b350acc46c49d5a65d6786
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2026-04-30 07:06:36 +0100

    update wine-proton

--- package/batocera/wine/wine-proton/wine-proton.mk
+++ package/batocera/wine/wine-proton/wine-proton.mk
@@ -3,8 +3,8 @@
 # wine-proton
 #
 ################################################################################
-# Version: Commits on Apr 25, 2026
-WINE_PROTON_VERSION = cachyos-10.0-20260425-proton-slr
+# Version: Commits on Apr 28, 2026
+WINE_PROTON_VERSION = cachyos-11.0-20260428-proton-slr
 WINE_PROTON_SOURCE = wine-$(WINE_PROTON_VERSION).tar.gz
 WINE_PROTON_SITE = $(call github,CachyOS,wine-cachyos,$(WINE_PROTON_VERSION))
 WINE_PROTON_LICENSE = LGPL-2.1+
