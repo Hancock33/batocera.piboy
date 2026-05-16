13e698bb973bd1ce4559bee2a6b8914216b8d8f4
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2026-05-16 09:52:56 +0100

    Update 7zip.mk

--- package/batocera/utils/7zip/7zip.mk
+++ package/batocera/utils/7zip/7zip.mk
@@ -6,7 +6,7 @@
 # Version: Commits on Apr 27, 2026
 7ZIP_VERSION = 26.01
 7ZIP_SOURCE = 7z$(subst .,,$(7ZIP_VERSION))-src.tar.xz
-7ZIP_SITE = $(call github,ip7z,7zip,$(7ZIP_VERSION))
+7ZIP_SITE = https://github.com/ip7z/7zip/releases/download/$(7ZIP_VERSION)
 7ZIP_LICENSE = LGPL-2.1+ with unRAR restriction
 7ZIP_LICENSE_FILES = DOC/License.txt
 7ZIP_CPE_ID_VENDOR = 7-zip
