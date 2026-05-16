f92087f921bedee50c54656d90bb5b5d1043da7c
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2026-05-16 06:35:54 +0100

    update kodi langs

--- package/batocera/kodi/kodi-resource-language-it_it/kodi_resource_language_it_it.mk
+++ package/batocera/kodi/kodi-resource-language-it_it/kodi_resource_language_it_it.mk
@@ -3,8 +3,8 @@
 # kodi_resource_language_it_it
 #
 ################################################################################
-# Version: Commits on Apr 18, 2026
-KODI_RESOURCE_LANGUAGE_IT_IT_VERSION = 11.0.103
+# Version: Commits on May 10, 2026
+KODI_RESOURCE_LANGUAGE_IT_IT_VERSION = 11.0.104
 KODI_RESOURCE_LANGUAGE_IT_IT_SOURCE = resource.language.it_it-$(KODI_RESOURCE_LANGUAGE_IT_IT_VERSION).zip
 KODI_RESOURCE_LANGUAGE_IT_IT_SITE = http://mirrors.kodi.tv/addons/piers/resource.language.it_it
 KODI_RESOURCE_LANGUAGE_IT_IT_PLUGINNAME=resource.language.it_it
