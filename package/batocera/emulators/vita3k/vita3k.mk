8e28fd95e1cfde205a15d04f7ba9f24ded597797
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2026-05-17 21:34:07 +0100

    revert vita3k

--- package/batocera/emulators/vita3k/vita3k.mk
+++ package/batocera/emulators/vita3k/vita3k.mk
@@ -3,14 +3,14 @@
 # vita3k
 #
 ################################################################################
-# Version: Commits on May 16, 2026
-VITA3K_VERSION = 4e03f06a0684e27fc4495c25a4751824d8aba723
+# Version: Commits on May 13, 2026
+VITA3K_VERSION = cb1f592c8a6cba8c84ceeaab43bdef286aface14
 VITA3K_SITE = https://github.com/vita3k/vita3k
 VITA3K_SITE_METHOD=git
 VITA3K_GIT_SUBMODULES=YES
 VITA3K_LICENSE = GPLv3
 VITA3K_DEPENDENCIES = boost fmt libcurl libgtk3 libogg libvorbis python-ruamel-yaml
-VITA3K_DEPENDENCIES += qt6base qt6svg qt6tools sdl3 zlib
+VITA3K_DEPENDENCIES += sdl3 zlib
 VITA3K_EMULATOR_INFO = vita3k.emulator.yml
 
 VITA3K_SUPPORTS_IN_SOURCE_BUILD = NO
@@ -20,12 +20,19 @@ VITA3K_CONF_OPTS += -DCMAKE_CXX_COMPILER=$(HOST_DIR)/bin/$(GNU_TARGET_NAME)-g++
 VITA3K_CONF_OPTS += -DBUILD_EXTERNAL=OFF
 VITA3K_CONF_OPTS += -DBUILD_SHARED_LIBS=OFF
 VITA3K_CONF_OPTS += -DUSE_DISCORD_RICH_PRESENCE=OFF
+VITA3K_CONF_OPTS += -DUSE_VITA3K_UPDATE=OFF
 
 ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_X86_64),y)
     VITA3K_CONF_OPTS += -DXXH_X86DISPATCH_ALLOW_AVX=ON
 else
     VITA3K_CONF_OPTS += -DXXH_X86DISPATCH_ALLOW_AVX=OFF
 endif
+
+define VITA3K_GET_SUBMODULE
+	mkdir -p $(@D)/external
+	cd $(@D)/external && git clone https://github.com/Vita3K/nativefiledialog-cmake
+endef
+VITA3K_POST_EXTRACT_HOOKS = VITA3K_GET_SUBMODULE
 
 define VITA3K_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/usr/bin/vita3k/
