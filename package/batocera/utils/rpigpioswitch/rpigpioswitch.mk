19b6758662c7e3cac2eda7cb04f3456c246a209d
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2026-05-29 21:16:27 +0100

    Update rpigpioswitch.mk

--- package/batocera/utils/rpigpioswitch/rpigpioswitch.mk
+++ package/batocera/utils/rpigpioswitch/rpigpioswitch.mk
@@ -8,7 +8,7 @@ RPIGPIOSWITCH_SOURCE =
 
 define RPIGPIOSWITCH_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0755 $(RPIGPIOSWITCH_PKGDIR)/S92switch						$(TARGET_DIR)/etc/init.d/S92switch
-	$(INSTALL) -D -m 0755 $(RPIGPIOSWITCH_PKGDIR)/rpi_gpioswitch.s				$(TARGET_DIR)/usr/bin/rpi_gpioswitch
+	$(INSTALL) -D -m 0755 $(RPIGPIOSWITCH_PKGDIR)/rpi_gpioswitch.sh				$(TARGET_DIR)/usr/bin/rpi_gpioswitch
 	$(INSTALL) -D -m 0755 $(RPIGPIOSWITCH_PKGDIR)/rpi-pin56-power.py			$(TARGET_DIR)/usr/bin/rpi-pin56-power
 	$(INSTALL) -D -m 0755 $(RPIGPIOSWITCH_PKGDIR)/rpi-pin356-power.py			$(TARGET_DIR)/usr/bin/rpi-pin356-power
 	$(INSTALL) -D -m 0755 $(RPIGPIOSWITCH_PKGDIR)/rpi-retroflag-64PiCase.py		$(TARGET_DIR)/usr/bin/rpi-retroflag-64PiCase
