ade649728867a4f7fe5bea1a5b581cfab24edcec
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2026-07-22 20:36:03 +0100

    Update rapidyaml.mk

--- package/batocera/libraries/rapidyaml/rapidyaml.mk
+++ package/batocera/libraries/rapidyaml/rapidyaml.mk
@@ -6,7 +6,7 @@
 # Version: Commits on Jul 22, 2026
 RAPIDYAML_VERSION = v0.16.0
 RAPIDYAML_SITE = https://github.com/biojppm/rapidyaml/releases/download/$(RAPIDYAML_VERSION)
-RAPIDYAML_SOURCE = rapidyaml-$(subst v,,$(RAPIDYAML_VERSION))-src.tgz
+RAPIDYAML_SOURCE = rapidyaml.$(RAPIDYAML_VERSION).src.tgz
 RAPIDYAML_LICENSE = GPL-2
 RAPIDYAML_SUPPORTS_IN_SOURCE_BUILD = NO
 RAPIDYAML_INSTALL_STAGING = YES
