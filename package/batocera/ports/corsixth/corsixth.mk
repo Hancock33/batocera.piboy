840966cf339d544a1a8fa6f707acdc4d768e4176
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2026-07-29 20:12:02 +0100

    revert corsixth

--- package/batocera/ports/corsixth/corsixth.mk
+++ package/batocera/ports/corsixth/corsixth.mk
@@ -3,8 +3,8 @@
 # corsixth
 #
 ################################################################################
-# Version: Commits on Jul 05, 2026
-CORSIXTH_VERSION = v0.70.0
+# Version: Commits on Jan 10, 2026
+CORSIXTH_VERSION = v0.69.2
 CORSIXTH_SITE = $(call github,CorsixTH,CorsixTH,$(CORSIXTH_VERSION))
 CORSIXTH_DEPENDENCIES = ffmpeg libcurl lpeg lua luafilesystem lua-lpeg-patterns luasec luasocket rtmidi
 CORSIXTH_DEPENDENCIES += sdl2 sdl2_image sdl2_mixer
