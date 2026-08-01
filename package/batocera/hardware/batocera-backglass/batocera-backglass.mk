5dfb8f0142aea558876b0980d7bd968ccf92617e
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2026-08-01 09:58:35 +0100

    move

--- package/batocera/hardware/batocera-backglass/batocera-backglass.mk
+++ package/batocera/hardware/batocera-backglass/batocera-backglass.mk
@@ -23,17 +23,14 @@ endef
 define BATOCERA_BACKGLASS_INSTALL_TARGET_CMDS
 	# Install main bash controls script
 	mkdir -p $(TARGET_DIR)/usr/bin
-	install -m 0755 $(BACKGLASS_PATH)/batocera-backglass.sh \
-		$(TARGET_DIR)/usr/bin/batocera-backglass
-	
+	install -m 0755 $(BACKGLASS_PATH)/batocera-backglass.sh $(TARGET_DIR)/usr/bin/batocera-backglass
+
 	# Install the compiled native backglass execution window binary
-	install -m 0755 $(@D)/batocera-backglass-window \
-		$(TARGET_DIR)/usr/bin/batocera-backglass-window
+	install -m 0755 $(@D)/batocera-backglass-window (TARGET_DIR)/usr/bin/batocera-backglass-window
 
 	# Install hook wrappers
 	mkdir -p $(TARGET_DIR)/usr/share/batocera-backglass/scripts
-	$(INSTALL) -m 0755 -D $(BACKGLASS_PATH)/scripts/*.sh \
-		$(TARGET_DIR)/usr/share/batocera-backglass/scripts/
+	$(INSTALL) -m 0755 -D $(BACKGLASS_PATH)/scripts/*.sh $(TARGET_DIR)/usr/share/batocera-backglass/scripts/
 endef
 
 $(eval $(generic-package))
