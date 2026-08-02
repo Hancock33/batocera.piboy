077d9abb0bd8fef5c881fad456dd44d4906bdd6a
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2026-08-02 19:17:44 +0100

    Update eoc.mk

--- package/batocera/ports/doom/doom3/eoc/eoc.mk
+++ package/batocera/ports/doom/doom3/eoc/eoc.mk
@@ -4,15 +4,16 @@
 #
 ################################################################################
 # Version: Commits on Jun 07, 2026
-EOC_VERSION = ad837f9b1ba70bf6e38e61fd8eb242f2f09d1a2a
+EOC_VERSION = d97448a28c6234e060dd75a158b4357fa5801b8a
 EOC_SITE = $(call github,dhewm,dhewm3-sdk,$(EOC_VERSION))
+EOC_BRANCH = eoc
 EOC_LICENSE = GPLv3
 EOC_LICENSE_FILES = COPYING.txt
 
 EOC_DEPENDENCIES = dhewm3 host-libjpeg libcurl libogg libvorbis openal sdl2 zlib
 
 define EOC_INSTALL_TARGET_CMDS
-    mkdir -p $(TARGET_DIR)/usr/lib/dhewm3
+	mkdir -p $(TARGET_DIR)/usr/lib/dhewm3
 	cp $(@D)/eoc*.so $(TARGET_DIR)/usr/lib/dhewm3/
 endef
 
