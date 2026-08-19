7916c760594e63ba5b7c6ffc21874690a1c6001b
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2026-08-19 23:03:43 +0100

    Update libretro-theodore.mk

--- package/batocera/emulators/retroarch/libretro/libretro-theodore/libretro-theodore.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-theodore/libretro-theodore.mk
@@ -5,7 +5,9 @@
 ################################################################################
 # Version: Commits on Aug 17, 2026
 LIBRETRO_THEODORE_VERSION = 4d469ce0f71ee046ceb78cdbf8e9f18364aaa918
-LIBRETRO_THEODORE_SITE = $(call github,Zlika,theodore,$(LIBRETRO_THEODORE_VERSION))
+LIBRETRO_THEODORE_SITE = https://github.com/Zlika/theodore.git
+LIBRETRO_THEODORE_SITE_METHOD = git
+LIBRETRO_THEODORE_GIT_SUBMODULES = YES
 LIBRETRO_THEODORE_LICENSE = GPLv3
 LIBRETRO_THEODORE_DEPENDENCIES += retroarch
 LIBRETRO_THEODORE_EMULATOR_INFO = theodore.libretro.core.yml
