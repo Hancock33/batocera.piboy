9f856d21d1b12ebe49e67926535f0fdd0c8d8497
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2026-08-23 22:13:03 +0100

    Update rpcs3Generator.py

--- package/batocera/core/batocera-configgen/configgen/configgen/generators/rpcs3/rpcs3Generator.py
+++ package/batocera/core/batocera-configgen/configgen/configgen/generators/rpcs3/rpcs3Generator.py
@@ -758,9 +758,7 @@ class Rpcs3Generator(Generator):
         )
 
     def writesToRom(self, config) -> bool:
-        if is_psn_squashfs:
-            return True
-        return False
+        return bool(is_psn_squashfs)
 
     def _generateGunConfig(self):
         # D-Pad mapping is face buttons of the PS Move buttons
