2201cc1a36237105e89cc2c7aba70c7f223d63ca
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2026-08-27 18:36:42 +0100

    bump supertux and use sdl3

--- package/batocera/ports/supertux2/supertux2.mk
+++ package/batocera/ports/supertux2/supertux2.mk
@@ -3,10 +3,11 @@
 # supertux2
 #
 ################################################################################
-# Version: Commits on Mar 15, 2026
-SUPERTUX2_VERSION = v0.7.0
-SUPERTUX2_SITE =  https://github.com/SuperTux/supertux/releases/download/$(SUPERTUX2_VERSION)
-SUPERTUX2_SOURCE = SuperTux-$(SUPERTUX2_VERSION)-Source.tar.gz
+# Version: Commits on Aug 23, 2026
+SUPERTUX2_VERSION = ad225fd672c8ab6907b17046bba4418c8c4ac786
+SUPERTUX2_SITE = https://github.com/SuperTux/supertux.git
+SUPERTUX2_SITE_METHOD=git
+SUPERTUX2_GIT_SUBMODULES=YES
 SUPERTUX2_SUPPORTS_IN_SOURCE_BUILD = NO
 
 # Supertux itself is GPL-3.0+, but it bundles a few libraries with different
@@ -14,13 +15,14 @@ SUPERTUX2_SUPPORTS_IN_SOURCE_BUILD = NO
 SUPERTUX2_LICENSE = GPL-3.0+ (code), CC-BY-SA-2.0, CC-BY-SA-3.0, CC-BY-SA-4.0, GPL-2.0+ (images music sounds)
 SUPERTUX2_LICENSE_FILES = LICENSE.txt data/AUTHORS
 SUPERTUX2_EMULATOR_INFO = supertux2.emulator.yml
-SUPERTUX2_DEPENDENCIES = boost freetype host-pkgconf libcurl libogg libpng libvorbis openal physfs sdl2 sdl2_image zlib
+SUPERTUX2_DEPENDENCIES = boost freetype host-pkgconf libcurl libogg libpng libvorbis openal physfs sdl3 sdl3_image zlib
 
 SUPERTUX2_CONF_OPTS += -DBUILD_DOCUMENTATION=OFF
 SUPERTUX2_CONF_OPTS += -DCMAKE_INTERPROCEDURAL_OPTIMIZATION=OFF
 SUPERTUX2_CONF_OPTS += -DINSTALL_SUBDIR_BIN="bin"
 SUPERTUX2_CONF_OPTS += -DINSTALL_SUBDIR_SHARE="share/supertux2"
-
+SUPERTUX2_CONF_OPTS += -DSUPERTUX_PCH=OFF
+ 
 define SUPERTUX2_INSTALL_TARGET_CMDS
 	# install media
 	mkdir -p $(TARGET_DIR)/usr/share/emulationstation/ports/supertux2
