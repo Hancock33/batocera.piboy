a027f153df1c798a44074e7c9d052e5f2f9c2f2d
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2026-08-29 19:58:26 +0100

    Update x86_wow-packager.sh

--- board/batocera/scripts/x86_wow-packager.sh
+++ board/batocera/scripts/x86_wow-packager.sh
@@ -5,7 +5,7 @@ DEST_DIR=/tmp/batocera-x86-32bit-libs-$CURRENTDATE
 mkdir -p $DEST_DIR/lib
 mkdir -p $DEST_DIR/usr/bin32
 mkdir -p $DEST_DIR/usr/share/vulkan/icd.d
-cp -a ${TARGET_DIR}/usr/bin/lindbergh                    $DEST_DIR/usr/bin32
+cp -a ${TARGET_DIR}/usr/bin/linuxloader                  $DEST_DIR/usr/bin32
 cp -a ${TARGET_DIR}/usr/bin/gst-*                        $DEST_DIR/usr/bin32
 cp -a ${TARGET_DIR}/usr/share/gst-plugins-base           $DEST_DIR/usr/share
 cp -a ${TARGET_DIR}/usr/share/gstreamer-*                $DEST_DIR/usr/share
