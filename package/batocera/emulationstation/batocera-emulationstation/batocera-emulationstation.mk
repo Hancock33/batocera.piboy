d636804fa12e6b700cc834e221f2928a1007b651
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2026-09-03 07:30:52 +0100

    Merge pull request #16371 from batocera-linux/fix-audio-list

--- package/batocera/emulationstation/batocera-emulationstation/batocera-emulationstation.mk
+++ package/batocera/emulationstation/batocera-emulationstation/batocera-emulationstation.mk
@@ -3,8 +3,8 @@
 # batocera-emulationstation
 #
 ################################################################################
-# Version: Commits on Aug 30, 2026
-BATOCERA_EMULATIONSTATION_VERSION = 6c005266b17c2959243c4e2889b5e3aa23dc9993
+# Version: Commits on Sept 03, 2026
+BATOCERA_EMULATIONSTATION_VERSION = 024ee1f0ce82e027c541bdc3b3464636b8862f6f
 BATOCERA_EMULATIONSTATION_SITE = https://github.com/batocera-linux/batocera-emulationstation
 BATOCERA_EMULATIONSTATION_SITE_METHOD = git
 BATOCERA_EMULATIONSTATION_GIT_SUBMODULES = YES
