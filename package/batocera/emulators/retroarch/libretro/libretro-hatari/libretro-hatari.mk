e4b3f4ab674999c6fd2c125bec78e17b0f4053c6
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2026-09-04 06:44:31 +0100

    libretro-hatari swap to cmake

--- package/batocera/emulators/retroarch/libretro/libretro-hatari/libretro-hatari.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-hatari/libretro-hatari.mk
@@ -10,32 +10,16 @@ LIBRETRO_HATARI_DEPENDENCIES = libcapsimage zlib retroarch
 LIBRETRO_HATARI_EMULATOR_INFO = hatari.libretro.core.yml
 LIBRETRO_HATARI_LICENSE = GPLv2
 
-ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_BCM2836),y)
-    LIBRETRO_HATARI_PLATFORM = rpi2
-else ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_BCM2837),y)
-    LIBRETRO_HATARI_PLATFORM = rpi3_64
-else ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_BCM2711),y)
-    LIBRETRO_HATARI_PLATFORM = rpi4
-else ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_BCM2712),y)
-    LIBRETRO_HATARI_PLATFORM = rpi5
-else ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_S812),y)
-    LIBRETRO_HATARI_PLATFORM = armv
-else ifeq ($(BR2_aarch64),y)
-    LIBRETRO_HATARI_PLATFORM = unix
-else
-    LIBRETRO_HATARI_PLATFORM = $(LIBRETRO_PLATFORM)
-endif
-
-define LIBRETRO_HATARI_BUILD_CMDS
-	$(SED) "s|\-O[23]|$(TARGET_OPTIMIZATION)|g" $(@D)/Makefile.libretro
-	$(TARGET_CONFIGURE_OPTS) $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D)/ -f Makefile.libretro platform="$(LIBRETRO_HATARI_PLATFORM)" \
-		GIT_VERSION="-$(shell echo $(LIBRETRO_HATARI_VERSION) | cut -c 1-7)"
-endef
+LIBRETRO_HATARI_CONF_OPTS += -DENABLE_LIBRETRO=ON
+LIBRETRO_HATARI_CONF_OPTS += -DENABLE_HATARI=OFF
+LIBRETRO_HATARI_CONF_OPTS += -DENABLE_TOOLS=OFF
+LIBRETRO_HATARI_CONF_OPTS += -DENABLE_STATIC_ZLIB=ON
+LIBRETRO_HATARI_CONF_OPTS += -DENABLE_STATIC_CAPSIMAGE=ON
 
 define LIBRETRO_HATARI_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/usr/lib/libretro
-	$(INSTALL) -D $(@D)/hatari_libretro.so $(TARGET_DIR)/usr/lib/libretro/hatari_libretro.so
+	$(INSTALL) -D $(@D)/src/hatari_libretro.so $(TARGET_DIR)/usr/lib/libretro/hatari_libretro.so
 endef
 
-$(eval $(generic-package))
+$(eval $(cmake-package))
 $(eval $(emulator-info-package))
