8f279a1ada7c5f082dd5780629b2d5abbc91845a
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2026-09-05 06:25:03 +0100

    Bump fastfetch to 2.68.1

--- package/batocera/utils/fastfetch/fastfetch.mk
+++ package/batocera/utils/fastfetch/fastfetch.mk
@@ -3,8 +3,8 @@
 # fastfetch
 #
 ################################################################################
-# Version: Commits on Oct 09, 2022
-FASTFETCH_VERSION = v1.0.1
+# Version: Commits on Sept 01, 2026
+FASTFETCH_VERSION = 2.68.1
 FASTFETCH_SITE = $(call github,fastfetch-cli,fastfetch,$(FASTFETCH_VERSION))
 FASTFETCH_LICENSE = MIT
 
