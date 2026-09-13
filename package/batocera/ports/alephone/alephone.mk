80d4eb113db5a37da116b6c56b38e6300cb8ddc9
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2026-09-13 19:30:28 +0100

    alephone use asio

--- package/batocera/ports/alephone/alephone.mk
+++ package/batocera/ports/alephone/alephone.mk
@@ -7,7 +7,7 @@
 ALEPHONE_VERSION = 1fd6128bbad4b52987e968a5b69f3d99eb01111c
 ALEPHONE_SITE = $(call github,Aleph-One-Marathon,alephone,$(ALEPHONE_VERSION))
 ALEPHONE_EMULATOR_INFO = alephone.emulator.yml
-ALEPHONE_DEPENDENCIES = boost sdl2 sdl2_image sdl2_ttf
+ALEPHONE_DEPENDENCIES = asio boost sdl2 sdl2_image sdl2_ttf
 ALEPHONE_LICENSE = GPLv3
 ALEPHONE_AUTORECONF = YES
 ALEPHONE_AUTORECONF_OPTS = --include=$(HOST_DIR)/share/autoconf-archive
