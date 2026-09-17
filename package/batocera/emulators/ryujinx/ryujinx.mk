a0e0d7b11ca55ccc78b5da4e530bf05a1a69fa97
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2026-09-17 20:33:10 +0100

    Update Ryujinx to source build
    Switches Ryujinx from the prebuilt Canary archive to the Kenji-NX source repository, Dot10 for some reason does not work

--- package/batocera/emulators/ryujinx/ryujinx.mk
+++ package/batocera/emulators/ryujinx/ryujinx.mk
@@ -3,24 +3,51 @@
 # ryujinx
 #
 ################################################################################
-# Version: Commits on Aug 26, 2026
-RYUJINX_VERSION = 1.3.351
-RYUJINX_SOURCE = ryujinx-canary-$(RYUJINX_VERSION)-linux_x64.tar.gz
-RYUJINX_SITE = https://git.ryujinx.app/Ryubing/Canary/releases/download/$(RYUJINX_VERSION)
+# Version: Commits on Sept 14, 2026
+RYUJINX_VERSION = e7cb0632c50063444013dcae4fb3b16e33cfd3e8
+RYUJINX_SITE = https://git.ryujinx.app/projects/Kenji-NX.git
+RYUJINX_BRANCH = libryujinx_bionic
+RYUJINX_SITE_METHOD=git
+RYUJINX_GIT_SUBMODULES=YES
 RYUJINX_LICENSE = MIT
-RYUJINX_DEPENDENCIES = adwaita-icon-theme hicolor-icon-theme librsvg openal sdl2
-RYUJINX_EMULATOR_INFO = ryujinx.emulator.yml
+RYUJINX_DEPENDENCIES = sdl3 openal hicolor-icon-theme adwaita-icon-theme librsvg
+RYUJINX_DOTNET_VERSION = 9.0.318
 
-define RYUJINX_EXTRACT_CMDS
-	mkdir -p $(@D)/target && cd $(@D)/target && tar xf $(DL_DIR)/$(RYUJINX_DL_SUBDIR)/$(RYUJINX_SOURCE)
+ifeq ($(BR2_x86_64),y)
+    RYUJINX_DOTNET_FILE = dotnet-sdk-$(RYUJINX_DOTNET_VERSION)-linux-x64.tar.gz
+    RYUJINX_EXTRA_DOWNLOADS = https://builds.dotnet.microsoft.com/dotnet/Sdk/$(RYUJINX_DOTNET_VERSION)/$(RYUJINX_DOTNET_FILE)
+else
+    RYUJINX_DOTNET_FILE = dotnet-sdk-$(RYUJINX_DOTNET_VERSION)-linux-arm64.tar.gz
+    RYUJINX_EXTRA_DOWNLOADS =  https://builds.dotnet.microsoft.com/dotnet/Sdk/$(RYUJINX_DOTNET_VERSION)/$(RYUJINX_DOTNET_FILE)
+endif
+
+define RYUJINX_BUILD_CMDS
+
+	#$(SED) "s|1.0.0-dirty|$(RYUJINX_VERSION)|g" $(@D)/src/*/*.csproj
+
+
+	cd $(@D) && PATH=$(HOST_DIR)/dotnet:$(PATH) DOTNET_CLI_TELEMETRY_OPTOUT=1 && \
+	dotnet nuget locals all -c && \
+	dotnet publish -c Release -r linux-x64 --nologo --self-contained true -p:DebugType=none -p:ExtraDefineConstants=DISABLE_UPDATER -o ./publish src/Ryujinx
 endef
 
 define RYUJINX_INSTALL_TARGET_CMDS
-	rm -rf   $(TARGET_DIR)/usr/bin/ryujinx
+	rm -rf $(TARGET_DIR)/usr/bin/ryujinx
 	mkdir -p $(TARGET_DIR)/usr/bin/ryujinx
-	cp -pr $(@D)/target/publish/* $(TARGET_DIR)/usr/bin/ryujinx
+	cp -avr $(@D)/publish/* $(TARGET_DIR)/usr/bin/ryujinx
 	rm -rf   $(TARGET_DIR)/usr/bin/ryujinx/{libSDL3.so,libav*,libglfw*}
+	cp -avr $(@D)/publish/mime/Ryujinx.xml $(TARGET_DIR)/usr/share/mime
 endef
 
+define RYUJINX_DOTNET_INSTALL
+	rm -rf $(HOST_DIR)/dotnet
+	mkdir $(HOST_DIR)/dotnet
+	tar -xf $(RYUJINX_DL_DIR)/$(RYUJINX_DOTNET_FILE) -C $(HOST_DIR)/dotnet
+	rm -rf $(HOME)/.local/share/NuGet
+	rm -rf $(HOME)/.nuget/packages
+	rm -rf /tmp/NuGetScratch*
+endef
+
+RYUJINX_POST_EXTRACT_HOOKS += RYUJINX_DOTNET_INSTALL
+
 $(eval $(generic-package))
-$(eval $(emulator-info-package))
