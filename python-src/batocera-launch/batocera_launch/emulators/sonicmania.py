63988b79c554c77169d37158da516aeed7e0ca21
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2026-09-19 15:21:47 +0100

    Fix SonicMania roms directory path

--- python-src/batocera-launch/batocera_launch/emulators/sonicmania.py
+++ python-src/batocera-launch/batocera_launch/emulators/sonicmania.py
@@ -79,7 +79,7 @@ class SonicMania(Emulator):
 
     @cached_property
     def roms_dir(self) -> Path:
-        return ROMS / 'ports' / 'sonic-mania'
+        return ROMS / 'ports' / 'sonicmania'
 
     @property
     def execution_path(self) -> Path | None:
